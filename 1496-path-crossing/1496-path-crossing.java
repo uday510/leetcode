d8fcd1078160f2efe391307b72646bd7e7e25da1
Uday <admin@udayteja.com>
2023-12-23 10:31:52 +0530

    Time: 1 ms (96.48%), Space: 41.8 MB (9.03%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.java
+++ 1496-path-crossing/1496-path-crossing.java
@@ -1,21 +1,18 @@
-public class Solution {
+class Solution {
     public boolean isPathCrossing(String path) {
         Set<Integer> vis = new HashSet<>();
-        int x = 0, y = 0;
+        int x = 0; int y = 0;
         vis.add(0);
-
-        for (char direction : path.toCharArray()) {
-            if (direction == 'N') x--;
-            else if (direction == 'E') y++;
-            else if (direction == 'S') x++;
-            else if (direction == 'W') y--;
-
-            int pos = x*1000 +  y;
-
+        for (char dir : path.toCharArray()) {
+            if (dir == 'N') --x;
+            else if (dir == 'E') ++y;
+            else if (dir == 'S') ++x;
+            else if (dir == 'W') --y;
+            
+            int pos = (x*1000) + y;
             if (vis.contains(pos)) return true;
             vis.add(pos);
         }
-        
         return false;
     }
 }

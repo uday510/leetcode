ef836d27ac13ec854cdd55f9f2b63b6fd3e48817
Uday <admin@udayteja.com>
2023-12-24 12:21:46 +0530

    Time: 1 ms (100.00%), Space: 43.4 MB (5.57%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -2,7 +2,6 @@ class Solution {
     public int minOperations(String s) {
         return Math.min(minOps(s, '0'), minOps(s, '1'));
     }
-
     public int minOps(String s, char start) {
         int ops = 0;
         for (char c : s.toCharArray()) {

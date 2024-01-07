407c2d2d3cafd9770169dd817600668d4cc60358
Uday <admin@udayteja.com>
2024-01-07 18:01:46 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (41.05%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.java
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -1,20 +1,15 @@
 class Solution {
-    int total;
     public int numberOfArithmeticSlices(int[] nums) {
-        total = 0;
-        dfs(nums, nums.length-1);
-        return total;
-    }
-    public int dfs(int[] nums, int i) {
-        if (i < 2) return 0;
+        int n = nums.length;
+        int[] dp = new int[n];
+        int total = 0;
         
-        int curr = 0;
-        if (nums[i] - nums[i-1] == nums[i-1] - nums[i-2]) {
-            curr = 1 + dfs(nums, i-1);
-            total += curr;
-        } else {
-            dfs(nums, i-1);
+        for (int i = 2; i < n; ++i) {
+            if (nums[i] - nums[i-1] == nums[i-1] - nums[i-2]) {
+                dp[i] = dp[i-1] + 1;
+                total += dp[i];
+            } 
         }
-        return curr;
+        return total;
     }
 }

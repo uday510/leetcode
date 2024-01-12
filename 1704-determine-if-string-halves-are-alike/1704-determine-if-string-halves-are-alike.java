725905856380323d85fdb7e1b9b07b2f66cdf07c
Uday <admin@udayteja.com>
2024-01-12 11:48:40 +0530

    Time: 5 ms (51.60%), Space: 41.7 MB (34.03%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,26 +1,17 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
         int n = s.length();
-        int cnt1 = 0;
-        int cnt2 = 0;
+        int cnt = 0;
         Set<Character> set = new HashSet<>();
-        // set.addAll('a', 'e','i', 'o', 'u', 'A', 'E', 'I');
-        set.add('a');  
-        set.add('e');  
-        set.add('i'); 
-        set.add('E');  
-        set.add('o');
-        set.add('u');
-        set.add('A');
-        set.add('I');
-        set.add('O');
-        set.add('U');
-        System.out.println(set);
+        set.addAll(Arrays.asList('a', 'e','i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
         
         for (int i = 0; i < n/2; ++i) {
-            if (set.contains(s.charAt(i))) cnt1++;
-            if (set.contains(s.charAt(i+n/2))) cnt2++;
+            if (set.contains(s.charAt(i))) {
+                 cnt++;
+            }
+            if (set.contains(s.charAt(i+n/2))) cnt--;
+            
         }
-        return cnt1 == cnt2;
+        return cnt == 0;
     }
 }

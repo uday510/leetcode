b3e78eddab17a082005e186dafa9ec54cfa495ad
Uday <admin@udayteja.com>
2024-01-18 00:04:05 +0530

    Time: 92 ms (36.78%), Space: 109.9 MB (13.37%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -31,11 +31,9 @@ class Solution {
 
     List<List<Integer>> res = new ArrayList<>();
     
-    // winners.sort((o1, o2) -> o1-o2);
-    // losers.sort((o1, o2) -> o1-o2);
-    Collections.sort(winners);
-    Collections.sort(losers);
-
+    winners.sort((o1, o2) -> o1-o2);
+    losers.sort((o1, o2) -> o1-o2);
+ 
     res.add(winners);
     res.add(losers);
     return res;

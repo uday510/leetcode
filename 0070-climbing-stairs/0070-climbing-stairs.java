360522e6999da3065c56d7ec6e9ccdf84bf27529
Uday <admin@udayteja.com>
2024-01-18 08:55:17 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (39.59%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,20 +1,21 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
     public int climbStairs(int n) {
-        // return dfs(n);
-        int step1 = 0;
-        int step2 = 1;
-        
-        for (int i = 1; i <= n; ++i) {
-            int currStep = step1 + step2;
-            step1 = step2;
-            step2 = currStep;
+        if (n <= 1) {
+            return n;
         }
-        return step2;
-    }
-    public int dfs(int n) {
-        if ( n <= 1) return 1;
-        // if (n <= 0) return 0;
-        
-        return dfs(n-1) + dfs(n-2);
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(1); // 1 way to reach position 1
+        queue.offer(1); // 1 way to reach position 2
+
+        for (int i = 3; i <= n; i++) {
+            int current = queue.poll() + queue.peek();
+            queue.offer(current);
+        }
+
+        return queue.poll() + queue.peek();
     }
 }

9ad07167cf076cd9e50f056e418eb3d28fb71af4
Uday <BUDAYTEJA009@GMAIL.COM>
2024-03-14 20:48:45 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (47.48%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int index = 0;
+        
+        for (int num : nums) {
+            if (num == val) continue;
+            nums[index++] = num;
+        }
+        return index;
+    }
+}

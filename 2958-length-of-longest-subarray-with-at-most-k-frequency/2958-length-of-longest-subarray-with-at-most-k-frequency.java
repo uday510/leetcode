a6670c83ccc88c0e2d45742c1487145ce0cb1e4b
Uday <BUDAYTEJA009@GMAIL.COM>
2024-03-28 09:51:24 +0530

    Time: 71 ms (40.34%), Space: 60.8 MB (36.96%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -7,12 +7,11 @@ class Solution {
         int len = nums.length;
         int maxLength = 1;
 
-        while (rightIdx < len) {
+        for (; rightIdx < len;) {
 
             map.put(nums[rightIdx], map.getOrDefault(nums[rightIdx], 0) + 1);
-            while (leftIdx < len && map.get(nums[rightIdx]) > k) {
+            for (; leftIdx < len && map.get(nums[rightIdx]) > k; ++leftIdx) {
                 map.put(nums[leftIdx], map.get(nums[leftIdx]) - 1);
-                leftIdx++;
             }
 
             maxLength = Math.max(maxLength, (rightIdx - leftIdx + 1));

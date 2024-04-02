167c6854743140feede27c37e8333a008f6e0e50
Uday <BUDAYTEJA009@GMAIL.COM>
2024-04-02 17:03:41 +0530

    Time: 12 ms (44.07%), Space: 42.4 MB (51.51%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> hm = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (!hm.containsKey(s.charAt(i))) {
+                if (hm.containsValue(t.charAt(i))) return false;
+                hm.put (s.charAt(i), t.charAt(i));
+            }
+            else {
+                if (hm.get(s.charAt(i)) != t.charAt(i)) return false;
+            }
+        }
+        return true;
+    }
+}

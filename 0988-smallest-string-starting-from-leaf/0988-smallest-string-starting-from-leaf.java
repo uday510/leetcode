8e308e630cb195521e33ec372e968d4b980225bd
Uday <BUDAYTEJA009@GMAIL.COM>
2024-04-17 22:42:37 +0530

    Time: 9 ms (27.25%), Space: 45.7 MB (7.76%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
@@ -1,42 +1,26 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public String smallestFromLeaf(TreeNode root) {
-        return dfs(root, new StringBuilder());
+        return dfs(root, "");
     }
-    public String dfs(TreeNode node, StringBuilder sb) {
-        
-        sb.append((char)('a' + node.val));
-        
-        if (node.left == null && node.right == null) {
-            return sb.reverse().toString();
-        }
-        
-        String left = node.left != null ? dfs(node.left, new StringBuilder(sb)) : null;
-        
-        String right = node.right != null ? dfs(node.right, new StringBuilder(sb)) : null;
-        
-        if (left == null) {
-            return right;
-        }
-        if (right == null) {
-            return left;
-        }
-        
-        return left.compareTo(right) < 0 ? left : right;
-        
-    }
+     public String dfs(TreeNode root, String suffix) {
+         
+        if (root == null) 
+            return suffix;
+         
+        suffix = (char)('a' + root.val) + suffix;
+         
+        if (root.left == null && root.right == null)
+            return suffix;
+         
+        if (root.left == null) 
+            return dfs(root.right, suffix);
+         
+        if (root.right == null)
+            return dfs(root.left, suffix);
+         
+        String left = dfs(root.left, suffix);
+        String right = dfs(root.right, suffix);
+         
+        return left.compareTo(right) <= 0 ? left : right;
+     }
 }

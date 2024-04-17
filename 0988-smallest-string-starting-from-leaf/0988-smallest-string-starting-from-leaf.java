59f22a516b65051084fe617bf74cc8adf855c4d8
Uday <BUDAYTEJA009@GMAIL.COM>
2024-04-17 22:25:01 +0530

    Time: 8 ms (39.83%), Space: 44.7 MB (52.41%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
@@ -4,10 +4,12 @@ class Solution {
     }
      public String dfs(TreeNode root, String suffix) {
         if (root == null) return suffix;
+         
         suffix = (char)('a' + root.val) + suffix;
         if (root.left == null && root.right == null) return suffix;
         if (root.left == null) return dfs(root.right, suffix);
         if (root.right == null) return dfs(root.left, suffix);
+         
         String left = dfs(root.left, suffix);
         String right = dfs(root.right, suffix);
         return left.compareTo(right) <= 0 ? left : right;

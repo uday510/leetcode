782d5eb7b570156c87abe7cfb501186d77014871
Uday <BUDAYTEJA009@GMAIL.COM>
2024-04-21 07:04:24 +0530

    Time: 13 ms (93.51%), Space: 112.3 MB (88.49%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+         DSU dsu = new DSU(n);
+        for(int[] edge : edges) {
+            dsu.union(edge[0], edge[1]);
+        }
+        return dsu.isConnected(source, destination);
+    }
+    class DSU {
+        int[] parent;
+        int[] rank;
+
+        public DSU(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            for(int i = 0; i < n; i++) {
+                parent[i] = i;
+                rank[i] = 1;
+            }
+        }
+
+        public int find(int x) {
+            while (x != parent[x]) {
+                parent[x] = parent[parent[x]];
+                x = parent[x];
+            }
+            return x;
+        }
+
+        public void union(int x, int y) {
+            int px = find(x);
+            int py = find(y);
+            if (px != py) {
+                if (rank[px] > rank[py]) {
+                    parent[py] = px;
+                } else if (rank[py] > rank[px]) {
+                    parent[px] = py;
+                } else {
+                    parent[px] = py;
+                    ++rank[py];
+                }
+            }
+        }
+
+        public boolean isConnected(int x, int y) {
+            return find(x) == find(y);
+        }
+    }
+}

ed65b9c8fd863d54e4e2ed7d2aa44a931f48aec0
Uday <BUDAYTEJA009@GMAIL.COM>
2024-04-26 16:10:57 +0530

    Time: 15 ms (27.51%), Space: 44.1 MB (57.66%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c)) 
+                return false;
+            
+            map.put(c, map.get(c) - 1);
+            
+            
+            if (map.get(c) == 0)
+                map.remove(c);
+        }
+        
+        return map.size() == 0;
+    }
+}

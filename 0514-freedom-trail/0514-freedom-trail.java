9e3e34730f32868d647e2286c17033265f275a15
Uday <BUDAYTEJA009@GMAIL.COM>
2024-04-27 23:39:36 +0530

    Time: 17 ms (48.06%), Space: 44.3 MB (95.63%) - LeetHub

---
+++ 0514-freedom-trail/0514-freedom-trail.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int findRotateSteps(String ring, String key) {
+        return dfs(ring, key, 0, 0, new Integer[ring.length()][key.length()]);
+
+    }
+     public int dfs(String ring, String key, int ringIndex, int keyIndex, Integer[][] dp) {
+
+        if (keyIndex == key.length()) {
+            return 0;
+        }
+
+        if (dp[ringIndex][keyIndex] != null) {
+            return dp[ringIndex][keyIndex];
+        }
+
+        int minSteps = Integer.MAX_VALUE;
+
+        for (int i = 0; i < ring.length(); i++) {
+            if (ring.charAt(i) == key.charAt(keyIndex)) {
+
+                int clockWise = Math.abs(ringIndex - i);
+                int antiClockWise = ring.length() - clockWise;
+
+                int steps = 1 + Math.min(clockWise, antiClockWise) + dfs(ring, key, i, keyIndex + 1, dp);
+
+                minSteps = Math.min(minSteps, steps);
+            }
+        }
+
+        dp[ringIndex][keyIndex] = minSteps;
+
+        return minSteps;
+    }
+}

64cb0b74aea2389a2374761d9c7dba5e5f3254a2
Uday <BUDAYTEJA009@GMAIL.COM>
2024-05-08 21:41:52 +0530

    Time: 7 ms (93.93%), Space: 45.6 MB (58.39%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -1,38 +1,46 @@
-class Solution {
+import java.util.PriorityQueue;
+
+public class Solution {
     public String[] findRelativeRanks(int[] score) {
         PriorityQueue<Pair> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o2.val, o1.val));
-        
-        for (int i = 0; i < score.length; ++i) {
+
+        for (int i = 0; i < score.length; i++) {
             pq.offer(new Pair(score[i], i));
         }
+
         String[] result = new String[score.length];
-        
+
         int index = 1;
         while (!pq.isEmpty()) {
             Pair p = pq.poll();
-            System.out.print(p.val + " " + p.idx + ", ");
-            
-            if (index == 1) {
-                result[p.idx] = "Gold Medal";
-            } else if (index == 2) {
-                result[p.idx] = "Silver Medal";
-            } else if (index == 3) {
-                result[p.idx] = "Bronze Medal";
-            } else {
-                result[p.idx] = Integer.toString(index);
+
+            switch (index) {
+                case 1:
+                    result[p.idx] = "Gold Medal";
+                    break;
+                case 2:
+                    result[p.idx] = "Silver Medal";
+                    break;
+                case 3:
+                    result[p.idx] = "Bronze Medal";
+                    break;
+                default:
+                    result[p.idx] = Integer.toString(index);
+                    break;
             }
-            index++;
-            
+            ++index;
         }
+
         return result;
     }
-}
-class Pair {
+
+    class Pair {
         int val;
         int idx;
-    
-        Pair (int val, int idx) {
+
+        Pair(int val, int idx) {
             this.val = val;
             this.idx = idx;
         }
     }
+}

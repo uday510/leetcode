91afb79ba120aceffcfb9cac132ec59fdd3adfef
Uday <BUDAYTEJA009@GMAIL.COM>
2024-05-09 22:55:06 +0530

    Time: 35 ms (51.47%), Space: 61.4 MB (77.94%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+        
+        long maxHappiness = 0L;
+        
+        int delay = 0;
+        int index = happiness.length - 1;
+        while (k-- > 0) {
+            maxHappiness += Math.max(happiness[index--] - delay++, 0)*1L;
+        }
+        
+        return maxHappiness;
+    }
+}

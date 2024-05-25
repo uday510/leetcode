1bf27c2b59e04893a4ca909d95b3ebd46b1ee254
Uday <BUDAYTEJA009@GMAIL.COM>
2024-05-25 21:24:27 +0530

    Time: 4 ms (55.64%), Space: 41.9 MB (28.79%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    Set<String> words;
+    List<String> sentences;
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        words = new HashSet<>(wordDict);
+        sentences = new ArrayList<>();
+        dfs(0, "", s);
+        return sentences;
+    }
+    public void dfs(int i, String curr, String s) {
+        if (i == s.length()) {
+            sentences.add(curr);
+            return;
+        }
+
+        for (int j = i; j < s.length(); ++j) {
+            String substr = s.substring(i, j+1);
+            if (words.contains(substr)) {
+                if (curr.length() == 0) {
+                    dfs(j+1, curr + substr, s);
+                } else {
+                    dfs(j+1, curr+ " " + substr, s);
+                }
+            }
+        }
+    }
+}

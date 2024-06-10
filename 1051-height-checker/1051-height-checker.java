468135c6e6be9324b8a64d6da4bbd4290363f9e7
Uday <BUDAYTEJA009@GMAIL.COM>
2024-06-10 23:49:02 +0530

    Time: 2 ms (88.93%), Space: 41.5 MB (19.46%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] expected = Arrays.copyOfRange(heights, 0, heights.length);
+        Arrays.sort(expected);
+        int cnt = 0;
+        
+      for (int i = 0; i < heights.length; ++i) {
+          if (heights[i] != expected[i]) {
+              ++cnt;
+          }
+      }
+        
+    return cnt;
+    }
+}

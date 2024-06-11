84cf158f2bf7b8ff51c038728f1fdbe81013bc64
Uday <BUDAYTEJA009@GMAIL.COM>
2024-06-11 22:28:11 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (97.97%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -1,35 +1,26 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        Map<Integer, Integer> map = new HashMap<>();
-        Set<Integer> set = new HashSet<>();
-        
-        int[] output = new int[arr1.length];
-        for (int num : arr1) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        int[] count = new int[1001];
+
+        for (int i : arr1) {
+            count[i]++;
         }
-        
+
         int index = 0;
-        for (int i = 0; i < arr2.length; ++i) {
-            int current = arr2[i];
-            set.add(current);
-            
-            while (map.get(current) > 0) {
-                output[index++] = current;
-                map.put(current, map.get(current) - 1);
+
+        for (int i : arr2) {
+            while (count[i] > 0) {
+                arr1[index++] = i;
+                count[i]--;
             }
         }
-        
-        List<Integer> list = new ArrayList<>();
-        for (int num : arr1) {
-            if (!set.contains(num))
-                list.add(num);
+
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr1[index++] = i;
+                count[i]--;
+            }
         }
-        Collections.sort(list);
-        
-        for (int num : list) {
-            output[index++] = num;
-        }
-        
-        return output;
+        return arr1;
     }
 }

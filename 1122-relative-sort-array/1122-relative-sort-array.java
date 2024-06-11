77f08f12c74296a8caf28db56aebc5263f5b1546
Uday <BUDAYTEJA009@GMAIL.COM>
2024-06-11 22:36:42 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (96.54%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -1,26 +1,25 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
         int[] count = new int[1001];
-
-        for (int i : arr1) {
-            count[i]++;
-        }
-
+        
+        for (int num : arr1) 
+            ++count[num];
+        
         int index = 0;
-
-        for (int i : arr2) {
-            while (count[i] > 0) {
-                arr1[index++] = i;
-                count[i]--;
+        for (int idx = 0; idx < arr2.length; ++idx) {
+            int currentNum = arr2[idx];
+            
+            while (count[currentNum]-- > 0) {
+                arr1[index++] = currentNum;
             }
+                
         }
-
-        for (int i = 0; i < count.length; i++) {
-            while (count[i] > 0) {
+        
+        for (int i = 0; i < count.length; ++i) {
+            while (count[i]-- > 0)
                 arr1[index++] = i;
-                count[i]--;
-            }
         }
+        
         return arr1;
     }
 }

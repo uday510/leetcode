a0d5eeff1e05d3e8fecb4bf86de53e199f169a81
Uday <BUDAYTEJA009@GMAIL.COM>
2024-06-12 21:26:27 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (88.84%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,26 +1,23 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int idx0 = 0;
-        int idx1 = 0;
-        int idx2 = nums.length - 1;
-        
-        while (idx1 <= idx2) {
-            if (nums[idx1] == 0) {
-                swap(idx0, idx1, nums);
-                idx0++;
-                idx1++;
-            } else if (nums[idx1] == 1) {
-                // idx0++;
-                idx1++;
+        int p0 = 0; int p1 = 0; int p2 = nums.length-1;
+
+        while (p1 <= p2) {
+            if (nums[p1] == 0) {
+                swap(p0, p1, nums);
+                p0++;
+                p1++;
+            } else if (nums[p1] == 2) {
+                swap(p1, p2, nums);
+                p2--;
             } else {
-                swap(idx1, idx2, nums);
-                idx2--;
+                p1++;
             }
         }
     }
     public void swap(int i, int j, int[] nums) {
-        int tmp = nums[j];
-        nums[j] = nums[i];
-        nums[i] = tmp;
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }

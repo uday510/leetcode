bbd830168e292f351bb03c8ad72535ef2e44bb98
Uday <BUDAYTEJA009@GMAIL.COM>
2024-07-01 01:00:55 +0530

    Time: 14 ms (60.50%), Space: 108.1 MB (19.33%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.java
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.java
@@ -1,116 +1,87 @@
-import java.util.*;
-
 class Solution {
-    private static final int ALICE = 1;
-    private static final int BOB = 2;
-    private static final int BOTH = 3;
-
+    private final int ALICE = 1;
+    private final int BOB = 2;
+    private final int BOTH = 3;
     public int maxNumEdgesToRemove(int n, int[][] edges) {
-        DSU alice = new DSU(n + 1);
-        DSU bob = new DSU(n + 1);
+        DSU alice = new DSU(n+1);
+        DSU bob = new DSU(n+1);
         
-        int res = 0;
-
+        int ans = 0;
         for (int type = BOTH; type > 0; --type) {
-            for (int[] edge : edges) {
+            for (int[] edge: edges) {
                 int t = edge[0];
                 int u = edge[1];
                 int v = edge[2];
-
-                if (t != type) {
+                
+                if (t != type)
                     continue;
-                }
-
-                if (t == ALICE) {
-                    if (alice.connected(u, v)) {
-                        res++;
-                    } else {
+                
+                if (t == BOTH) {
+                    if (alice.connected(u, v) && bob.connected(u, v))
+                        ++ans;
+                    else {
                         alice.union(u, v);
-                    }
-                } else if (t == BOB) {
-                    if (bob.connected(u, v)) {
-                        res++;
-                    } else {
                         bob.union(u, v);
                     }
+                } else if (t == ALICE) {
+                    if (alice.connected(u, v))
+                        ++ans;
+                    else 
+                        alice.union(u, v);
                 } else {
-                    if (alice.connected(u, v) && bob.connected(u, v)) {
-                        res++;
-                    } else {
-                        alice.union(u, v);
-                        bob.union(u, v);
+                    if (t == BOB) {
+                        if (bob.connected(u, v))
+                            ++ans;
+                         else
+                             bob.union(u, v);
                     }
                 }
             }
         }
-
-        if (alice.components() != 1 || bob.components() != 1) {
-            return -1;
-        }
-        return res;
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int n = 4;
-        int[][] edges = {
-            {3, 1, 2},
-            {3, 2, 3},
-            {1, 1, 3},
-            {1, 2, 4},
-            {1, 1, 4},
-            {2, 3, 4}
-        };
-        System.out.println(solution.maxNumEdgesToRemove(n, edges));  // Expected output: 2
+        if (alice.components() != 1 || bob.components() != 1)
+              return -1;
+            
+        return ans;
     }
 }
-
 class DSU {
-    private int[] root;
-    private int[] rank;
-
-    public DSU(int n) {
+    int[] root;
+    
+    DSU(int n) {
         root = new int[n];
-        rank = new int[n];
         for (int i = 0; i < n; ++i) {
             root[i] = i;
-            rank[i] = 1;
         }
     }
-
+    
     public int find(int x) {
-        if (x == root[x]) {
+        if (x == root[x]) 
             return x;
-        }
-        return root[x] = find(root[x]);  // Path compression
+        
+        return root[x] = find(root[x]);
     }
-
+    
     public void union(int x, int y) {
         int rootX = find(x);
         int rootY = find(y);
-        if (rootX != rootY) {
-            if (rank[rootX] > rank[rootY]) {
-                root[rootY] = rootX;
-            } else if (rank[rootX] < rank[rootY]) {
-                root[rootX] = rootY;
-            } else {
-                root[rootY] = rootX;
-                rank[rootX]++;
-            }
-        }
+        
+        if (rootX == rootY)
+            return;
+        
+        root[rootX] = rootY;
     }
-
     public boolean connected(int x, int y) {
         return find(x) == find(y);
     }
-
+    
     public int components() {
-        int count = 0;
+        int cnt = 0;
+        
         for (int i = 1; i < root.length; ++i) {
-            if (root[i] == i) {
-                count++;
-            }
+            if (root[i] == i)
+                ++cnt;
         }
-        return count;
+        
+        return cnt;
     }
 }

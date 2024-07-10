b81f6378e11db9e7b7232c662f7830f78e3e6bbc
Uday <BUDAYTEJA009@GMAIL.COM>
2024-07-11 00:13:34 +0530

    Time: 1 ms (99.42%), Space: 42.4 MB (8.45%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -3,10 +3,13 @@ class Solution {
         int ops = 0;
         
         for (String log : logs) {
-            if (log.equals("../")) {
-                if (ops > 0) ops--;
-            } else if (!log.equals("./")) {
+            if (log.equals("./")) {
+                continue;
+            }
+            if (!log.equals("../")) {
                 ops++;
+            } else {
+               ops = Math.max(0, ops-1);
             }
         }
         return ops;

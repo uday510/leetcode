b48a8e88b142a28619348c0ad99e73d0bd507559
Uday <BUDAYTEJA009@GMAIL.COM>
2024-07-10 09:56:43 +0530

    Time: 1 ms (99.42%), Space: 42.2 MB (26.85%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        int ops = 0;
+        
+        for (String log : logs) {
+            
+            if (log.equals("./")) {
+                continue;
+            }
+            if (!log.equals("../")) {
+                ops++;
+            } else {
+                ops--;
+            }
+            ops = Math.max(0, ops);
+        }
+        
+        return ops;
+    }
+}

6894fea883b01bf33fc69fe1609ed277450c3fd9
Uday <BUDAYTEJA009@GMAIL.COM>
2024-07-21 16:49:53 +0530

    Time: 2 ms (53.69%), Space: 41.4 MB (58.46%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -10,18 +10,15 @@ class Solution {
         
         for (int i = 0; i < s.length(); ++i) {
             char c = s.charAt(i);
-            
-           if (open(c)) {
+            boolean open = !map.containsKey(c);
+            if (open) {
                stack.push(c);
-           } else {
+            } else {
                char top = stack.isEmpty() ? '#' : stack.pop();
                if (map.get(c) != top)
                    return false;
             }
         }
         return stack.isEmpty();
-    }
-    public boolean open(char c) {
-        return !map.containsKey(c);
     }
 }

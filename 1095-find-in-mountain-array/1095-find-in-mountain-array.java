64f00e29e7f1974b2efad78f8360bd827c11cd5e
Uday <BUDAYTEJA009@GMAIL.COM>
2024-07-26 03:06:38 +0530

    Time: 5 ms (7.37%), Space: 45.1 MB (8.81%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int peak = findPeak(mountainArr);
+
+        System.out.println("peak "  + peak);
+
+        int s = bs(mountainArr, 0, peak, target, 1);
+        if (mountainArr.get(s) == target) {
+            return s;
+        }
+
+        int e = bs(mountainArr, peak+1, mountainArr.length() - 1, target, -1);
+        if (mountainArr.get(e) == target) {
+            return e;
+        }
+
+        return -1;
+    }
+
+    public int bs(MountainArray mArr, int left, int right, int target, int sign) {
+        target *= sign;
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            if (mArr.get(mid)*sign < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    public int findPeak(MountainArray mArr) {
+        int left = 0;
+        int right = mArr.length() - 1;
+
+        while (left < right) {
+            int mid = (left + right) >> 1;
+
+            if (mArr.get(mid) > mArr.get(mid + 1)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}

b4226a354e60449bd04a3df4211fde458680c851
Uday <BUDAYTEJA009@GMAIL.COM>
2024-07-26 00:35:26 +0530

    Time: 2191 ms (5.02%), Space: 61 MB (5.27%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,46 +1,36 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        mergeSort(nums, 0, nums.length-1);
+        quickSort(nums, 0, nums.length-1);
         return nums;
     }
-    public void mergeSort(int[] nums, int low, int high) {
+    public void quickSort(int[] nums, int low, int high) {
         if (high <= low)
             return;
         
-        int mid = (high + low) >> 1;
-        mergeSort(nums, low, mid);
-        mergeSort(nums, mid+1, high);
-        merge(nums, low, mid, mid+1, high);
+        int pivot = partition(nums, low, high);
+        quickSort(nums, low, pivot-1);
+        quickSort(nums, pivot+1, high);
     }
-    public void merge(int[] nums, int lStart, int lEnd, int rStart, int rEnd) {
-       int len1 = lEnd-lStart+1;
-       int len2 = rEnd-rStart+1;
-       int[] tmp1 = new int[len1];
-       int[] tmp2 = new int[len2];
-      
-      int i = 0;
-      int j = 0;
-      for (; i < len1; ++i)
-          tmp1[i] = nums[i+lStart];
-      
-      for (; j < len2; ++j)
-          tmp2[j] = nums[j+rStart];
-        
-     i=0;
-     j=0;
-     int k = lStart;
-        
-     while (i < len1 && j < len2)
-         if (tmp1[i] <= tmp2[j])
-             nums[k++] = tmp1[i++];
-         else 
-             nums[k++] = tmp2[j++];
-        
-    
-    while (i < len1)
-        nums[k++] = tmp1[i++];
-        
-    while (j < len2)
-        nums[k++] = tmp2[j++];
+    public int partition(int[] nums, int low, int high) {
+        int ridx = ThreadLocalRandom.current().nextInt(low, high + 1);
+        swap(nums, ridx, low);
+        int idx = low;
+        int pivot = nums[low];
+            
+        for (int i = low+1; i <= high; ++i) {
+            if (nums[i] >= pivot) continue;
+            if (nums[i] > pivot)
+                continue;
+            
+            ++idx;
+            swap(nums, i, idx);
+        }
+        swap(nums, low, idx);
+        return idx;
+    }
+    public void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }

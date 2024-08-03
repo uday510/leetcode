cbe07de6a212b13ce57de06d5c01dadbca3c8aa0
Uday <BUDAYTEJA009@GMAIL.COM>
2024-08-03 11:13:47 +0530

    Time: 1 ms (100.00%), Space: 44.1 MB (56.26%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
@@ -3,13 +3,14 @@ class Solution {
         int[] cnt = new int[1001];
         int n = target.length;
         int total = 0;
-
+        
         for (int val : arr) {
             ++cnt[val];
             ++total;
         }
-
-        for (int i = 0;  i < n && cnt[target[i]]-- > 0 && total-- > 0;  ++i);
+        
+        int i = 0;
+        for (; i < n && cnt[target[i++]]-- > 0 && total-- > 0;);
 
         return total == 0;
     }

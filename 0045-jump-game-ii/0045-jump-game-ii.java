0998731215c2800fba0dcad86dae0c412b1fdc60
Uday <BUDAYTEJA009@GMAIL.COM>
2024-08-18 18:03:40 +0530

    Time: 2 ms (59.82%), Space: 44.8 MB (84.98%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,15 +1,21 @@
 class Solution {
     public int jump(int[] nums) {
-        int N = nums.length;
-        int[] dp = new int[N];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
+        int jumps = 0;
+        int currentJumpEnd = 0;
+        int farthestJump = 0;
         
-        for (int i = 0; i < N; ++i) {
-            for (int j = i+1; j <= i + nums[i] && j < N; ++j) {
-                dp[j] = Math.min(dp[j], dp[i]+1);
+        for (int i = 0; i < nums.length-1; ++i) {
+            farthestJump = Math.max(i+nums[i], farthestJump);
+            
+            if (currentJumpEnd == i) {
+                ++jumps;
+                currentJumpEnd = farthestJump;
+                
+                if (currentJumpEnd <= i) {
+                    return -1;
+                }
             }
         }
-        return dp[N-1];
+        return jumps;
     }
 }

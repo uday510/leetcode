ba469fb272dc53a4921326f9554a96f7717a06d1
Uday <BUDAYTEJA009@GMAIL.COM>
2024-08-19 23:32:37 +0530

    Time: 8 ms (52.13%), Space: 40.4 MB (55.42%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -5,13 +5,13 @@ class Solution {
     public int dfs(int curr, int clip, int n) {
         if (curr > n) return n + 1;
         if (curr == n) return 0;
-
-       int pasteOnly = n + 1;
-        if (clip != 0) {
+        
+        int pasteOnly = n + 1;
+        if (clip != 0)
             pasteOnly = 1 + dfs(curr + clip, clip, n);
-        }
-
+        
         int copyPaste = 2 + dfs(curr + curr, curr, n);
+        
         return Math.min(pasteOnly, copyPaste);
     }
 }

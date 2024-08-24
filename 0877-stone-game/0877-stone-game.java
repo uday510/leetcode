af0bf46b3ba72f6619c727c5158ff85b695015a1
Uday <BUDAYTEJA009@GMAIL.COM>
2024-08-24 13:30:29 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (82.55%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,33 +1,5 @@
 class Solution {
-    Boolean[][] dp;
     public boolean stoneGame(int[] piles) {
-       dp = new Boolean[piles.length][piles.length];
-       // return dfs(piles, 0, 0, 0, piles.length-1); 
         return true;
-    }
-    public boolean dfs(int[] piles, int alice, int bob, int i, int j) {
-        if (i > j) {
-            return alice > bob;
-        } 
-
-        if (dp[i][j] != null) {
-            return dp[i][j];
-        }
-
-        boolean p1 = dfs(piles, alice+piles[i], bob+piles[j], i+1, j-1);
-
-        boolean p2 = dfs(piles, alice+piles[j], bob+piles[i], i+1, j-1);
-
-        boolean p = p1 || p2;
-
-        if (i+1 <= j) {
-           p = p || dfs(piles, alice+piles[i], bob+piles[i+1], i+2, j);
-        }
-        
-        if (i <= j-1) {
-            p = p || dfs(piles, alice+piles[j], bob+piles[j-1], i, j-2);
-        }
-
-        return dp[i][j] = p;
     }
 }

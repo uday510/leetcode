ccd58330fb397cf1a2ac42a0a513c26b384f3188
Uday <BUDAYTEJA009@GMAIL.COM>
2024-08-28 22:22:55 +0530

    Time: 4 ms (47.85%), Space: 48.9 MB (91.37%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,38 +1,38 @@
 class Solution {
-    private final int[][] directions = {
-        {0,1},
-        {1,0},
-        {-1,0},
-        {0,-1}
-    };
+    boolean[][] seen;
+    int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     public int numIslands(char[][] grid) {
-        int N = grid.length;
-        int M = grid[0].length;
-        int totalIslands = 0;
-        boolean[][] vis = new boolean[N][M];
-
-        for (int i = 0; i < N; ++i) {
-            for (int j = 0; j < M; ++j) {
-                if (grid[i][j] == '1' && !vis[i][j]) {
-                    totalIslands++;
-                    dfs(i,j,grid,vis);
+        int m = grid.length;
+        int n = grid[0].length;
+        seen = new boolean[m][n];
+    
+        int islands = 0;
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int currVal = grid[i][j];
+                if (currVal == '0' || seen[i][j]) {
+                    continue;
                 }
+                ++islands;
+                dfs(i,j, m, n, grid);
             }
         }
-        return totalIslands;
+        return islands;
     }
-    public void dfs(int i,int j, char[][] grid, boolean[][] vis) {
-        if (i < 0 || i >= grid.length 
-            || j < 0 || j >= grid[0].length ||
-            grid[i][j] == '0' || vis[i][j]) {
-                return;
+    
+    private void dfs(int i, int j, int m, int n, char[][] grid) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0' || seen[i][j]) {
+            return;
         }
+        
+        seen[i][j] = true;
+        for (int[] dir: directions) {
+            int newI = dir[0] + i;
+            int newJ = dir[1] + j;
+        
+            dfs(newI, newJ, m, n, grid);
+        }
+    }
 
-        vis[i][j] = true;
-        for (int[] dir : directions) {
-            int newRow = dir[0] + i;
-            int newCol = dir[1] + j;
-            dfs(newRow, newCol, grid, vis);
-        }
-    }
 }

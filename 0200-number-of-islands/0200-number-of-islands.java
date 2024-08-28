a1b1439ad480c02384f7fc3489ddb4de66fc6c39
Uday <BUDAYTEJA009@GMAIL.COM>
2024-08-28 22:30:32 +0530

    Time: 3 ms (84.98%), Space: 49.5 MB (51.43%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,39 +1,38 @@
 class Solution {
-    boolean[][] seen;
-    int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        seen = new boolean[m][n];
-    
-        int islands = 0;
+         int count = 0;
+        if (grid == null || grid.length == 0) {
+            return count;
+        }
         
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                int currVal = grid[i][j];
-                if (currVal == '0' || seen[i][j]) {
-                    continue;
+        int row = grid.length;
+        int col = grid[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++){
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
                 }
-                ++islands;
-                dfs(i,j, m, n, grid);
             }
         }
-        return islands;
+        return count;
     }
-    
-    private void dfs(int i, int j, int m, int n, char[][] grid) {
-        if (!valid(i,j,m,n,grid)) 
+
+    private void dfs(char[][] grid, int i, int j) {
+        int row = grid.length;
+        int col = grid[0].length;
+        if (!isValid(grid, i, j) || grid[i][j] == '0') {
             return;
-        
-        seen[i][j] = true;
-        for (int[] dir: directions) {
-            int newI = dir[0] + i;
-            int newJ = dir[1] + j;
-        
-            dfs(newI, newJ, m, n, grid);
         }
+        grid[i][j] = '0';
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
     }
-    private boolean valid(int i, int j, int m, int n, char[][] grid) {
-        return !(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0' || seen[i][j]);
+    private boolean isValid(char[][] grid, int i, int j) {
+        int row = grid.length;
+        int col = grid[0].length;
+        return i >= 0 && j >= 0 && i < row && j < col;
     }
 }

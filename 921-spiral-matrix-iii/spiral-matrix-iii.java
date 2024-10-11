31c2465971c9fee77cd0451d4a66774046bff8f2
Uday <BUDAYTEJA009@GMAIL.COM>
2024-10-12 00:16:04 +0530

    Time: 5 ms (59.64%) | Memory: 45.4 MB (27.82%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.java
+++ 921-spiral-matrix-iii/spiral-matrix-iii.java
@@ -2,30 +2,30 @@ class Solution {
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
         List<int[]> result = new ArrayList<>();
         int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        
+
         result.add(new int[]{rStart, cStart});
 
         if (rows * cols == 1) {
-            return result.toArray(new int[1][2]);
-        } 
+            return new int[1][2];
+        }
         int step = 1;
-        int dirIndex = 0;
+        int dirIdx = 0;
 
         while (result.size() < rows * cols) {
-
+            
             for (int i = 0; i < 2; ++i) {
                 for (int s = 0; s < step; ++s) {
-                    rStart += directions[dirIndex][0];
-                    cStart += directions[dirIndex][1];
+                    rStart += directions[dirIdx][0];
+                    cStart += directions[dirIdx][1];
 
-                    if (rStart >= 0 && cStart >= 0 && rStart < rows && cStart < cols) {
+                    if (rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols) {
                         result.add(new int[]{rStart, cStart});
                     }
                 }
-                dirIndex = (dirIndex + 1) % 4;
+                dirIdx = (dirIdx + 1) % 4;
             }
             ++step;
         }
-        return result.toArray(new int[rows * cols][2]);
+        return result.toArray(new int[rows][cols]);
     }
 }

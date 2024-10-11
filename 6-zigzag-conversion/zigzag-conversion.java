082980bd552501c769d838e2de890e93cb61fe0c
Uday <BUDAYTEJA009@GMAIL.COM>
2024-10-12 01:39:22 +0530

    Time: 5 ms (69.21%) | Memory: 45.5 MB (28.89%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+        var rows = new ArrayList<StringBuilder>();
+        for (int i = 0; i < numRows; ++i) {
+            rows.add(new StringBuilder());
+        }
+
+        int currRow = 0;
+        int step = 1;
+
+        for (char c : s.toCharArray()) {
+            rows.get(currRow).append(c);
+
+            currRow += step;
+
+            if (currRow == 0 || currRow == numRows - 1) {
+                step *= -1;
+            }
+        }
+
+        var result = new StringBuilder();
+        for (var row : rows) {
+            result.append(row);
+        }
+        return result.toString();
+    }
+}

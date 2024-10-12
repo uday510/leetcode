854763b0a8c3b7c296a3b15dd57b9954bb6c4423
Uday <BUDAYTEJA009@GMAIL.COM>
2024-10-12 16:26:04 +0530

    Time: 34 ms (96.61%) | Memory: 51.6 MB (22.60%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
@@ -1,36 +1,32 @@
 class Solution {
     public int smallestChair(int[][] times, int targetFriend) {
         List<int[]> list = new ArrayList<>();
+        var assigned = new PriorityQueue<int[]>((o1, o2) -> o1[0] - o2[0]);
+        var available = new PriorityQueue<Integer>();
+        int currChair = 0;
 
         for (int i = 0; i < times.length; ++i) {
             list.add(new int[]{times[i][0], times[i][1], i});
         }
 
-        Collections.sort(list, (o1, o2) -> o1[0] - o2[0]);
-
-        var pq = new PriorityQueue<int[]>((o1, o2) -> o1[0] - o2[0]);
-
-        PriorityQueue<Integer> availableChairs = new PriorityQueue<>();
-
-        int currChair = 0;
+        list.sort((o1, o2) -> o1[0] - o2[0]);
 
         for (int[] time : list) {
-            int arrives = time[0];
+            int arrival = time[0];
             int leaves = time[1];
             int index = time[2];
 
-            while (!pq.isEmpty() && pq.peek()[0] <= arrives) {
-                availableChairs.offer(pq.poll()[1]);
+            while (!assigned.isEmpty() && assigned.peek()[0] <= arrival) {
+                available.offer(assigned.poll()[1]);
             }
 
             if (index == targetFriend) {
-                return availableChairs.isEmpty() ? currChair : availableChairs.peek();
+                return available.isEmpty() ? currChair : available.poll();
             }
 
-            int assignedChair = availableChairs.isEmpty() ? currChair++ : availableChairs.poll();
-            pq.offer(new int[]{leaves, assignedChair});
+            int assignedChair = available.isEmpty() ? currChair++ : available.poll();
+            assigned.offer(new int[]{leaves, assignedChair});
         }
-
-        return -1;  
+        return -1;
     }
 }

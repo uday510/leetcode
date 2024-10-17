e40424b5faddf5f6aa9770f66f1090ec13692bb0
Uday <BUDAYTEJA009@GMAIL.COM>
2024-10-17 23:31:23 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (66.95%) - LeetSync

---
+++ 670-maximum-swap/maximum-swap.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public int maximumSwap(int num) {
+        char[] numStr = Integer.toString(num).toCharArray();
+        int n = numStr.length;
+        int maxDigitIndex = -1, swapIdx1 = -1, swapIdx2 = -1;
+
+        // Traverse the string from right to left, tracking the max digit and
+        // potential swap
+        for (int i = n - 1; i >= 0; --i) {
+            if (maxDigitIndex == -1 || numStr[i] > numStr[maxDigitIndex]) {
+                maxDigitIndex = i; // Update the index of the max digit
+            } else if (numStr[i] < numStr[maxDigitIndex]) {
+                swapIdx1 = i; // Mark the smaller digit for swapping
+                swapIdx2 = maxDigitIndex; // Mark the larger digit for swapping
+            }
+        }
+
+        // Perform the swap if a valid swap is found
+        if (swapIdx1 != -1 && swapIdx2 != -1) {
+            char temp = numStr[swapIdx1];
+            numStr[swapIdx1] = numStr[swapIdx2];
+            numStr[swapIdx2] = temp;
+        }
+
+        return Integer.parseInt(new String(numStr)); // Return the new number or the original if no
+        // swap occurred
+    }
+}

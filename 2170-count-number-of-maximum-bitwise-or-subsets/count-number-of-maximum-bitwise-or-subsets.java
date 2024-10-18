2c262f25829940e29880521bbb1241c7276e298a
Uday <BUDAYTEJA009@GMAIL.COM>
2024-10-18 20:44:21 +0530

    Time: 259 ms (5.14%) | Memory: 59.8 MB (7.31%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,29 +1,26 @@
-import java.util.*;
-
 class Solution {
     public int countMaxOrSubsets(int[] nums) {
         var subsets = new ArrayList<List<Integer>>();
-        subsets.add(new ArrayList<>()); 
-        var map = new HashMap<Integer, Integer>(); 
+        subsets.add(new ArrayList<>());
+        var map = new HashMap<Integer, Integer>();
         int maxOR = 0;
 
         for (int num : nums) {
-            int len = subsets.size(); 
+            int len = subsets.size();
             for (int idx = 0; idx < len; ++idx) {
-                List<Integer> newSubset = new ArrayList<>(subsets.get(idx));
-                newSubset.add(num);
-                subsets.add(newSubset);
+                List<Integer> tmp = new ArrayList<>(subsets.get(idx));
+                tmp.add(num);
+                 int curr = 0;
+                    for (Integer val : tmp) {
+                        curr |= val;
+                    }
+                subsets.add(tmp);
 
-                int currOR = 0;
-                for (Integer val : newSubset) {
-                    currOR |= val;
-                }
+                map.put(curr, map.getOrDefault(curr, 0) + 1);
 
-                map.put(currOR, map.getOrDefault(currOR, 0) + 1);
-                maxOR = Math.max(maxOR, currOR); 
+                maxOR = Math.max(maxOR, curr);
             }
         }
-
         return map.get(maxOR);
     }
 }

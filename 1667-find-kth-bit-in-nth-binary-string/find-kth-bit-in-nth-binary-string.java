daeadb92253d2444cf05c1e6550cd0db31cf537c
Uday <BUDAYTEJA009@GMAIL.COM>
2024-10-20 00:35:28 +0530

    Time: 173 ms (14.01%) | Memory: 55.2 MB (7.97%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
@@ -1,27 +1,24 @@
 class Solution {
     public char findKthBit(int n, int k) {
-        return dfs(n, k).charAt(k - 1);
+        return dfs(n, k).charAt(k-1);
     }
     private String dfs(int n, int k) {
-        if (n == 1) {
+        if (n == 1) 
             return "0";
-        }
 
         String str = dfs(n - 1, k);
-        return STR."\{str}1\{reverse(invert(str))}";
+
+        return str + "1" + reverse(invert(str));
     }
     private String invert(String str) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < str.length(); i++) {
-            sb.append(str.charAt(i) == '0' ? '1' : '0');
+
+        for (char c : str.toCharArray()) {
+            sb.append (c == '1' ? 0 : 1);
         }
         return sb.toString();
     }
-     private String reverse(String str) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = str.length() - 1; i >= 0; i--) {
-            sb.append(str.charAt(i));
-        }
-        return sb.toString();
+    private String reverse(String str) {
+        return new StringBuffer(str).reverse().toString();
     }
 }

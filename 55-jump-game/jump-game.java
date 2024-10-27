19be79e151ed8b3f53db99ba0e3325085d4119b7
Uday <BUDAYTEJA009@GMAIL.COM>
2024-10-27 17:23:45 +0530

    Time: 66 ms (14.13%) | Memory: 45.5 MB (57.02%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,25 +1,18 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return canJumpFromPos(0, nums, dp);
-    }
-    private boolean canJumpFromPos(int pos, int[] nums, int[] dp) {
-        if (pos >= nums.length - 1) 
-            return true;
-
-        if (dp[pos] != -1) 
-            return dp[pos] == 1 ? true : false;
+          int len = nums.length;
+        boolean[] dp = new boolean[len];
+        dp[len-1] = true;
         
-        int farthestJump = Math.min(pos+nums[pos], nums.length-1);
-
-        for (int nextJump = pos+1; nextJump <= farthestJump; ++nextJump) {
-            if (canJumpFromPos(nextJump, nums, dp)) {
-                dp[pos] = 1; 
-                return true;
+        for (int i = len -2; i > -1; --i) {
+            int farthestJump = Math.min(i + nums[i], len - 1);
+            for (int j = i+1; j <= farthestJump; ++j) {
+                if (dp[j])  {
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-        dp[pos] = 0;
-        return false;
+        return dp[0];
     }
 }

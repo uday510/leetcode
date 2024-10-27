295d6b319ae03029b5b73a339e1c37fbfb8c5afe
Uday <BUDAYTEJA009@GMAIL.COM>
2024-10-27 11:53:59 +0530

    Time: 6 ms (70.73%) | Memory: 54.9 MB (63.01%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -1,26 +1,32 @@
 class Solution {
     public int countSquares(int[][] matrix) {
-        int cols = matrix[0].length;
         int rows = matrix.length;
+        int cols = matrix[0].length;
         int[][] dp = new int[rows][cols];
-        int count = 0;
-        for(int i = 0; i < rows; i++) {
-            dp[i][0] = matrix[i][0];
-            count += dp[i][0];
-        }
-        for(int i = 0; i < cols; i++) {
-            dp[0][i] = matrix[0][i];
-            count += dp[0][i];
-        }
-        count -= dp[0][0];
-        for(int i = 1; i < rows; i++) {
-            for(int j = 1; j < cols; j++) {
-                if(matrix[i][j] == 1) {
-                    dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1;
-                    count += dp[i][j];
-                }
+        int cnt = fill(dp, matrix);
+
+        for (int i = 1; i < rows; ++i) {
+            for (int j = 1; j < cols; ++j) {
+                if (matrix[i][j] == 0)
+                    continue;
+                dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+                cnt += dp[i][j];
             }
         }
-        return count;
+        return cnt;
+    }
+    private int fill(int[][] dp, int[][] matrix) {
+        int cnt = 0;
+
+        for (int i = 0; i < dp.length; ++i) {
+            dp[i][0] = matrix[i][0];
+            cnt += dp[i][0];
+        }
+        for (int j = 1; j < dp[0].length; ++j) {
+            dp[0][j] = matrix[0][j];
+            cnt += dp[0][j];
+        }
+
+        return cnt;
     }
 }

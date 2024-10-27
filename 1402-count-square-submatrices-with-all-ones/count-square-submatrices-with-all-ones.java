81dcdd461897a92e0496b761c65ddca10d5ab96d
Uday <BUDAYTEJA009@GMAIL.COM>
2024-10-27 11:55:05 +0530

    Time: 6 ms (70.73%) | Memory: 54.9 MB (54.27%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -18,14 +18,11 @@ class Solution {
     private int fill(int[][] dp, int[][] matrix) {
         int cnt = 0;
 
-        for (int i = 0; i < dp.length; ++i) {
-            dp[i][0] = matrix[i][0];
-            cnt += dp[i][0];
-        }
-        for (int j = 1; j < dp[0].length; ++j) {
-            dp[0][j] = matrix[0][j];
-            cnt += dp[0][j];
-        }
+        for (int i = 0; i < dp.length; ++i)
+            cnt += dp[i][0] = matrix[i][0];
+            
+        for (int j = 1; j < dp[0].length; ++j)
+            cnt += dp[0][j] = matrix[0][j];
 
         return cnt;
     }

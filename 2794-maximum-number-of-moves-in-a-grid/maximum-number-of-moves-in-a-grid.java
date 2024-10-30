0a2d2a43c518eed251250fbf6fe6075d79429713
Uday <BUDAYTEJA009@GMAIL.COM>
2024-10-30 22:09:18 +0530

    Time: 5 ms (100.00%) | Memory: 56.9 MB (16.46%) - LeetSync

--- 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.java
+++ 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.java
@@ -1,54 +1,77 @@
+// class Solution {
+//     int[][] dirs = {{-1,1}, {0,1}, {1,1}};
+//     int N;
+//     int M;
+//     public int maxMoves(int[][] grid) {
+//         int max = 0;
+//         N = grid.length;
+//         M = grid[0].length;
+//         for (int i = 0; i < N; ++i) {
+//             max = Math.max(max, moves(i, 0, grid));
+//         }
+//         return max;
+//     }
+//     private int moves(int row, int col, int[][] grid) {
+//         int moved = 0;
+//         int moves = 0;
+
+//         for (int[] d : dirs) {
+//             int R = d[0] + row;
+//             int C = d[1] + col;
+
+//             if (R < 0 || R > N-1 || C < 0 || C > M-1) 
+//                 continue;
+
+//             moves = Math.max(moves, moves(R,C,grid));
+//             moved = 1;
+//         }
+
+//         return moves + moved;
+//     }
+// }
+
 class Solution {
-
-    // The three possible directions for the next column.
-    private final int[] dirs = { -1, 0, 1 };
-
     public int maxMoves(int[][] grid) {
-        int M = grid.length, N = grid[0].length;
+        int max = 0;
+        int[][] dp = new int[grid.length][grid[0].length];
+        for (int[] arr : dp) 
+            Arrays.fill(arr, -1);
 
-        Queue<int[]> q = new LinkedList<>();
-        boolean[][] vis = new boolean[M][N];
+        for (int i = 0; i < grid.length; ++i) {
+            max = Math.max(max, moves(i, 0, grid, dp));
+        }
+        return max;
+    }
+    private int moves(int row, int col, int[][] grid, int[][] dp) {
+        if (row < 0 || row >= grid.length || col >= grid[0].length)
+            return 0;
 
-        // Enqueue the cells in the first column.
-        for (int i = 0; i < M; i++) {
-            vis[i][0] = true;
-            q.offer(new int[] { i, 0, 0 });
+        if (dp[row][col] != -1) 
+            return dp[row][col];
+
+        int m1 = 0;
+        int m2 = 0;
+        int m3 = 0;
+        int moved = 0;
+
+        if (row-1 > -1 && col + 1 < grid[0].length 
+            && grid[row-1][col+1] > grid[row][col]) {
+
+            m1 = moves(row-1, col+1, grid, dp);
+            moved = 1; 
         }
 
-        int maxMoves = 0;
-        while (!q.isEmpty()) {
-            int sz = q.size();
-
-            while (sz-- > 0) {
-                int[] v = q.poll();
-
-                // Current cell with the number of moves made so far.
-                int row = v[0], col = v[1], count = v[2];
-
-                maxMoves = Math.max(maxMoves, count);
-
-                for (int dir : dirs) {
-                    // Next cell after the move.
-                    int newRow = row + dir, newCol = col + 1;
-
-                    // If the next cell isn't visited yet and is greater than
-                    // the current cell value, add it to the queue with the
-                    // incremented move count.
-                    if (
-                        newRow >= 0 &&
-                        newCol >= 0 &&
-                        newRow < M &&
-                        newCol < N &&
-                        !vis[newRow][newCol] &&
-                        grid[row][col] < grid[newRow][newCol]
-                    ) {
-                        vis[newRow][newCol] = true;
-                        q.offer(new int[] { newRow, newCol, count + 1 });
-                    }
-                }
-            }
+        if (col + 1 <  grid[0].length && grid[row][col+1] > grid[row][col]) {
+            m2 = moves(row, col + 1, grid, dp);
+            moved = 1;
         }
 
-        return maxMoves;
+        if (row + 1 < grid.length && col + 1 < grid[0].length && 
+            grid[row + 1][col + 1] > grid[row][col]) {
+                m3 = moves(row+1, col+1, grid, dp);
+                moved = 1;
+        }
+
+        return dp[row][col] = Math.max(m1, Math.max(m2, m3)) + moved;
     }
 }

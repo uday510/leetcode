a302c9b2e24081a2768aad349551921f7853a508
Uday <BUDAYTEJA009@GMAIL.COM>
2024-10-30 22:18:10 +0530

    Time: 7 ms (71.15%) | Memory: 56.2 MB (68.29%) - LeetSync

--- 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.java
+++ 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.java
@@ -1,77 +1,42 @@
-// class Solution {
-//     int[][] dirs = {{-1,1}, {0,1}, {1,1}};
-//     int N;
-//     int M;
-//     public int maxMoves(int[][] grid) {
-//         int max = 0;
-//         N = grid.length;
-//         M = grid[0].length;
-//         for (int i = 0; i < N; ++i) {
-//             max = Math.max(max, moves(i, 0, grid));
-//         }
-//         return max;
-//     }
-//     private int moves(int row, int col, int[][] grid) {
-//         int moved = 0;
-//         int moves = 0;
-
-//         for (int[] d : dirs) {
-//             int R = d[0] + row;
-//             int C = d[1] + col;
-
-//             if (R < 0 || R > N-1 || C < 0 || C > M-1) 
-//                 continue;
-
-//             moves = Math.max(moves, moves(R,C,grid));
-//             moved = 1;
-//         }
-
-//         return moves + moved;
-//     }
-// }
-
 class Solution {
+    int[][] dirs = {{-1,1}, {0,1}, {1,1}};
+    int[][] dp;
+    int N;
+    int M;
     public int maxMoves(int[][] grid) {
         int max = 0;
-        int[][] dp = new int[grid.length][grid[0].length];
+        N = grid.length;
+        M = grid[0].length;
+        dp = new int[N][M];
+
         for (int[] arr : dp) 
             Arrays.fill(arr, -1);
-
-        for (int i = 0; i < grid.length; ++i) {
-            max = Math.max(max, moves(i, 0, grid, dp));
+        
+        for (int i = 0; i < N; ++i) {
+            max = Math.max(max, moves(i, 0, grid));
         }
         return max;
     }
-    private int moves(int row, int col, int[][] grid, int[][] dp) {
-        if (row < 0 || row >= grid.length || col >= grid[0].length)
-            return 0;
-
+    private int moves(int row, int col, int[][] grid) {
         if (dp[row][col] != -1) 
             return dp[row][col];
-
-        int m1 = 0;
-        int m2 = 0;
-        int m3 = 0;
+        
         int moved = 0;
+        int moves = 0;
 
-        if (row-1 > -1 && col + 1 < grid[0].length 
-            && grid[row-1][col+1] > grid[row][col]) {
+        for (int[] d : dirs) {
+            int R = d[0] + row;
+            int C = d[1] + col;
 
-            m1 = moves(row-1, col+1, grid, dp);
-            moved = 1; 
-        }
+           if (R < 0 || R >= N || C < 0 || C >= M || grid[R][C] <= grid[row][col]) 
+                continue;
 
-        if (col + 1 <  grid[0].length && grid[row][col+1] > grid[row][col]) {
-            m2 = moves(row, col + 1, grid, dp);
+
+            moves = Math.max(moves, moves(R,C,grid));
             moved = 1;
         }
 
-        if (row + 1 < grid.length && col + 1 < grid[0].length && 
-            grid[row + 1][col + 1] > grid[row][col]) {
-                m3 = moves(row+1, col+1, grid, dp);
-                moved = 1;
-        }
 
-        return dp[row][col] = Math.max(m1, Math.max(m2, m3)) + moved;
+        return dp[row][col] = moves + moved;
     }
 }

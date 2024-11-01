6e1d90d2771b454f3055658b24b5c545d92e4d2a
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-01 20:22:19 +0530

    Time: 1 ms (61.48%) | Memory: 41.7 MB (62.17%) - LeetSync

--- 68-text-justification/text-justification.java
+++ 68-text-justification/text-justification.java
@@ -1,23 +1,24 @@
 class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
         var ans = new ArrayList<String>();
-        var currWords = new ArrayList<String>();
-        int currLen = 0;
+        var curr = new ArrayList<String>();
+        int len = 0;
 
         for (String word : words) {
-            if (currWords.size() + currLen + word.length() > maxWidth) {
-                int totalSpace = maxWidth - currLen;
-                int gaps = currWords.size() - 1;
+            if (curr.size() + len + word.length() > maxWidth) {
+                int totalSpace = maxWidth - len;
+                int gaps = curr.size() - 1;
                 if (gaps == 0) {
-                    ans.add(currWords.getFirst() + " ".repeat(totalSpace));
+                    ans.add(curr.getFirst() + " ".repeat(totalSpace));
                 } else {
                     int spaceBtw = totalSpace / gaps;
                     int extraSpace = totalSpace % gaps;
+                    var line = new StringBuilder();
 
-                    StringBuilder line = new StringBuilder();
-                    for (int i = 0; i < currWords.size(); ++i) {
-                        String curr = currWords.get(i);
-                        line.append(curr);
+                    for (int i = 0; i < curr.size(); ++i) {
+                        String str = curr.get(i);
+                        line.append(str);
+
                         if (i < gaps) {
                             line.append(" ".repeat(spaceBtw));
                             if (i < extraSpace) {
@@ -27,20 +28,22 @@ class Solution {
                     }
                     ans.add(line.toString());
                 }
-                currWords.clear();
-                currLen = 0;
-                    
-            } 
-            currWords.add(word);
-            currLen += word.length();
+                curr.clear();
+                len = 0;
+            }
+            curr.add(word);
+            len += word.length();
         }
+        
+        var lastLine = new StringBuilder(String.join(" ", curr));
 
-        StringBuilder lastLine = new StringBuilder(String.join(" ", currWords));
+        System.out.println(lastLine);
+
         while (lastLine.length() < maxWidth) {
             lastLine.append(" ");
         }
-
         ans.add(lastLine.toString());
+
         return ans;
     }
 }

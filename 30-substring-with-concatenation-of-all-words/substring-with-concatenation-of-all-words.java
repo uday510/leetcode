9281d2fc56df7f77b9de9c2284c743f5f298ad20
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-02 00:23:45 +0530

    Time: 172 ms (14.19%) | Memory: 45.9 MB (23.67%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -1,22 +1,23 @@
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        var ans = new ArrayList<Integer>();
         int N = s.length();
         int window = words[0].length();
-        int total = words.length * window;
+        var ans = new ArrayList<Integer>();
         var map = new HashMap<String, Integer>();
 
-        for (String word: words) 
-            map.put(word, map.getOrDefault(word, 0) + 1);
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);            
+        }
 
         for (int i = 0; i < window; ++i) {
             int left = i;
             int right = i;
             int cnt = 0;
             var tmp = new HashMap<String, Integer>();
-
+            System.out.println(" ---- ");
             while (right + window <= N) {
-                String str = s.substring(right, right + window);
+                String str = s.substring(right, right+window);
+                System.out.println(str);
                 right += window;
 
                 if (!map.containsKey(str)) {
@@ -28,13 +29,16 @@ class Solution {
                     ++cnt;
 
                     while (tmp.get(str) > map.get(str)) {
-                        String leftWord = s.substring(left, left+window);
-                        tmp.put(leftWord, tmp.get(leftWord)-1);
-                        --cnt;
+                        String leftStr = s.substring(left, left+window);
+
+                        tmp.put(leftStr, tmp.get(leftStr)-1);
                         left += window;
+                        --cnt;
                     }
-                    if (cnt == words.length)
+
+                    if (cnt == words.length) {
                         ans.add(left);
+                    }
                 }
             }
         }

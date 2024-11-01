0138db1f5b8abaf6456b769d2934283d92badb67
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-01 10:59:05 +0530

    Time: 8 ms (45.04%) | Memory: 42.3 MB (96.52%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,17 +1,52 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] strs = s.trim().split(" ");
+        StringBuilder sb = trim(s);
+
+        reverse(sb, 0, sb.length() - 1);
+
+        reverseEachWord(sb);
+
+        return sb.toString();
+    }
+    private StringBuilder trim(String s) {
+        int leftIdx = 0;
+        int rightIdx = s.length() - 1;
+
+        while (leftIdx <= rightIdx && s.charAt(leftIdx) == ' ') ++leftIdx;
+
+        while (leftIdx <= rightIdx && s.charAt(rightIdx) == ' ') --rightIdx;
 
         StringBuilder sb = new StringBuilder();
-        sb.append(strs[strs.length-1]);
 
-        for (int idx = strs.length-2; idx > -1; --idx) {
-            if (strs[idx].isEmpty()) {
-                continue;
-            }
-            sb.append(" ");
-            sb.append(strs[idx]);
+        while (leftIdx <= rightIdx) {
+            char curr = s.charAt(leftIdx);
+
+           if (curr != ' ') sb.append(curr);
+           else if (sb.charAt(sb.length()-1)!= ' ')
+                sb.append(curr);
+            ++leftIdx;
         }
-        return sb.toString();
+        return sb;
+    }
+    private void reverse(StringBuilder sb, int leftIdx, int rightIdx) {
+        while (leftIdx < rightIdx) {
+            char tmp = sb.charAt(leftIdx);
+            sb.setCharAt(leftIdx++, sb.charAt(rightIdx));
+            sb.setCharAt(rightIdx--, tmp);
+        }
+    }
+    private void reverseEachWord(StringBuilder sb) {
+        int N = sb.length();
+        int leftIdx = 0;
+        int rightIdx = 0;
+
+        while (rightIdx < N) {
+            while (rightIdx < N && sb.charAt(rightIdx) != ' ') 
+                ++rightIdx;
+
+            reverse(sb, leftIdx, rightIdx-1);
+            leftIdx = rightIdx + 1;
+            ++rightIdx;
+        }
     }
 }

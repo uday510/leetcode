ef52a8e171da60a37697e59b53a3260f4c886813
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-02 00:12:22 +0530

    Time: 11 ms (97.16%) | Memory: 45.3 MB (79.49%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -1,48 +1,43 @@
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-       var ans = new ArrayList<Integer>();
+        var ans = new ArrayList<Integer>();
         int N = s.length();
         int window = words[0].length();
         int total = words.length * window;
-        
-        var map = new java.util.HashMap<String, Integer>();
-        
-        for (String word : words) {
+        var map = new HashMap<String, Integer>();
+
+        for (String word: words) 
             map.put(word, map.getOrDefault(word, 0) + 1);
-        }
-        
-        for (int i = 0; i < window; i++) {
+
+        for (int i = 0; i < window; ++i) {
             int left = i;
             int right = i;
-            int count = 0;
-            var tempMap = new java.util.HashMap<String, Integer>();
-            
+            int cnt = 0;
+            var tmp = new HashMap<String, Integer>();
+
             while (right + window <= N) {
-                String word = s.substring(right, right + window);
+                String str = s.substring(right, right + window);
                 right += window;
-                
-                if (!map.containsKey(word)) {
-                    count = 0;
+
+                if (!map.containsKey(str)) {
+                    cnt = 0;
                     left = right;
-                    tempMap.clear();
+                    tmp.clear();
                 } else {
-                    tempMap.put(word, tempMap.getOrDefault(word, 0) + 1);
-                    count++;
-                    
-                    while (tempMap.getOrDefault(word, 0) > map.getOrDefault(word, 0)) {
-                        String leftWord = s.substring(left, left + window);
-                        tempMap.put(leftWord, tempMap.get(leftWord) - 1);
-                        count--;
+                    tmp.put(str, tmp.getOrDefault(str, 0) + 1);
+                    ++cnt;
+
+                    while (tmp.get(str) > map.get(str)) {
+                        String leftWord = s.substring(left, left+window);
+                        tmp.put(leftWord, tmp.get(leftWord)-1);
+                        --cnt;
                         left += window;
                     }
-                    
-                    if (count == words.length) {
+                    if (cnt == words.length)
                         ans.add(left);
-                    }
                 }
             }
         }
-        
         return ans;
     }
 }

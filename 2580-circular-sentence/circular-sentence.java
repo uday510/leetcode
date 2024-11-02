b750ff58d78620a62ee7eb3e6de6eb0149d28377
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-02 23:36:28 +0530

    Time: 1 ms (96.81%) | Memory: 42.1 MB (37.29%) - LeetSync

--- 2580-circular-sentence/circular-sentence.java
+++ 2580-circular-sentence/circular-sentence.java
@@ -3,13 +3,9 @@ class Solution {
         String[] s = sentence.split(" ");
         int N = s.length;
         
-        for (int i = 0; i < N-1; ++i) {
-            char en = s[i].charAt(s[i].length()-1);
-            char st = s[i+1].charAt(0);   
-
-            if (st != en)
+        for (int i = 0; i < N-1; ++i)
+             if (s[i+1].charAt(0) != s[i].charAt(s[i].length()-1))
                 return false;
-        }
 
         return s[0].charAt(0) == s[N-1].charAt(s[N-1].length()-1);
     }

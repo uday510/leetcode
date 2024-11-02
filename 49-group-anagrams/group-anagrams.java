f73f7316a3da30020856ef33e8f1a6010fd2e653
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-02 22:21:12 +0530

    Time: 6 ms (97.52%) | Memory: 47.7 MB (54.88%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,17 +1,17 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-       var map = new HashMap<String, List<String>>();
+        var map = new HashMap<String, List<String>>();
 
-       for (String s : strs) {
-        char[] ch = s.toCharArray();
-        Arrays.sort(ch);
-        String str = new String(ch);
+        for (String str : strs) {
+            char[] ch = str.toCharArray();
+            Arrays.sort(ch);
+            String s = new String(ch);
 
-        if (!map.containsKey(str))
-            map.put(str, new ArrayList<>());
-
-        map.get(str).add(s);
-       }
-       return new ArrayList<>(map.values());
+            if (!map.containsKey(s))
+                map.put(s, new ArrayList<>());
+            
+            map.get(s).add(str);
+        }
+        return new ArrayList(map.values());
     }
 }

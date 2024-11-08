7a5c3df33e6fec7b240079b08be6fcf6eac69838
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-08 14:53:21 +0530

    Time: 6 ms (75.85%) | Memory: 44.9 MB (23.56%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,42 +1,37 @@
-import java.util.Stack;
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    private final Map<String, Operation> operators = new HashMap<>();
+    private final Map<String, Operation> OPERATIONS = new HashMap<>();
 
     public Solution() {
-        operators.put("+", (a, b) -> a + b);
-        operators.put("-", (a, b) -> a - b);
-        operators.put("*", (a, b) -> a * b);
-        operators.put("/", (a, b) -> a / b);
+        OPERATIONS.put("+", (num1, num2) -> num1 + num2);
+        OPERATIONS.put("-", (num1, num2) -> num1 - num2);
+        OPERATIONS.put("*", (num1, num2) -> num1 * num2);
+        OPERATIONS.put("/", (num1, num2) -> num1 / num2);
     }
-
     public int evalRPN(String[] tokens) {
-        Stack<Integer> stack = new Stack<>();
+        var stack = new Stack<Integer>();
 
         for (String token : tokens) {
-            if (operators.containsKey(token)) {
-                handleOperation(stack, token);
+            if (OPERATIONS.containsKey(token)) {
+                handleOperation(OPERATIONS.get(token), stack);
             } else {
                 stack.push(Integer.parseInt(token));
             }
         }
         return stack.pop();
     }
-
-    private void handleOperation(Stack<Integer> stack, String operator) {
+    private void handleOperation(Operation operation, Stack<Integer> stack) {
         try {
             int right = stack.pop();
             int left = stack.pop();
-            int result = operators.get(operator).apply(left, right);
-            stack.push(result);
-        } catch (Exception e) {
-        } 
-    }
 
+            int result = operation.eval(left, right);
+            stack.push(result);
+        } catch(Exception e) {
+
+        }
+    }
     @FunctionalInterface
     private interface Operation {
-        int apply(int a, int b);
+        int eval(int num1, int num2);
     }
 }

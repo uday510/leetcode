0ccf9e927e06688f9d19882f4f0d412b53d304c9
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-08 18:32:07 +0530

    Time: 14 ms (86.00%) | Memory: 44.2 MB (99.62%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,32 +1,36 @@
 class Solution {
     public int calculate(String s) {
         int n = s.length();
-        int currNum = 0;
-        int lastNum = 0;
+        int currentNumber = 0;
+        int lastNumber = 0;
         int result = 0;
-        char operation = '+';
+        char lastOperator = '+';
 
-        for (int i = 0; i < n; ++i) {
-            char currChar = s.charAt(i);
+        for (int i = 0;i < n; ++i) {
+            char currentChar = s.charAt(i);
 
-            if (Character.isDigit(currChar)) {
-                currNum = (currNum * 10) + (currChar - '0');
+            if (Character.isDigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
             }
 
-            if (!Character.isDigit(currChar) && !Character.isWhitespace(currChar) || i == n - 1) {
-                if (operation == '+' || operation == '-') {
-                    result += lastNum;
-                    lastNum = (operation == '+') ? currNum : -currNum;
-                } else if (operation == '*') {
-                   lastNum = lastNum * currNum;
-                } else if (operation == '/') {
-                    lastNum = lastNum / currNum;
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == n-1) {
+
+                if (lastOperator == '+' || lastOperator == '-') {
+                    result += lastNumber;
+                    lastNumber = (lastOperator == '+') ? currentNumber : -currentNumber;
+                } else if (lastOperator == '*') {
+                    lastNumber = (lastNumber * currentNumber);
+                } else if (lastOperator == '/') {
+                    lastNumber = (lastNumber / currentNumber);
                 }
-                currNum = 0;
-                operation = currChar;
+
+                currentNumber = 0;
+                lastOperator = currentChar;
             }
         }
-        result += lastNum;
+
+        result += lastNumber;
+
         return result;
     }
 }

b9c7c2f007e5988ed77a9dfb301538aa7ab2804c
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-08 17:15:53 +0530

    Time: 14 ms (86.00%) | Memory: 45.1 MB (76.35%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,39 +1,32 @@
 class Solution {
     public int calculate(String s) {
         int n = s.length();
-        var stack = new Stack<Integer>();
         int currNum = 0;
-        int operation = '+';
+        int lastNum = 0;
+        int result = 0;
+        char operation = '+';
 
         for (int i = 0; i < n; ++i) {
-            char c = s.charAt(i);
-            if (Character.isDigit(c)) {
-                currNum = (currNum * 10) + (c - '0');
-            }
-            
-            if (!Character.isDigit(c) && c != ' ' || i == n - 1) {
+            char currChar = s.charAt(i);
 
-                if (operation == '+') {
-                    stack.push(currNum);
-                } else if (operation == '-') {
-                    stack.push(-currNum);
+            if (Character.isDigit(currChar)) {
+                currNum = (currNum * 10) + (currChar - '0');
+            }
+
+            if (!Character.isDigit(currChar) && !Character.isWhitespace(currChar) || i == n - 1) {
+                if (operation == '+' || operation == '-') {
+                    result += lastNum;
+                    lastNum = (operation == '+') ? currNum : -currNum;
                 } else if (operation == '*') {
-                    stack.push(stack.pop() * currNum);
+                   lastNum = lastNum * currNum;
                 } else if (operation == '/') {
-                    stack.push(stack.pop() / currNum);
+                    lastNum = lastNum / currNum;
                 }
-
-                operation = c;
                 currNum = 0;
+                operation = currChar;
             }
         }
-
-        int result = 0;
-
-        while (!stack.isEmpty()) {
-            result += stack.pop();
-        }
-
+        result += lastNum;
         return result;
     }
 }

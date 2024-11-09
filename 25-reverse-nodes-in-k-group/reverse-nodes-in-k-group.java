dbd55002a643270ee8dcd7648157078ab88d6f98
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-09 13:41:14 +0530

    Time: 4 ms (6.18%) | Memory: 44.6 MB (9.10%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -3,28 +3,30 @@ class Solution {
         return dfs(head, k);
     }
     private ListNode dfs(ListNode node, int k) {
-        if (node == null) return null;
-        
-        ListNode current = node;
-        for (int i = 0; i < k; i++) {
-            if (current == null) return node;
-            current = current.next;
-        }
-        
-        ListNode prev = null;
         ListNode curr = node;
+        for (int i = 0; i < k; ++i) {
+            if (curr == null) {
+                return node;
+            }
+            
+            curr = curr.next;
+        }
+        System.out.println(node.val);
+
+        ListNode prev = null;
+        curr = node;
         
         for (int i = 0; i < k; ++i) {
             ListNode next = curr.next;
-            
+
             curr.next = prev;
             prev = curr;
-            
+
             curr = next;
         }
-        
+
         node.next = dfs(curr, k);
-        
+
         return prev;
     }
 }

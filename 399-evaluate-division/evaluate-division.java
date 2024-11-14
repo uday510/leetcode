070dcbeffba06e624cf91e7f043c35cd29a8feba
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-14 21:07:18 +0530

    Time: 5 ms (7.29%) | Memory: 42.7 MB (7.45%) - LeetSync

--- 399-evaluate-division/evaluate-division.java
+++ 399-evaluate-division/evaluate-division.java
@@ -1,38 +1,39 @@
 class Solution {
     Map<String, List<String>> graph;
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-
         graph = new HashMap<>();
 
         buildGraph(equations, values);
-        
-        int n = queries.size();
-        double[] res = new double[n];
-        for (int i = 0; i < n; ++i) {
-            List<String> q = queries.get(i);
-            String u = q.get(0);
-            String v = q.get(1);
 
-            res[i] = dfs(u, v, new HashSet<>());
+        int n = queries.size();
+        var res = new double[n];
+        
+        for (int i = 0; i < n; ++i) {
+            List<String> query = queries.get(i);
+            String src = query.get(0);
+            String dest = query.get(1);
+
+            res[i] = dfs(src, dest, new HashSet<>());
         }
         return res;
     }
-    private double dfs(String src, String des, Set<String> vis) {
-        if (!graph.containsKey(src) || !graph.containsKey(des) || vis.contains(src)) {
+
+    private double dfs(String src, String dest, Set<String> vis) {
+        if (!graph.containsKey(src) || !graph.containsKey(dest) || vis.contains(src)) {
             return -1;
         }
 
-        if (src.equals(des)) {
+        if (src.equals(dest)) {
             return 1;
         }
 
         vis.add(src);
-        var neis = graph.get(src);
-        for (int i = 0; i < neis.size(); i += 2) {
-            String nei = neis.get(i);
+        List<String> neighbors = graph.get(src);
+        for (int i = 0; i < neighbors.size(); i += 2) {
+            String nei = neighbors.get(i);
+            double val = Double.parseDouble(neighbors.get(i + 1));
 
-            double val = Double.parseDouble(neis.get(i + 1));
-            double res = dfs(nei, des, vis);
+            double res = dfs(nei, dest, vis);
 
             if (res != -1) {
                 return val * res;
@@ -40,18 +41,22 @@ class Solution {
         }
         return -1;
     }
-    private void buildGraph(List<List<String>> eqs, double[] vls) {
-        for (int i = 0; i < eqs.size(); ++i) {
-            List<String> eq = eqs.get(i);
-            String u = eq.get(0);
-            String v = eq.get(1);
-            double w = vls[i];
 
-            graph.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
-            graph.get(u).add(String.valueOf(w));
+    private void buildGraph(List<List<String>> equations, double[] values) {
+        int n = equations.size();
 
-            graph.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
-            graph.get(v).add(String.valueOf(1/w));
+        for(int i = 0; i < n; ++i) {
+            List<String> equation = equations.get(i);
+            
+            String src = equation.get(0);
+            String dest = equation.get(1);
+            Double dist =  values[i];
+
+            graph.computeIfAbsent(src, k -> new ArrayList<>()).add(dest);
+            graph.get(src).add(String.valueOf(dist));
+
+            graph.computeIfAbsent(dest, k -> new ArrayList<>()).add(src);
+            graph.get(dest).add(String.valueOf(1/dist));
         }
     }
 }

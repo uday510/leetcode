33e7c96be85e1c7952792164fc67e812a4384f58
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-15 23:28:51 +0530

    Time: 7 ms (51.72%) | Memory: 45.5 MB (44.34%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,44 +1,53 @@
 class Solution {
+    Map<Integer, List<Integer>> graph;
+    Queue<Integer> queue;
+    int[] indegree;
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        var indegree = new int[numCourses];
-        var graph = new HashMap<Integer, List<Integer>>();
-        var queue = new LinkedList<Integer>();
-
-        for (int i = 0; i < numCourses; ++i) {
-            graph.put(i, new ArrayList<>());
-        }
-
-        for (int[] pre : prerequisites) {
-            graph.get(pre[1]).add(pre[0]);
-            ++indegree[pre[0]];
-        }
-        
-        for (int i = 0; i < indegree.length; ++i) {
-            if (indegree[i] == 0) {
-                queue.offer(i);
-            
-            }
-        }
-
+        graph = new HashMap<Integer, List<Integer>>();
+        indegree = new int[numCourses];
+        queue = new LinkedList<Integer>();
         int vis = 0;
 
-        while (!queue.isEmpty() && vis != numCourses) {
-            int ele = queue.poll();
+        buildGraph(prerequisites);
+
+        addNodes(numCourses);
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
             ++vis;
 
-            List<Integer> neigs = graph.get(ele);
+            addNodesToQueue(node);
+        }
 
-            System.out.println(ele + " " + neigs);
-
-            for (int nei : neigs) {
-
-                --indegree[nei];
-
-                if (indegree[nei] == 0) {
-                    queue.offer(nei);
-                }
+        return canAllCoursesFinished();
+    }
+    private void buildGraph(int[][] prerequisites) {
+         for (int[] pre : prerequisites) {
+            graph.computeIfAbsent(pre[1], k -> new ArrayList<>()).add(pre[0]);
+            ++indegree[pre[0]];
+        }
+    }
+    private void addNodes(int numCourses) {
+        for (int i = 0; i < numCourses; ++i) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
             }
         }
-        return vis == numCourses;
+    }
+    private boolean canAllCoursesFinished() {
+        for (int i = 0; i < indegree.length; ++i) {
+            if (indegree[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private void addNodesToQueue(int node) {
+        for (int nei : graph.getOrDefault(node, new ArrayList<>())) {
+            --indegree[nei];
+            if (indegree[nei] == 0) {
+                queue.offer(nei);
+            }
+        }
     }
 }

a8d2009fd396e60c3d04401df3b28b1941a88bb2
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-16 19:31:04 +0530

    Time: 265 ms (20.69%) | Memory: 97.2 MB (5.20%) - LeetSync

---
+++ 127-word-ladder/word-ladder.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Queue<String> queue = new LinkedList<>();
+        Set<String> set = new HashSet<>(wordList);
+        Set<String> vis = new HashSet<>();
+        // set.remove(beginWord);
+        queue.add(beginWord);
+        vis.add(beginWord);
+        int level = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            ++level;
+            for (int i = 0; i < size; ++i) {
+                String s = queue.poll();
+                if (s.equals(endWord)) return level;
+                List<String> neighbors = getNeighbors(s);
+                for (String nei : neighbors) {
+                    if (vis.contains(nei)) {
+                        continue;
+                    }
+                    vis.add(nei);
+                    if (set.contains(nei)) {
+                        // set.remove(nei);
+                        queue.offer(nei);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+    public List<String> getNeighbors(String s) {
+        char[] chars = s.toCharArray();
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < chars.length; ++i) {
+            char tmp = chars[i];
+            for (char c = 'a'; c <= 'z'; ++c) {
+                chars[i] = c;
+                String nei = new String(chars);
+                list.add(nei);
+            }
+            chars[i] = tmp;
+        }
+        return list;
+    }
+}

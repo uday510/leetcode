14e004a27a5e43ec169a9b3df3bca0e64983231a
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-16 20:21:12 +0530

    Time: 215 ms (37.88%) | Memory: 92.9 MB (65.35%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.java
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.java
@@ -1,16 +1,19 @@
 class WordDictionary {
-    private TrieNode root;
+    TrieNode root;
     public WordDictionary() {
         root = new TrieNode('$');
     }
     
     public void addWord(String word) {
         TrieNode currNode = root;
+
         for (char c : word.toCharArray()) {
             int index = c - 'a';
+
             if (currNode.children[index] == null) {
                 currNode.children[index] = new TrieNode(c);
             }
+
             currNode = currNode.children[index];
             currNode.freq++;
         }
@@ -20,26 +23,26 @@ class WordDictionary {
     public boolean search(String word) {
         return dfs(word, 0, root);
     }
-    private boolean dfs(String word, int index, TrieNode currNode) {
-        if (currNode == null) {
+    private boolean dfs(String word, int index, TrieNode node) {
+        if (node == null) {
             return false;
         }
-
         if (index == word.length()) {
-            return currNode.eow;
+            return node.eow;
         }
 
         char c = word.charAt(index);
+        
         if (c == '.') {
             for (int i = 0; i < 26; ++i) {
-                if (dfs(word, index + 1, currNode.children[i])) {
+                if (dfs(word, index + 1, node.children[i])) {
                     return true;
                 }
             }
             return false;
         }
-
-        return dfs(word, index + 1, currNode.children[c - 'a']);
+        
+        return dfs(word, index + 1, node.children[c - 'a']);
     }
     private class TrieNode {
         char data;
@@ -55,3 +58,5 @@ class WordDictionary {
         }
     }
 }
+
+

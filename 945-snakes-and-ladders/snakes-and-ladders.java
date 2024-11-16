0a870d0053ac2559bde7d2ef01c98104a65f83d3
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-16 18:30:54 +0530

    Time: 9 ms (33.57%) | Memory: 44.5 MB (64.45%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.java
+++ 945-snakes-and-ladders/snakes-and-ladders.java
@@ -3,57 +3,71 @@ import java.util.*;
 class Solution {
     public int snakesAndLadders(int[][] board) {
         int n = board.length;
-        Map<Integer, int[]> map = new HashMap<>();
-        makeMapping(board, map, n);
-
-        var queue = new LinkedList<Integer>();
+        Map<Integer, int[]> positionMap = createPositionMap(board, n);
+        
+        Queue<Integer> queue = new LinkedList<>();
         queue.offer(1);
         Set<Integer> visited = new HashSet<>();
         visited.add(1);
-
+        
         int moves = 0;
-
+        
         while (!queue.isEmpty()) {
-            int size = queue.size();
+            int levelSize = queue.size();
             moves++;
-            for (int i = 0; i < size; i++) {
-                int curr = queue.poll();
-
-                for (int nextMove = curr + 1; nextMove <= Math.min(curr + 6, n * n); nextMove++) {
-                    if (visited.contains(nextMove)) continue;
-                    visited.add(nextMove);
-
-                    int[] nextPos = map.get(nextMove);
-                    int row = nextPos[0];
-                    int col = nextPos[1];
-
-                    int finalMove = board[row][col] == -1 ? nextMove : board[row][col];
-
-                    if (finalMove == n * n) {
+            
+            for (int i = 0; i < levelSize; i++) {
+                int currentPosition = queue.poll();
+                
+                for (int nextMove = 1; nextMove <= 6; nextMove++) {
+                    int nextPosition = currentPosition + nextMove;
+                    
+                    if (nextPosition > n * n) {
+                        continue;
+                    }
+                    
+                    if (visited.contains(nextPosition)) {
+                        continue;
+                    }
+                    
+                    visited.add(nextPosition);
+                    int[] nextCoordinates = positionMap.get(nextPosition);
+                    int row = nextCoordinates[0];
+                    int col = nextCoordinates[1];
+                    
+                    int finalPosition = board[row][col] == -1 ? nextPosition : board[row][col];
+                    
+                    if (finalPosition == n * n) {
                         return moves;
                     }
-                    queue.offer(finalMove);
+                    
+                    queue.offer(finalPosition);
                 }
             }
         }
+        
         return -1;
     }
-
-    private void makeMapping(int[][] board, Map<Integer, int[]> map, int n) {
-        int num = 1;
-        boolean right = true;
-
+    
+    private Map<Integer, int[]> createPositionMap(int[][] board, int n) {
+        Map<Integer, int[]> positionMap = new HashMap<>();
+        int number = 1;
+        boolean moveRight = true;
+        
         for (int i = n - 1; i >= 0; i--) {
-            if (right) {
+            if (moveRight) {
                 for (int j = 0; j < n; j++) {
-                    map.put(num++, new int[]{i, j});
+                    positionMap.put(number++, new int[] {i, j});
                 }
             } else {
                 for (int j = n - 1; j >= 0; j--) {
-                    map.put(num++, new int[]{i, j});
+                    positionMap.put(number++, new int[] {i, j});
                 }
             }
-            right = !right;
+            
+            moveRight = !moveRight;
         }
+        
+        return positionMap;
     }
 }

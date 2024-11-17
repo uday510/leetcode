0c6c0f932e55384b506119fb585017fe3cc4e30d
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-17 18:28:07 +0530

    Time: 1 ms (84.12%) | Memory: 40 MB (86.89%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -1,8 +1,8 @@
 class Solution {
+    String[][] matrix;
     boolean[] cols;
     boolean[] dia45;
     boolean[] dia135;
-    String[][] matrix;
     int ans;
     public int totalNQueens(int n) {
         intialize(n);
@@ -18,33 +18,32 @@ class Solution {
         }
 
         for (int col = 0; col < n; ++col) {
+
             if (!valid(row, col, n)) {
                 continue;
             }
 
-            matrix[row][col] = "Q";
-
-            cols[col] = true;
-            dia45[row + col] = true;
-            dia135[row - col + n - 1] = true;
+            flip(row, col, n);
 
             dfs(row + 1, n);
 
-           matrix[row][col] = ".";
+            flip(row, col, n);
 
-            cols[col] = false;
-            dia45[row + col] = false;
-            dia135[row - col + n - 1] = false;
         }
+    }
+    private void flip(int row, int col, int n) {
+        cols[col] = !cols[col];
+        dia45[row + col] = !dia45[row + col];
+        dia135[row - col + n - 1] = !dia135[row - col + n - 1];
     }
     private boolean valid(int row, int col, int n) {
         return (!cols[col] && !dia45[row + col] && !dia135[row - col + n - 1]);
     }
     private void intialize(int n) {
+        matrix = new String[n][n];
         cols = new boolean[n];
         dia45 = new boolean[2 * n - 1];
         dia135 = new boolean[2 * n - 1];
-        matrix = new String[n][n];
         ans = 0;
 
         for (String[] row : matrix) {

501528eb4e51855e205ebefd6c3b0bf41a3514f0
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-17 18:29:38 +0530

    Time: 1 ms (84.12%) | Memory: 40.7 MB (42.22%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -1,53 +1,44 @@
-class Solution {
-    String[][] matrix;
-    boolean[] cols;
-    boolean[] dia45;
-    boolean[] dia135;
-    int ans;
+public class Solution {
+    private boolean[] cols;
+    private boolean[] dia45;
+    private boolean[] dia135;
+    private int ans;
+
     public int totalNQueens(int n) {
-        intialize(n);
-
+        initialize(n);
         dfs(0, n);
-
         return ans;
     }
+
     private void dfs(int row, int n) {
-        if (row >= n) {
-            ++ans;
+        if (row == n) {
+            ans++;
             return;
         }
 
-        for (int col = 0; col < n; ++col) {
+        for (int col = 0; col < n; col++) {
+            if (!isValid(row, col, n)) continue;
 
-            if (!valid(row, col, n)) {
-                continue;
-            }
-
-            flip(row, col, n);
-
+            toggle(row, col, n);
             dfs(row + 1, n);
-
-            flip(row, col, n);
-
+            toggle(row, col, n);
         }
     }
-    private void flip(int row, int col, int n) {
+
+    private void toggle(int row, int col, int n) {
         cols[col] = !cols[col];
         dia45[row + col] = !dia45[row + col];
         dia135[row - col + n - 1] = !dia135[row - col + n - 1];
     }
-    private boolean valid(int row, int col, int n) {
-        return (!cols[col] && !dia45[row + col] && !dia135[row - col + n - 1]);
+
+    private boolean isValid(int row, int col, int n) {
+        return !cols[col] && !dia45[row + col] && !dia135[row - col + n - 1];
     }
-    private void intialize(int n) {
-        matrix = new String[n][n];
+
+    private void initialize(int n) {
         cols = new boolean[n];
         dia45 = new boolean[2 * n - 1];
         dia135 = new boolean[2 * n - 1];
         ans = 0;
-
-        for (String[] row : matrix) {
-            Arrays.fill(row, ".");
-        }
     }
 }

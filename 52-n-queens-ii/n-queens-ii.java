826357d01b6d765b8f8d52ae924ed6fd702e910e
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-17 18:30:30 +0530

    Time: 1 ms (84.12%) | Memory: 39.9 MB (93.47%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -3,35 +3,36 @@ public class Solution {
     private boolean[] dia45;
     private boolean[] dia135;
     private int ans;
+    private int n;
 
     public int totalNQueens(int n) {
         initialize(n);
-        dfs(0, n);
+        dfs(0);
         return ans;
     }
 
-    private void dfs(int row, int n) {
+    private void dfs(int row) {
         if (row == n) {
             ans++;
             return;
         }
 
         for (int col = 0; col < n; col++) {
-            if (!isValid(row, col, n)) continue;
+            if (!isValid(row, col)) continue;
 
-            toggle(row, col, n);
-            dfs(row + 1, n);
-            toggle(row, col, n);
+            toggle(row, col);
+            dfs(row + 1);
+            toggle(row, col);
         }
     }
 
-    private void toggle(int row, int col, int n) {
+    private void toggle(int row, int col) {
         cols[col] = !cols[col];
         dia45[row + col] = !dia45[row + col];
         dia135[row - col + n - 1] = !dia135[row - col + n - 1];
     }
 
-    private boolean isValid(int row, int col, int n) {
+    private boolean isValid(int row, int col) {
         return !cols[col] && !dia45[row + col] && !dia135[row - col + n - 1];
     }
 
@@ -40,5 +41,6 @@ public class Solution {
         dia45 = new boolean[2 * n - 1];
         dia135 = new boolean[2 * n - 1];
         ans = 0;
+        this.n = n;
     }
 }

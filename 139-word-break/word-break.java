18e31f85b17209740631d28e3230880c10c2972c
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-24 16:28:16 +0530

    Time: 7 ms (50.95%) | Memory: 44.6 MB (30.84%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,33 +1,38 @@
 class Solution {
-    static Set<String> set;
-    static Map<Integer, Boolean> seen;
+    Set<String> dictonary;
+    Map<Integer, Boolean> seen;
     public boolean wordBreak(String s, List<String> wordDict) {
-        set = new HashSet<>();
-        seen = new HashMap<>();
-        
-        for (String str : wordDict) 
-            set.add(str);
-        
+        intialize(wordDict);
+
         return dfs(0, s);
     }
-    public boolean dfs(int index, String str) {
-        if (index == str.length()) 
+    private boolean dfs(int startIdx, String string) {
+        if (startIdx >= string.length()) {
             return true;
-        
-        if (seen.containsKey(index))
-            return seen.get(index);
-        
-        for (int j = index; j < str.length(); ++j) {
-            String substr = str.substring(index, j + 1);
-        
-            if (set.contains(substr) && dfs(j+1, str)) {
-                seen.put(index, true);
-                return true;
-            } 
         }
-        
-        seen.put(index, false);
+
+        if (seen.containsKey(startIdx)) {
+            return seen.get(startIdx);
+        }
+
+        for (int endIdx = startIdx; endIdx < string.length(); ++endIdx) {
+            String substr = string.substring(startIdx, endIdx + 1);
+
+            if (dictonary.contains(substr)) {
+                boolean segmented = dfs(endIdx + 1, string);
+                if (segmented) {
+                    return true;
+                }
+
+                seen.put(startIdx, false);
+            }
+        }
+
+        seen.put(startIdx, false);
         return false;
     }
+    private void intialize(List<String> wordDict) {
+        dictonary = new HashSet<>(wordDict);
+        seen = new HashMap<>();
+    }
 }
-

ca60b040b1e5d9dca38147083cab8a8de6180c17
Uday <BUDAYTEJA009@GMAIL.COM>
2024-11-28 00:09:55 +0530

    Time: 211 ms (35.27%) | Memory: 45.8 MB (15.95%) - LeetSync

--- 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
@@ -1,53 +1,49 @@
-class Solution {
-    Map<Integer, List<Integer>> graph;
-    int SOURCE;
-    int DESTINATION;
+import java.util.*;
+
+public class Solution {
+    private static final int SOURCE = 0;
+    private Map<Integer, List<Integer>> graph;
+
     public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
-        graph = new HashMap<>();
-        SOURCE = 0;
-        DESTINATION = n - 1;
-
-        for (int i = 0; i <= n; ++i) {
-            graph.put(i, new ArrayList<>());
-
-            graph.get(i).add(i + 1);
-        }
-
+        initializeGraph(n);
         int[] result = new int[queries.length];
-        int index = 0;
 
-        for (int[] query : queries) {
-            int src = query[0];
-            int dest = query[1];
-
-            graph.get(src).add(dest);
-
-            System.out.println(src + " : " + graph.get(src));
-
-            result[index++] = getShortestDistance();
+        for (int i = 0; i < queries.length; i++) {
+            addEdge(queries[i][0], queries[i][1]);
+            result[i] = getShortestDistance(n);
         }
 
         return result;
     }
-    private int getShortestDistance() {
 
+    private void initializeGraph(int n) {
+        graph = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            graph.put(i, new ArrayList<>());
+            if (i < n - 1) {
+                graph.get(i).add(i + 1);
+            }
+        }
+    }
+
+    private void addEdge(int source, int destination) {
+        graph.get(source).add(destination);
+    }
+
+    private int getShortestDistance(int n) {
         Queue<int[]> queue = new LinkedList<>();
-        int currentDistance = 0;
         Set<Integer> seen = new HashSet<>();
-
         queue.offer(new int[]{SOURCE, 0});
         seen.add(SOURCE);
 
         while (!queue.isEmpty()) {
-            int[] curr = queue.poll();
-            int node = curr[0];
-            int dist = curr[1];
+            int[] current = queue.poll();
+            int node = current[0];
+            int distance = current[1];
 
-            if (node == DESTINATION) {
-                return dist;
+            if (node == n - 1) {
+                return distance;
             }
-
-            currentDistance += 1;
 
             for (Integer neighbor : graph.getOrDefault(node, new ArrayList<>())) {
                 if (seen.contains(neighbor)) {
@@ -55,9 +51,8 @@ class Solution {
                 }
 
                 seen.add(neighbor);
-                queue.offer(new int[]{neighbor, dist + 1});
-
-            }        
+                queue.offer(new int[]{neighbor, distance + 1});
+            }
         }
 
         return -1;

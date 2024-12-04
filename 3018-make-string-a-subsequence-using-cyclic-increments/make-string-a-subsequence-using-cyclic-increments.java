d67025e59988e9332ef4cb9cde77a18e14a2fcea
Uday <BUDAYTEJA009@GMAIL.COM>
2024-12-05 03:18:51 +0530

    Time: 5 ms (100.00%) | Memory: 45.9 MB (10.83%) - LeetSync

---
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canMakeSubsequence(String str1, String str2) {
+        
+        int idx1, idx2, len1, len2;
+        idx1 = idx2 = 0;
+        len1 = str1.length();
+        len2 = str2.length();
+
+        while (idx1 < len1 && idx2 < len2) {
+
+            char c1 = str1.charAt(idx1);
+            char nextC1 = (char) (c1 + 1);
+            if (c1 == 'z') {
+                nextC1 = 'a';
+            }
+            char c2 = str2.charAt(idx2);
+
+            if (c1 == c2 || nextC1 == c2) {
+                ++idx1;
+                ++idx2;
+            } else {
+                ++idx1;
+            }
+        }
+
+        return idx2 == len2;
+    }
+}

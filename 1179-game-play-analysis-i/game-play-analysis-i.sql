483b66beb39273f8e9cd134c930c39bdeb0fcbcc
Uday <BUDAYTEJA009@GMAIL.COM>
2024-12-07 11:57:08 +0530

    Time: 506 ms (73.36%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,10 +1,10 @@
-SELECT DISTINCT
-  A.player_id,
-  FIRST_VALUE(A.event_date) OVER (
-    PARTITION BY
-      A.player_id
-    ORDER BY
-      A.event_date
-  ) AS first_login
+SELECT 
+    DISTINCT A.player_id,
+    FIRST_VALUE(A.event_date) OVER (
+        PARTITION BY 
+            A.player_id
+        ORDER BY 
+            A.event_date
+    ) AS first_login
 FROM
-  Activity A;
+    Activity A;

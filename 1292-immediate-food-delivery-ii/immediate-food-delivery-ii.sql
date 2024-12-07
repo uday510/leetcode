b25741476157c8cea269326ee17d309c3f1b1406
Uday <BUDAYTEJA009@GMAIL.COM>
2024-12-07 11:19:20 +0530

    Time: 1825 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,21 +1,19 @@
-SELECT 
-    ROUND(
-        (SUM(CASE 
-            WHEN order_date = customer_pref_delivery_date THEN 1 
-            ELSE 0 
-        END) / COUNT(*)) * 100, 2
-    ) AS immediate_percentage
-FROM (
-    SELECT 
-        customer_id,
-        order_date,
-        customer_pref_delivery_date
-    FROM 
-        Delivery d
-    WHERE 
-        order_date = (
-            SELECT MIN(order_date)
-            FROM Delivery
-            WHERE customer_id = d.customer_id
-        )
-) AS first_orders;
+SELECT ROUND(((SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END)) / COUNT(*)) * 100, 2) 
+        AS immediate_percentage
+        FROM (
+            SELECT 
+            customer_id,
+            order_date,
+            customer_pref_delivery_date
+        FROM 
+            Delivery d
+        WHERE 
+            order_date = (
+                SELECT 
+                    MIN(order_date)
+                FROM
+                    Delivery 
+                WHERE 
+                    customer_id = d.customer_id
+            )
+        ) AS first_orders

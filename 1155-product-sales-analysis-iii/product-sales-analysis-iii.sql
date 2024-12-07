aa112ee7f42aa64f2f0143ae9064e64e6b6d2124
Uday <BUDAYTEJA009@GMAIL.COM>
2024-12-07 14:37:24 +0530

    Time: 1091 ms (59.68%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,16 @@
+SELECT 
+    s.product_id,
+    s.year AS first_year,
+    s.quantity,
+    s.price
+FROM 
+    Sales s
+WHERE (s.product_id, s.year) IN (
+    SELECT 
+        product_id,
+        MIN(year) AS year
+    FROM 
+        Sales
+    GROUP BY 
+        product_id
+) ;

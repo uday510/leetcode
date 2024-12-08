d4495e448704e625f3b3011abaf99b871f14cf21
Uday <BUDAYTEJA009@GMAIL.COM>
2024-12-08 11:05:13 +0530

    Time: 297 ms (98.48%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,10 +1,13 @@
 SELECT 
-    employee_id
-FROM 
-    Employees
-WHERE 
-    Salary < 30000
-AND manager_id NOT IN (
-    SELECT employee_id FROM Employees
-)
-ORDER BY employee_id
+    e1.employee_id
+FROM
+    Employees e1
+LEFT JOIN 
+    Employees e2 
+    ON e1.manager_id = e2.employee_id
+WHERE
+    e1.salary < 30000 AND 
+    e2.employee_id IS NULL AND e1.manager_id IS NOT NULL
+ORDER BY 
+    e1.employee_id
+;

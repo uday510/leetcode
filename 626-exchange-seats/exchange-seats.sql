7b836d1f7886aa2cb5697619cc0394fb625eb649
Uday <BUDAYTEJA009@GMAIL.COM>
2024-12-08 11:50:50 +0530

    Time: 392 ms (41.42%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,13 @@
+SELECT 
+    (CASE
+       WHEN MOD(id, 2) != 0 AND counts != id THEN id + 1
+       WHEN MOD(id, 2) != 0 AND counts = id THEN id
+       ELSE id - 1
+    END) AS id,
+    student
+FROM 
+    seat, 
+    (SELECT COUNT(*) AS counts FROM seat) AS seats_count
+ORDER BY 
+    id
+;

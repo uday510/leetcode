ab2b1b410b6b50a88861b110b964e7d9d39bf524
Uday <BUDAYTEJA009@GMAIL.COM>
2024-12-15 12:24:17 +0530

    Time: 635 ms (20.06%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,11 +1,9 @@
-# Write your MySQL query statement below
 SELECT 
-    w1.id
-FROM
-    Weather w1
-JOIN
-    Weather w2
-ON
-    DATEDIFF(w1.recordDate, w2.recordDate) = 1
-AND 
-    w1.temperature > w2.temperature;
+    w2.id
+FROM 
+    Weather w1, Weather w2
+WHERE
+    w2.recordDate = w1.recordDate + INTERVAL 1 DAY
+    AND 
+    w2.temperature > w1.temperature
+;

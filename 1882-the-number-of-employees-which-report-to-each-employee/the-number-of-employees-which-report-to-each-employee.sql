8cdf802dd2760a593195a1e32291db2f80ca517e
Uday <BUDAYTEJA009@GMAIL.COM>
2024-12-17 14:14:44 +0530

    Time: 973 ms (15.77%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,15 +1,17 @@
 SELECT 
-    m.employee_id,
-    m.name,
-    COUNT(emp.employee_id) AS reports_count,
-    ROUND(
-        AVG(emp.age)
-    ) AS average_age
-    FROM 
-        employees emp
-        JOIN
-             employees m ON emp.reports_to = m.employee_id
-    GROUP BY 
-        m.employee_id
-    ORDER BY 
-        m.employee_id
+    e2.employee_id,
+    e2.name,
+    COUNT(e1.reports_to) AS reports_count,
+    ROUND(AVG(e1.age)) AS average_age
+FROM 
+    Employees e2
+JOIN 
+    Employees e1
+    ON 
+    e1.reports_to = e2.employee_id
+GROUP BY
+    e1.reports_to
+ORDER BY 
+    e2.employee_id
+;
+

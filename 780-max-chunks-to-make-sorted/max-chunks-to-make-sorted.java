fbfb50e8cd57b1dce4f051ba968602cba57611be
Uday <BUDAYTEJA009@GMAIL.COM>
2024-12-20 23:36:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (6.63%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
@@ -1,28 +1,17 @@
 class Solution {
-
     public int maxChunksToSorted(int[] arr) {
-        int n = arr.length;
-        int[] prefixMax = arr.clone();
-        int[] suffixMin = arr.clone();
+        int len = arr.length;
+        int result = 0;
+        int currSum = 0;
 
-        // Fill the prefixMax array
-        for (int i = 1; i < n; i++) {
-            prefixMax[i] = Math.max(prefixMax[i - 1], prefixMax[i]);
-        }
+        for (int idx = 0; idx < arr.length; ++idx) {
+            currSum += arr[idx];
 
-        // Fill the suffixMin array in reverse order
-        for (int i = n - 2; i >= 0; i--) {
-            suffixMin[i] = Math.min(suffixMin[i + 1], suffixMin[i]);
-        }
-
-        int chunks = 0;
-        for (int i = 0; i < n; i++) {
-            // A new chunk can be created
-            if (i == 0 || suffixMin[i] > prefixMax[i - 1]) {
-                chunks++;
+            if (currSum == idx * (idx + 1) / 2) {
+                result += 1;
             }
         }
 
-        return chunks;
+        return result;
     }
 }

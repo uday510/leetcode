bbc2793cbb7fbc4db3020c637d467051ec1db312
Uday <BUDAYTEJA009@GMAIL.COM>
2024-12-21 02:32:43 +0530

    Time: 119 ms (15.00%) | Memory: 72.3 MB (33.33%) - LeetSync

--- 2745-convert-object-to-json-string/convert-object-to-json-string.js
+++ 2745-convert-object-to-json-string/convert-object-to-json-string.js
@@ -1,8 +1,25 @@
-/**
- * @param {null|boolean|number|string|Array|Object} object
- * @return {string}
- */
 var jsonStringify = function(object) {
-    
-    return JSON.stringify(object);
+    if (object == null) {
+        return 'null';
+    }
+
+    if (Array.isArray(object)) {
+        const nums = object.map(num => jsonStringify(num));
+
+        return `[${nums.join(',')}]`;
+    }
+
+    if (typeof object == 'object') {
+        const keys = Object.keys(object);
+        const pairs = keys.map(key => `"${key}":${jsonStringify(object[key])}`);
+
+        return `{${pairs.join(",")}}`;
+    }
+
+    if (typeof object === 'string') {
+        return `"${object}"`
+    }
+
+    return String(object);
+
 };

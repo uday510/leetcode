e3eb2fa9a8c5481d13ad86fbf43ec088255f7bd7
Uday <BUDAYTEJA009@GMAIL.COM>
2025-01-09 00:58:32 +0530

    Time: 52 ms (25.82%) | Memory: 115 MB (46.43%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,27 +1,30 @@
 class LRUCache {
+
     Node head;
     Node tail;
     Map<Integer, Node> cache;
     int capacity;
+
     public LRUCache(int capacity) {
-        cache = new HashMap<>();
         head = new Node(-1, -1);
         tail = new Node(-1, -1);
+        cache = new HashMap<>();
         this.capacity = capacity;
+
         head.next = tail;
         tail.prev = head;
     }
     
     public int get(int key) {
+        int value = -1;
+
         if (cache.containsKey(key)) {
             Node node = cache.get(key);
             remove(node);
             insert(node);
-
-            return node.val;
-        } else {
-            return -1;
+            value = node.value;
         }
+        return value;
     }
     
     public void put(int key, int value) {
@@ -31,31 +34,38 @@ class LRUCache {
         } else if (cache.size() == capacity) {
             remove(tail.prev);
         }
+
         insert(new Node(key, value));
     }
+
     private void insert(Node node) {
         cache.put(node.key, node);
 
         Node headNext = head.next;
+        head.next = node;
         node.prev = head;
         node.next = headNext;
-        head.next = node;
         headNext.prev = node;
     }
+
     private void remove(Node node) {
         cache.remove(node.key);
+
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
+
     private class Node {
         Node prev;
-        int key;
-        int val;
         Node next;
+        int key;
+        int value;
 
-        Node (int key, int val) {
+        Node(int key, int value) {
             this.key = key;
-            this.val = val;
+            this.value = value;
         }
     }
+
 }
+

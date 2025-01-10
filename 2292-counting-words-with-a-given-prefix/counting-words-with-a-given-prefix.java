c7e1ce0eaa5ab8e9b5dc9012e6e7c5604bf12deb
Uday <BUDAYTEJA009@GMAIL.COM>
2025-01-10 18:37:11 +0530

    Time: 1 ms (30.25%) | Memory: 42.8 MB (82.58%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
@@ -1,11 +1,57 @@
 class Solution {
+    Trie trie;
     public int prefixCount(String[] words, String pref) {
+        trie = new Trie();
+        intialize(words, pref);
+
+        int result = findPrefixCount(pref);
+        return result;
+    }
+    private int findPrefixCount(String prefix) {
         int count = 0;
+        Trie curr = trie;
+        for (char ch : prefix.toCharArray()) {
+            int index = ch - 'a';
+            if (curr.nodes[index] == null) {
+                return 0;
+            }
 
-        for (String word : words) {
-            count += word.startsWith(pref) ? 1 : 0;
+            curr = curr.nodes[index];
         }
+        return curr.count;
+    }
 
-        return count;
+    private void intialize(String[] words, String pref) {
+        for (String word : words) {
+            insertIntoTrie(word, pref);
+        }
+    }
+
+    private void insertIntoTrie(String word, String pref) {
+        Trie curr = trie;
+
+        for (int idx = 0; idx < Math.min(pref.length(), word.length()); ++idx) {
+            if (word.charAt(idx) != pref.charAt(idx)) {
+                return;
+            }
+
+            int index = word.charAt(idx) - 'a';
+            if (curr.nodes[index] == null) {
+                curr.nodes[index] = new Trie();
+            }
+
+            curr = curr.nodes[index];
+            curr.count++;
+        }
+    }
+
+    private class Trie {
+        Trie[] nodes;
+        int count;
+
+        Trie() {
+            this.nodes = new Trie[26];
+            count = 0;
+        }
     }
 }

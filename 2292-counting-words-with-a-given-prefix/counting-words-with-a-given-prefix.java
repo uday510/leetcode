67f34b2d44e4dde2a18c60f9bd68a784cb8aa45f
Uday <BUDAYTEJA009@GMAIL.COM>
2025-01-10 18:48:36 +0530

    Time: 1 ms (30.25%) | Memory: 43.3 MB (18.35%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
@@ -22,25 +22,21 @@ class Solution {
     private void insertIntoTrie(String word) {
         Trie curr = trie;
         int len = Math.min(word.length(), prefix.length());
+        int idx;
 
-        for (int idx = 0; idx < len; ++idx) {
-            if (word.charAt(idx) != prefix.charAt(idx)) {
-                return;
-            }
-
+        for (idx = 0; isValid(word, idx); ++idx) {
             int index = word.charAt(idx) - 'a';
             if (curr.nodes[index] == null) {
                 curr.nodes[index] = new Trie();
             }
-
-            if (idx == prefix.length() - 1) {
-                result += 1;
-            }
         }
 
-        
+        result += idx == prefix.length() ? 1 : 0;
     }
-
+    private boolean isValid(String word, int idx) {
+        return idx < Math.min(word.length(), prefix.length()) && 
+            word.charAt(idx) == prefix.charAt(idx);
+    }
     private class Trie {
         Trie[] nodes;
         int count;

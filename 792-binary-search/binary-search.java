0d907ab405764e182385888ded1a5709deb4c76b
Uday <BUDAYTEJA009@GMAIL.COM>
2025-01-13 21:58:49 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (54.78%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,28 +1,26 @@
 class Solution {
     public int search(int[] nums, int target) {
-        // Set the left and right boundaries
-        int left = 0, right = nums.length - 1;
-        
-        // Under this condition
-        while (left <= right) {
-            // Get the middle index and the middle value.
-            int mid = left + (right - left) / 2;
-            
-            // Case 1, return the middle index.
-            if (nums[mid] == target) {
-                return mid;
-            } 
-            // Case 2, discard the smaller half.
-            else if (nums[mid] < target) {
-                left = mid + 1;   
-            } 
-            // Case 3, discard the larger half.
-            else {
-                right = mid - 1;
+        return bs(nums, target);
+    }
+
+    private int bs(int[] nums, int target) {
+        int leftIdx = 0;
+        int rightIdx = nums.length;
+
+        while (leftIdx < rightIdx) {
+            int midIdx = (leftIdx + rightIdx) >> 1;
+
+            if (nums[midIdx] == target) {
+                return midIdx;
+            }
+
+            if (nums[midIdx] < target) {
+                leftIdx = midIdx + 1;
+            } else {
+                rightIdx = midIdx;
             }
         }
-        
-        // If we finish the search without finding target, return -1.
+
         return -1;
     }
 }

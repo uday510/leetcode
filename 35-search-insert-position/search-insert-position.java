53e818dd4ae9e879c2b8c50caf5556f82358a9d9
Uday <BUDAYTEJA009@GMAIL.COM>
2025-01-13 22:04:56 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (61.10%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,11 +1,15 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        return bisectLeft(0, nums.length, target, nums);
+        
+        return bs(nums, target);
     }
-    private int bisectLeft(int leftIdx, int rightIdx, int target, int[] nums) {
+
+    private int bs(int[] nums, int target) {
+        int leftIdx = 0;
+        int rightIdx = nums.length;
 
         while (leftIdx < rightIdx) {
-            int midIdx = (leftIdx + rightIdx) >> 1;
+            int midIdx = (leftIdx + rightIdx) >>> 1;
 
             if (nums[midIdx] < target) {
                 leftIdx = midIdx + 1;

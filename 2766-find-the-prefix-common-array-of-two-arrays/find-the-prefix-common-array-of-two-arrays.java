267f4f8c4384c972e06f0142550b5d2576a434d1
Uday <BUDAYTEJA009@GMAIL.COM>
2025-01-14 11:30:49 +0530

    Time: 2 ms (95.83%) | Memory: 45.5 MB (58.59%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
@@ -1,40 +1,24 @@
 class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
-
+        int[] frequency = new int[51];
         int[] result = new int[A.length];
-
+        int common = 0;
 
         for (int idx = 0; idx < A.length; ++idx) {
-            set1.add(A[idx]);
-            set2.add(B[idx]);
+            frequency[A[idx]]++;
 
-            result[idx] = countCommon(set1, set2);
+            if (frequency[A[idx]] == 2) {
+                common++;
+            }
+
+            frequency[B[idx]]++;
+            if (frequency[B[idx]] == 2) {
+                common++;
+            }
+
+            result[idx] = common;
         }
+
         return result;
     }
-
-    private int countCommon(Set<Integer> set1, Set<Integer> set2) {
-
-        int count = 0;
-
-        for (var num : set2) {
-            if (set1.contains(num)) {
-                count++;
-            }
-        }
-
-        return count;
-    }
 }
-
-
-/**
-
-[1, 3, 2, 4]
-[3, 1, 2, 4]
-
-[0, 0, 0, 0]
-
- */

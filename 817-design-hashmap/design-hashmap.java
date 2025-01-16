0f18f6cde6298c6fb2bc62ad4d38957a1c5c85c6
Uday <BUDAYTEJA009@GMAIL.COM>
2025-01-16 10:09:08 +0530

    Time: 22 ms (51.62%) | Memory: 50.2 MB (53.53%) - LeetSync

--- 817-design-hashmap/design-hashmap.java
+++ 817-design-hashmap/design-hashmap.java
@@ -1,9 +1,17 @@
-import java.util.LinkedList;
-
 class MyHashMap {
-    private static final double LOAD_FACTOR = 0.75;
+
+    private static double LOAD_FACTOR = 0.75;
     private LinkedList<Entry>[] buckets;
     private int size;
+
+    public MyHashMap() {
+        buckets = new LinkedList[16];
+        size = 0;
+    }
+
+    private int getIndex(int key) {
+        return key % buckets.length;
+    }
 
     private static class Entry {
         int key, value;
@@ -13,67 +21,79 @@ class MyHashMap {
             this.value = value;
         }
     }
-
-    public MyHashMap() {
-        buckets = new LinkedList[16]; // Start small
-        size = 0;
-    }
-
-    private int getBucketIndex(int key) {
-        return key % buckets.length;
-    }
-
+    
     public void put(int key, int value) {
         if ((double) size / buckets.length > LOAD_FACTOR) {
             resize();
         }
 
-        int index = getBucketIndex(key);
+        int index = getIndex(key);
         if (buckets[index] == null) {
             buckets[index] = new LinkedList<>();
         }
 
-        for (Entry entry : buckets[index]) {
+        for (var entry : buckets[index]) {
             if (entry.key == key) {
-                entry.value = value; // Update if key exists
+                entry.value = value;
                 return;
             }
         }
 
         buckets[index].add(new Entry(key, value));
         size++;
-    }
 
+    }
+    
     public int get(int key) {
-        int index = getBucketIndex(key);
-        if (buckets[index] == null) return -1;
+        int index = getIndex(key);
+        int value = -1;
 
-        for (Entry entry : buckets[index]) {
-            if (entry.key == key) return entry.value;
+        if (buckets[index] == null) {
+            return value;
         }
 
-        return -1;
+        for (var entry : buckets[index]) {
+            if (entry.key == key) {
+                value = entry.value;
+                break;
+            }
+        }
+
+        return value;
     }
-
+    
     public void remove(int key) {
-        int index = getBucketIndex(key);
-        if (buckets[index] != null) {
-            buckets[index].removeIf(entry -> entry.key == key);
-            size--;
+        int index = getIndex(key);
+
+        if (buckets[index] == null) {
+            return;
         }
+
+        buckets[index].removeIf(entry -> entry.key == key);
+        size--;
+
     }
 
     private void resize() {
-        LinkedList<Entry>[] oldBuckets = buckets;
-        buckets = new LinkedList[oldBuckets.length * 2];
+        LinkedList<Entry>[] old = buckets;
+        buckets = new LinkedList[old.length * 2];
         size = 0;
 
-        for (LinkedList<Entry> bucket : oldBuckets) {
-            if (bucket != null) {
-                for (Entry entry : bucket) {
-                    put(entry.key, entry.value);
-                }
+        for (var bucket: old) {
+            if (bucket == null) 
+                continue;
+                
+            for (var entry : bucket) {
+                put(entry.key, entry.value);
             }
         }
     }
 }
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */

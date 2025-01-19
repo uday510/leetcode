39afa51053e7e13d7e37d22fe6ffa960e21aedb0
Uday <BUDAYTEJA009@GMAIL.COM>
2025-01-19 13:33:44 +0530

    Time: 1 ms (99.96%) | Memory: 46.6 MB (7.62%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,39 +1,14 @@
-enum Index {
-    GOOD,
-    BAD,
-    UNKNOWN
-}
-
 class Solution {
-
-    Index[] memo;
-
     public boolean canJump(int[] nums) {
-        memo = new Index[nums.length];
+        int len = nums.length;
+        int pos = nums.length - 1;
 
-        Arrays.fill(memo, Index.UNKNOWN);
-        memo[nums.length - 1] = Index.GOOD;
-
-        return dfs(0, nums);
-    }
-
-    private boolean dfs(int pos, int[] nums) {
-        if (memo[pos] != Index.UNKNOWN) {
-            return memo[pos] == Index.GOOD;
-        }
-
-        int furthest = Math.min(pos + nums[pos], nums.length - 1);
-
-        for (int nextPos = pos + 1; nextPos <= furthest; ++nextPos) {
-
-            if (dfs(nextPos, nums)) {
-                memo[pos] = Index.GOOD;
-                return true;
+        for (int idx = len - 1; idx > -1; --idx) {
+            if (idx + nums[idx] >= pos) {
+                pos = idx;
             }
-
         }
 
-        memo[pos] = Index.BAD;
-        return false;
+        return pos == 0;
     }
 }

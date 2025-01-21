ad858a7a45d2887f63f80c4521e0f4321fbcadbd
Uday <BUDAYTEJA009@GMAIL.COM>
2025-01-21 11:32:09 +0530

    Time: 1 ms (99.90%) | Memory: 44.6 MB (37.80%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,26 +1,46 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((o1, o2) -> o1.val - o2.val);
+        return dfs(0, lists.length - 1, lists);
+    }
 
+    private ListNode dfs(int i, int j, ListNode[] lists) {
+        if (i > j) {
+            return null;
+        }
+
+        if (i == j) {
+            return lists[i];
+        }
+
+        int midIdx = (i + j) >> 1;
+
+        ListNode left = dfs(i, midIdx, lists);
+        ListNode right = dfs(midIdx + 1, j, lists);
+
+        ListNode merged = merge(left, right);
+
+        return merged;
+    }
+
+    private ListNode merge(ListNode node1, ListNode node2) {
+        ListNode p1 = node1;
+        ListNode p2 = node2;
         ListNode dummy = new ListNode(0);
         ListNode tail = dummy;
 
-        for (ListNode list : lists) {
-            if (list != null) {
-                pq.offer(list);
+        while (p1 != null && p2 != null) {
+            if (p1.val < p2.val) {
+                tail.next = p1;
+                p1 = p1.next;
+            } else {
+                tail.next = p2;
+                p2 = p2.next;
             }
-        }
 
-        while (!pq.isEmpty()) {
-            ListNode list = pq.poll();
-
-            tail.next = list;
             tail = tail.next;
-
-            if (list.next != null) {
-                pq.offer(list.next);
-            }
         }
+
+        tail.next = (p1 == null) ? p2 : p1;
 
         return dummy.next;
     }

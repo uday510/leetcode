e468dd97ba4d2eeed9ddee7732c3f80dc17de178
Uday <BUDAYTEJA009@GMAIL.COM>
2025-02-03 22:23:10 +0530

    Time: 8 ms (47.03%) | Memory: 40.3 MB (78.85%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,17 +1,11 @@
 class Solution {
+
     public int fib(int n) {
         return dfs(n);
     }
 
-    private int dfs(int n) { // 1
-       if (n < 1) {
-            return 0;
-       }
-       
-       if (n == 1) {
-            return 1;
-       }
-       
-       return dfs(n - 1) + dfs(n - 2);
+    private int dfs(int n) {
+        
+       return n < 2 ? n : dfs(n - 1) + dfs(n - 2);
     }
 }

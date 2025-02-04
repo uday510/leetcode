ffc88232d1a1dbf2683dc3d3b472cec250ac3df5
Uday <BUDAYTEJA009@GMAIL.COM>
2025-02-05 00:48:42 +0530

    Time: 8 ms (71.62%) | Memory: 60.8 MB (18.71%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,23 +1,23 @@
-public class Solution {
-
+class Solution {
     public int maximalSquare(char[][] matrix) {
-        int rows = matrix.length, cols = matrix[0].length;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
         int[][] dp = new int[rows + 1][cols + 1];
-        int maxsqlen = 0;
-        // for convenience, we add an extra all zero column and row
-        // outside of the actual dp table, to simpify the transition
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= cols; j++) {
-                if (matrix[i - 1][j - 1] == '1') {
-                    dp[i][j] = Math.min(
-                        Math.min(dp[i][j - 1], dp[i - 1][j]),
-                        dp[i - 1][j - 1]
-                    ) +
-                    1;
-                    maxsqlen = Math.max(maxsqlen, dp[i][j]);
+        int maximum = 0;
+        
+        for (int row = 1; row <= rows; ++row) {
+            for (int col = 1; col <= cols; ++col) {
+                if (matrix[row - 1][col - 1] == '0') {
+                    continue;
                 }
+
+                dp[row][col] = Math.min(dp[row - 1][col - 1],
+                        Math.min(dp[row - 1][col], dp[row][col - 1])) + 1;
+
+                maximum = Math.max(dp[row][col], maximum);
             }
         }
-        return maxsqlen * maxsqlen;
+        
+        return maximum * maximum;
     }
 }

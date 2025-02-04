281b4d5c6b762968baf62438ec6a6c7419eae0c7
Uday <BUDAYTEJA009@GMAIL.COM>
2025-02-05 01:13:50 +0530

    Time: 17 ms (66.75%) | Memory: 44.7 MB (55.83%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,42 +1,30 @@
 class Solution {
     public String longestPalindrome(String s) {
         int[] longest = {0, 1};
-        int[] current = {0, 1};
-
+        int[] current = longest;
 
         for (int idx = 0; idx < s.length(); ++idx) {
-
-            int[] odd = lps(idx - 1, idx + 1, s);
-            int[] even = lps(idx, idx + 1, s);
+            int[] odd = getPalindromeLength(idx - 1, idx + 1, s);
+            int[] even = getPalindromeLength(idx, idx + 1, s);
 
             current = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
 
             longest = current[1] - current[0] > longest[1] - longest[0] ? current : longest;
         }
 
-        /// babad
 
         return s.substring(longest[0], longest[1]);
     }
 
-    private int[] lps(int leftIdx, int rightIdx, String s) {
-
-            while (leftIdx > -1 && rightIdx < s.length()) {
-                if (s.charAt(leftIdx) != s.charAt(rightIdx)) {
-                    return new int[] {leftIdx + 1, rightIdx};
-                }
-                leftIdx--;
-                rightIdx++;
+    private int[] getPalindromeLength(int leftIdx, int rightIdx, String s) {
+        while (leftIdx > -1 && rightIdx < s.length()) {
+            if (s.charAt(leftIdx) != s.charAt(rightIdx)) {
+                break;
             }
+            --leftIdx;
+            ++rightIdx;
+        }
 
         return new int[] {leftIdx + 1, rightIdx};
     }
 }
-
-/**
-             d a  b a c
-
- */
-
- // cbbd
- 

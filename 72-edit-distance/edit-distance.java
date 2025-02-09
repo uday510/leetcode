e990517d61c6d25b7ed643645f31b996f8e313e2
Uday <BUDAYTEJA009@GMAIL.COM>
2025-02-09 16:23:54 +0530

    Time: 2 ms (100.00%) | Memory: 44.5 MB (96.14%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,45 +1,48 @@
 class Solution {
+
     String word1;
     String word2;
-    int[][] dp;
-    public int minDistance(String word1, String word2) {
-        this.word1 = word1;
-        this.word2 = word2;
-        dp = new int[word1.length()][word2.length()];
+    int length1;
+    int length2;
+    Integer[][] memo;
 
-        for (int[] arr : dp) {
-            Arrays.fill(arr, -1);
-        }
+    public int minDistance(String word1, String word2) {
+        initialize(word1, word2);
 
         return dfs(0, 0);
     }
 
-    private int dfs(int idx1, int idx2) {
-        if (idx1 == word1.length() && idx2 == word2.length()) {
-            return 0;
+    private int dfs(int index1, int index2) {
+        if (index1 == length1) {
+            return length2 - index2;
+        } 
+
+        if (index2 == length2) {
+            return length1 - index1;
         }
 
-        if (idx1 == word1.length()) {
-            return word2.length() - idx2;
+        if (memo[index1][index2] != null) {
+            return memo[index1][index2];
         }
 
-        if (idx2 == word2.length()) {
-            return word1.length() - idx1;
+        if (word1.charAt(index1) == word2.charAt(index2)) {
+            memo[index1][index2] = dfs(index1 + 1, index2 + 1);
+        } else {
+            int insert = dfs(index1, index2 + 1) + 1;
+            int delete = dfs(index1 + 1, index2) + 1;
+            int replace = dfs(index1 + 1, index2 + 1) + 1;
+
+            memo[index1][index2] = Math.min(insert, Math.min(delete, replace));
         }
 
-        if (dp[idx1][idx2] != -1) {
-            return dp[idx1][idx2];
-        }
+        return memo[index1][index2];
+    }
 
-        if (word1.charAt(idx1) == word2.charAt(idx2)) {
-            return dfs(idx1 + 1, idx2 + 1);
-        }
-
-        int insert = dfs(idx1, idx2 + 1) + 1;
-        int delete = dfs(idx1 + 1, idx2) + 1;
-        int replace = dfs(idx1 + 1, idx2 + 1) + 1;
-
-        return dp[idx1][idx2] = Math.min(delete, Math.min(replace, insert));
+    private void initialize(String word1, String word2) {
+        this.word1 = word1;
+        this.word2 = word2;
+        length1 = word1.length();
+        length2 = word2.length();
+        memo = new Integer[length1][length2];
     }
 }
-

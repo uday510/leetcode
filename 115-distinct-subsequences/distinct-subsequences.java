ca53f89344a4b2b3989401fd1a2328516527c154
Uday <BUDAYTEJA009@GMAIL.COM>
2025-02-10 23:24:39 +0530

    Time: 18 ms (77.70%) | Memory: 51.4 MB (42.05%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    int[][] dp;
+    String s;
+    String t;
+    int len1;
+    int len2;
+
+    public int numDistinct(String s, String t) {
+        initialize(s, t);
+        return dfs(0, 0);
+    }
+
+    private int dfs(int idx1, int idx2) {
+        if (idx2 >= len2) {
+            return 1;
+        }
+
+        if (idx1 >= len1) {
+            return idx2 >= len2 ? 1 : 0;
+        }
+
+        if (dp[idx1][idx2] != -1) return dp[idx1][idx2];
+
+        if (s.charAt(idx1) == t.charAt(idx2)) {
+            dp[idx1][idx2] = dfs(idx1 + 1, idx2) + dfs(idx1 + 1, idx2 + 1);
+        } else {
+            dp[idx1][idx2] = dfs(idx1 + 1, idx2);
+        }
+
+        return dp[idx1][idx2];
+    }
+
+    private void initialize(String s, String t) {
+        this.s = s;
+        this.t = t;
+        len1 = s.length();
+        len2 = t.length();
+        
+        dp = new int[len1][len2];
+
+        for (var arr : dp) 
+            Arrays.fill(arr, -1);
+    }
+}

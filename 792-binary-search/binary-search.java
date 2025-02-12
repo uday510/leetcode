e61e5b9ed1e2a40751df6ce29857ece065637084
Uday <BUDAYTEJA009@GMAIL.COM>
2025-02-12 15:14:11 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (59.22%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,18 +1,11 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return bs(nums, target);
-    }
-
-    private int bs(int[] nums, int target) {
         int leftIdx = 0;
         int rightIdx = nums.length;
 
         while (leftIdx < rightIdx) {
-            int midIdx = (leftIdx + rightIdx) >> 1;
 
-            if (nums[midIdx] == target) {
-                return midIdx;
-            }
+            int midIdx = (leftIdx + rightIdx) >> 1;
 
             if (nums[midIdx] < target) {
                 leftIdx = midIdx + 1;
@@ -21,6 +14,6 @@ class Solution {
             }
         }
 
-        return -1;
+        return leftIdx < nums.length && nums[leftIdx] == target ? leftIdx : -1;
     }
 }

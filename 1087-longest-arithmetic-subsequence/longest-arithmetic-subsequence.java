7ee3ad4d9ca0219df041dcfd05f265306216777b
Uday <BUDAYTEJA009@GMAIL.COM>
2025-02-13 12:16:46 +0530

    Time: 579 ms (21.37%) | Memory: 76.2 MB (17.12%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.java
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.java
@@ -1,43 +1,19 @@
 class Solution {
-    Map<String, Integer> dp;
     public int longestArithSeqLength(int[] nums) {
-       int len = nums.length;
-        int maxLength = 2;
+        int len = nums.length;
         Map<Integer, Integer>[] dp = new HashMap[len];
+        int longest = 2;
 
-       for (int right = 0; right < len; ++right) {
-           dp[right] = new HashMap<>();
-           for (int left = 0; left < right; ++left) {
-              int diff = nums[right] - nums[left];
-               dp[right].put(diff,dp[left].getOrDefault(diff, 1) + 1);
+        for (int right = 0; right < len; ++right) {
+            dp[right] = new HashMap<>();
+            for (int left = 0; left < right; ++left) {
+                int diff = nums[right] - nums[left];
+                dp[right].put(diff, dp[left].getOrDefault(diff, 1) + 1);
 
-               maxLength = Math.max(maxLength, dp[right].get(diff));
-           }
-       }
-        return maxLength;
-    }
-
-    private int dfs(int prevIdx, int currIdx, int diff, int[] nums) {
-        if (currIdx >= nums.length) return 0;
-
-        String key = prevIdx + "," + currIdx + "," + diff;
-
-        if (dp.containsKey(key)) return dp.get(key);
-
-        int skip = dfs(prevIdx, currIdx + 1, diff, nums);
-
-        int pick = 0;
-        if (prevIdx == -1) {
-            for (int nextIdx = currIdx + 1; nextIdx < nums.length; ++nextIdx) {
-                pick = Math.max(pick, 1 + dfs(currIdx, nextIdx, nums[nextIdx] - nums[currIdx], nums));
+                longest = Math.max(longest, dp[right].get(diff));
             }
-        } else if (nums[currIdx] - nums[prevIdx] == diff) {
-            pick = 1 + dfs(currIdx, currIdx + 1, diff, nums);
         }
 
-       int result = Math.max(skip, pick);
-       dp.put(key, result);
-
-       return result;
+        return longest;
     }
 }

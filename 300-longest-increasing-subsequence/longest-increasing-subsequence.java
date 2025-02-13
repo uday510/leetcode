4da3d001cc0e7716c769c6c184688c95eb6cff03
Uday <BUDAYTEJA009@GMAIL.COM>
2025-02-13 14:38:12 +0530

    Time: 6 ms (87.85%) | Memory: 45 MB (24.20%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,22 +1,22 @@
 class Solution {
-    Integer[][] dp;
     public int lengthOfLIS(int[] nums) {
-        dp = new Integer[nums.length + 1][nums.length];
-        return dfs(-1, 0, nums);
-    }
+        var lis = new ArrayList<Integer>();
+        int len = nums.length;
+        lis.add(nums[0]);
 
-    private int dfs(int prevIdx, int currIdx, int[] nums) {
-        if (currIdx >= nums.length) return 0;
-
-        if (dp[prevIdx + 1][currIdx] != null) return dp[prevIdx + 1][currIdx];
-
-        int skip = dfs(prevIdx, currIdx + 1, nums);
-
-        int take = 0;
-        if (prevIdx == -1 || nums[prevIdx] < nums[currIdx]) {
-            take = 1 + dfs(currIdx, currIdx + 1, nums);
+        for (int idx = 1; idx < len; ++idx) {
+            int num = nums[idx];
+            if (num > lis.getLast()) {
+                lis.add(num);
+            } else {
+                int j = 0;
+                while (num > lis.get(j)) {
+                    j++;
+                }
+                lis.set(j, num);
+            }
         }
 
-        return dp[prevIdx + 1][currIdx] = Math.max(skip, take);
+        return lis.size();
     }
 }

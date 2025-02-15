ba4733ede5e99fbc444ba32565f76a99edb95b5e
Uday <BUDAYTEJA009@GMAIL.COM>
2025-02-15 14:32:56 +0530

    Time: 180 ms (32.67%) | Memory: 181 MB (28.00%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,71 +1,62 @@
+import java.util.*;
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        Graph graph = new Graph(edges);
+        Graph graph = new Graph(edges, false);
         DFS dfs = new DFS(graph);
-
         return dfs.pathExists(source, destination);
     }
 }
 
 class DFS {
-    private final Map<Integer, List<int[]>> graph;
+    private final Map<Integer, List<Integer>> graph;
 
     DFS(Graph g) {
         this.graph = g.getGraph();
     }
 
     public boolean pathExists(int startNode, int targetNode) {
+        if (startNode == targetNode) return true;  // Edge case
+
         Set<Integer> visited = new HashSet<>();
         Stack<Integer> stack = new Stack<>();
-
         stack.push(startNode);
 
         while (!stack.isEmpty()) {
             int node = stack.pop();
-
             if (visited.contains(node)) continue;
 
             visited.add(node);
-
             if (node == targetNode) return true;
 
-            List<int[]> neighbors = graph.getOrDefault(node, Collections.emptyList());
-            // neighbors.sort((a, b) -> Integer.compare(b[0], a[0]));
-
-            for (int[] neighbor : neighbors) {
-                if (visited.contains(neighbor[0])) {
-                    continue;
+            for (int neighbor : graph.getOrDefault(node, Collections.emptyList())) {
+                if (!visited.contains(neighbor)) {
+                    stack.push(neighbor);
                 }
-
-                stack.push(neighbor[0]);
             }
         }
-
         return false;
     }
 }
-class Graph {
-    private final Map<Integer, List<int[]>> graph;
 
-    public Graph(int[][] edges) {
+class Graph {
+    private final Map<Integer, List<Integer>> graph;
+
+    public Graph(int[][] edges, boolean isDirected) {
         graph = new HashMap<>();
-        buildGraph(edges);
+        buildGraph(edges, isDirected);
     }
 
-    private void buildGraph(int[][] edges) {
+    private void buildGraph(int[][] edges, boolean isDirected) {
         for (int[] edge : edges) {
             int u = edge[0], v = edge[1];
-            addEdge(u, v);
+            graph.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
+            if (!isDirected) {
+                graph.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
+            }
         }
     }
 
-    private void addEdge(int u, int v) {
-        graph.computeIfAbsent(u, k -> new ArrayList<>()).add(new int[]{v});
-        graph.computeIfAbsent(v, k -> new ArrayList<>()).add(new int[]{u});
-    
-    }
-
-    public Map<Integer, List<int[]>> getGraph() {
+    public Map<Integer, List<Integer>> getGraph() {
         return graph;
     }
 }

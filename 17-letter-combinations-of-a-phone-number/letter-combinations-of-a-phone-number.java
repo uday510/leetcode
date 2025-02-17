a34aae53bf3b71391af0387727abbe902dcff368
Uday <BUDAYTEJA009@GMAIL.COM>
2025-02-17 13:17:09 +0530

    Time: 1 ms (58.36%) | Memory: 42.1 MB (65.99%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,42 +1,47 @@
-class Solution {
-    Map<Character, String[]>map;
-    List<String> res;
-    public List<String> letterCombinations(String digits) {
-        intialize();
+import java.util.*;
 
-        if (digits.length() == 0) {
-            return res;
+class Solution {
+    
+    private static final Map<Character, List<Character>> DIGIT_TO_LETTERS = initializeMapping();
+
+    public List<String> letterCombinations(String digits) {
+        if (digits == null || digits.isEmpty()) {
+            return new ArrayList<>();
         }
 
-        dfs(0, digits, new StringBuilder());
-
-        return res;
+        List<String> result = new ArrayList<>();
+        backtrack(0, digits, new StringBuilder(), result);
+        return result;
     }
-    private void dfs(int index, String digits, StringBuilder sb) {
-        if (index >= digits.length()) {
-            res.add(sb.toString());
+
+    private void backtrack(int index, String digits, StringBuilder sb, List<String> result) {
+        if (index == digits.length()) {
+            result.add(sb.toString());
             return;
         }
 
-        for (String s : map.get(digits.charAt(index))) {
-            sb.append(s);
-            dfs(index + 1, digits, sb);
+        char digit = digits.charAt(index);
+        List<Character> letters = DIGIT_TO_LETTERS.get(digit);
+
+        if (letters == null) return; 
+
+        for (char letter : letters) {
+            sb.append(letter);
+            backtrack(index + 1, digits, sb, result);
             sb.deleteCharAt(sb.length() - 1);
         }
-
     }
-    private void intialize() {
-        map = new HashMap<>();
-        res = new ArrayList<>();
 
-        map.put('2', new String[]{"a", "b", "c"});
-        map.put('3', new String[]{"d", "e", "f"});
-        map.put('4', new String[]{"g", "h", "i"});
-        map.put('5', new String[]{"j", "k", "l"});
-        map.put('6', new String[]{"m", "n", "o"});
-        map.put('7', new String[]{"p", "q", "r", "s"});
-        map.put('8', new String[]{"t", "u", "v"});
-        map.put('9', new String[]{"w", "x", "y", "z"});
-
+    private static Map<Character, List<Character>> initializeMapping() {
+        Map<Character, List<Character>> map = new HashMap<>();
+        map.put('2', Arrays.asList('a', 'b', 'c'));
+        map.put('3', Arrays.asList('d', 'e', 'f'));
+        map.put('4', Arrays.asList('g', 'h', 'i'));
+        map.put('5', Arrays.asList('j', 'k', 'l'));
+        map.put('6', Arrays.asList('m', 'n', 'o'));
+        map.put('7', Arrays.asList('p', 'q', 'r', 's'));
+        map.put('8', Arrays.asList('t', 'u', 'v'));
+        map.put('9', Arrays.asList('w', 'x', 'y', 'z'));
+        return map;
     }
 }

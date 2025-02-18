7dc62d39e6c23e14af0cf6a19a8f723a4512503d
Uday <BUDAYTEJA009@GMAIL.COM>
2025-02-18 11:04:04 +0530

    Time: 3 ms (66.54%) | Memory: 44.7 MB (69.22%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -1,53 +1,42 @@
 class Solution {
+    List<Integer>[] adjList;
+    boolean[] visited;
+    int numComponets;
     public int countComponents(int n, int[][] edges) {
-        UnionFind uf = new UnionFind(n);
-        int numComponents = n;
+        initialize(n, edges);
+
+        for (int idx = 0; idx < n; ++idx) {
+            if (!visited[idx]) {
+                numComponets++;
+                dfs(idx);
+            }
+        }
+
+        return numComponets;
+    }
+
+    private void dfs(int node) {
+        visited[node] = true;
+
+        for (int neighbor : adjList[node]) {
+            if (!visited[neighbor]) {
+                dfs(neighbor);
+            }
+        }
+    }
+
+    private void initialize(int n, int[][] edges) {
+        adjList = new ArrayList[n];
+        visited = new boolean[n];
+        numComponets = 0;
+
+        for (int idx = 0; idx < n; ++idx) {
+            adjList[idx] = new ArrayList<>();
+        }
 
         for (int[] edge : edges) {
-            numComponents -= uf.union(edge[0], edge[1]);
-        }
-
-        return numComponents;
-    }
-    class UnionFind {
-        int[] root;
-        int[] rank;
-
-        UnionFind(int n) {
-            this.root = new int[n];
-            this.rank = new int[n];
-
-            for (int idx = 0; idx < n; ++idx) {
-                root[idx] = idx;
-                rank[idx] = 1;
-            }
-        }
-
-        int union(int x, int y) {
-            int rootX = find(root[x]);
-            int rootY = find(root[y]);
-
-            if (rootX == rootY) {
-                return 0;
-            }
-
-            if (rank[rootX] > rank[rootY]) {
-                root[rootY] = rootX; 
-            } else if (rank[rootY] > rank[rootX]) {
-                root[rootX] = rootY;
-            } else {
-                root[rootY] = rootX;
-                rank[rootX] += 1;
-            }
-            return 1;
-        }
-
-        int find(int x) {
-            if (x == root[x]) {
-                return x;
-            }
-
-            return root[x] = find(root[x]);
+            adjList[edge[0]].add(edge[1]);
+            adjList[edge[1]].add(edge[0]);
         }
     }
 }

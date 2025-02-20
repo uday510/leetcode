983802f17166f0142ecfeef13e8ebcdff8553fa8
Uday <BUDAYTEJA009@GMAIL.COM>
2025-02-20 23:43:33 +0530

    Time: 0 ms (100.00%) | Memory: 58.1 MB (7.63%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,23 +1,36 @@
 class Solution {
+    int[] nums;
+    int len;
     public void rotate(int[] nums, int k) {
-        k %= nums.length;
+        this.nums = nums;
+        len = nums.length;
+        k = k % len;
 
-        reverse(0, nums.length - 1, nums);
-        reverse(0, k - 1, nums);
-        reverse(k, nums.length - 1, nums);
+        reverse(0, len - 1);
+        reverse(0, k - 1);
+        reverse(k, len - 1);
     }
 
-    private void reverse(int leftIdx, int rightIdx, int[] nums) {
-
+    private void reverse(int leftIdx, int rightIdx) {
         while (leftIdx < rightIdx) {
-            swap(leftIdx++, rightIdx--, nums);
+            swap(leftIdx, rightIdx);
+            leftIdx++;
+            rightIdx--;
         }
     }
 
-    private void swap(int idx1, int idx2, int[] nums) {
-        int temp = nums[idx2];
-        nums[idx2] = nums[idx1];
-        nums[idx1] = temp;
+    private void swap(int leftIdx, int rightIdx) {
+        int temp = nums[leftIdx];
+        nums[leftIdx] = nums[rightIdx];
+        nums[rightIdx] = temp;
     }
-    
 }
+
+
+
+/**
+
+1,2,3,4,5,6,7]
+
+
+ */

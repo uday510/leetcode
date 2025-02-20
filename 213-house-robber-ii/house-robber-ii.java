f3c46c22784c84889bd62b3376da7353170b5d95
Uday <BUDAYTEJA009@GMAIL.COM>
2025-02-20 21:05:17 +0530

    Time: 3 ms (2.58%) | Memory: 41.7 MB (6.12%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    int[] nums;
+    int[][] dp;
+    int len;
+    
+    public int rob(int[] nums) {
+        initialize(nums);
+        int start0 = dfs(1, 0);
+        int start1 = dfs(0, 1);
+
+        System.out.println(Arrays.toString(dp));
+        return Math.max(Math.max(start0, nums[nums.length - 1]), start1);
+    }
+
+    private void initialize(int[] nums) {
+        this.nums = nums;
+        this.len = nums.length;
+        this.dp = new int[len][2];
+        for (var arr : dp) {
+            Arrays.fill(arr, -1);
+        }
+    }
+
+    private int dfs(int start, int idx) {
+        if (idx >= nums.length) {
+            return 0;
+        }
+
+        if (idx == nums.length - 1) {
+            if (start == 1) {
+                return 0;
+            }
+        }
+
+        if (dp[idx][start] != -1) {
+            return dp[idx][start];
+        }
+
+        int pick = nums[idx] + dfs(start, idx + 2);
+        int dont = dfs(start, idx + 1);
+
+        return dp[idx][start] = Math.max(pick, dont);
+    }
+}

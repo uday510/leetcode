179d3f816044b2c863e82e68c70da21978a8618a
Uday <BUDAYTEJA009@GMAIL.COM>
2025-03-15 14:33:31 +0530

    Time: 2 ms (78.99%) | Memory: 41.6 MB (85.28%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -1,38 +1,39 @@
 class Solution {
     public String reorganizeString(String s) {
-         int[] arr = new int[128];
-        
-        for (char c : s.toCharArray()) {
-            arr[c]++;
-        }
-        
         PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o2[1] - o1[1]);
-        
+        int[] arr = new int[128];
+        for (char ch : s.toCharArray()) {
+            arr[ch]++;
+        }
+
         for (int i = 97; i < 128; ++i) {
             if (arr[i] == 0) continue;
-            pq.offer(new int[] {i, arr[i]});
-            System.out.println((char) i + " " + arr[i]);
+            if (arr[i] > (s.length() + 1) / 2) return "";
+            pq.offer(new int[]{i, arr[i]});
         }
-        
+
         StringBuilder sb = new StringBuilder();
         while (!pq.isEmpty()) {
-            int[] first =  pq.poll();
+            int[] first = pq.poll();
+
             if (sb.isEmpty() || sb.charAt(sb.length() - 1) != first[0]) {
-                sb.append((char) first[0]);
+                sb.append((char)first[0]);
+
                 if (--first[1] > 0) {
-                   pq.offer(first);
+                    pq.offer(first);
                 }
             } else {
                 if (pq.isEmpty()) return "";
-                int[] second =  pq.poll();
-                sb.append((char) second[0]);
+                int[] second = pq.poll();
+                sb.append((char)second[0]);
+
                 if (--second[1] > 0) {
                     pq.offer(second);
                 }
                 pq.offer(first);
             }
         }
-        
+
         return sb.toString();
     }
 }

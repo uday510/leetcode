ae05ac2bcba9c7f68b79891567fe457a498b0a0b
Uday <BUDAYTEJA009@GMAIL.COM>
2025-03-15 21:07:51 +0530

    Time: 1 ms (99.90%) | Memory: 45 MB (9.32%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -2,46 +2,32 @@ class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         return dfs(0, lists.length - 1, lists);
     }
-
-    private ListNode dfs(int i, int j, ListNode[] lists) {
-        if (i > j) {
-            return null;
-        }
-
-        if (i == j) {
-            return lists[i];
-        }
-
-        int midIdx = (i + j) >> 1;
-
-        ListNode left = dfs(i, midIdx, lists);
-        ListNode right = dfs(midIdx + 1, j, lists);
-
+    private ListNode dfs(int st, int en, ListNode[] lists) {
+        if (st > en) return null;
+        if (st == en) return lists[st];
+        int mid = (st + en) >> 1;
+        ListNode left = dfs(st, mid, lists);
+        ListNode right = dfs(mid + 1, en, lists);
         ListNode merged = merge(left, right);
-
         return merged;
     }
-
-    private ListNode merge(ListNode node1, ListNode node2) {
-        ListNode p1 = node1;
-        ListNode p2 = node2;
-        ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
-
-        while (p1 != null && p2 != null) {
-            if (p1.val < p2.val) {
-                tail.next = p1;
-                p1 = p1.next;
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode head = new ListNode(-1);
+        ListNode tail = head;
+        
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                tail.next = left;
+                left = left.next;
             } else {
-                tail.next = p2;
-                p2 = p2.next;
+                tail.next = right;
+                right = right.next;
             }
-
             tail = tail.next;
         }
 
-        tail.next = (p1 == null) ? p2 : p1;
+        tail.next = (left == null) ? right : left;
 
-        return dummy.next;
+        return head.next;
     }
 }

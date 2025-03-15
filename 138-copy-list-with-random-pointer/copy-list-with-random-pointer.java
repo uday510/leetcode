d7e1ca8de61486f65632dbe496ab26a9bd02b574
Uday <BUDAYTEJA009@GMAIL.COM>
2025-03-15 21:41:14 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (29.63%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -1,15 +1,14 @@
+
 class Solution {
     public Node copyRandomList(Node head) {
-        if (head == null) return head;
-        
+        if (head == null) return null;
+
         Node currNode = head;
 
         while (currNode != null) {
             Node newNode = new Node(currNode.val);
-
             newNode.next = currNode.next;
             currNode.next = newNode;
-
             currNode = newNode.next;
         }
 
@@ -17,23 +16,24 @@ class Solution {
         while (currNode != null) {
             Node oldRandom = currNode.random;
             Node newNode = currNode.next;
-
             if (oldRandom != null)
                 newNode.random = oldRandom.next;
-
+            
             currNode = newNode.next;
         }
 
         Node newHead = head.next;
         Node newNode = newHead;
         Node oldNode = head;
+
         while (newNode != null && newNode.next != null) {
             oldNode.next = oldNode.next.next;
             newNode.next = newNode.next.next;
-
+            
             oldNode = oldNode.next;
             newNode = newNode.next;
         }
+
         oldNode.next = null;
 
         return newHead;

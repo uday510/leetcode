fb8dae809f656713dd811c8200348796e929a652
Uday <BUDAYTEJA009@GMAIL.COM>
2025-03-15 12:49:16 +0530

    Time: 34 ms (74.35%) | Memory: 49.5 MB (48.53%) - LeetSync

--- 403-frog-jump/frog-jump.java
+++ 403-frog-jump/frog-jump.java
@@ -8,23 +8,21 @@ class Solution {
 
         return dfs(0, 0, stones[stones.length - 1]);
     }
+    private boolean dfs(int curr, int k, int last) {
+        if (curr == last) return true;
 
-    private boolean dfs(int currPos, int k, int lastStone) {
-        if (currPos == lastStone) return true;
-
-        String key = currPos + "_" + k;
+        String key = curr + "_" + k;
         if (dp.containsKey(key)) return dp.get(key);
 
         for (int nextK : new int[]{k - 1, k, k + 1}) {
-            int nextJump = currPos + nextK;
-            if (!stones.contains(nextJump) || nextK <= 0) continue;
+            int jump = curr + nextK;
+            if (nextK <= 0 || !stones.contains(jump)) continue;
 
-            if (dfs(nextJump, nextK, lastStone)) {
+            if (dfs(jump, nextK, last)) {
                 dp.put(key, true);
                 return true;
             }
         }
-
 
         dp.put(key, false);
         return false;

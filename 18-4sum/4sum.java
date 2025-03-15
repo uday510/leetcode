f6dbb2a8fcfca864dd00f514e9a05acddfce71a2
Uday <BUDAYTEJA009@GMAIL.COM>
2025-03-15 17:40:07 +0530

    Time: 15 ms (88.65%) | Memory: 45.3 MB (17.94%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,29 +1,45 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
+        return dfs(nums, (long) target, 4, 0);
+    }
 
-        for (int i = 0; i < nums.length - 3; ++i) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            for (int j = i + 1; j < nums.length - 2; ++j) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-                int k = j + 1, l = nums.length - 1;
+    private List<List<Integer>> dfs(int[] nums, long target, int k, int i) {
+        List<List<Integer>> res = new ArrayList<>();
 
-                while (k < l) {
-                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
-                    if (sum < target) k++;
-                    else if (sum > target) l--;
-                    else {
-                        list.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
-                        k++; l--;
+        if (k == 2) {
+            int left = i, right = nums.length - 1;
+            while (left < right) {
+                long sum = (long) nums[left] + (long) nums[right];
 
-                    while (k < l && nums[k] == nums[k - 1]) k++;
-                    while (k < l && nums[l] == nums[l + 1]) l--;
-                    }
+                if (sum < target) left++;
+                else if (sum > target) right--;
+                else {
+                    res.add(Arrays.asList(nums[left], nums[right]));
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+
+                    while (left < right && nums[right] == nums[right + 1]) right--;
                 }
+            }
+            return res;
+        }
+
+        for (int idx = i; idx < nums.length - (k - 1); ++idx) {
+            if (idx > i && nums[idx] == nums[idx - 1]) continue;
+
+            List<List<Integer>> subResults = dfs(nums, target - nums[idx], k - 1, idx + 1);
+            for (List<Integer> sub : subResults) {
+                List<Integer> temp = new ArrayList<>(sub);
+                temp.add(0, nums[idx]);
+                res.add(temp);
             }
         }
 
-        return list;
+        return res;
     }
 }

b43e1fe3313647e3fd30c4f3799dfb0c3559fd42
Uday <BUDAYTEJA009@GMAIL.COM>
2025-03-23 14:10:53 +0530

    Time: 13 ms (69.17%) | Memory: 42.4 MB (88.37%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,36 @@
+class Solution {
+    Map<TreeNode, TreeNode> parentNodes;
+    List<Integer> kDistanceNodes;
+    Set<TreeNode> visited;
+    TreeNode target;
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        parentNodes = new HashMap<>();
+        kDistanceNodes = new ArrayList<>();
+        visited = new HashSet<>();
+        assignParentNodes(root, null);
+
+        dfs(target, k);
+        return kDistanceNodes;
+    }
+
+    private void dfs(TreeNode node, int currDistance) {
+        if (node == null || !visited.add(node)) return;
+
+        if (currDistance == 0) {
+            kDistanceNodes.add(node.val);
+            return;
+        }
+
+        dfs(parentNodes.get(node), currDistance - 1);
+        dfs(node.left, currDistance - 1);
+        dfs(node.right, currDistance - 1);
+    }
+
+    private void assignParentNodes(TreeNode node, TreeNode parent) {
+        if (node == null) return;
+
+        parentNodes.put(node, parent);
+        assignParentNodes(node.left, node);
+        assignParentNodes(node.right, node);
+    }
+}

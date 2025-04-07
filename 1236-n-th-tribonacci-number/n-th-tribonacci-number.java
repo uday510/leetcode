cc616c083e0aef5fcefe339e2152ca2f3a6d6fc1
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-07 16:37:18 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (9.91%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int tribonacci(int n) {
+        if (n == 0 || n == 1) return n;
+        int s1 = 0, s2 = 1, s3 = 1;
+
+        for (int idx = 3; idx <= n; ++idx) {
+            int s4 = s1 + s2 + s3;
+            s1 = s2;
+            s2 = s3;
+            s3 = s4;
+        }
+
+        return s3;
+    }
+}

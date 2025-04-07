c63dbb3304d75d24c8efe9b21eb073f566c39369
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-07 19:53:58 +0530

    Time: 2 ms (49.58%) | Memory: 48 MB (36.97%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,36 +1,28 @@
 class Solution {
-    boolean[] visited;
-    int[][] graph;
+    int[][] edges;
+    Set<Integer> vis;
     int n;
     public int findCircleNum(int[][] isConnected) {
-       this.n = isConnected.length;
-       this.graph = isConnected;
-       this.visited = new boolean[n];
-       int numProvinces = 0;
+        edges = isConnected;
+        vis = new HashSet<>();
+        n = edges.length;
+        int numProvinces = 0;
 
-       for (int idx = 0; idx < n; ++idx) {
-            if (!visited[idx]) {
-                numProvinces += 1;
-                bfs(idx);
+        for (int i = 0; i < n; ++i) {
+            if (!vis.contains(i)) {
+                numProvinces++;
+                dfs(i, edges[i]);
             }
-       }
-    
-    return numProvinces;
+        }
+
+        return numProvinces;
     }
-    
-    private void bfs(int node) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(node);
-        visited[node] = true;
-
-        while (!queue.isEmpty()) {
-            node = queue.poll();
-
-            for (int idx = 0; idx < n; ++idx) {
-                if (graph[node][idx] == 1 && !visited[idx]) {
-                    queue.offer(idx);
-                    visited[idx] = true;
-                }
+    private void dfs(int node, int[] nei) {
+        
+        for (int i = 0; i < n; ++i) {
+            if (nei[i] == 1 && !vis.contains(i)) {
+                vis.add(i);
+                dfs(i, edges[i]);
             }
         }
     }

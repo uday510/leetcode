0232cb9ae854e89a8a56720e19f76a1e194f9208
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-10 20:07:53 +0530

    Time: 218 ms (27.34%) | Memory: 189.2 MB (18.80%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,38 +1,40 @@
 class Solution {
-    List<int[]>[] adjList;
-    Set<Integer> visited;
+
+    int n;
+    List<Integer>[] adjList;
+    Set<Integer> vis;
+    int dest;
+
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        adjList = new ArrayList[n];
-        visited = new HashSet<>();
-        for (int idx = 0; idx < n; ++idx) {
-            adjList[idx] = new ArrayList<>();
-        }
+        initialize(n, edges, destination);
 
-        for (int[] edge : edges) {
-            int src = edge[0];
-            int dest = edge[1];
-            adjList[src].add(new int[]{dest});
-            adjList[dest].add(new int[]{src});
-        }
-
-        return dfs(source, destination);
+        return dfs(source);
     }
+    private boolean dfs(int node) {
+        if (node == dest) return true;
+        if (!vis.add(node)) return false;
 
-    private boolean dfs(int node, int destination) {
-        if (!visited.add(node)) {
-            return false;
-        }
-
-        if (node == destination) {
-            return true;
-        }
-
-        for (int[] neighbor : adjList[node]) {
-            if (dfs(neighbor[0], destination)) {
-                return true;
-            }
+        for (Integer vertex : adjList[node]) {
+            if (dfs(vertex)) return true;
         }
 
         return false;
+     }
+
+    private void initialize(int n, int[][] edges, int destination) {
+        this.n = n;
+        adjList = new ArrayList[n];
+        vis = new HashSet<>();
+        dest = destination;
+
+        for (int i = 0; i < n; ++i) adjList[i] = new ArrayList<>();
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            adjList[u].add(v);
+            adjList[v].add(u);
+        }
     }
 }

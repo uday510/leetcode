d45e9c78117ef2af4f6c50c0e922536d0c16ad0e
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-13 12:00:14 +0530

    Time: 12 ms (64.72%) | Memory: 55.5 MB (58.03%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,48 +1,36 @@
 class Solution {
     List<Integer>[] adjList;
-    Set<Integer> seen;
+    int[] state;
     int n;
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        intialize(graph);
-        List<Integer> list = new ArrayList<>();
-
-        for (int node = 0; node < n; ++node) {
-            if (dfs(node, new HashSet<Integer>())) {
-                list.add(node);
-            }
-        }
-
-        Collections.sort(list);
-        return list;
-    }
-
-    private boolean dfs(int node, Set<Integer> vis) {
-        if (seen.contains(node)) return true;
-        if (!vis.add(node)) return false;
-
-        for (int neighbor : adjList[node]) {
-            if (!dfs(neighbor, vis)) {
-                return false;
-            }
-        }
-
-        seen.add(node);
-        return true;
-    }
-
-    private void intialize(int[][] graph) {
         n = graph.length;
         adjList = new ArrayList[n];
-        seen = new HashSet<>();
+        state = new int[n];
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; ++i) adjList[i] = new ArrayList<>();
 
         for (int i = 0; i < n; ++i) {
-            adjList[i] = new ArrayList<>();
+            for (int nei : graph[i]) {
+                adjList[i].add(nei);
+            }
         }
 
         for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < graph[i].length; ++j) {
-                adjList[i].add(graph[i][j]);
-            }
-       }
+            if (dfs(i)) list.add(i);
+        }
+
+        return list;
+    }
+    private boolean dfs(int node) {
+        if (state[node] != 0) return state[node] == 2;
+
+        state[node] = 1;
+        for (int nei : adjList[node]) {
+            if (!dfs(nei)) return false;
+        }
+
+        state[node] = 2;
+        return true;
     }
 }

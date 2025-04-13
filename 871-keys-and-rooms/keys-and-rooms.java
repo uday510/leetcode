b0a7e499920d915cdeaee31d2ff7ef84141466a2
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-13 12:46:47 +0530

    Time: 1 ms (74.41%) | Memory: 44.3 MB (61.70%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.java
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -1,36 +1,20 @@
 class Solution {
-    int n;
-    List<Integer>[] adjList;
+    List<List<Integer>> adjList;
     Set<Integer> vis;
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        initialize(rooms);
+        adjList = rooms;
+        vis = new HashSet<>();
 
         dfs(0);
 
-        return vis.size() == n;
+        return vis.size() == rooms.size();
     }
 
     private void dfs(int node) {
         if (!vis.add(node)) return;
 
-        for (int nei : adjList[node]) {
+        for (int nei : adjList.get(node)) {
             dfs(nei);
-        }
-    }
-
-    private void initialize(List<List<Integer>> rooms) {
-        n = rooms.size();
-        adjList = new ArrayList[n];
-        vis = new HashSet<>();
-
-        for (int i = 0; i < n; ++i) {
-            adjList[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < n; ++i) {
-            for (int neighbor : rooms.get(i)) {
-                adjList[i].add(neighbor);
-            }
         }
     }
 }

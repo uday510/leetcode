9194fb1a7785a27b1bab8cb1a71288884f30c19c
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-14 17:18:11 +0530

    Time: 14 ms (80.17%) | Memory: 46.1 MB (23.89%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
@@ -1,49 +1,29 @@
 class Solution {
     public boolean canChange(String start, String target) {
-        return bfs(start, target);
-    }
-     private boolean bfs(String start, String target) {
-        Queue<Pair<Character, Integer>> startQueue = new LinkedList<>();
-        Queue<Pair<Character, Integer>> targetQueue = new LinkedList<>();
+        int startCurrIdx = 0, targetCurrIdx = 0;
+        int startLen = start.length();
+        int targetLen = target.length();
 
-        for (int idx = 0; idx < start.length(); ++idx) {
-            char ch = start.charAt(idx);
-            if (ch != '_') {
-                startQueue.offer(new Pair<>(ch, idx));
+        while (startCurrIdx < startLen || targetCurrIdx < targetLen) {
+
+            while (startCurrIdx < startLen && start.charAt(startCurrIdx) == '_') startCurrIdx++;
+
+            while (targetCurrIdx < targetLen && target.charAt(targetCurrIdx) == '_') targetCurrIdx++;
+
+            if (startCurrIdx == startLen || targetCurrIdx == targetLen) {
+                return startCurrIdx == startLen && targetCurrIdx == targetLen;
             }
-            ch = target.charAt(idx);
-            if (ch != '_') {
-                targetQueue.offer(new Pair<>(ch, idx));
-            }
-        }
 
-        if (startQueue.size() != targetQueue.size()) {
-            return false;
-        }
+            if (
+                start.charAt(startCurrIdx) != target.charAt(targetCurrIdx) ||
+                start.charAt(startCurrIdx) == 'L' && startCurrIdx < targetCurrIdx ||
+                start.charAt(startCurrIdx) == 'R' && startCurrIdx > targetCurrIdx
+                )  return false;
 
-        while (!startQueue.isEmpty() && !targetQueue.isEmpty()) {
-            var st = startQueue.poll();
-            var tg = targetQueue.poll();
-
-            assert tg != null;
-            if (st.ch != tg.ch || 
-                (st.ch == 'L' && st.idx < tg.idx) || 
-                (st.ch == 'R' && st.idx > tg.idx)) {
-                    
-                return false;
-            }
+            startCurrIdx++;
+            targetCurrIdx++;
         }
 
         return true;
-    }
-    
-    private class Pair<T1, T2> {
-        T1 ch;
-        T2 idx;
-
-        Pair(T1 ch, T2 idx) {
-            this.ch = ch;
-            this.idx = idx;
-        }
     }
 }

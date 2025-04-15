875ef31421dac670a21462c02fc91a109c4ab4e2
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-15 21:17:12 +0530

    Time: 3 ms (28.14%) | Memory: 46.3 MB (64.50%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -1,42 +1,36 @@
 class Solution {
 
-    int n;
-    List<Integer>[] adjList;
-    List<List<Integer>> paths;
+    private List<Integer>[] adjList;
+    private int destination;
+    private List<List<Integer>> paths;
 
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        intialize(graph);
-        dfs(0, new ArrayList<>());
-        return paths;
-    }
-    
-    private void dfs(Integer node, List<Integer> path) {
-        path.add(node);
-        if (node == n - 1) {
-            paths.add(new ArrayList<>(path));
-            return;
-        }
-
-        for (Integer v : adjList[node]) {
-            dfs(v, path);
-            path.removeLast();
-        }
-    }
-    
-    private void intialize(int[][] graph) {
-        this.n = graph.length;
-        adjList = new ArrayList[n];
+        adjList = new ArrayList[graph.length];
+        destination = graph.length - 1;
         paths = new ArrayList<>();
 
-        for (int i = 0; i < n; ++i) {
-            adjList[i] = new ArrayList<>();
-        }
+        for (int i = 0; i < graph.length; ++i) adjList[i] = new ArrayList<>();
 
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < graph.length; ++i) {
             for (int v : graph[i]) {
                 adjList[i].add(v);
             }
         }
 
+        dfs(0, new ArrayList<>(Arrays.asList(0)));
+
+        return paths;
+    }
+    private void dfs(int node, List<Integer> path) {
+        if (node == destination) {
+            paths.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int nei : adjList[node]) {
+            path.add(nei);
+            dfs(nei, path);
+            path.removeLast();
+        }
     }
 }

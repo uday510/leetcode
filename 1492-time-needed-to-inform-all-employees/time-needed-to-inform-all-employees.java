a38975c85b0630c084328599eb98d89681a18457
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-18 13:26:53 +0530

    Time: 50 ms (83.42%) | Memory: 60.1 MB (87.88%) - LeetSync

--- 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
@@ -1,13 +1,15 @@
 class Solution {
+    int[] time;
     List<Integer>[] adjList;
-    int[] informTime;
+    boolean[] visited;
+    int n;
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        this.n = n;
+        this.time = informTime;
         adjList = new ArrayList[n];
-        this.informTime = informTime;
+        visited = new boolean[n];
 
-        for (int i = 0; i < n; ++i) {
-            adjList[i] = new ArrayList<>();
-        }
+        for (int i = 0; i < n; ++i) adjList[i] = new ArrayList<>();
 
         for (int i = 0; i < n; ++i) {
             if (manager[i] != -1) {
@@ -18,12 +20,15 @@ class Solution {
         return dfs(headID);
     }
     private int dfs(int node) {
-        int maxTime = 0;
+        int curr = 0;
 
-        for (int nei : adjList[node]) {
-            maxTime = Math.max(maxTime, dfs(nei));
-        }
+        for (Integer next : adjList[node]) {
+            if (!visited[next]) {
+                visited[next] = true;
+                curr = Math.max(curr, dfs(next));
+            }
+        } 
 
-        return maxTime + informTime[node];
+        return curr + time[node];
     }
 }

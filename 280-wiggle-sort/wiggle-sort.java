eae162cbbf401defc509ff14a62ee3ce83b690a8
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-18 11:42:52 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (60.79%) - LeetSync

--- 280-wiggle-sort/wiggle-sort.java
+++ 280-wiggle-sort/wiggle-sort.java
@@ -3,15 +3,9 @@ class Solution {
     public void wiggleSort(int[] nums) {
         this.nums = nums;
 
-        for (int i = 0; i < nums.length - 1; i += 1) {
-            if (i % 2 == 0) {
-                if (nums[i] > nums[i + 1]) {
+        for (int i = 0; i < nums.length - 1; ++i) {
+            if (i % 2 == 0 && nums[i] > nums[i+1] || i % 2 == 1 && nums[i] < nums[i + 1]) {
                     swap(i, i + 1);
-                }
-            } else {
-                if (nums[i] < nums[i + 1]) {
-                    swap(i, i + 1);
-                }
             }
         }
     }

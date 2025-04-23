aca3b6419000b32ab3ae44deb8d7524021e50efe
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-23 22:55:08 +0530

    Time: 3 ms (41.79%) | Memory: 41.8 MB (66.36%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,24 +1,28 @@
 class Solution {
-    Map<Character, Character> mappings;
     public boolean isValid(String s) {
         Deque<Character> stack = new ArrayDeque<>();
-        mappings = new HashMap<>();
-        mappings.put(')', '(');
-        mappings.put(']', '[');
-        mappings.put('}', '{');
+        Map<Character, Character> map = new HashMap<>();
+        Set<Character> open = new HashSet<>();
+
+        open.add('(');
+        open.add('{');
+        open.add('[');
+
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
 
         for (char ch : s.toCharArray()) {
-            if (isOpen(ch)) {
+            if (open.contains(ch)) {
                 stack.push(ch);
             } else {
-                if (stack.isEmpty() || stack.peek() != mappings.get(ch)) 
-                    return false;
+                if (stack.isEmpty() ||
+                     stack.peek() != map.get(ch)) return false;
+
                 stack.pop();
             }
         }
+
         return stack.isEmpty();
-    }
-    private boolean isOpen(char ch) {
-        return !mappings.containsKey(ch);
     }
 }

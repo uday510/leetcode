3d249ada3af77e50016be3e51b22fcfdfbae548b
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-23 21:08:09 +0530

    Time: 1 ms (100.00%) | Memory: 45.9 MB (84.68%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,34 +1,40 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        List<Integer> list = new ArrayList<>();
-        int i = 0, j = 0, n = nums1.length, m = nums2.length;
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
 
-        while (i < n && j < m) {
-            
-            if (nums1[i] < nums2[j]) {
-                list.add(nums1[i++]);
-            } else {
-                list.add(nums2[j++]);
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+
+        int total = n1 + n2;
+        int target = (total + 1) / 2;
+
+        int l = 0;
+        int r = n1;
+
+        while (true) {
+            int take1 = (l + r) >> 1;
+            int take2 = target - take1;
+
+            int max1 = (take1 == 0) ? Integer.MIN_VALUE : nums1[take1 - 1];
+            int min1 = (take1 == n1) ? Integer.MAX_VALUE : nums1[take1];
+            int max2 = (take2 == 0) ? Integer.MIN_VALUE : nums2[take2 - 1];
+            int min2 = (take2 == n2) ? Integer.MAX_VALUE : nums2[take2];
+
+            if (max1 > min2) {
+                r = take1 - 1;
+                continue;
             }
+
+            if (max2 > min1) {
+                l = take1 + 1;
+                continue;
+            }
+
+            int medianValOdd = Math.max(max1, max2);
+            if (total % 2 == 1) return medianValOdd;
+            return (medianValOdd + Math.min(min1, min2)) / 2.0;
         }
-
-        while (i < n) {
-        list.add(nums1[i++]);
-        }
-
-        while (j < m) {
-            list.add(nums2[j++]);
-        }
-
-        int total = list.size();
-
-        if (total % 2 == 0) {
-            int t1 = list.get(total / 2);
-            int t2 = list.get((total / 2) - 1);
-            return (t1 + t2) / 2.0;
-        }
-
-        return list.get(total / 2);
-
     }
 }

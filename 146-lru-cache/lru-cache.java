06c04f48d7bb44cb11f7088bf5fdabe3306d6b46
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-23 15:16:34 +0530

    Time: 42 ms (93.13%) | Memory: 111.1 MB (98.30%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,73 +1,21 @@
-class Node {
-    Node prev;
-    int key;
-    int val;
-    Node next;
-
-    Node (int key, int val) {
-        this.key = key;
-        this.val = val;
-    }
-}
-
 class LRUCache {
-    
-    Map<Integer, Node> cache;
-    int capacity;
-    Node head;
-    Node tail;
-    
+
+    Map<Integer, Integer> cache;
+
     public LRUCache(int capacity) {
-        cache = new HashMap<>();
-        this.capacity = capacity;
-        head = new Node(-1, -1);
-        tail = new Node(-1, -1);
-        head.next = tail;
-        tail.prev = head;
+        cache = new LinkedHashMap<>(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return size() > capacity;
+            }
+        };
     }
-
+    
     public int get(int key) {
-        if (!cache.containsKey(key)) return -1;
-        
-        Node node = cache.get(key);
-        
-        remove(node);
-        addToTail(node);
-        
-        return node.val;
-    }
-
-    public void put(int key, int value) {
-        if (cache.containsKey(key)) {
-            Node node = cache.get(key);
-            remove(node);
-            node.val = value;
-            addToTail(node);
-            return;
-        }
-        
-        if (cache.size() >= capacity) {
-            Node lru = head.next;
-            remove(lru);
-            cache.remove(lru.key);
-        }
-        
-        Node newNode = new Node(key, value);
-        addToTail(newNode);
-        cache.put(key, newNode);
+        return cache.getOrDefault(key, -1);
     }
     
-    private void addToTail(Node node) {
-        Node tailPrev = tail.prev;
-        
-        tailPrev.next = node;
-        node.prev = tailPrev;
-        node.next = tail;
-        tail.prev = node;
-    }
-    
-    private void remove(Node node) {
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
+    public void put(int key, int value) { 
+        cache.put(key, value);
     }
 }

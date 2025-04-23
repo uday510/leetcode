c2c825e84f40f2eb6c9238835978fd4f35625ee8
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-23 18:12:53 +0530

    Time: 1 ms (99.91%) | Memory: 44.3 MB (90.19%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -2,19 +2,22 @@ class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         return dfs(0, lists.length - 1, lists);
     }
-    private ListNode dfs(int st, int en, ListNode[] lists) {
-        if (st > en) return null;
-        if (st == en) return lists[st];
-        int mid = (st + en) >> 1;
-        ListNode left = dfs(st, mid, lists);
-        ListNode right = dfs(mid + 1, en, lists);
-        ListNode merged = merge(left, right);
-        return merged;
+
+    private ListNode dfs(int i, int j, ListNode[] lists) {
+        if (i > j) return null;
+        if (i == j) return lists[i];
+
+        int mid = (i + j) >> 1;
+        ListNode left = dfs(i, mid, lists);
+        ListNode right = dfs(mid + 1, j, lists);
+
+        return merge(left, right);
     }
+
     private ListNode merge(ListNode left, ListNode right) {
         ListNode head = new ListNode(-1);
         ListNode tail = head;
-        
+
         while (left != null && right != null) {
             if (left.val < right.val) {
                 tail.next = left;

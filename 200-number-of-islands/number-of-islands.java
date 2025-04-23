1c0f08e2ac2808536e5083475f820826f2b9ef3b
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-23 15:33:41 +0530

    Time: 3 ms (84.68%) | Memory: 50 MB (30.30%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,62 +1,35 @@
 class Solution {
 
-    int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
+    int[][] dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
     int numRows;
     int numCols;
+    char[][] grid;
 
     public int numIslands(char[][] grid) {
         numRows = grid.length;
         numCols = grid[0].length;
-        int numIslands = 0;
+        this.grid = grid;
+        int islands = 0;
 
-        for (int row = 0; row < numRows; ++row) {
-            for (int col = 0; col < numCols; ++col) {
-               if (grid[row][col] != '1') continue;
-
-                dfs(row, col, grid);
-                numIslands++;
+        for (int i = 0; i < numRows; ++i) {
+            for (int j = 0; j < numCols; ++j) {
+                if (grid[i][j] == '1') {
+                    dfs(i, j);
+                    islands++;
+                }
             }
         }
 
-//  https://leetcode.com/discuss/post/6538154/amazon-onsite-question-sde-1-by-anonymou-hq8q/
-        // for (int row = 0; row < numRows; ++row) {
-        //     for (int col = 0; col < numCols; ++col) {
-
-        //         if (grid[row][col] == '#' && canMerge(row, col, grid)) {
-        //             numIslands--;
-        //         }
-        //     }
-        // }
-
-        return numIslands;
+        return islands;
     }
 
-    private void dfs(int row, int col, char[][] grid) {
-         if (row < 0 || row >= numRows || col < 0 || col >= numCols 
-            || grid[row][col] != '1') return;
+    private void dfs(int i, int j) {
+        if (i < 0 || i >= numRows || j < 0 || j >= numCols || grid[i][j] != '1') return;
 
-        grid[row][col] = '#';
 
+        grid[i][j] = '#';
         for (int[] dir : dirs) {
-            int R = dir[0] + row;
-            int C = dir[1] + col;
-            dfs(R, C, grid);
+            dfs(dir[0] + i, dir[1] + j);
         }
-    }
-
-    private boolean canMerge(int row, int col, char[][] grid) {
-        int islands = 0;
-
-        for (int[] dir : dirs) {
-            int R = row + dir[0];
-            int C = col + dir[1];
-
-            if (R < 0 || R >= numRows || C < 0 || C >= numCols || grid[R][C] != '#') 
-                continue;
-            
-            islands++;
-        }
-
-        return islands >= 2;
     }
 }

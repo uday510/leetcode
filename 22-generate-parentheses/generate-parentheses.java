a02f5df0c8be3d5b3b9dfaa76550c98a42fcb26d
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-23 23:06:14 +0530

    Time: 1 ms (81.97%) | Memory: 43.5 MB (35.67%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,19 +1,29 @@
 class Solution {
+
     List<String> list;
+    int n;
+
     public List<String> generateParenthesis(int n) {
+        this.n = n;
         list = new ArrayList<>();
-        dfs(0, 0, n, "");
+        dfs(0, 0, new StringBuilder());
         return list;
     }
-    private void dfs(int open, int close, int n, String str) {
-        if (str.length() == 2 * n) {
-            list.add(str);
+
+    private void dfs(int open, int close, StringBuilder curr) {
+        if (curr.length() == 2 * n) {
+            list.add(curr.toString());
             return;
         }
 
-        if (open < n) 
-            dfs(open + 1, close, n, str + "(");
-        if (open > close)
-            dfs(open, close + 1, n, str + ")");
+        if (open < n) {
+            curr.append("(");
+            dfs(open + 1, close, curr);
+            curr.deleteCharAt(curr.length() - 1);
+        }  if(close < open) {
+            curr.append(")");
+            dfs(open, close + 1, curr);
+            curr.deleteCharAt(curr.length() - 1);
+        }
     }
 }

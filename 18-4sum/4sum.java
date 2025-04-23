94d21f547c1108bb3a1b627f71930751b31168ff
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-23 13:25:07 +0530

    Time: 16 ms (79.35%) | Memory: 45.6 MB (11.74%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,42 +1,61 @@
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
+    List<List<Integer>> res;
+    int[] nums;
+    int n;
+    public List<List<Integer>> fourSum(int[] nums, long target) {
+
         Arrays.sort(nums);
-        return dfs(nums, target, 0, 4);
+        res = new ArrayList<>();
+        this.nums = nums;
+        n = nums.length;
+
+        return dfs(0, 4, target);
     }
-     private List<List<Integer>> dfs(int[] nums, long target, int i, int k) {
-        List<List<Integer>> res = new ArrayList<>();
+
+    private List<List<Integer>> dfs(int idx, int k, long target) {
 
         if (k == 2) {
-            int left = i;
-            int right = nums.length - 1;
-
-            while (left < right) {
-                long sum = (long) nums[left] + (long) nums[right];
-                if (sum < target) left++;
-                else if (sum > target) right--;
-                else {
-                    res.add(new ArrayList<>(Arrays.asList(nums[left], nums[right])));
-                    left++;
-                    right--;
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
-                }
-            }
-            return res;
+            return twoSum(idx, target);
         }
 
-        for (int idx = i; idx < (nums.length - k + 1); ++idx) {
-            if (idx > i && (nums[idx] == nums[idx - 1])) continue;
-            System.out.println(idx);
-            List<List<Integer>> list = dfs(nums, target - nums[idx], idx + 1, k - 1);
-            System.out.println(list);
-            for (List<Integer> subList : list) {
-                List<Integer> temp = new ArrayList<>();
-                temp.add(nums[idx]);
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = idx; i < (n - k + 1); ++i) {
+            if (i > idx && nums[i] == nums[i - 1]) continue;
+
+            List<List<Integer>> list = dfs(i + 1, k - 1, target - nums[i]);
+
+            for (var subList : list) {
+                var temp = new ArrayList<Integer>();
+                temp.add(nums[i]);
                 temp.addAll(subList);
-                res.add(temp);
+                result.add(temp);
             }
         }
-        return res;
+
+        return result;
+    }
+
+    private List<List<Integer>> twoSum(int idx, long target) {
+        List<List<Integer>> list = new ArrayList<>();
+        int leftIdx = idx;
+        int rightIdx = nums.length - 1;
+
+        while (leftIdx < rightIdx) {
+            long curr = (long) nums[leftIdx] + nums[rightIdx];
+
+            if (curr < target) leftIdx++;
+            else if (curr > target) rightIdx--;
+            else {
+                list.add(new ArrayList<>(Arrays.asList(nums[leftIdx], nums[rightIdx])));
+                leftIdx++;
+                rightIdx--;
+
+                while (leftIdx < rightIdx && nums[leftIdx] == nums[leftIdx - 1]) leftIdx++;
+                while (leftIdx < rightIdx && nums[rightIdx] == nums[rightIdx + 1]) rightIdx--;
+            }
+        }
+
+        return list;
     }
 }

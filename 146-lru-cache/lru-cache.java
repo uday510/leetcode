b87b0a026feefc25950bf76c3fb82c2cf5df9954
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-23 15:02:33 +0530

    Time: 43 ms (86.88%) | Memory: 113.4 MB (62.49%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -11,32 +11,32 @@ class Node {
 }
 
 class LRUCache {
-
+    
     Map<Integer, Node> cache;
     int capacity;
     Node head;
     Node tail;
-
+    
     public LRUCache(int capacity) {
         cache = new HashMap<>();
         this.capacity = capacity;
-
         head = new Node(-1, -1);
         tail = new Node(-1, -1);
         head.next = tail;
         tail.prev = head;
     }
-    
+
     public int get(int key) {
         if (!cache.containsKey(key)) return -1;
-
+        
         Node node = cache.get(key);
+        
         remove(node);
         addToTail(node);
-
+        
         return node.val;
     }
-    
+
     public void put(int key, int value) {
         if (cache.containsKey(key)) {
             Node node = cache.get(key);
@@ -45,30 +45,29 @@ class LRUCache {
             addToTail(node);
             return;
         }
-
+        
         if (cache.size() >= capacity) {
             Node lru = head.next;
             remove(lru);
             cache.remove(lru.key);
         }
-
+        
         Node newNode = new Node(key, value);
-        cache.put(key, newNode);
         addToTail(newNode);
+        cache.put(key, newNode);
     }
-
+    
     private void addToTail(Node node) {
-
         Node tailPrev = tail.prev;
+        
         tailPrev.next = node;
         node.prev = tailPrev;
         node.next = tail;
         tail.prev = node;
     }
-
+    
     private void remove(Node node) {
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
 }
-

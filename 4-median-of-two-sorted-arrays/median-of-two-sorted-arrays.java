051eb8289042202ac3e5c7f8d1bdada2ea5da8a6
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-23 18:44:03 +0530

    Time: 2 ms (55.58%) | Memory: 46.3 MB (26.58%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,38 +1,34 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int totalLength = nums1.length + nums2.length;
-        if (totalLength % 2 == 1) {
-            return findKthElement(nums1, nums2, totalLength / 2 + 1);
-        } else {
-            return (findKthElement(nums1, nums2, totalLength / 2) +
-                    findKthElement(nums1, nums2, totalLength / 2 + 1)) / 2.0;
-        }
-    }
-    
-    private int findKthElement(int[] nums1, int[] nums2, int k) {
-        int index1 = 0, index2 = 0;
-        while (true) {
-            if (index1 == nums1.length) {
-                return nums2[index2 + k - 1];
-            }
-            if (index2 == nums2.length) {
-                return nums1[index1 + k - 1];
-            }
-            if (k == 1) {
-                return Math.min(nums1[index1], nums2[index2]);
-            }
+        List<Integer> list = new ArrayList<>();
+        int i = 0, j = 0, n = nums1.length, m = nums2.length;
+
+        while (i < n && j < m) {
             
-            int halfK = k / 2;
-            int newIndex1 = Math.min(index1 + halfK, nums1.length) - 1;
-            int newIndex2 = Math.min(index2 + halfK, nums2.length) - 1;
-            
-            if (nums1[newIndex1] <= nums2[newIndex2]) {
-                k -= (newIndex1 - index1 + 1);
-                index1 = newIndex1 + 1;
+            if (nums1[i] < nums2[j]) {
+                list.add(nums1[i++]);
             } else {
-                k -= (newIndex2 - index2 + 1);
-                index2 = newIndex2 + 1;
+                list.add(nums2[j++]);
             }
         }
+
+        while (i < n) {
+        list.add(nums1[i++]);
+        }
+
+        while (j < m) {
+            list.add(nums2[j++]);
+        }
+
+        int total = list.size();
+
+        if (total % 2 == 0) {
+            int t1 = list.get(total / 2);
+            int t2 = list.get((total / 2) - 1);
+            return (t1 + t2) / 2.0;
+        }
+
+        return list.get(total / 2);
+
     }
 }

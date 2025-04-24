c11919352328c960a4d31a37d693a39623d2bec6
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-24 09:34:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (11.98%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,13 +1,13 @@
 class Solution {
     public int search(int[] nums, int target) {
-        
-        int left = 0;
-        int right = nums.length - 1;
+        int left = 0, right = nums.length - 1;
 
         while (left <= right) {
+
             int mid = (left + right) >> 1;
 
             if (nums[mid] == target) return mid;
+
 
             if (nums[left] <= nums[mid]) {
                 if (nums[left] <= target && target < nums[mid]) {
@@ -15,16 +15,14 @@ class Solution {
                 } else {
                     left = mid + 1;
                 }
-            } 
-            else {
-                if (target > nums[mid] && target <= nums[right]) {
+            } else {
+                if (nums[right] >= target && target > nums[mid])  {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
                 }
             }
         }
-
         return -1;
     }
 }

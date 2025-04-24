dd03aee4ace27dbd15a1814073dead4a0d720c1d
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-24 15:46:34 +0530

    Time: 17 ms (64.44%) | Memory: 44.9 MB (52.16%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -2,28 +2,28 @@ class Solution {
     public String longestPalindrome(String s) {
         int[] longest = {0, 1};
 
-        for (int i = 0; i < s.length(); ++i) {
-            int[] odd = getLength(i - 1, i + 1, s);
-            int[] even = getLength(i, i + 1, s);
 
-            int[] current = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
-            longest = current[1] - current[0] > longest[1] - longest[0] ? current : longest;
+        for (int i = 0; i < s.length(); ++i) {
+            int[] odd = len(i - 1, i + 1, s);
+            int[] even = len(i, i + 1, s);
+
+            int[] curr = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
+            longest = curr[1] - curr[0] > longest[1] - longest[0] ? curr : longest;
         }
 
         return s.substring(longest[0], longest[1]);
     }
 
-    private int[] getLength(int left, int right, String s) {
+    private int[] len(int i, int j, String s) {
 
-        while (left > -1 && right < s.length()) {
-            if (s.charAt(left) != s.charAt(right)) {
+        while (i > -1 && j < s.length()) {
+            if (s.charAt(i) != s.charAt(j)) 
                 break;
-            }
 
-            left--;
-            right++;
+            i--;
+            j++;
         }
 
-        return new int[]{left + 1, right};
+        return new int[]{i + 1, j};
     }
 }

61e1e45324ed30ade00354ad32ece700371af455
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-26 22:46:39 +0530

    Time: 1206 ms (2.31%) | Memory: 48.6 MB (6.49%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,32 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int profit = 0;
 
-        for (int idx = 1; idx < prices.length; ++idx) {
-           profit += Math.max(0, prices[idx] - prices[idx - 1]);
+    int[] prices;
+    int len;
+    int[] dp;
+    int maxProfit;
+
+    public int maxProfit(int[] prices) {
+      this.prices = prices;
+      len = prices.length;
+      dp = new int[len];
+      Arrays.fill(dp, -1);
+      maxProfit = 0;
+
+      dfs(0);
+      return maxProfit;
+    }
+
+    private int dfs(int idx) {
+        if (idx >= len) return 0;
+        if (dp[idx] != -1) return dp[idx];
+
+        int currProfit = 0;
+        for (int currIdx = idx; currIdx < len; ++currIdx) {
+            currProfit = Math.max(currProfit, prices[currIdx] - prices[idx] + dfs(currIdx + 1));
+
+            maxProfit = Math.max(maxProfit, currProfit);
         }
 
-        return profit;
+        return dp[idx] = maxProfit;
     }
 }

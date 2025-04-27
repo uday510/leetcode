83b08692e18e28487dd7d650dff1d35dcb946f91
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-27 23:29:56 +0530

    Time: 1 ms (82.06%) | Memory: 41.3 MB (82.91%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.java
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.java
@@ -1,34 +1,35 @@
 class Solution {
     public int minMutation(String startGene, String endGene, String[] bank) {
-        
-        Set<String> set = new HashSet<>(Arrays.asList(bank));
+        Set<String> validGenes = new HashSet<>(Arrays.asList(bank));
         Queue<String> queue = new ArrayDeque<>();
-        int level = 0;
+        int mutations = 0;
+
+        if (!validGenes.contains(endGene)) return -1;
         queue.offer(startGene);
 
         while (!queue.isEmpty()) {
             int size = queue.size();
-            level++;
+            mutations++;
             for (int idx = 0; idx < size; ++idx) {
-                String curr = queue.poll();
-                StringBuilder sb = new StringBuilder(curr);
+                String currentGene = queue.poll();
+                StringBuilder geneBuilder = new StringBuilder(currentGene);
 
-                for (int i = 0; i < curr.length(); ++i) {
-                    char prev = sb.charAt(i);
+                for (int pos = 0; pos < currentGene.length(); ++pos) {
+                    char originalChar = geneBuilder.charAt(pos);
 
                     for (char ch : new char[]{'A', 'C', 'G', 'T'}) {
-                        sb.setCharAt(i, ch);
+                        geneBuilder.setCharAt(pos, ch);
 
-                        String gene = sb.toString();
+                        String mutatedGene = geneBuilder.toString();
 
-                        if (!set.contains(gene)) continue;
+                        if (!validGenes.contains(mutatedGene)) continue;
                         
-                        if (gene.equals(endGene)) return level;
+                        if (mutatedGene.equals(endGene)) return mutations;
                     
-                        set.remove(gene);
-                        queue.offer(gene);
+                        validGenes.remove(mutatedGene);
+                        queue.offer(mutatedGene);
                     }
-                    sb.setCharAt(i, prev);
+                    geneBuilder.setCharAt(pos, originalChar);
                 }
             }
         }

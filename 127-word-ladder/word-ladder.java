f800fd10fd78f73a50d032f3ae9402e395bc275f
Uday <BUDAYTEJA009@GMAIL.COM>
2025-04-28 10:46:51 +0530

    Time: 65 ms (84.17%) | Memory: 46.2 MB (73.84%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -2,52 +2,51 @@ class Solution {
     Set<String> words;
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         words = new HashSet<>(wordList);
+        wordList.remove(beginWord);
+
+        if (!words.contains(endWord)) return 0;
+
         Queue<String> queue = new ArrayDeque<>();
-        int level = 0;
-
-        if (!words.contains(endWord)) return level;
-
+        int level = 1;
         queue.offer(beginWord);
-        words.remove(beginWord);
 
         while (!queue.isEmpty()) {
             int size = queue.size();
-            
             level++;
-            for (int i = 0; i < size; ++i) {
-                String word = queue.poll();
-                if (word.equals(endWord)) return level;
-                var neighbors = getNeighbors(word);
-                for (var neighbor : neighbors) {
+
+            for (int pos = 0; pos < size; ++pos) {
+                String currentString = queue.poll();
+
+                List<String> neighbors = getNeighbors(currentString);
+
+                for (String neighbor : neighbors) {
+                    if (neighbor.equals(endWord)) return level;
                     queue.offer(neighbor);
                 }
             }
-
         }
 
         return 0;
     }
 
-    private List<String> getNeighbors(String word) {
-        char[] chars = word.toCharArray();
-        List<String> list = new ArrayList<>();
+    private List<String> getNeighbors(String string) {
+        StringBuilder stringBuilder = new StringBuilder(string);
+        List<String> neighbors = new ArrayList<>();
 
-        for (int idx = 0; idx < chars.length; ++idx) {
-            char temp = chars[idx];
+        for (int idx = 0; idx < stringBuilder.length(); ++idx) {
+            char originalCharacter = stringBuilder.charAt(idx);
+
             for (char ch = 'a'; ch <= 'z'; ++ch) {
-                if (ch == temp) continue;
-
-                chars[idx] = ch;
-                String str = new String(chars);
-
-                if (!words.contains(str)) continue;
-
-                list.add(str);
-                words.remove(str);
+                stringBuilder.setCharAt(idx, ch);
+                String formedString = stringBuilder.toString();
+                if (!words.contains(formedString)) continue;
+                words.remove(formedString);
+                neighbors.add(formedString);
             }
 
-            chars[idx] = temp;
+            stringBuilder.setCharAt(idx, originalCharacter);
         }
-        return list;
+
+        return neighbors;
     }
 }

dd5777f6d42c3a6c46332ac2391b34dab522c94e
Uday <BUDAYTEJA009@GMAIL.COM>
2025-05-04 00:42:50 +0530

    Time: 26 ms (51.55%) | Memory: 43 MB (22.86%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -1,26 +1,56 @@
-class Solution {
-    Map<Node, Node> visited;
-    public Node cloneGraph(Node node) {
-        visited = new HashMap<>();
-
-        return dfs(node);        
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
     }
-
-    private Node dfs(Node node) {
-        if (node == null) {
-            return null;
-        }
-
-        if (visited.containsKey(node)) {
-            return visited.get(node);
-        }
-
-        Node clonedNode = new Node(node.val, new ArrayList<>());
-        visited.put(node, clonedNode);
-        for (Node neighbor : node.neighbors) {
-            clonedNode.neighbors.add(dfs(neighbor));
-        }
-
-        return clonedNode;
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
     }
 }
+*/
+
+ class Solution {
+    Map<Node, Node> dp;
+    public Node cloneGraph(Node node) {
+        dp = new HashMap<>();
+        return dfs(node);
+    }
+    private Node dfs(Node node) {
+        if (node == null) return null;
+
+        if (dp.containsKey(node)) return dp.get(node);
+
+        Node newNode = new Node(node.val);
+        dp.put(node, newNode); 
+
+        for (Node nei : node.neighbors) {
+            newNode.neighbors.add(dfs(nei));
+        }
+
+        return newNode;
+    }
+}
+
+/**
+
+
+1 --> [2, 4]
+2 --> [1, 3]
+3 --> [4, 2]
+
+
+1 -> []
+
+
+
+
+ */

85e8d81be8a21f042a8dd53821257c03c8184239
Uday <BUDAYTEJA009@GMAIL.COM>
2025-05-13 22:59:44 +0530

    Time: 16 ms (43.67%) | Memory: 47.9 MB (77.10%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,28 +1,28 @@
 class Solution {
     private static final int UNREACHABLE = (int) 1e9; 
     public int networkDelayTime(int[][] times, int n, int k) {
-        List<Edge>[] adjList = new ArrayList[n + 1];
+        List<int[]>[] adjList = new ArrayList[n + 1];
         for (int i = 1; i <= n; ++i) adjList[i] = new ArrayList<>();
 
-        for (int[] time : times) adjList[time[0]].add(new Edge(time[1], time[2]));
+        for (int[] time : times) adjList[time[0]].add(new int[]{time[1], time[2]});
 
         int[] dists = new int[n + 1];
         Arrays.fill(dists, UNREACHABLE);
 
-        Queue<Edge> queue = new PriorityQueue<>((edge1, edge2) -> edge1.dist - edge2.dist);
-        queue.offer(new Edge(k, 0));
+        Queue<int[]> queue = new PriorityQueue<>((edge1, edge2) -> edge1[1] - edge2[1]);
+        queue.offer(new int[]{k, 0});
         dists[k] = 0;
 
         while (!queue.isEmpty()) {
-            Edge currEdge = queue.poll();
-            int currNode = currEdge.to, currDist = currEdge.dist;
+            int[] currEdge = queue.poll();
+            int currNode = currEdge[0], currDist = currEdge[1];
 
             if (dists[currNode] < currDist) continue;
             dists[currNode] = currDist;
 
-            for (Edge neiNode : adjList[currNode]) {
-                if (dists[neiNode.to] != UNREACHABLE) continue;
-                Edge newEdge = new Edge(neiNode.to, neiNode.dist + currDist);
+            for (int[] neiNode : adjList[currNode]) {
+                if (dists[neiNode[0]] != UNREACHABLE) continue;
+                int[] newEdge = new int[]{neiNode[0], neiNode[1] + currDist};
                 queue.offer(newEdge);
             }
         }

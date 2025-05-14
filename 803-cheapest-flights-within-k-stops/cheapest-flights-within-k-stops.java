e1b71a09816e50d4c8ca0fbd0c53d58fa790a559
Uday <BUDAYTEJA009@GMAIL.COM>
2025-05-15 00:12:40 +0530

    Time: 5 ms (97.38%) | Memory: 44.3 MB (83.63%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,38 +1,42 @@
 class Solution {
-
+    
     private static final int UNREACHABLE = (int) 1e9;
 
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         List<int[]>[] adjList = new ArrayList[n];
-        for (int i = 0; i < n; ++i) adjList[i] = new ArrayList<>();
-        for (int[] flight : flights) {
-            adjList[flight[0]].add(new int[] {flight[1], flight[2]});
+
+        for (int idx = 0; idx < n; ++idx) {
+            adjList[idx] = new ArrayList<>();
         }
+
+        for (int[] flight : flights) {
+            int from = flight[0], to = flight[1], dest = flight[2];
+            adjList[from].add(new int[] {to, dest});
+        }
+
+        int[] dists = new int[n];
+        Arrays.fill(dists, UNREACHABLE);
 
         Queue<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[] {src, 0, 0}); // node, cost, stops
+        queue.offer(new int[] {src, 0, 0});
 
-        int[] minCostAtStops = new int[n];
-        Arrays.fill(minCostAtStops, UNREACHABLE);
-        minCostAtStops[src] = 0;
+       while (!queue.isEmpty()) {
+           int[] curr = queue.poll();
+           int node = curr[0], cost = curr[1], stops = curr[2];
 
-        while (!queue.isEmpty()) {
-            int[] curr = queue.poll();
-            int node = curr[0], cost = curr[1], stops = curr[2];
+           if (stops > k) continue;
 
-            if (stops > k) continue;
+           for (int[] neighbor : adjList[node]) {
+               int nextNode = neighbor[0], price = neighbor[1];
+               int newCost = cost + price;
 
-            for (int[] neighbor : adjList[node]) {
-                int nextNode = neighbor[0], price = neighbor[1];
-                int newCost = cost + price;
+               if (newCost > dists[nextNode]) continue;
 
-                if (newCost < minCostAtStops[nextNode]) {
-                    minCostAtStops[nextNode] = newCost;
-                    queue.offer(new int[] {nextNode, newCost, stops + 1});
-                }
-            }
-        }
+               dists[nextNode] = newCost;
+               queue.offer(new int[] {nextNode, newCost, stops + 1});
+           }
+       }
 
-        return minCostAtStops[dst] == UNREACHABLE ? -1 : minCostAtStops[dst];
+        return dists[dst] == UNREACHABLE ? -1 : dists[dst];
     }
 }

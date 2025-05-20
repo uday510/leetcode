1c9dd7b79a9111ce89e3ea8d38d1ac29ddbb500a
Uday <BUDAYTEJA009@GMAIL.COM>
2025-05-20 16:44:16 +0530

    Time: 7 ms (47.36%) | Memory: 44.2 MB (83.81%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,29 +1,25 @@
 class Solution {
-    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        if (src == dst) {
-            return 0;
-        }
-
-        int[] previous = new int[n];
-        int[] current = new int[n];
-
-        for (int i = 0; i < n; ++i) {
-            previous[i] = current[i] = Integer.MAX_VALUE;
-        }
-        previous[src] = 0;
-
-        for (int i = 0; i <= k; ++i) {
-            for (int[] flight : flights) {
-                int from = flight[0];
-                int to = flight[1];
-                int cost = flight[2];
-
-                if (previous[from] < Integer.MAX_VALUE) {
-                    current[to] = Math.min(current[to], previous[from] + cost);
-                }
-            }
-            previous = current.clone();
-        }
-        return current[dst] == Integer.MAX_VALUE ? -1 : current[dst];
+   public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+    int INF = (int) 1e9;
+    int[][] dp = new int[k + 2][n];
+    
+    for (int i = 0; i <= k + 1; i++) {
+        Arrays.fill(dp[i], INF);
     }
+    dp[0][src] = 0;
+
+    for (int i = 1; i <= k + 1; i++) {
+        for (int j = 0; j < n; j++) {
+            dp[i][j] = dp[i - 1][j]; 
+        }
+        for (int[] flight : flights) {
+            int u = flight[0], v = flight[1], w = flight[2];
+            if (dp[i - 1][u] != INF) {
+                dp[i][v] = Math.min(dp[i][v], dp[i - 1][u] + w);
+            }
+        }
+    }
+
+    return dp[k + 1][dst] == INF ? -1 : dp[k + 1][dst];
+}
 }

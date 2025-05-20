260189c1567b11ad63b587be1a52fbe3396a6270
Uday <BUDAYTEJA009@GMAIL.COM>
2025-05-20 20:24:01 +0530

    Time: 115 ms (11.75%) | Memory: 45.1 MB (88.36%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -1,71 +1,55 @@
 class Solution {
-    private static final int[][] directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+    private final int[][] directions = { {0, 1}, {1, 0}, {-1, 0}, {0, -1} };
     public int minimumEffortPath(int[][] heights) {
 
-      int left = 0;
-      int right = 1000000;
-      int res = right;
-
-      while (left <= right) {
-        int midIdx = (left + right) / 2;
-
-        if (exists(heights, midIdx)) {
-          res = midIdx;
-          right = midIdx - 1;
-        } else {
-          left = midIdx + 1;
-        }
-      }
-      return res;
-    }
-
-    boolean exists(int[][] heights, int k) {
-      int row = heights.length;
-      int col = heights[0].length;
-
-      boolean[][] visited = new boolean[row][col];
-      Queue<Cell> queue = new LinkedList<>();
-
-      queue.offer(new Cell(0, 0));
-      visited[0][0] = true;
-
-      while (!queue.isEmpty()) {
-          Cell cell = queue.poll();
-          int x = cell.x;
-          int y = cell.y;
-
-          if (x == row - 1 && y == col - 1) {
-            return true;
-          }
-
-          for (int[] dir : directions) {
-            int adjX = x + dir[0];
-            int adjY = y + dir[1];
-
-            if (isValid(adjX, adjY, row, col) && !visited[adjX][adjY]) {
-              
-              int currDiff = Math.abs( heights[x][y] - heights[adjX][adjY]);
-
-              if (currDiff <= k) {
-                visited[adjX][adjY] = true;
-                Cell newCell = new Cell(adjX, adjY);
-                queue.offer(newCell);
-              }
+        int left = 0, right = (int) 1e6;
+        
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            
+            boolean pathExists = isPathExists(heights, mid);
+            if (!pathExists) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
-          }
-      }
-      return false;
-    }
-    boolean isValid(int i, int j, int m, int n) {
-      return (i > -1 && i < m && j > -1 && j < n);
-    }
-}
-class Cell {
-  int x;
-  int y;
+        }
 
-  Cell(int i, int j) {
-    x = i;
-    y = j;
-  }
+        return left;
+    }
+
+    private boolean isPathExists(int[][] heights, int k) {
+        int rows = heights.length, columns = heights[0].length;
+        boolean[][] vis = new boolean[rows][columns];
+        Queue<int[]> queue = new ArrayDeque<>();
+
+        queue.offer(new int[] {0, 0});
+        vis[0][0] = true;
+
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int row = curr[0], col = curr[1];
+
+            if (row == rows - 1 && col == columns - 1) return true;
+
+            for (int[] direction : directions) {
+                int nextRow = direction[0] + row;
+                int nextCol = direction[1] + col;
+
+                if (nextRow < 0 || nextRow >= rows || 
+                nextCol < 0 || nextCol >= columns || vis[nextRow][nextCol]) continue;
+
+
+                int newDifference = Math.abs(heights[nextRow][nextCol] - heights[row][col]);
+
+                if (newDifference > k) continue;
+
+                vis[nextRow][nextCol] = true;
+                
+                queue.offer(new int[] {nextRow, nextCol});
+            }
+        }
+        return false;
+    }
 }

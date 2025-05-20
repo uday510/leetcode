dc11b4ac04e5a483374a37b6e4704aa930399a77
Uday <BUDAYTEJA009@GMAIL.COM>
2025-05-20 18:23:51 +0530

    Time: 6 ms (83.48%) | Memory: 44.9 MB (15.20%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,17 +1,15 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         List<int[]>[] adjList = new ArrayList[n];
-        for (int idx = 0; idx < n; ++idx) {
-            adjList[idx] = new ArrayList<>();
-        }
+        for (int i = 0; i < n; ++i) adjList[i] = new ArrayList<>();
 
         for (int[] flight : flights) {
             int u = flight[0], v = flight[1], w = flight[2];
             adjList[u].add(new int[] {v, w});
         }
-
-        int[] dist = new int[n];
+        
         int INF = (int) 1e9;
+        int[] dist = new int[n];
         Arrays.fill(dist, INF);
 
         Queue<int[]> queue = new ArrayDeque<>();
@@ -20,19 +18,19 @@ class Solution {
 
         while (!queue.isEmpty()) {
             int[] curr = queue.poll();
-            int node = curr[0], w = curr[1], steps = curr[2];
+            int u = curr[0], weight = curr[1], stops = curr[2];
 
-            if (steps > k) continue;
-            
-            for (int[] next : adjList[node]) {
-                int nei = next[0], neiW = next[1];
-                int newW = w + neiW;
-                
-                if (newW < dist[nei]) {
-                    dist[nei] = newW;
-                    queue.offer(new int[] {nei, newW, steps + 1});
+            if (stops > k) continue;
+
+            for (int[] next : adjList[u]) {
+                int v = next[0], w = next[1];
+                int newW = w + weight;
+
+                if (newW < dist[v]) {
+                    dist[v] = newW;
+                    queue.offer(new int[] {v, newW, stops + 1});
                 }
-            } 
+            }
         }
 
         return dist[dst] == INF ? -1 : dist[dst];

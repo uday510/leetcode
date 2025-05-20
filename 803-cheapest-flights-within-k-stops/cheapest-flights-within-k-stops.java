80dd2660c04c790ce43386294333a6a5a3ae9273
Uday <BUDAYTEJA009@GMAIL.COM>
2025-05-20 17:01:08 +0530

    Time: 6 ms (83.48%) | Memory: 43.8 MB (99.75%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,25 +1,40 @@
 class Solution {
-   public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-    int INF = (int) 1e9;
-    int[][] dp = new int[k + 2][n];
-    
-    for (int i = 0; i <= k + 1; i++) {
-        Arrays.fill(dp[i], INF);
-    }
-    dp[0][src] = 0;
-
-    for (int i = 1; i <= k + 1; i++) {
-        for (int j = 0; j < n; j++) {
-            dp[i][j] = dp[i - 1][j]; 
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        List<int[]>[] adjList = new ArrayList[n];
+        for (int idx = 0; idx < n; ++idx) {
+            adjList[idx] = new ArrayList<>();
         }
+
         for (int[] flight : flights) {
             int u = flight[0], v = flight[1], w = flight[2];
-            if (dp[i - 1][u] != INF) {
-                dp[i][v] = Math.min(dp[i][v], dp[i - 1][u] + w);
-            }
+            adjList[u].add(new int[] {v, w});
         }
-    }
 
-    return dp[k + 1][dst] == INF ? -1 : dp[k + 1][dst];
-}
+        int[] dist = new int[n];
+        int INF = (int) 1e9;
+        Arrays.fill(dist, INF);
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[] {src, 0, 0});
+        dist[src] = 0;
+
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int node = curr[0], w = curr[1], steps = curr[2];
+
+            if (steps > k) continue;
+            
+            for (int[] next : adjList[node]) {
+                int nei = next[0], neiW = next[1];
+                int newW = w + neiW;
+                
+                if (newW < dist[nei]) {
+                    dist[nei] = newW;
+                    queue.offer(new int[] {nei, newW, steps + 1});
+                }
+            } 
+        }
+
+        return dist[dst] == INF ? -1 : dist[dst];
+    }
 }

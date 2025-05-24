afcf548d2747df0c5ad86557484a2fef15f698e8
Uday <BUDAYTEJA009@GMAIL.COM>
2025-05-24 11:27:23 +0530

    Time: 17 ms (24.48%) | Memory: 44.8 MB (23.50%) - LeetSync

---
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int swimInWater(int[][] grid) {
+        int n = grid.length;
+        int[][] dists = new int[n][n];
+        int INF = (int) 1e9;
+
+        for (int[] row : dists) Arrays.fill(row, INF);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        pq.offer(new int[] {0, 0, grid[0][0]});
+        dists[0][0] = grid[0][0];
+
+        int[][] dirs = { {0, 1}, {1, 0}, {-1, 0}, {0, -1} };
+
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int r = curr[0], c = curr[1], d = curr[2];
+
+            if (dists[r][c] < d) continue;
+
+            for (int[] dir : dirs) {
+                int R = dir[0] + r;
+                int C = dir[1] + c;
+
+                if (R < 0 || R >= n || C < 0 || C >= n) continue;
+
+                int D = Math.max(d, grid[R][C]);
+
+                if (D < dists[R][C]) {
+                    dists[R][C] = D;
+                    pq.offer(new int[] {R, C, D} );
+                }
+            }
+        }
+
+        for (int[] row : dists) System.out.println(Arrays.toString(row));
+
+        return dists[n - 1][n - 1];
+    }
+}

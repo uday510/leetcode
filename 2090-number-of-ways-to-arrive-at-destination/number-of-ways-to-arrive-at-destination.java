e7c2d7db7d134254477ec5e4400edc7212bf648f
Uday <BUDAYTEJA009@GMAIL.COM>
2025-05-24 13:42:36 +0530

    Time: 12 ms (84.52%) | Memory: 49.4 MB (41.03%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -1,54 +1,48 @@
 class Solution {
-    public int countPaths(int n, int[][] roads) {
+    
+     public int countPaths(int n, int[][] roads) {
+        long INF = Long.MAX_VALUE;
         List<int[]>[] adjList = new ArrayList[n];
-        long[] dist = new long[n];
-        long[] ways = new long[n];
-
-        for (int idx = 0; idx < n; ++idx) {
-            adjList[idx] = new ArrayList<>();
-        }
+        for (int i = 0; i < n; ++i) adjList[i] = new ArrayList<>();
 
         for (int[] road : roads) {
             int u = road[0], v = road[1], w = road[2];
-
             adjList[u].add(new int[]{v, w});
             adjList[v].add(new int[]{u, w});
         }
 
-        Arrays.fill(dist, Long.MAX_VALUE);
-        var pq = new PriorityQueue<long[]>(Comparator.comparingLong(a -> a[1]));
+        long[] dists = new long[n];
+        long[] ways = new long[n];
+        Arrays.fill(dists, INF);
+
+        PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong(k -> k[1]));
         pq.offer(new long[]{0, 0});
-        dist[0] = 0;
+        dists[0] = 0;
         ways[0] = 1;
 
-        long MOD = (long) 1e9 + 7;
-
+        int MOD = (int) 1e9 + 7;
+        
         while (!pq.isEmpty()) {
             long[] curr = pq.poll();
             int u = (int) curr[0];
             long d = curr[1];
-
-            if (d > dist[u]) continue;
-
-            for (int[] nei : adjList[u]) {
-                int v = nei[0];
-                long newDistance = nei[1] + d;
-
-                if (newDistance > dist[v]) continue;
-
-                if (newDistance < dist[v]) {
-                    dist[v] = newDistance;
+        
+            if (dists[u] < d) continue;
+            
+            for (int[] next : adjList[u]) {
+                int v = next[0], w = next[1];
+                long newTime = d + w;
+                
+                if (newTime < dists[v]) {
                     ways[v] = ways[u];
-                    pq.offer(new long[]{v, newDistance});
-                } 
-                else if (newDistance == dist[v]) {
-                    ways[v] += ways[u];
+                    dists[v] = newTime;
+                    pq.offer(new long[] {v, newTime});
+                } else if (newTime == dists[v]) {
+                    ways[v] = (ways[u] + ways[v]) % MOD;
                 }
-
-                ways[v] %= MOD;
             }
         }
-
+        
         return (int) ways[n - 1];
     }
 }

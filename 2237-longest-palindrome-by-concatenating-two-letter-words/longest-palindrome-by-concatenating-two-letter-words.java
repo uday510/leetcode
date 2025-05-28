6c77e07902d3fd7feada438be3d586c384464121
Uday <BUDAYTEJA009@GMAIL.COM>
2025-05-29 01:15:57 +0530

    Time: 53 ms (71.21%) | Memory: 58.3 MB (66.82%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
@@ -10,15 +10,16 @@ class Solution {
         int longest = 0;
         
         for (var s : words) {
+            if (!map.containsKey(s)) continue;
             String rev = new StringBuilder(s).reverse().toString();
-            if (s.charAt(0) == s.charAt(1) && map.containsKey(s)) {
+            if (s.charAt(0) == s.charAt(1)) {
                 int count = map.get(s);
                 longest += (count / 2) * 4;
                 if (count % 2 == 1) {
                     flag = true;
                 }
                 map.remove(s);
-            }  else if (map.containsKey(rev) && map.containsKey(s)) {
+            }  else if (map.containsKey(rev)) {
                 int min = Math.min(map.get(rev), map.get(s));
                 longest = Math.max(longest, longest + 4 * min);
                 map.remove(rev);

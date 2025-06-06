49ae16127a1849514745ab0b9a4dc300c25d2164
Uday <BUDAYTEJA009@GMAIL.COM>
2025-06-06 20:06:08 +0530

    Time: 11 ms (6.00%) | Memory: 45.3 MB (65.73%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,29 +1,26 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int[][] dp = new int[rows][cols];
+        
+        int n = matrix.length;
+        int[][] dp = new int[n][n];
+        int ans = (int) 1e9;
 
-        for (int row = 0; row < rows; ++row) {
-            for (int col = 0; col < cols; ++col) {
-                if (row == 0) {
-                    dp[row][col] = matrix[row][col];
-                } else if (col == 0) {
-                    dp[row][col] = Math.min(dp[row - 1][col], dp[row - 1][col + 1]) + matrix[row][col];
-                } else if (col == cols - 1) {
-                    dp[row][col] = Math.min(dp[row - 1][col], dp[row - 1][col - 1]) + matrix[row][col];
-                } else {
-                    dp[row][col] = Math.min(dp[row - 1][col], Math.min(dp[row - 1][col - 1], dp[row - 1][col + 1])) + matrix[row][col];
-                }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                dp[i][j] = matrix[i][j];
 
+                int min = (int) 1e9;
+
+                if (i - 1 >= 0 && j >= 0) min = Math.min(min, dp[i - 1][j]);
+                if (i - 1 >= 0 && j + 1 < n) min = Math.min(min, dp[i - 1][j + 1]);
+                if (i - 1 >= 0 && j - 1 >= 0) min = Math.min(min, dp[i - 1][j - 1]);
+
+                dp[i][j] += (min == (int) 1e9 ? 0 : min);
+
+                if (i == n - 1) ans = Math.min(ans, dp[i][j]);
             }
         }
 
-        int minFallingSum = Integer.MAX_VALUE;
-        for (int col = 0; col < cols; ++col) {
-            minFallingSum = Math.min(minFallingSum, dp[rows - 1][col]);
-        }
-
-        return minFallingSum;
+        return ans;
     }
 }

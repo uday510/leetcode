f309d0f9631cb27636d06e5c20f75fdf93b74c08
Uday <BUDAYTEJA009@GMAIL.COM>
2025-06-11 17:03:42 +0530

    Time: 34 ms (81.70%) | Memory: 55.2 MB (57.00%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,28 +1,19 @@
 class Solution {
-    Integer[][] memo;
     public int longestPalindromeSubseq(String s) {
-        initialize(s);
-
-        return dfs(0, s.length() - 1, s);
-    }
-
-    private int dfs(int leftIdx, int rightIdx, String s) {
-        if (leftIdx > rightIdx) return 0;
-        if (leftIdx == rightIdx) return 1;
-
-        if (memo[leftIdx][rightIdx] != null) 
-            return memo[leftIdx][rightIdx];
-
-        if (s.charAt(leftIdx) == s.charAt(rightIdx)) {
-            memo[leftIdx][rightIdx] = 2 + dfs(leftIdx + 1, rightIdx - 1, s);
-        } else {
-            memo[leftIdx][rightIdx] = Math.max(dfs(leftIdx + 1, rightIdx, s), dfs(leftIdx, rightIdx - 1, s));
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        
+        for (int i = n - 1; i > -1; --i) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; ++j) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
         }
 
-        return memo[leftIdx][rightIdx];
-    }
-
-    private void initialize(String s) {
-        memo = new Integer[s.length()][s.length()];
+        return dp[0][n - 1];
     }
 }

993069d044a3f9f9f0b47c5e47a5598c6c3acab4
Uday <BUDAYTEJA009@GMAIL.COM>
2025-06-11 15:33:20 +0530

    Time: 25 ms (35.21%) | Memory: 44.8 MB (80.91%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,47 +1,43 @@
 class Solution {
-
-    Integer[][] dp;
-    int len1;
-    int len2;
+    int m;
+    int n;
+    int[][] dp;
 
     public int minimumDeleteSum(String s1, String s2) {
-        len1 = s1.length();
-        len2 = s2.length();
-        dp = new Integer[len1][len2];
+        m = s1.length();
+        n = s2.length();
+        dp = new int[m][n];
+
+        for (var row : dp) Arrays.fill(row, -1);
 
         return dfs(0, 0, s1, s2);
     }
 
-    private int dfs(int idx1, int idx2, String s1, String s2) {
-        if (idx1 >= len1) {
-            return getCount(idx2, s2);
-        } else if (idx2 >= len2) {
-            return getCount(idx1, s1);
+    private int dfs(int i, int j, String s1, String s2) {
+        if (i >= m && j >= n) return 0;
+
+        int curr = 0;
+        if (i >= m) {
+            for (; j < n; ++j) curr += s2.charAt(j);
+            return curr;
         }
 
-        if (dp[idx1][idx2] != null) {
-            return dp[idx1][idx2];
+        if (j >= n) {
+            for (; i < m; ++i) curr += s1.charAt(i);
+            return curr;
         }
 
-        if (s1.charAt(idx1) == s2.charAt(idx2)) {
-            dp[idx1][idx2] = dfs(idx1 + 1, idx2 + 1, s1, s2);
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (s1.charAt(i) == s2.charAt(j)) {
+            dp[i][j] = dfs(i + 1, j + 1, s1, s2);
         } else {
-            int d1 = s1.charAt(idx1) + dfs(idx1 + 1, idx2, s1, s2);
-            int d2 = s2.charAt(idx2) + dfs(idx1, idx2 + 1, s1, s2);
-
-            dp[idx1][idx2] = Math.min(d1, d2);
+            dp[i][j] = Math.min(
+                s1.charAt(i) + dfs(i + 1, j, s1, s2),
+                s2.charAt(j) + dfs(i, j + 1, s1, s2)
+            );
         }
 
-        return dp[idx1][idx2];
-    }
-
-    private int getCount(int idx, String s) {
-        int current = 0;
-
-        for (int index = idx; index < s.length(); ++index) {
-            current += s.charAt(index);
-        }
-
-        return current;
+        return dp[i][j];
     }
 }

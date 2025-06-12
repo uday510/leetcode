72ac16b9ef4c35c9b4494a71b90df3e9baa0d11d
Uday <BUDAYTEJA009@GMAIL.COM>
2025-06-12 15:22:17 +0530

    Time: 6 ms (61.45%) | Memory: 44.5 MB (53.75%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,27 +1,29 @@
 class Solution {
+
     Set<String> words;
-    Map<Integer, Boolean> dp;
+    int[] dp;
+
     public boolean wordBreak(String s, List<String> wordDict) {
         words = new HashSet<>(wordDict);
-        dp = new HashMap<>();
+        dp = new int[s.length()];
+        Arrays.fill(dp, -1);
         return dfs(0, s);
     }
-    private boolean dfs(int index, String s) {
-        if (index >= s.length()) return true;
 
-        if (dp.containsKey(index)) return dp.get(index);
+    private boolean dfs(int i, String s) {
+        if (i >= s.length()) return true;
 
-        for (int i = index; i < s.length(); ++i) {
-            String substr = s.substring(index, i+1);
-            if (words.contains(substr)) {
-                if (dfs(i + 1, s)) {
-                    dp.put(index, true);
+        if (dp[i] != -1) return dp[i] == 1;
+        for (int j = i; j < s.length(); ++j) {
+            if (words.contains(s.substring(i, j + 1))) {
+                if (dfs(j + 1, s)) {
+                    dp[i] = 1;
                     return true;
                 }
             }
         }
 
-        dp.put(index, false);
+        dp[i] = 0;
         return false;
     }
 }

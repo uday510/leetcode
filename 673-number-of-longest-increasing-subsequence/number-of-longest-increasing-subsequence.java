d015f1796652c46a0663a475517b4fd70a6cf3bd
Uday <BUDAYTEJA009@GMAIL.COM>
2025-06-16 10:29:50 +0530

    Time: 17 ms (90.70%) | Memory: 44.2 MB (49.52%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
@@ -1,31 +1,31 @@
 class Solution {
     public int findNumberOfLIS(int[] nums) {
-        int len = nums.length;
-        int[] length = new int[len];
-        int[] count = new int[len];
-        int longest = -1;
         
-        for (int i = 0; i < len; ++i) {
-            length[i] = 1;
-            count[i] = 1;
+       int n = nums.length;
+        int[] dp = new int[n];
+        int[] cnt = new int[n];
+        int longest = 1;
+
+        for (int i = 0; i < n; ++i) {
+            dp[i] = cnt[i] = 1;
             for (int j = 0; j < i; ++j) {
                 if (nums[j] < nums[i]) {
-                    if (length[j] + 1 > length[i]) {
-                        length[i] = length[j] + 1;
-                        count[i] = count[j];
-                    } else if (length[j] + 1 == length[i]) {
-                        count[i] += count[j];
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        cnt[i] = cnt[j];
+                    } else if (dp[j] + 1 == dp[i]) {
+                        cnt[i] += cnt[j];
                     }
-                }
+                } 
             }
-            longest = Math.max(longest, length[i]);
+            longest = Math.max(longest, dp[i]);
         }
-        
-        int result = 0;
-        for (int idx = 0; idx < len; ++idx) {
-            result += length[idx] == longest ? count[idx] : 0;
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (dp[i] == longest) ans += cnt[i];
         }
-        
-        return result;
+
+        return ans;
     }
 }

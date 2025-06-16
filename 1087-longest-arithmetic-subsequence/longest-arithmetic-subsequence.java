7a7a599147923fa7a38d5733f996512017445d41
Uday <BUDAYTEJA009@GMAIL.COM>
2025-06-16 18:01:39 +0530

    Time: 45 ms (83.69%) | Memory: 55.8 MB (79.03%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.java
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.java
@@ -1,14 +1,15 @@
 class Solution {
     public int longestArithSeqLength(int[] nums) {
         int n = nums.length;
-        int offset = 100;
+        int offset = 500;
         int[][] dp = new int[n][1001];
         for (int[] row : dp) Arrays.fill(row, 1);
         int longest = 0;
 
         for (int j = 0; j < n; ++j) {
+            dp[j][0] = 1;
             for (int i = 0; i < j; ++i) {
-                int diff = nums[j] - nums[i] + 500;
+                int diff = nums[j] - nums[i] + offset;
                 dp[j][diff] = Math.max(dp[j][diff], dp[i][diff] + 1);
                 longest = Math.max(longest, dp[j][diff]);
             }

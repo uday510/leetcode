bb7157eb9d7d50cccd0fc79465a703ca47f1335b
Uday <BUDAYTEJA009@GMAIL.COM>
2025-06-26 15:44:58 +0530

    Time: 41 ms (58.41%) | Memory: 44 MB (94.02%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,27 +1,20 @@
 class Solution {
-    int n;
-    List<Integer> lis;
     public int lengthOfLIS(int[] nums) {
-        n = nums.length;
-        lis = new ArrayList<Integer>();
+        int n = nums.length;
+        int[] dp = new int[n];
+        int longest = 1;
 
         for (int i = 0; i < n; ++i) {
-            int index = bisectLeft(nums[i]);
-            if (index == lis.size()) lis.add(nums[i]);
-            lis.set(index, nums[i]);
+            dp[i] = 1;
+
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+            longest = Math.max(longest, dp[i]);
         } 
-        
-        return lis.size();
-    }
-    private int bisectLeft(int target) {
-        int leftIdx = 0, rightIdx = lis.size();
+        return longest;
 
-        while (leftIdx < rightIdx) {
-            int midIdx = (leftIdx + rightIdx) >> 1;
-            if (lis.get(midIdx) < target) leftIdx = midIdx + 1;
-            else rightIdx = midIdx;
-        }
-
-        return leftIdx;
     }
 }

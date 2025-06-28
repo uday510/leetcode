1c82c740164dc618a03b2a3534039c27934e3d6b
Uday <BUDAYTEJA009@GMAIL.COM>
2025-06-28 13:58:19 +0530

    Time: 49 ms (42.76%) | Memory: 101.2 MB (83.86%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -1,46 +1,41 @@
 class Solution {
-    int n;
-    List<Integer> lis;
     public int maxEnvelopes(int[][] en) {
-        n = en.length;
-
+        int n = en.length;
         Arrays.sort(en, (a, b) -> {
             if (a[0] == b[0]) return b[1] - a[1];
             return a[0] - b[0];
         });
 
+        List<Integer> lis = new ArrayList<>();
         int[] arr = new int[n];
+
         for (int i = 0; i < n; ++i) arr[i] = en[i][1];
 
-        lis = new ArrayList<>();
         for (int i = 0; i < n; ++i) {
-            int index = bisectLeft(arr[i]);
+            int index = bisectLeft(lis, arr[i]);
             if (index == lis.size()) lis.add(arr[i]);
             lis.set(index, arr[i]);
         }
 
-        System.out.println(lis);
         return lis.size();
     }
-    private int bisectLeft(int target) {
-        int i = 0, j = lis.size();
 
-        while (i < j) {
-            int mid = (i + j) >> 1;
-            if (lis.get(mid) < target) i = mid + 1;
-            else j = mid;
+    private int bisectLeft(List<Integer> lis, int target) {
+        int leftIdx = 0, rightIdx = lis.size();
+
+        while (leftIdx < rightIdx) {
+            int midIdx = (leftIdx + rightIdx) >> 1;
+            if (lis.get(midIdx) < target) leftIdx = midIdx + 1;
+            else rightIdx = midIdx;
         }
-
-        return i;
+        return leftIdx;
     }
-
 }
+
 
 /**
 
+[1, 2]
 
-[3, 3, 4, 4, 7]
-
-[3, 7]
 
  */

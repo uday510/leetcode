2f569b2d9bc703b23aaf69a358ee88866c8e9c9e
Uday <BUDAYTEJA009@GMAIL.COM>
2025-06-28 15:22:30 +0530

    Time: 22 ms (44.79%) | Memory: 50.6 MB (86.71%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -11,17 +11,19 @@ class Solution {
         s2 = text2;
         dp = new int[n + 1][m + 1];
 
-        for (int i = n - 1; i > -1; --i) {
-            for (int j = m - 1; j > -1; --j) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
                 if (s1.charAt(i) == s2.charAt(j)) {
-                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                    dp[i][j] = 1 + (i > 0 && j > 0 ? dp[i - 1][j - 1] : 0);
                 } else {
-                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                    int p1 = (i > 0 ? dp[i - 1][j] : 0);
+                    int p2 = (j > 0 ? dp[i][j - 1] : 0);
+                    dp[i][j] = Math.max(p1, p2);
                 }
             }
         }
         
-        return dp[0][0];
+       return dp[n - 1][m - 1];
     }
     private int dfs(int i, int j) {
         if (i >= n || j >= m) return 0;

bbe2f8b1b5fc3f145f76a034c445b4f5de34727c
Uday <BUDAYTEJA009@GMAIL.COM>
2025-06-28 15:25:12 +0530

    Time: 20 ms (74.37%) | Memory: 51 MB (42.75%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,9 +1,9 @@
 class Solution {
-    
+
     int n, m;
     String s1, s2;
     int[][] dp;
-    
+
     public int longestCommonSubsequence(String text1, String text2) {
         n = text1.length();
         m = text2.length();
@@ -11,31 +11,43 @@ class Solution {
         s2 = text2;
         dp = new int[n + 1][m + 1];
 
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (s1.charAt(i) == s2.charAt(j)) {
-                    dp[i][j] = 1 + (i > 0 && j > 0 ? dp[i - 1][j - 1] : 0);
+//        for (int i = n - 1; i > -1; --i) {
+//            for (int j = m - 1; j > -1; --j) {
+//                if (s1.charAt(i) == s2.charAt(j)) {
+//                    dp[i][j] = 1 + dp[i + 1][j + 1];
+//                } else {
+//                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+//                }
+//            }
+//        }
+//
+//        return dp[0][0];
+
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
-                    int p1 = (i > 0 ? dp[i - 1][j] : 0);
-                    int p2 = (j > 0 ? dp[i][j - 1] : 0);
-                    dp[i][j] = Math.max(p1, p2);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        
-       return dp[n - 1][m - 1];
+
+       return dp[n][m];
+
     }
     private int dfs(int i, int j) {
         if (i >= n || j >= m) return 0;
-        
+
         if (dp[i][j] != -1) return dp[i][j];
-        
+
         if (s1.charAt(i) == s2.charAt(j)) {
             dp[i][j] = 1 + dfs(i + 1, j + 1);
         } else {
             dp[i][j] = Math.max(dfs(i, j + 1), dfs(i + 1, j));
         }
-        
+
         return dp[i][j];
     }
 }

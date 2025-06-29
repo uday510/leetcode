2af82c6f27753c5e2b46a2e2d7f2105cd8df879e
Uday <BUDAYTEJA009@GMAIL.COM>
2025-06-29 20:44:09 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (48.83%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.java
+++ 337-house-robber-iii/house-robber-iii.java
@@ -1,19 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public int rob(TreeNode root) {
-        int[] result = dfs(root);
-        return Math.max(result[0], result[1]);
+        int[] res = dfs(root);
+
+        return Math.max(res[0], res[1]);
     }
+
     private int[] dfs(TreeNode node) {
-        if (node == null) {
-            return new int[]{0, 0};
-        }
+        if (node == null) return new int[] {0, 0};
 
         int[] left = dfs(node.left);
         int[] right = dfs(node.right);
 
         int pick = node.val + left[1] + right[1];
-        int dont = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
 
-        return new int[]{pick, dont};
+        int skip = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+
+        return new int[] {pick, skip};
     }
 }

b69c5f21da42871dd214ed9cd9934d1929a4b0f2
Uday <BUDAYTEJA009@GMAIL.COM>
2025-07-02 15:14:41 +0530

    Time: 5 ms (96.60%) | Memory: 108.4 MB (63.14%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -1,25 +1,19 @@
 class Solution {
+    public long mostPoints(int[][] ques) {
+        int n = ques.length;
+        long[] dp = new long[n + 1];
 
-    long[] dp;
-    int[][] ques;
-    public long mostPoints(int[][] questions) {
-        ques = questions;
-        dp = new long[questions.length];
-        Arrays.fill(dp, -1L);
+        for (int i = n - 1; i > -1; --i) {
+            int points = ques[i][0];
+            int skip = ques[i][1];
+            int skipIdx = i + skip + 1;
 
-        return dfs(0);
+            long inc = points + (skipIdx < n ? dp[skipIdx] : 0);
+            long exc = dp[i + 1];
+
+            dp[i] = Math.max(inc, exc);
+        }
+
+        return dp[0];
     }
-
-    private long dfs(int i) {
-        if (i >= ques.length) return 0L;
-
-        if (dp[i] != -1L) return dp[i];
-
-        long exc = dfs(i + 1);
-
-        long inc = (long) ques[i][0] + dfs(i + ques[i][1] + 1);
-
-        return dp[i] = (long) Math.max(inc, exc);
-    }
-    
 }

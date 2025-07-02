ed61e9122d8082374f1e65c8bff6fa8a02fa687e
Uday <BUDAYTEJA009@GMAIL.COM>
2025-07-02 14:28:09 +0530

    Time: 9 ms (63.79%) | Memory: 44.6 MB (35.80%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,16 +1,24 @@
 class Solution {
+    int[][] dp;
+    int offset;
+
     public int findTargetSumWays(int[] nums, int target) {
+        offset = Arrays.stream(nums).sum();
+        dp = new int[nums.length][2 * offset + 1];
+        for (int[] row : dp) Arrays.fill(row, Integer.MIN_VALUE);
         return dfs(0, 0, nums, target);
     }
 
     private int dfs(int curr, int idx, int[] nums, int target) {
         if (curr == target && idx == nums.length) return 1;
         if (idx >= nums.length) return 0;
+        if (dp[idx][curr + offset] != Integer.MIN_VALUE) return dp[idx][curr + offset];
 
         int pos = dfs(curr + nums[idx], idx + 1, nums, target);
 
         int neg = dfs(curr - nums[idx], idx + 1, nums, target);
 
-        return pos + neg;
+        return dp[idx][curr + offset] = pos + neg;
     }
+
 }

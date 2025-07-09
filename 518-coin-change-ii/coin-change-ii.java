169c210f6028c51ab92951d8094e96617b832648
Uday <BUDAYTEJA009@GMAIL.COM>
2025-07-09 14:34:14 +0530

    Time: 23 ms (11.81%) | Memory: 49.4 MB (26.06%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    int[][] dp;
+    int[] coins;
+    int n;
+
+    public int change(int amount, int[] coins) {
+        this.coins = coins;
+        this.n = coins.length;
+        dp = new int[n][amount + 1];
+
+        for (int[] row : dp) Arrays.fill(row, -1);
+
+        return dfs(0, amount);
+    }
+    private int dfs(int i, int rem) {
+        if (rem == 0) return 1;
+        if (rem < 0 || i >= coins.length) return 0;
+        
+        if (dp[i][rem] != -1) return dp[i][rem];
+
+        int inc = dfs(i, rem - coins[i]);
+        int exc = dfs(i + 1, rem);
+
+        return dp[i][rem] = inc + exc;
+    }
+}

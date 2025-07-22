522d80e230b9f4e758788572515540ec778c12ac
Uday <BUDAYTEJA009@GMAIL.COM>
2025-07-22 16:10:51 +0530

    Time: 1 ms (90.04%) | Memory: 43.4 MB (19.78%) - LeetSync

--- 256-paint-house/paint-house.java
+++ 256-paint-house/paint-house.java
@@ -1,27 +1,36 @@
 class Solution {
+
     int[][] costs;
     int[][] dp;
+    int n;
+
     public int minCost(int[][] costs) {
         this.costs = costs;
-        dp = new int[costs.length][4];
-        for (int[] row :dp) Arrays.fill(row, -1);
+        n = costs.length;
+        this.dp = new int[n][3];
+        
+        for (int[] row : dp) Arrays.fill(row, -1);
 
-        return dfs(0, -1);
+        return Math.min(dfs(0, 0), Math.min(dfs(0, 1), dfs(0, 2)));
     }
 
-    private int dfs(int i, int exc) {
-        if (i >= costs.length) return 0;
+    private int dfs(int i, int color) {
+        if (i >= n) return 0;
 
-        if (dp[i][exc + 1] != -1) return dp[i][exc + 1];
+        if (dp[i][color] != -1) return dp[i][color];
 
         int curr = (int) 1e9;
 
-        for (int j = 0; j < costs[i].length; ++j) {
-            if (j == exc) continue;
-
-            curr = Math.min(curr, costs[i][j] + dfs(i + 1, j));
+        if (color == 0) {
+            curr = Math.min(dfs(i + 1, 1), dfs(i + 1, 2));
+        } else if (color == 1) {
+            curr = Math.min(dfs(i + 1, 0), dfs(i + 1, 2));
+        } else if (color == 2) {
+            curr = Math.min(dfs(i + 1, 0), dfs(i + 1, 1));
         }
 
-        return dp[i][exc + 1] = curr;
+        curr += costs[i][color];
+
+        return dp[i][color] = curr;
     }
 }

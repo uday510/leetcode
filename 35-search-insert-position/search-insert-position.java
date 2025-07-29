748f26c89218cee19b97eab312dd5828d2f22a1d
Uday <BUDAYTEJA009@GMAIL.COM>
2025-07-29 15:24:32 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (50.92%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,22 +1,18 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
+        
         return bs(nums, target);
     }
-
     private int bs(int[] nums, int target) {
-        int leftIdx = 0;
-        int rightIdx = nums.length;
+        int l = 0, r = nums.length;
 
-        while (leftIdx < rightIdx) {
-            int midIdx = (leftIdx + rightIdx) >> 1;
+        while (l < r) {
+            int mid = (l + r) >> 1;
 
-            if (nums[midIdx] < target) {
-                leftIdx = midIdx + 1;
-            } else {
-                rightIdx = midIdx;
-            }
+            if (nums[mid] < target) l = mid + 1;
+            else r = mid;
         }
 
-        return leftIdx;
+        return l;
     }
 }

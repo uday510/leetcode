2b219d631d594a1899c310a7ae9e72e3bb82e17e
Uday <BUDAYTEJA009@GMAIL.COM>
2025-07-29 13:58:23 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (46.33%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,6 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return bs(nums, target);
+        return bisectLeft(nums, target);
+    }
+
+    private int bisectLeft(int[] nums, int target) {
+        int l = 0, r = nums.length;
+
+        while (l < r) {
+            int mid = (l + r) >> 1;
+
+            if (nums[mid] < target) l = mid + 1;
+            else r = mid;
+        }
+
+        return l < nums.length && nums[l] == target ? l : -1;
     }
 
     private int bs(int[] nums, int target) {

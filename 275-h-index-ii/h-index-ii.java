d0a2ea6c00df492ca079b47316424e92214e01ce
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-05 16:38:09 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (13.06%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int l = 0, r = citations.length;
+        int n = citations.length;
+
+        while (l < r) {
+            int m = (l + r ) >> 1;
+
+            int h = n - m;
+            if (citations[m] < h) l = m + 1;
+            else r = m;
+        }
+
+        return n - l;
+    }
+}

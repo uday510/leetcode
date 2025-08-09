1eceee80418ab5a44f2c2bb01228ac2ea8eb62d9
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-09 20:28:56 +0530

    Time: 3 ms (60.59%) | Memory: 63 MB (5.36%) - LeetSync

--- 3954-maximum-balanced-shipments/maximum-balanced-shipments.java
+++ 3954-maximum-balanced-shipments/maximum-balanced-shipments.java
@@ -1,13 +1,19 @@
 class Solution {
     public int maxBalancedShipments(int[] weight) {
+
         int n = weight.length;
         int cnt = 0;
+        
+        for (int i = 0; i < weight.length; ++i) {
+            int j = i + 1;
+            while (j < n && weight[j] >= weight[j - 1]) j++;
 
-        for (int i = 0; i < weight.length - 1; ++i) {
-            if (weight[i] > weight[i + 1]) {
-                cnt++;
-                i++;
-            }
+            int len = j - i + 1;
+            if (j == n) return cnt;
+
+            cnt += len > 1 && weight[j] < weight[j - 1] ? 1 : 0;
+
+            i = j;
         }
 
         return cnt;

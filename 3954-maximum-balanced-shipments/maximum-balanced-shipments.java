a7b4866419dddf24db0de1cdae9fa602e64bbf5c
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-09 20:23:35 +0530

    Time: 3 ms (60.59%) | Memory: 62.5 MB (43.95%) - LeetSync

--- 3954-maximum-balanced-shipments/maximum-balanced-shipments.java
+++ 3954-maximum-balanced-shipments/maximum-balanced-shipments.java
@@ -6,15 +6,13 @@ class Solution {
         
         for (int i = 0; i < weight.length; ++i) {
             int j = i + 1;
-            // List<Integer> list = new ArrayList<>();
-            // list.add(weight[i]);
+            while (j < n && weight[j] >= weight[j - 1]) j++;
 
-            while (j < n && weight[j] >= weight[j - 1]) {
-                j++;
-            }
+            int len = j - i + 1;
+            if (j == n) return cnt;
 
-            int size = (j - i + 1);
-            cnt += size > 1 && j < n && weight[j] < weight[j - 1] ? 1 : 0;
+            cnt += len > 1 && weight[j] < weight[j - 1] ? 1 : 0;
+
             i = j;
         }
 

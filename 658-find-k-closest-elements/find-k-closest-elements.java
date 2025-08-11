c1df7b5fde3a55431f5887b92a4d7be84664af62
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-11 14:12:54 +0530

    Time: 5 ms (55.49%) | Memory: 46 MB (62.58%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,55 +1,36 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        List<Integer> result = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<>();
+        int n = arr.length;
+        int l = 0;
+        int r = n;
         
-        // // Base case
-        // if (arr.length == k) {
-        //     for (int i = 0; i < k; i++) {
-        //         result.add(arr[i]);
-        //     }
-            
-        //     return result;
-        // }
-        
-        // Binary search to find the closest element
-        int left = 0;
-        int right = arr.length;
-        int mid = 0;
-        while (left < right) {
-            mid = (left + right) / 2;
-            if (arr[mid] >= x) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
+        while (l < r) {
+            int m = (l + r) >> 1;
+
+            if (arr[m] < x) l = m + 1;
+            else r = m;
         }
-        
-        // Initialize our sliding window's bounds
-        left -= 1;
-        right = left + 1;
-        
-        // While the window size is less than k
-        while (right - left - 1 < k) {
-            // Be careful to not go out of bounds
-            if (left == -1) {
-                right += 1;
+
+        r = l;
+
+        while (r - l - 1 < k) {
+            if (l == -1) {
+                r++;
                 continue;
             }
-            
-            // Expand the window towards the side with the closer number
-            // Be careful to not go out of bounds with the pointers
-            if (right == arr.length || Math.abs(arr[left] - x) <= Math.abs(arr[right] - x)) {
-                left -= 1;
-            } else {
-                right += 1;
-            }
-        } 
 
-        // Build and return the window
-        for (int i = left + 1; i < right; i++) {
-            result.add(arr[i]);
+            if (r == n || Math.abs(arr[l] - x) <= Math.abs(arr[r] - x)) {
+                l--;
+            } else {
+                r++;
+            }
         }
-        
-        return result;
+
+        for (int i = l + 1; i < r; ++i) {
+            list.add(arr[i]);
+        }
+
+        return list;
     }
 }

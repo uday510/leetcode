93d4891440accc4e6180eff383bf92aa9925e309
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-11 17:17:51 +0530

    Time: 2 ms (100.00%) | Memory: 41.8 MB (69.66%) - LeetSync

---
+++ 1083-two-sum-less-than-k/two-sum-less-than-k.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int twoSumLessThanK(int[] nums, int k) {
+        Arrays.sort(nums);
+        int l = 0;
+        int r = nums.length - 1;
+        int max = -1;
+
+        while (l < r) {
+            int curr = nums[l] + nums[r];
+            if (curr < k) l++;
+            else r--;
+            if (curr < k) max = Math.max(max, curr);  
+        }
+
+        return max;
+    }
+}
+
+// 10 20 30

7f4494bdcb6e1f1398867c12a0bfe88d8712df92
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-14 16:26:27 +0530

    Time: 53 ms (34.87%) | Memory: 100.5 MB (85.91%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -1,43 +1,48 @@
 class Solution {
-    public int maxEnvelopes(int[][] en) {
-        int n = en.length;
-        Arrays.sort(en, (a, b) -> {
+    List<Integer> lis;
+    public int maxEnvelopes(int[][] arr) {
+        int n = arr.length;
+
+        Arrays.sort(arr, (a, b) -> {
             if (a[0] == b[0]) return b[1] - a[1];
             return a[0] - b[0];
         });
 
-        List<Integer> lis = new ArrayList<>();
-        int[] arr = new int[n];
+        lis = new ArrayList<>();
 
         for (int i = 0; i < n; ++i) {
-            arr[i] = en[i][1];
-        }
-
-        for (int i = 0; i < n; ++i) {
-            int index = bisectLeft(lis, arr[i]);
-            if (index == lis.size()) lis.add(arr[i]);
-            lis.set(index, arr[i]);
+            int idx = bs(arr[i][1]);
+            if (idx == lis.size()) lis.add(arr[i][1]);
+            lis.set(idx, arr[i][1]);
         }
 
         return lis.size();
     }
+    private int bs(int target) {
+        int l = 0, r = lis.size();
 
-    private int bisectLeft(List<Integer> lis, int target) {
-        int leftIdx = 0, rightIdx = lis.size();
-
-        while (leftIdx < rightIdx) {
-            int midIdx = (leftIdx + rightIdx) >> 1;
-            if (lis.get(midIdx) < target) leftIdx = midIdx + 1;
-            else rightIdx = midIdx;
+        while (l < r) {
+            int m = (l + r) >> 1;
+            if (lis.get(m) < target) l = m + 1;
+            else r = m;
         }
-        return leftIdx;
+        
+        return l;
     }
 }
 
 
 /**
 
-[1, 2]
+// eg: envelopes = [[4,5],[4,6],[6,7],[2,3],[1,1]]
+
+
+Consider an input [[1, 3], [1, 4], [1, 5], [2, 3]].
+If we simply sort and extract the second dimension we get [3, 4, 5, 3], which implies that we can fit three envelopes (3, 4, 5). The problem is that we can only fit one envelope, since envelopes that are equal in the first dimension can't be put into each other.
+
+In order fix this, we don't just sort increasing in the first dimension - we also sort decreasing on the second dimension, so two envelopes that are equal in the first dimension can never be in the same increasing subsequence.
+
+Now when we sort and extract the second element from the input we get [5, 4, 3, 3], which correctly reflects an LIS of one.
 
 
  */

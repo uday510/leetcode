a5c8796a5046d8e92b3595eae393940c60959692
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-15 12:59:40 +0530

    Time: 11 ms (76.31%) | Memory: 47 MB (93.67%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,13 +1,15 @@
 class Solution {
+    
     public int shipWithinDays(int[] weights, int days) {
         int l = 0, r = 0;
+
         for (int w : weights) {
-            l = Math.max(w, l);
+            l = Math.min(l, w);
             r += w;
         }
 
         while (l < r) {
-            int m = (l + r) >> 1;
+            int m = l + ((r - l) >> 1);
             if (canShip(weights, m, days)) r = m;
             else l = m + 1;
         }
@@ -15,22 +17,20 @@ class Solution {
         return l;
     }
 
-    private boolean canShip(int[] weights, int perDayWeight, int days) {
-        int currDays = 1;
-        int currWeight = 0;
+    private boolean canShip(int[] w, int m, int d) {
+        int curr = 0;
+        int days = 1;
 
-        for (int w : weights) {
+        for (int i = 0; i < w.length && days <= d; ++i) {
+            if (w[i] > m) return false;
+            if (curr + w[i] > m) {
+                curr = w[i];
+                days++;
+            } else curr += w[i];
 
-            if (w > perDayWeight) return false;
-
-            if (currWeight + w > perDayWeight) {
-                currDays += 1;
-                currWeight = w;
-            } else currWeight += w;
-            
-            if (currDays > days) return false;
         }
 
-        return currDays <= days;
+        return days <= d;
     }
+
 }

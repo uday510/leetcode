cf7f6edda4b80312688061b18f39887ccd03ace6
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-15 17:21:26 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (77.96%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,8 +1,6 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
         if (n == 1) return true;
-        if (n % 4 != 0 || n <= 0) return false;
-        
-        return isPowerOfFour(n/4);
+        return n > 1 && n % 4 == 0 && isPowerOfFour(n/4);
     }
 }

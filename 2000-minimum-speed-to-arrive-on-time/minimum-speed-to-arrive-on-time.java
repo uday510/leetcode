17af0de336f9cf24c3ea6928703c7f3f0c582bb4
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-15 16:07:58 +0530

    Time: 40 ms (92.09%) | Memory: 57.4 MB (31.99%) - LeetSync

--- 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.java
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.java
@@ -1,20 +1,20 @@
 class Solution {
 
     int[] dist;
+    double hr;
     int n;
-    double hrs;
 
     public int minSpeedOnTime(int[] dist, double hour) {
         this.dist = dist;
+        hr = hour;
         this.n = dist.length;
-        hrs = hour;
 
-        int l = 1;
-        int r = 1;
+        int l = 1, r = 1;
         while (!canReach(r)) r <<= 1;
 
         while (l < r) {
             int m = l + ((r - l) >> 1);
+
             if (canReach(m)) r = m;
             else l = m + 1;
         }
@@ -22,16 +22,13 @@ class Solution {
         return canReach(l) ? l : -1;
     }
 
-    private boolean canReach(int k) {
+    private boolean canReach(int m) {
         double h = 0.0;
 
-        for (int i = 0; i < n - 1; ++i) {
-            int d = dist[i];
-            h += (d + k - 1) / k;
-        }
+        for (int i = 0; i < n - 1; ++i) h += (dist[i] + m - 1) / m;
 
-        h += (double) dist[n - 1] / k;
+        h += (double) dist[n - 1] / m;
 
-        return h <= hrs;
+        return h <= hr;
     }
 }

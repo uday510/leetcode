32ed0eaf5d7b5731d2df6a89c1f53e81a7b1e092
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-15 15:52:03 +0530

    Time: 10 ms (70.11%) | Memory: 45.8 MB (5.25%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,25 +1,30 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         
-        int l = 1;
-        int r = Arrays.stream(piles).max().getAsInt();
+        int minBananasPerHr = 1;
+        int maxBananasPerHr = 1;
+        // Arrays.stream(piles).max().getAsInt();
 
-        while (l < r) {
-            int m = (l + r) >> 1;
+        while (!canEatAllBananasWithKPerHr(piles, maxBananasPerHr, h)) maxBananasPerHr <<= 2;
 
-            if (canEat(m, piles, h)) r = m;
-            else l = m + 1;
+        while (minBananasPerHr < maxBananasPerHr) {
+
+            int k = (minBananasPerHr + maxBananasPerHr) >> 1;
+
+            if (canEatAllBananasWithKPerHr(piles, k, h)) maxBananasPerHr = k; // reduce time
+            else minBananasPerHr = k + 1;
         }
 
-        return l;
+        return minBananasPerHr;
     }
-    
-    private boolean canEat(int k, int[] piles, int reqHrs) {
+    private boolean canEatAllBananasWithKPerHr(int[] piles, int k, int reqHrs) {
+        int currHrs = 0;
 
-        for (int i = 0; i < piles.length && reqHrs >= 0; ++i) {
-            reqHrs -= Math.ceil((double) piles[i] / k);
+        for (int pile : piles) {
+            currHrs += (int) Math.ceil(( (double) pile / k));
+            if (currHrs > reqHrs) return false;
         }
 
-        return reqHrs >= 0;
-    }
+        return currHrs <= reqHrs;
+    } 
 }

ea1cb4283b1bd4fc2bf77801c3c986f41a4dc08d
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-16 16:13:48 +0530

    Time: 5 ms (70.51%) | Memory: 45.1 MB (64.10%) - LeetSync

---
+++ 788-minimize-max-distance-to-gas-station/minimize-max-distance-to-gas-station.java
@@ -0,0 +1,29 @@
+class Solution {
+    public double minmaxGasDist(int[] stations, int k) {
+        double l = 0;
+        double r = 1e8;
+        double res = -1;
+
+        while (r - l > 1e-6) {
+            double m = (l + r) / 2.0;
+            if (isPossible(m, stations, k)) {
+                res = m;
+                r = m;
+            }
+            else l = m;
+        }
+
+        return res;
+    }
+
+    private boolean isPossible(double limit, int[] stations, int k) {
+        int curr = 0;
+
+        for (int i = 1; i < stations.length && curr <= k; ++i) {
+            double d = stations[i] - stations[i - 1];
+            if(d > limit) curr += (int) ( d/ limit);
+        }
+
+        return curr <= k;
+    }
+}

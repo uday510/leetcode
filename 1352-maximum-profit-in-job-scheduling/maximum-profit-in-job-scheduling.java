2554450c0a4f40e3880064ec70362feddf104636
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-16 17:14:01 +0530

    Time: 35 ms (65.46%) | Memory: 62.3 MB (27.84%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
@@ -1,17 +1,17 @@
 class Solution {
 
+    int[][] jobs;
     int[] dp;
-    int[][] jobs; 
     int n;
 
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        this.n = profit.length;
+        n = startTime.length;
         jobs = new int[n][3];
         dp = new int[n];
-        
-        Arrays.fill(dp, -1);
 
         for (int i = 0; i < n; ++i) {
+            dp[i] = -1;
+            jobs[i] = new int[3];
             jobs[i][0] = startTime[i];
             jobs[i][1] = endTime[i];
             jobs[i][2] = profit[i];
@@ -19,36 +19,32 @@ class Solution {
 
         Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
 
-        return dfs(0);
+        return dfs(0);    
     }
 
-    private int dfs(int idx) {
-        if (idx >= n) return 0;
-        
-        if (dp[idx] != -1) return dp[idx];
+    private int dfs(int i) {
+        if (i >= n) return 0;
 
-        int skip = dfs(idx + 1);
-        int take = jobs[idx][2] + dfs(bs(jobs[idx][1]));
-        
-        return dp[idx] = Math.max(skip, take);
+        if (dp[i] != -1) return dp[i];
+
+        int skip = dfs(i + 1);
+        int take = jobs[i][2] + dfs(bs(i, jobs[i][1]));
+
+        return dp[i] = Math.max(skip, take);
     }
 
-    private int bs(int end) {
-        // start has to >= end
-        int left = 0, right = n;
+    private int bs(int l, int target) {
+        int r = n;
 
-        while (left < right) {
-            int mid = (left + right) >> 1;
+        while (l < r) {
+            int m = l + ((r - l) >> 1);
 
-            int start = jobs[mid][0];
-            if (end > start) left = mid + 1;
-            else right = mid;
+           if (target > jobs[m][0]) l = m + 1;
+           else r = m;
         }
 
-        return left;
+        return l;
     }
-}
-
 
 // 0 -> [1, 3] ->  20
 // 1 -> [2, 5] ->  20
@@ -60,3 +56,4 @@ class Solution {
 // [[1, 3, 20], [2, 5, 20], [3, 10, 100], [4, 6, 70], [6, 9, 60]]
 
 // end: 3
+}

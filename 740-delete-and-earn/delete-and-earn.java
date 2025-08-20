013cd00d97f11fe601d20098bc37ada5d87283a8
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-20 17:53:21 +0530

    Time: 10 ms (38.52%) | Memory: 44.2 MB (89.90%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -9,20 +9,16 @@ class Solution {
             points.merge(num, num, Integer::sum);
         }
 
-        int[] dp = new int[max + 1];
-        dp[1] = points.getOrDefault(1, 0);
-        
-        int maxPoints = dp[1];
+        int p0 = 0;
+        int p1 = points.getOrDefault(1, 0);
 
         for (int i = 2; i <= max; ++i) {
-            int pick = points.getOrDefault(i, 0) + dp[i - 2];
-            int dont = dp[i - 1];
-            int curr = Math.max(pick, dont);
+            int p2 = Math.max(p1, p0 + points.getOrDefault(i, 0));
 
-            dp[i] = curr;
-            maxPoints = Math.max(curr, maxPoints);
+            p0 = p1;
+            p1 = p2;
         }
 
-        return maxPoints;
+        return p1;
     }
 }

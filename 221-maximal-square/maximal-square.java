6f789985a11f70772d6d54aaa9b6b91518a683e0
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-20 20:06:44 +0530

    Time: 9 ms (35.00%) | Memory: 60.5 MB (36.07%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,30 +1,23 @@
 class Solution {
-
-public int maximalSquare(char[][] matrix) {
-
-        int m = matrix.length;
-        int n = matrix[0].length;
-
+    public int maximalSquare(char[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
         int[][] dp = new int[m][n];
-        int largest = 0;
+        int maximalSquare = 0;
 
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 char ch = matrix[i][j];
                 if (ch == '0') continue;
-                dp[i][j] = 1;
-                
-                if (i > 0 && j > 0) {
-                    dp[i][j] += Math.min(
-                            dp[i-1][j-1],
-                            Math.min(dp[i-1][j], dp[i][j-1]));
-                }
 
-                largest = Math.max(largest, dp[i][j]);
+                int diagonal = 0, left = 0, up = 0;
+                if (i > 0) up = dp[i - 1][j];
+                if (j > 0) left = dp[i][j - 1];
+                if (i > 0 && j > 0) diagonal = dp[i - 1][j - 1];
+                dp[i][j] = 1 + Math.min(up, Math.min(left, diagonal));
+                maximalSquare = Math.max(maximalSquare, dp[i][j]);
             }
         }
 
-        return largest * largest;
+        return maximalSquare * maximalSquare;
     }
-
 }

9b57afe921df5ad3e019b26895819a79ec27eae9
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-21 20:47:33 +0530

    Time: 5 ms (27.13%) | Memory: 47.6 MB (55.88%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -5,11 +5,14 @@ class Solution {
 
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (i == 0) dp[i][j] = grid[i][j] + (j > 0 ? dp[i][j - 1] : 0);
-                else if (j == 0) dp[i][j] = grid[i][j] + (i > 0 ? dp[i - 1][j] : 0);
-                else {
-                    dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
-                }
+                dp[i][j] = grid[i][j];
+
+                if (i != 0 && j != 0) {
+                    dp[i][j] += Math.min(dp[i - 1][j], dp[i][j - 1]);
+                } 
+
+                else if (i > 0) dp[i][j] += dp[i - 1][j];
+                else if (j > 0) dp[i][j] += dp[i][j - 1];
             }
         }
         return dp[m - 1][n - 1];

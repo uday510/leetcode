d21117e3c42d25f263ef8e2f5f75c7836debe20e
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-23 13:53:18 +0530

    Time: 3 ms (94.37%) | Memory: 44.8 MB (93.32%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,34 +1,32 @@
 class Solution {
-
-    int m;
-    int n;
-
+    int m, n;
+    String s1, s2;
     int[][] dp;
-
     public int minDistance(String word1, String word2) {
         m = word1.length();
         n = word2.length();
-
+        s1 = word1;
+        s2 = word2;
         dp = new int[m][n];
-        for (var row : dp) Arrays.fill(row, -1);
+        for (int[] row : dp) Arrays.fill(row, -1);
 
-        return dfs(0, 0, word1, word2);
+        return dfs(0, 0);
     }
-    private int dfs(int i, int j, String s1, String s2) {
-        if (i >= s1.length()) return n - j;
 
-        if (j >= s2.length()) return m - i;
+    private int dfs(int i, int j) {
+        if (i >= m && j >= n) return 0;
+        if (i >= m) return n - j;
+        if (j >= n) return m - i;
 
         if (dp[i][j] != -1) return dp[i][j];
 
-        if (s1.charAt(i) == s2.charAt(j)) {
-            return dp[i][j] = dfs(i + 1, j + 1, s1, s2);
-        };
+        if (s1.charAt(i) == s2.charAt(j))
+             return dp[i][j] = dfs(i + 1, j + 1);
 
-        int ins = dfs(i, j + 1, s1, s2);
-        int del = dfs(i + 1, j, s1, s2);
-        int rep = dfs(i + 1, j + 1, s1, s2);
+        int ins = dfs(i, j + 1);
+        int del = dfs(i + 1, j);
+        int repl = dfs(i + 1, j + 1);
 
-        return dp[i][j] = 1 + Math.min(ins, Math.min(del, rep));
+        return dp[i][j] = 1 + Math.min(ins, Math.min(del, repl));
     }
 }

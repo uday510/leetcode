8500ded0d54261c1565c702c1969ecb470a1e8c6
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-23 15:25:04 +0530

    Time: 24 ms (34.47%) | Memory: 44.9 MB (52.90%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,43 +1,37 @@
 class Solution {
-    int m;
-    int n;
+    String s1;
+    String s2;
+    int m, n;
     int[][] dp;
-
     public int minimumDeleteSum(String s1, String s2) {
+        this.s1 = s1;
+        this.s2 = s2;
         m = s1.length();
         n = s2.length();
         dp = new int[m][n];
 
-        for (var row : dp) Arrays.fill(row, -1);
+        for (int[] row : dp) Arrays.fill(row, -1);
 
-        return dfs(0, 0, s1, s2);
+        return dfs(0, 0);
     }
-
-    private int dfs(int i, int j, String s1, String s2) {
+    private int dfs(int i, int j) {
         if (i >= m && j >= n) return 0;
-
-        int curr = 0;
-        if (i >= m) {
-            for (; j < n; ++j) curr += s2.charAt(j);
-            return curr;
-        }
-
-        if (j >= n) {
-            for (; i < m; ++i) curr += s1.charAt(i);
-            return curr;
-        }
+        if (i >= m) return asciiSum(s2, j);
+        if (j >= n) return asciiSum(s1, i);
 
         if (dp[i][j] != -1) return dp[i][j];
 
-        if (s1.charAt(i) == s2.charAt(j)) {
-            dp[i][j] = dfs(i + 1, j + 1, s1, s2);
-        } else {
-            dp[i][j] = Math.min(
-                s1.charAt(i) + dfs(i + 1, j, s1, s2),
-                s2.charAt(j) + dfs(i, j + 1, s1, s2)
+        if (s1.charAt(i) == s2.charAt(j)) return dp[i][j] = dfs(i + 1, j + 1);
+    
+        return dp[i][j] = Math.min(
+            s1.charAt(i) + dfs(i + 1, j), 
+            s2.charAt(j) + dfs(i, j + 1)
             );
-        }
+    }
 
-        return dp[i][j];
+    private int asciiSum(String s, int idx) {
+        int sum = 0;
+        while (idx < s.length()) sum += s.charAt(idx++);
+        return sum;
     }
 }

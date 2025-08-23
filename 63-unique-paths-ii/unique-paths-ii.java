13efa70ef25ecbf1ec487873fbe4f47579700210
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-23 10:11:25 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (27.57%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,31 +1,40 @@
 class Solution {
-
-    int m, n;
+    int m;
+    int n;
+    int[][] arr;
     int[][] dp;
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        m = obstacleGrid.length;
-        n = obstacleGrid[0].length;
+    public int uniquePathsWithObstacles(int[][] arr) {
+        m = arr.length;
+        n = arr[0].length;
+        this.arr = arr;
         dp = new int[m][n];
 
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
+        for (int[] row : dp) Arrays.fill(row, -1);
 
-        return dfs(0, 0, obstacleGrid);
+        return dfs(0, 0);
     }
 
-    private int dfs(int i, int j, int[][] grid) {
-        if (i >= m || j >= n) return 0;
+    private int dfs(int i, int j) {
+        if (isOutOfBounds(i, j) || isObstacle(i, j)) return 0;
+        if (isDestination(i, j)) return isObstacle(i, j) ? 0 : 1;
+        if (isCached(i, j)) return dp[i][j];
 
-        if (i == m - 1 && j == n - 1) {
-            if (grid[i][j] == 0) return 1;
-            return 0;
-        }
+        return dp[i][j] = dfs(i + 1, j) + dfs(i, j + 1);
+    }
 
-        if (grid[i][j] == 1) return 0;
+    private boolean isOutOfBounds(int i, int j) {
+        return i >= m || j >= n;
+    }
 
-        if (dp[i][j] != -1) return dp[i][j];
+    private boolean isObstacle(int i, int j) {
+        return arr[i][j] == 1;
+    }
 
-        return dp[i][j] = dfs(i + 1, j, grid) + dfs(i, j + 1, grid);
+    private boolean isDestination(int i, int j) {
+        return i == m - 1 && j == n - 1;
+    }
+
+    private boolean isCached(int i, int j) {
+        return dp[i][j] != -1;
     }
 }

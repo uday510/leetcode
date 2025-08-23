4fa531bf8827b50075c312177847eb700101eb9e
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-23 11:57:22 +0530

    Time: 9 ms (35.30%) | Memory: 60.1 MB (75.30%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,24 +1,41 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        int m = matrix.length, n = matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
         int[][] dp = new int[m][n];
-        int maximalSquare = 0;
+
+        int maxLen = 0;
 
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                char ch = matrix[i][j];
-                if (ch == '0') continue;
+                if (matrix[i][j] == '0') continue;
                 
-                dp[i][j] = 1;
-                if (i > 0 && j > 0) {
-                    dp[i][j] += Math.min(
-                    dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
-                }
+                int up = 0, left = 0, dia = 0;
+                if (i > 0) up = dp[i - 1][j];
+                if (j > 0) left = dp[i][j - 1];
+                if (i > 0 && j > 0) dia = dp[i - 1][j - 1];
 
-                maximalSquare = Math.max(maximalSquare, dp[i][j]);
+                dp[i][j] = 1 + Math.min(up, Math.min(left, dia));
+                maxLen = Math.max(maxLen, dp[i][j]);
             }
         }
 
-        return maximalSquare * maximalSquare;
+        return maxLen * maxLen;
     }
 }
+
+/**
+
+
+
+[
+
+    ["1","1","1","0","0"]
+    ["1","2","2","1","1"]
+    ["1","2","1","1","1"]
+    ["1","0","0","1","0"]
+
+]
+
+
+ */

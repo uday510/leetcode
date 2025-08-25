8aed42c17a2b1eb9e872fe35c27258be457c6fd0
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-25 13:18:24 +0530

    Time: 484 ms (42.59%) | Memory: 76.2 MB (15.85%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.java
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.java
@@ -1,34 +1,23 @@
 class Solution {
     public int longestArithSeqLength(int[] nums) {
-        // int n = nums.length;
-        // Map<Integer, Integer>[] dp = new HashMap[n];
-        // int longest = 0;
-        
-        // for (int i = 0; i < n; ++i) {
-        //     dp[i] = new HashMap<>();
-        //     for (int j = 0; j < i; ++j) {
-        //       int diff = nums[i] - nums[j];
-        //       int len = dp[j].getOrDefault(diff, 1) + 1;
-        //       dp[i].put(diff, len);
-        //       longest = Math.max(longest, len);
-        //     }
-        // }
-
-        // return longest;
-
         int n = nums.length;
-        int offset = 500;
-        int[][] dp = new int[n][1001];
-        for (int[] row : dp) Arrays.fill(row, 1);
-        int longest = 2;
+        if (n <= 2) return n;
+
+        Map<Integer, Integer>[] dp = new HashMap[n];
+        int max = 1;
+
+        for (int i = 0; i < n; ++i) dp[i] = new HashMap<>();
 
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < i; ++j) {
-                int diff = nums[i] - nums[j] + offset;
-                dp[i][diff] = Math.max(dp[i][diff], dp[j][diff] + 1);
-                longest = Math.max(longest, dp[i][diff]);
+                int diff = nums[j] - nums[i];
+                int len = dp[j].getOrDefault(diff, 1) + 1;
+                int currMax = Math.max(dp[i].getOrDefault(diff, 0), len);
+                dp[i].put(diff, currMax);
+                max = Math.max(max, currMax);
             }
         }
-        return longest;
+
+        return max;
     }
 }

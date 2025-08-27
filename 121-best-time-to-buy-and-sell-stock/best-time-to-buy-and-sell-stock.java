cc4da0ce72734df0a046da08d851e359d4d334ae
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-27 19:26:42 +0530

    Time: 4 ms (2.42%) | Memory: 58.7 MB (97.79%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,18 +1,28 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int minBuy = (int) 1e9;
-        int maxProfit = 0;
+        int k = 1;
+        int n = prices.length;
+        int[][] dp = new int[k + 1][n];
 
-        for (int price : prices) {
-            minBuy = Math.min(minBuy, price);
 
-            int currProfit = price - minBuy;
-            if (currProfit > maxProfit) {
-                maxProfit = currProfit;
+        for (int txn = 1; txn <= k; ++txn) {
+            int max = -(int) 1e9;
+
+            for (int day = 1; day < n; ++day) {
+                
+                if (dp[txn - 1][day - 1] - prices[day - 1] > max) {
+                    max = dp[txn - 1][day - 1] - prices[day - 1];
+                }
+
+                if (max + prices[day] > dp[txn][day - 1]) {
+                    dp[txn][day] = max + prices[day];
+                } else {
+                    dp[txn][day] = dp[txn][day - 1];
+                }
+
             }
-
         }
 
-        return maxProfit;
+        return dp[k][n - 1];
     }
 }

a388751a953a4aaa5faf38ee9876494d077e74d3
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-29 10:21:27 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (63.33%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -3,12 +3,22 @@ class Solution {
         int[] dp = new int[n + 1];
         dp[0] = dp[1] = 1;
 
-        for (int i = 2; i <= n; ++i) {
+        for (int i = 2; i <= n; ++i) { // root i
             for (int j = 1; j <= i; ++j) {
-                dp[i] += dp[j - 1] * dp[i - j];
+                int l = dp[j - 1]; // left bsts, root j
+                int r = dp[i - j]; // right bsts, root j
+
+                dp[i] += l * r;
             }
         }
 
         return dp[n];
     }
 }
+
+/**
+
+    3
+
+
+ */

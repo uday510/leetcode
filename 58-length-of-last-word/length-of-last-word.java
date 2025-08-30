23bc11f507f516550ca6275407b2155b7362b119
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-30 16:26:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (19.07%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,18 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int N = s.length();
-        int i = N-1;
-        int cnt = 0;
+        int n = s.length();
+        int i = n - 1;
 
-        while (i > -1 && s.charAt(i) == ' ') {
-             i--;
-        }
-           
+        while (i > -1 && s.charAt(i) == ' ') i--;
 
+        int len = 0;
         while (i > -1 && s.charAt(i) != ' ') {
-            --i;
-            cnt++;
+            i--;
+            len++;
         }
-        return cnt;
+
+        return len;
     }
 }

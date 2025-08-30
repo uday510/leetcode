cff928336a66287671273a0f7c714fc963cead8c
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-30 16:06:47 +0530

    Time: 2 ms (97.45%) | Memory: 42.2 MB (32.00%) - LeetSync

--- 1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.java
+++ 1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
         Arrays.sort(arr);
-        
+        Set<Integer> seen = new HashSet<>();
+
         int k = arr[1] - arr[0];
         for (int i = 2; i < arr.length; i++) {
-            if (arr[i] - arr[i-1] != k)
-                return false;
+            if (arr[i] - arr[i - 1] != k) return false;
         }
+
         return true;
     }
 }

e0f3f882f2639e47bf099dc223d7ab9b1267fa59
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-31 14:28:45 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (63.91%) - LeetSync

--- 1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.java
+++ 1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 2; i < C.length; i++) {
             int x3 = C[i][0], y3 = C[i][1];
 
-            if ((y2 - y1) * (x3 - x1) != (y3 - y1) * (x2 - x1)) return false;
+            if ((y3 - y2) * (x3 - x1) != (y3 - y1) * (x3 - x2)) return false;
         }
 
         return true;

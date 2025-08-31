f9aab0320eb5826cf1b4fca0c80fd3d0c55beede
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-31 14:28:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (79.60%) - LeetSync

---
+++ 1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkStraightLine(int[][] C) {
+        int x1 = C[0][0], y1 = C[0][1], x2 = C[1][0], y2 = C[1][1];
+
+        for (int i = 2; i < C.length; i++) {
+            int x3 = C[i][0], y3 = C[i][1];
+
+            if ((y2 - y1) * (x3 - x1) != (y3 - y1) * (x2 - x1)) return false;
+        }
+
+        return true;
+    }
+}

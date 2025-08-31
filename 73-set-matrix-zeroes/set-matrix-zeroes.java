18d4937bbc7a4d9a1b0232113998bdb3e27f2432
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-31 13:08:41 +0530

    Time: 1 ms (71.63%) | Memory: 45.7 MB (46.86%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,34 +1,32 @@
 class Solution {
-    public void setZeroes(int[][] matrix) {
+    public void setZeroes(int[][] mat) {
         boolean col0 = false;
-        int m = matrix.length, n = matrix[0].length;
+        int n = mat.length, m = mat[0].length;
 
-        for (int i = 0; i < m; ++i) {
-            if (matrix[i][0] == 0) col0 = true;
-            for (int j = 1; j < n; ++j) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = matrix[0][j] = 0;
-                }
+        for (int i = 0; i < n; i++) {
+            if (mat[i][0] == 0) col0 = true;
+            for (int j = 1; j < m; j++) {
+                if (mat[i][j] == 0) 
+                    mat[i][0] = mat[0][j] = 0; 
             }
         }
 
-        for (int i = 1; i < m; ++i) {
-            for (int j = 1; j < n; ++j) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
-                }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (mat[i][0] == 0 || mat[0][j] == 0) 
+                    mat[i][j] = 0;
             }
         }
 
-        if (matrix[0][0] == 0) {
-            for (int j = 0; j < n; ++j) {
-                matrix[0][j] = 0;
+        if (mat[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                mat[0][j] = 0;
             }
         }
 
         if (col0) {
-            for (int i = 0; i < m; ++i) {
-                matrix[i][0] = 0;
+            for (int i = 0; i < n; i++) {
+                mat[i][0] = 0;
             }
         }
     }

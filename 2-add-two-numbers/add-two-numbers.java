bf6a54268439878fb5f8408f11870d6051feb476
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-31 15:28:40 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (68.23%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -1,19 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = new ListNode(-1);
-        ListNode tail = head;
+        ListNode head = new ListNode(0), tail = head;
+        int carry = 0;
 
-        int rem = 0;
-        ListNode p1 = l1, p2 = l2;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
 
-        while (p1 != null || p2 != null || rem != 0) {
-            int val = rem + (p1 == null ? 0 : p1.val) + (p2 == null ? 0 : p2.val);
-            tail.next = new ListNode(val % 10);
+            ListNode newNode = new ListNode(sum % 10);
+            tail.next = newNode;
             tail = tail.next;
+            
+            carry = sum / 10;
 
-            rem = val / 10;
-            p1 = p1 == null ? null : p1.next;
-            p2 = p2 == null ? null : p2.next;
+            l1 = (l1 == null ? null : l1.next);
+            l2 = (l2 == null ? null : l2.next);
         }
 
         return head.next;

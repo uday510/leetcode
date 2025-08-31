1fe11d31d95931b772afba415f75d6e0bdf4dd4d
uday <BUDAYTEJA009@GMAIL.COM>
2025-08-31 13:26:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (42.53%) - LeetSync

---
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double average(int[] salaries) {
+        int minSalary = Integer.MAX_VALUE;
+        int maxSalary = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int salary : salaries) {
+            sum += salary;
+
+            minSalary = Math.min(minSalary, salary);
+            maxSalary = Math.max(maxSalary, salary);
+        }
+
+        return (double)( sum - minSalary - maxSalary) / (salaries.length - 2);
+    }
+}

d55faef8802afdaa770c656331d0d6f63a4178c7
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-03 09:20:48 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (45.55%) - LeetSync

---
+++ 65-valid-number/valid-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isNumber(String s) {
+        boolean seenDigit = false;
+        boolean seenDot = false;
+        boolean seenExponent = false;
+
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                seenDigit = true;
+            } else if (ch == '+' || ch == '-') {
+                if (i > 0 && (s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E'))  
+                    return false;
+            } else if (ch == 'e' || ch == 'E') {
+                if (seenExponent || !seenDigit) return false;
+                seenExponent = true;
+                seenDigit = false;
+            } else if (ch == '.') {
+                if (seenDot || seenExponent) return false;
+                seenDot = true;
+            } else
+                return false;
+        }
+        
+        return seenDigit;
+    }
+}

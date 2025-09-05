74e2f45400b19533d08618c1142beb6d2fa735e6
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-05 20:35:28 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (88.81%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.java
+++ 337-house-robber-iii/house-robber-iii.java
@@ -14,28 +14,21 @@
  * }
  */
 class Solution {
-    Map<TreeNode, int[]> dp;
     public int rob(TreeNode root) {
-        dp = new HashMap<>();
+        
         int[] res = dfs(root);
-
         return Math.max(res[0], res[1]);
     }
 
     private int[] dfs(TreeNode node) {
         if (node == null) return new int[] {0, 0};
 
-        if (dp.containsKey(node)) return dp.get(node);
-        int[] left = dfs(node.left);
-        int[] right = dfs(node.right);
+        int[] l = dfs(node.left);
+        int[] r = dfs(node.right);
 
-        int pick = node.val + left[1] + right[1];
-
-        int skip = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
-
-        int[] curr = {pick, skip};
-        
-        dp.put(node, curr);
+        int[] curr = new int[2];
+        curr[0] = node.val + l[1] + r[1];
+        curr[1] = Math.max(l[0], l[1]) + Math.max(r[0], r[1]);
 
         return curr;
     }

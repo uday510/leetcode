b0ae74ef3586e1f54192f98d034424ce4d3509f5
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-05 12:07:42 +0530

    Time: 3 ms (43.66%) | Memory: 41.6 MB (81.56%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.java
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -2,33 +2,20 @@ class Solution {
     public int nthUglyNumber(int n) {
         int[] uglies = new int[n];
         uglies[0] = 1;
+
         int i2 = 0, i3 = 0, i5 = 0;
 
         for (int i = 1; i < n; i++) {
-            int next2 = uglies[i2] * 2;
-            int next3 = uglies[i3] * 3;
-            int next5 = uglies[i5] * 5;
+            int n2 = uglies[i2] * 2, n3 = uglies[i3] * 3, n5 = uglies[i5] * 5;
+            int nextUgly = Math.min(n2, Math.min(n3, n5));
 
-            int nextUgly = Math.min(next2, Math.min(next3, next5));
             uglies[i] = nextUgly;
 
-            if (nextUgly == next2) i2++;
-            if (nextUgly == next3) i3++;
-            if (nextUgly == next5) i5++;
-
+            if (nextUgly == n2) i2++;
+            if (nextUgly == n3) i3++;
+            if (nextUgly == n5) i5++;
         }
 
         return uglies[n - 1];
     }
 }
-
-/**
-n = 10
-
-uglies = [1, 2, 3, 4, 5]
-
-i2 = 2, i3 = 1, i5 = 1
-
-min(8, 9, 25)
-
- */

447d8f1b7a8cbf9fdbeb3386e3d2138ecdb54ff2
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-06 16:33:05 +0530

    Time: 6 ms (64.15%) | Memory: 108.4 MB (59.23%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -5,10 +5,20 @@ class Solution {
     public long mostPoints(int[][] questions) {
         A = questions;
         n = A.length;
-        dp = new long[n];
-        Arrays.fill(dp, -1l);
+        dp = new long[n + 1];
+        // Arrays.fill(dp, -1l);
 
-        return dfs(0);
+        // return dfs(0);
+
+        for (int i = n - 1; i > -1; i--) {
+            int[] q = A[i];
+
+            long exc = dp[i + 1];
+            long inc = q[0] + (q[1] + i + 1 < n ? dp[q[1] + i + 1] : 0);
+
+            dp[i] = Math.max(inc, exc);
+        }
+        return dp[0];
     }
 
     private long dfs (int i) {

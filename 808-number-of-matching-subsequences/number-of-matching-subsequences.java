02f7a441f82178fb6aba7e3b49830387a4332cee
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-06 11:09:42 +0530

    Time: 46 ms (85.02%) | Memory: 47 MB (60.42%) - LeetSync

---
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int numMatchingSubseq(String s, String[] words) {
+        ArrayList<Node>[] heads = new ArrayList[26];
+        int res = 0;
+
+        for (int i = 0; i < 26; i++) 
+            heads[i] = new ArrayList<Node>();
+
+        for (String w : words) 
+            heads[w.charAt(0) - 'a'].add(new Node(w, 0));
+
+        for (char c : s.toCharArray()) {
+            ArrayList<Node> old = heads[c - 'a'];
+            heads[c - 'a'] = new ArrayList<Node>();
+
+            for (Node node : old) {
+                node.idx++;
+
+                if (node.idx == node.word.length()) res++;
+                else heads[node.word.charAt(node.idx) - 'a'].add(node);
+            }
+        }   
+        return res;
+    }
+
+    class Node {
+       String word;
+       int idx;
+
+       Node (String w, int i) {
+        word = w;
+        idx = i;
+       }
+    }
+}

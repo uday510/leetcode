1e34b3330a1e62363e32c7eca9ae3de84d2e7d2a
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-06 12:08:53 +0530

    Time: 3 ms (99.80%) | Memory: 41 MB (94.92%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,40 +1,13 @@
 class Solution {
-
-    // int[][] dp;
-    int[] coins;
-    int n;
-
-    public int change(int amount, int[] coins) {
-        // this.coins = coins;
-        this.n = coins.length;
-        // dp = new int[n][amount + 1];
-
-        // for (int[] row : dp) Arrays.fill(row, -1);
-
-        // return dfs(0, amount);
-        int[] dp = new int[amount + 1];
+    public int change(int amt, int[] coins) {
+        int[] dp = new int[amt + 1];
         dp[0] = 1;
 
-        for (int coin : coins) {
-            for (int rem = 1; rem <= amount; ++rem) {
-                if (coin <= rem) {
-                    dp[rem] += dp[rem-coin];
-                }
+         for (int c : coins) {
+            for (int i = c; i <= amt; i++) {
+                dp[i] += dp[i - c];
             }
         }
-        return dp[amount];
-
+        return dp[amt];
     }
-
-    // private int dfs(int i, int rem) {
-    //     if (rem == 0) return 1;
-    //     if (rem < 0 || i >= coins.length) return 0;
-        
-    //     if (dp[i][rem] != -1) return dp[i][rem];
-
-    //     int inc = dfs(i, rem - coins[i]);
-    //     int exc = dfs(i + 1, rem);
-
-    //     return dp[i][rem] = inc + exc;
-    // }
 }

1a25785650141b51dda98bb9de4cc9eebfff9b1b
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-09 10:09:33 +0530

    Time: 55 ms (18.82%) | Memory: 52.9 MB (19.21%) - LeetSync

--- 1332-count-vowels-permutation/count-vowels-permutation.java
+++ 1332-count-vowels-permutation/count-vowels-permutation.java
@@ -1,19 +1,36 @@
 class Solution {
-
+    static Map<Character, char[]> map = new HashMap<>();
+    int MOD = (int) 1e9 + 7;
+    static {
+        map.put('#', new char[]{'a', 'e', 'i', 'o', 'u'});
+        map.put('a', new char[]{'e'});
+        map.put('e', new char[]{'a', 'i'});
+        map.put('i', new char[]{'a', 'e', 'o', 'u'});
+        map.put('o', new char[]{'i', 'u'});
+        map.put('u', new char[]{'a'});
+    }
+    int n;
+    int[][] dp;
     public int countVowelPermutation(int n) {
-        long MOD = (int) 1e9 + 7;
-        long a = 1, e = 1, i = 1, o = 1, u = 1;
+        this.n = n;
+        dp = new int[n][26];
+        for (int[] row : dp) Arrays.fill(row, -1);
 
-        for (int idx = 2; idx <= n; ++idx) {
-            long newA = (e) % MOD,
-                 newE = (a + i) % MOD,
-                 newI = (a + e + o + u) % MOD,
-                 newO = (i + u) % MOD,
-                 newU = a % MOD;
+        return dfs(0, '#');
+    }
+    private int dfs(int i, char ch) {
+        if (i >= n) return 1;
 
-            a = newA % MOD; e = newE % MOD; i = newI % MOD; o = newO % MOD; u = newU % MOD;
+        if (ch != '#' && dp[i][ch - 'a'] != -1) return dp[i][ch - 'a'];
+
+        int cur = 0;
+
+        for (char nxt : map.get(ch)) {
+            cur = (cur + dfs(i + 1, nxt)) % MOD;    
         }
 
-        return (int) ((a + e + i + o + u) % MOD);
+        if (ch != '#') dp[i][ch - 'a'] = cur;
+        
+        return cur;
     }
 }

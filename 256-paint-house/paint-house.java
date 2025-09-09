bd28232b6cc4cce48619ed85bd63ef97af8d4fd4
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-09 09:55:09 +0530

    Time: 1 ms (87.35%) | Memory: 43.3 MB (19.59%) - LeetSync

--- 256-paint-house/paint-house.java
+++ 256-paint-house/paint-house.java
@@ -1,14 +1,29 @@
 class Solution {
-    public int minCost(int[][] c) {
-        int n = c.length;
-        int[][] dp = new int[n + 1][3];
+    int[][] dp; int[][] c;
+    int k, n;
+    public int minCost(int[][] costs) {
+        c = costs;
+        k = c[0].length;
+        n = c.length;
+        dp = new int[n][k + 1];
 
-        for (int i = n - 1; i > -1; i--) {
-            dp[i][0] = c[i][0] + Math.min(dp[i + 1][1], dp[i + 1][2]);
-            dp[i][1] = c[i][1] + Math.min(dp[i + 1][0], dp[i + 1][2]);
-            dp[i][2] = c[i][2] + Math.min(dp[i + 1][0], dp[i + 1][1]);
+        for (int[] r : dp) Arrays.fill(r, -1);
+
+        return dfs(0, -1);
+    }
+    private int dfs(int i, int exc) {
+        if (i >= n) return 0;
+
+        int cur = (int) 1e9;
+
+        if (dp[i][exc + 1] != -1) return dp[i][exc + 1];
+
+        for (int j = 0; j < k; j++) {
+            if (j == exc) continue;
+
+            cur = Math.min(cur, c[i][j] + dfs(i + 1, j));
         }
 
-        return Math.min(dp[0][0], Math.min(dp[0][1], dp[0][2]));
+        return dp[i][exc + 1] = cur;
     }
 }

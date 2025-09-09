4d267dc691aa0e7082e27a11da15a7117b00d959
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-09 09:38:39 +0530

    Time: 1 ms (87.35%) | Memory: 42.8 MB (75.24%) - LeetSync

--- 256-paint-house/paint-house.java
+++ 256-paint-house/paint-house.java
@@ -1,14 +1,14 @@
 class Solution {
-    public int minCost(int[][] costs) {
-        int n = costs.length;
-        int[][] memo = new int[n + 1][3];
-       
-       for (int i = n - 1; i > -1; --i) {
-            memo[i][0] = costs[i][0] + Math.min(memo[i + 1][1], memo[i + 1][2]);
-            memo[i][1] = costs[i][1] + Math.min(memo[i + 1][0], memo[i + 1][2]);
-            memo[i][2] = costs[i][2] + Math.min(memo[i + 1][0], memo[i + 1][1]);     
+    public int minCost(int[][] c) {
+        int n = c.length;
+        int[][] dp = new int[n + 1][3];
+
+        for (int i = n - 1; i > -1; i--) {
+            dp[i][0] = c[i][0] + Math.min(dp[i + 1][1], dp[i + 1][2]);
+            dp[i][1] = c[i][1] + Math.min(dp[i + 1][0], dp[i + 1][2]);
+            dp[i][2] = c[i][2] + Math.min(dp[i + 1][0], dp[i + 1][1]);
         }
 
-        return Math.min(memo[0][0], Math.min(memo[0][1], memo[0][2]));
+        return Math.min(dp[0][0], Math.min(dp[0][1], dp[0][2]));
     }
 }

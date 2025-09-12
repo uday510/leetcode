3226beef627a0f57e239cd2e69573c3c8513304d
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-12 09:58:39 +0530

    Time: 2 ms (54.83%) | Memory: 46.1 MB (60.81%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,18 +1,7 @@
 class Solution {
     public double findMedianSortedArrays(int[] a1, int[] a2) {
         int n = a1.length, m = a2.length;
-        int[] a = new int[n + m];
-
-        int i = 0, j = 0, k = 0;
-
-        while (i < n && j < m) {
-            if (a1[i] < a2[j]) a[k++] = a1[i++];
-            else a[k++] = a2[j++];
-        }
-
-        while (i < n) a[k++] = a1[i++];
-
-        while (j < m) a[k++] = a2[j++];
+        int[] a = merge(a1, a2);
 
         if (((n + m) & 1) == 0) {
             int idx = (n + m) / 2;
@@ -21,4 +10,30 @@ class Solution {
 
         return a[(n + m) / 2];
     }
+
+    public int[] merge(int[] arr1, int[] arr2) {
+        int n = arr1.length;
+        int m = arr2.length;
+        int[] res = new int[n + m];
+        
+        int i = 0, j = 0, k = 0;
+        
+        while (i < n && j < m) {
+            if (arr1[i] < arr2[j]) {
+                res[k++] = arr1[i++];
+            } else {
+                res[k++] = arr2[j++];
+            }
+        }
+        
+        addRemNums(i, k, arr1, res);
+        addRemNums(j, k, arr2, res);
+        
+        return res;
+    }
+    
+    private void addRemNums(int i, int k, int[] src, int[] dest) {
+        while (i < src.length) dest[k++] = src[i++];
+    }
+
 }

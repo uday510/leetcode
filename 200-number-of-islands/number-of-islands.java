16e3b2271d13edccfe8463615910ef0ebe960e1d
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-14 15:54:15 +0530

    Time: 3 ms (84.00%) | Memory: 49.9 MB (40.18%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,35 +1,36 @@
 class Solution {
-
-    int[][] dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-    int numRows;
-    int numCols;
+    final static int[][] dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+    int n, m;
     char[][] grid;
-
     public int numIslands(char[][] grid) {
-        numRows = grid.length;
-        numCols = grid[0].length;
-        this.grid = grid;
         int islands = 0;
+        n = grid.length;
+        m = grid[0].length;
+        this.grid = grid;
 
-        for (int i = 0; i < numRows; ++i) {
-            for (int j = 0; j < numCols; ++j) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (grid[i][j] == '1') {
-                    dfs(i, j);
                     islands++;
+                    dfs(i, j);
                 }
             }
         }
+
 
         return islands;
     }
 
     private void dfs(int i, int j) {
-        if (i < 0 || i >= numRows || j < 0 || j >= numCols || grid[i][j] != '1') return;
+        if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] != '1') return;
 
+        grid[i][j] = '0';
 
-        grid[i][j] = '#';
         for (int[] dir : dirs) {
-            dfs(dir[0] + i, dir[1] + j);
+            int dx = dir[0] + i;
+            int dy = dir[1] + j;
+
+            dfs(dx, dy);
         }
     }
 }

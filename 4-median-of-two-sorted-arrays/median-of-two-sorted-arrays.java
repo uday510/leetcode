e097acb53954cbb33e55141777f1db4aa111c6f0
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-14 09:21:16 +0530

    Time: 1 ms (100.00%) | Memory: 46.2 MB (45.35%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,39 +1,38 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums2.length < nums1.length)    
+        if (nums1.length > nums2.length) 
             return findMedianSortedArrays(nums2, nums1);
 
-        int n1 = nums1.length, n2 = nums2.length;
+        int n = nums1.length, m = nums2.length;
+        int total = n + m;
+        int target = (total + 1) >> 1;
 
-        int total = n1 + n2;
-        int left = (total+ 1) >> 1;
-        int l = 0, r = n1;
-
+        int l = 0, r = n;
 
         while (l <= r) {
             int m1 = (l + r) >> 1;
-            int m2 = left - m1;
+            int m2 = target - m1;
 
             int l1 = safeGet(m1 - 1, nums1), l2 = safeGet(m2 - 1, nums2);
             int r1 = safeGet(m1, nums1), r2 = safeGet(m2, nums2);
 
             if (l1 <= r2 && l2 <= r1) {
-                int midOdd = Math.max(l1, l2);
-                if ((total & 1) == 1) return midOdd;
+                if ((total & 1 )== 1) return Math.max(l1, l2);
 
-                return (midOdd + Math.min(r1, r2)) / 2.0;
+                return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
             }
-            else if (l1 > r2) r = m1 - 1;
-            else if (l2 > r1) l = m1 + 1;
 
+            if (l1 > r2) r = m1 - 1;
+            if (l2 > r1) l = m1 + 1;
         }
+
 
         return -1;
     }
 
     private int safeGet(int i, int[] nums) {
-        if (i < 0) return Integer.MIN_VALUE;
         if (i >= nums.length) return Integer.MAX_VALUE;
+        if (i <= -1) return Integer.MIN_VALUE;
 
         return nums[i];
     }

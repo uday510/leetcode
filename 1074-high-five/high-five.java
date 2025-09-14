246a24de48c1fcbd2a7c3a66c8572693da10785e
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-14 12:03:26 +0530

    Time: 4 ms (99.47%) | Memory: 45 MB (56.58%) - LeetSync

--- 1074-high-five/high-five.java
+++ 1074-high-five/high-five.java
@@ -1,37 +1,39 @@
 class Solution {
     public int[][] highFive(int[][] items) {
-        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
-        int ids = 0;
+       Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+       int k = 5;
 
-        for (int[] i : items) {
-            int id = i[0], scr = i[1];
+       for (int[] i : items) {
+            int id = i[0], src = i[1];
 
             if (!map.containsKey(id)) {
                 map.put(id, new PriorityQueue<>((a, b) -> a - b));
-                ids++;
             }
 
             var pq = map.get(id);
+            pq.offer(src);
+            if (pq.size() > k) pq.poll();
+       }
+
+       int[][] res = new int[map.size()][2];
+       int idx = 0;
+
+       for (var es : map.entrySet()) {
+            int key = es.getKey();
+
+            int cur = 0;
+            var pq = map.get(key);
+            int size = pq.size();
+
+            while (!pq.isEmpty()) {
+                cur += pq.poll();
+            }
             
-            pq.offer(scr);
-            if (pq.size() > 5) pq.poll();
-        }
+            res[idx][0] = key;
+            res[idx][1] = cur / size;
+            idx++;
+       }
 
-        int[][] res = new int[ids][2];
-        int idx = 0;
-
-        for (var kv : map.entrySet()) {
-            int k = kv.getKey();
-
-            int sum = 0;
-
-            PriorityQueue<Integer> v = kv.getValue();
-            for (int c : v) sum += c;
-            
-            res[idx][0] = k;
-            res[idx++][1] = sum / v.size();
-        }
-
-        return res;
+       return res;
     }
 }

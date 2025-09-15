8c3a95c8c36ab1eefe866665aafd72eea8fcbcf8
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-15 17:27:52 +0530

    Time: 16 ms (68.60%) | Memory: 45.4 MB (73.63%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -4,15 +4,14 @@ class Solution {
 
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if (!st.isEmpty()) {
-                if (st.peekLast() == ch) st.pollLast();
-                else st.offerLast(ch);
+            if (!st.isEmpty() && st.peekLast() == ch) {
+                st.pollLast();
             } else st.offerLast(ch);
         }
 
        StringBuilder sb = new StringBuilder();
        for (char c : st) sb.append(c);
-
+       
        return sb.toString();
     }
 }

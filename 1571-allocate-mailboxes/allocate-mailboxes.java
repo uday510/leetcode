91665e5c696cf5525c5a56851708c29c949b673c
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-15 16:11:57 +0530

    Time: 10 ms (69.36%) | Memory: 41.6 MB (91.33%) - LeetSync

--- 1571-allocate-mailboxes/allocate-mailboxes.java
+++ 1571-allocate-mailboxes/allocate-mailboxes.java
@@ -1,22 +1,19 @@
 class Solution {
-
-    int[][] cost, dp;
-    int[] H;
     int n;
-
+    int[] H;
+    int[][] C, dp;
     public int minDistance(int[] houses, int k) {
         Arrays.sort(houses);
+        n = houses.length;
         H = houses;
-        n = H.length;
-
-        cost = new int[n][n];
+        C = new int[n][n];
         dp = new int[n][k + 1];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 int m = (i + j) >> 1;
                 for (int t = i; t <= j; t++) {
-                    cost[i][j] += Math.abs(H[t] - H[m]);
+                    C[i][j] += Math.abs(H[m] - H[t]);
                 }
             }
         }
@@ -33,9 +30,8 @@ class Solution {
         if (dp[i][k] != -1) return dp[i][k];
 
         int min = (int) 1e9;
-
         for (int j = i; j < n; j++) {
-            min = Math.min(min, cost[i][j] + dfs(j + 1, k - 1));
+            min = Math.min(min, C[i][j] + dfs(j + 1, k - 1));
         }
 
         return dp[i][k] = min;

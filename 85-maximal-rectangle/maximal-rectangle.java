7462519a723e0ea5bc23bf50243ad2b3f93b90f5
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-16 16:47:58 +0530

    Time: 8 ms (89.78%) | Memory: 45.5 MB (82.58%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -1,30 +1,28 @@
 class Solution {
     public int maximalRectangle(char[][] A) {
-        if (A.length == 0) return 0;
-        int n = A.length, m = A[0].length;
-        int maxArea = 0;
-        int[] dp = new int[m];
+        int N = A.length, M = A[0].length;
+        int[] dp = new int[M];
+        int maximalRectangle = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
                 dp[j] = A[i][j] == '1' ? dp[j] + 1 : 0;
             }
 
-            maxArea = Math.max(maxArea, largestRectangleArea(dp));
+            maximalRectangle = Math.max(maximalRectangle, largestRectangleArea(dp));
         }
-        return maxArea;
+
+        return maximalRectangle;
     }
-
-    // leeetcode84
-    public int largestRectangleArea(int[] H) {
+    private int largestRectangleArea(int[] H) {
         int n = H.length;
-        int[] NSL = getNextSmallerLeft(H, n);
-        int[] NSR = getNextSmallerRight(H, n);
-
+        int[] NSL = getNSL(H, n);
+        int[] NSR = getNSR(H, n);
         int maxArea = 0;
+
         for (int i = 0; i < n; i++) {
             int w = NSR[i] - NSL[i] - 1;
-            int a = H[i] * w;
+            int a = w * H[i];
 
             maxArea = Math.max(maxArea, a);
         }
@@ -32,35 +30,38 @@ class Solution {
         return maxArea;
     }
 
-    private int[] getNextSmallerLeft(int[] H, int n) {
-        ArrayDeque<Integer> st = new ArrayDeque<>();
+    private int[] getNSL(int[] H, int n) {
+        Deque<Integer> st = new ArrayDeque<>();
         int[] NSL = new int[n];
         NSL[0] = -1;
 
         for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && H[i] <= H[st.peek()]) st.pop();
+            
+            while (!st.isEmpty() && H[st.peek()] >= H[i]) st.pop();
 
             if (st.isEmpty()) NSL[i] = -1;
             else NSL[i] = st.peek();
 
             st.push(i);
         }
+
         return NSL;
     }
 
-    private int[] getNextSmallerRight(int[] H, int n) {
-        ArrayDeque<Integer> st = new ArrayDeque<>();
+    private int[] getNSR(int[] H, int n) {
+        Deque<Integer> st = new ArrayDeque<>();
         int[] NSR = new int[n];
         NSR[n - 1] = n;
 
         for (int i = n - 1; i > -1; i--) {
-            while (!st.isEmpty() && H[i] <= H[st.peek()]) st.poll();
+            while (!st.isEmpty() && H[st.peek()] >= H[i]) st.pop();
 
             if (st.isEmpty()) NSR[i] = n;
             else NSR[i] = st.peek();
 
             st.push(i);
         }
+
         return NSR;
     }
 }

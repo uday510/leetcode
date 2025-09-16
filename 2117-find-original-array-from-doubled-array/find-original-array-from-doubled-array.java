4041c670daa99280744519a6bcffc1eb57794a77
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-16 17:19:07 +0530

    Time: 84 ms (82.61%) | Memory: 63.7 MB (13.41%) - LeetSync

---
+++ 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] findOriginalArray(int[] C) {
+        if ((C.length & 1) == 1) return new int[] {};
+
+        Arrays.sort(C);
+
+        int n = C.length;
+        int[] A = new int[n / 2];
+        int idx = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i : C) map.merge(i, 1, Integer::sum);
+
+        for (int a : C) {
+            if (map.get(a) > 0) {
+                map.merge(a, -1, Integer::sum);
+
+                Integer doubleVal = map.get(a * 2);
+
+                if (doubleVal == null || doubleVal == 0) return new int[] {};
+
+                map.merge(a * 2, -1, Integer::sum);
+
+                A[idx++] = a; 
+            }
+        }
+
+        return A;
+    }
+}

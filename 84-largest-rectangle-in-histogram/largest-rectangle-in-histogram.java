d0dce57c72cb10b9797b7c794351734699d551ce
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-16 16:30:33 +0530

    Time: 39 ms (88.73%) | Memory: 57.3 MB (72.83%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,13 +1,13 @@
 class Solution {
     public int largestRectangleArea(int[] H) {
         int n = H.length;
-        int[] NSL = getNextSmallerLeft(H, n);
-        int[] NSR = getNextSmallerRight(H, n);
-
+        int[] NSL = getNSL(H, n);
+        int[] NSR = getNSR(H, n);
         int maxArea = 0;
+
         for (int i = 0; i < n; i++) {
             int w = NSR[i] - NSL[i] - 1;
-            int a = H[i] * w;
+            int a = w * H[i];
 
             maxArea = Math.max(maxArea, a);
         }
@@ -15,35 +15,38 @@ class Solution {
         return maxArea;
     }
 
-    private int[] getNextSmallerLeft(int[] H, int n) {
-        ArrayDeque<Integer> st = new ArrayDeque<>();
+    private int[] getNSL(int[] H, int n) {
+        Deque<Integer> st = new ArrayDeque<>();
         int[] NSL = new int[n];
         NSL[0] = -1;
 
         for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && H[i] <= H[st.peek()]) st.pop();
+            
+            while (!st.isEmpty() && H[st.peek()] >= H[i]) st.pop();
 
             if (st.isEmpty()) NSL[i] = -1;
             else NSL[i] = st.peek();
 
             st.push(i);
         }
+
         return NSL;
     }
 
-    private int[] getNextSmallerRight(int[] H, int n) {
-        ArrayDeque<Integer> st = new ArrayDeque<>();
+    private int[] getNSR(int[] H, int n) {
+        Deque<Integer> st = new ArrayDeque<>();
         int[] NSR = new int[n];
         NSR[n - 1] = n;
 
         for (int i = n - 1; i > -1; i--) {
-            while (!st.isEmpty() && H[i] <= H[st.peek()]) st.poll();
+            while (!st.isEmpty() && H[st.peek()] >= H[i]) st.pop();
 
             if (st.isEmpty()) NSR[i] = n;
             else NSR[i] = st.peek();
 
             st.push(i);
         }
+
         return NSR;
     }
 }

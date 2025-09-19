4f7b7ec979b8c0e41c1ccb7896d1d9be048ea1dc
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-19 19:31:40 +0530

    Time: 7 ms (25.88%) | Memory: 44.8 MB (40.58%) - LeetSync

--- 1085-the-earliest-moment-when-everyone-become-friends/the-earliest-moment-when-everyone-become-friends.java
+++ 1085-the-earliest-moment-when-everyone-become-friends/the-earliest-moment-when-everyone-become-friends.java
@@ -1,56 +1,72 @@
 class Solution {
     public int earliestAcq(int[][] logs, int n) {
         Arrays.sort(logs, Comparator.comparingInt(log -> log[0]));
+
         UnionFind uf = new UnionFind(n);
-        int numGroups = n;
         
         for (int[] log : logs) {
-            numGroups -= uf.union(log[1], log[2]);
-            if (numGroups == 1) return log[0];
-        }
+            int u = log[1], v = log[2], t = log[0];
+            
+            uf.union(u, v);
 
+            if (uf.getComponents() == 1) return t;
+        }
+        
         return -1;
     }
+}
 
-    class UnionFind {
-        int[] root;
-        int[] rank;
+class UnionFind {
+    private final int[] root;
+    // Use a rank array to record the height of each vertex, i.e., the "rank" of each vertex.
+    private final int[] rank;
 
-        UnionFind (int n) {
-            root = new int[n];
-            rank = new int[n];
+    private int components;
 
-            for (int idx = 0; idx < n; ++idx) {
-                root[idx] = idx;
-                rank[idx] = 1;
-            }
+    public UnionFind(int size) {
+        root = new int[size];
+        rank = new int[size];
+        components = size;
+
+        for (int i = 0; i < size; i++) {
+            root[i] = i;
+            rank[i] = 1;  // The initial "rank" of each vertex is 1, because each of them is
+            // a standalone vertex with no connection to other vertices.
+        }
+    }
+
+    // The find function here is the same as that in the disjoint set with path compression.
+    public int find(int x) {
+        if (x == root[x]) return x;
+
+        // Some ranks may become obsolete so they are not updated
+        return root[x] = find(root[x]);
+    }
+
+    // The union function with union by rank
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX == rootY) return;
+
+        if (rank[rootX] > rank[rootY]) {
+            root[rootY] = rootX;
+        } else if (rank[rootX] < rank[rootY]) {
+            root[rootX] = rootY;
+        } else {
+            root[rootY] = rootX;
+            rank[rootX] += 1;
         }
 
-        int union(int x, int y) {
-            int rootX = find(root[x]);
-            int rootY = find(root[y]);
+        components--;
+    }
 
-            if (rootX == rootY) {
-                return 0;
-            }
+    public boolean connected(int x, int y) {
+        return find(x) == find(y);
+    }
 
-            if (rank[rootX] > rank[rootY]) {
-                root[rootY] = rootX;
-            } else if (rank[rootY] > rank[rootX]) {
-                root[rootX] = rootY;
-            } else {
-                root[rootY] = rootX;
-                rank[rootX] += 1;
-            }
-
-            return 1;
-        }
-
-        int find(int x) {
-            if (x == root[x]) {
-                return x;
-            }
-            return root[x] = find(root[x]);
-        }
+    public int getComponents() {
+        return components;
     }
 }

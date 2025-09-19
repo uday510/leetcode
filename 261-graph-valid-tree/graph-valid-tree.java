3c4cad18090f6b8e9d3d1611c13da7746f95bb56
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-19 18:54:23 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (89.64%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.java
+++ 261-graph-valid-tree/graph-valid-tree.java
@@ -1,36 +1,43 @@
 class Solution {
     public boolean validTree(int n, int[][] edges) {
         if (edges.length != n - 1) return false;
-
         UnionFind uf = new UnionFind(n);
-        for (int[] edge : edges) {
-            if (!uf.union(edge[0], edge[1])) {
-                return false;
-            }
+
+        for (int[] e : edges) {
+            int u = e[0], v = e[1];
+
+            if (uf.connected(u, v)) return false;
+               
+            uf.union(u, v);
         }
+
         return true;
     }
 
     class UnionFind {
-        int[] root;
-        int[] rank;
+        int[] root, rank;
 
         UnionFind(int n) {
             root = new int[n];
             rank = new int[n];
-            for (int idx = 0; idx < n; ++idx) {
-                root[idx] = idx;
-                rank[idx] = 1;
+
+            for (int i = 0; i < n; i++) {
+                root[i] = i;
+                rank[i] = 1;
             }
         }
 
-        boolean union(int x, int y) {
+        int find(int x) {
+            if (root[x] == x) return x;
+
+            return root[x] = find(root[x]);
+        }
+
+        void union(int x, int y) {
             int rootX = find(x);
             int rootY = find(y);
 
-            if (rootX == rootY) {
-                return false;
-            }
+            if (rootX == rootY) return;
 
             if (rank[rootX] > rank[rootY]) {
                 root[rootY] = rootX;
@@ -38,16 +45,12 @@ class Solution {
                 root[rootX] = rootY;
             } else {
                 root[rootY] = rootX;
-                rank[rootX] += 1;
+                rank[rootX]++;
             }
-            return true;
         }
 
-        int find(int x) {
-            if (x == root[x]) {
-                return x;
-            }
-            return root[x] = find(root[x]);
+        boolean connected(int x, int y) {
+            return find(x) == find(y);
         }
     }
 }

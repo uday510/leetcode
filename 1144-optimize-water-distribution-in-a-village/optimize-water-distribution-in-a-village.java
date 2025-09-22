ec7c64073396e8edef77ecfcf9dd3ce63051bc17
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-22 22:36:44 +0530

    Time: 36 ms (38.33%) | Memory: 51 MB (26.11%) - LeetSync

--- 1144-optimize-water-distribution-in-a-village/optimize-water-distribution-in-a-village.java
+++ 1144-optimize-water-distribution-in-a-village/optimize-water-distribution-in-a-village.java
@@ -1,39 +1,48 @@
 class Solution {
     public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
-        List<int[]>[] adj = new ArrayList[n + 1];
+        int minCost = prims(n, wells, pipes);
+        
+        return minCost;
+    }
 
+    private int prims(int n, int[] wells, int[][] pipes) {
+        List<int[]>[] adj = new ArrayList[n + 1];
         for (int i = 0; i <= n; i++) adj[i] = new ArrayList<>();
 
         for (int[] e : pipes) {
             int u = e[0], v = e[1], w = e[2];
-
-            adj[u].add(new int[]{v, w});
-            adj[v].add(new int[]{u, w});
+            
+            adj[u].add(new int[] {v, w});
+            adj[v].add(new int[] {u, w});
         }
 
         for (int i = 0; i < n; i++) {
-            adj[0].add(new int[]{i + 1, wells[i]});
-            adj[i + 1].add(new int[]{0, wells[i]});
+            int u = 0, v = i + 1, w = wells[i];
+
+            adj[u].add(new int[] {v, w});
+            adj[v].add(new int[] {u, w});
         }
 
-        boolean[] vis = new boolean[n + 1];
         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-        int minCost = 0;
+        boolean[] vis = new boolean[n + 1];
+        int minCost = 0, visCnt = 0;
 
-        pq.offer(new int[]{1, 0});
+        pq.offer(new int[] {0, 0});
 
-        while (!pq.isEmpty()) {
+        while (!pq.isEmpty() && visCnt <= n) {
             int[] cur = pq.poll();
-            int v = cur[0], w = cur[1];
+            int u = cur[0], w = cur[1];
 
-            if (vis[v]) continue;
-
-            vis[v] = true;
+            if (vis[u]) continue;
+            vis[u] = true;
             minCost += w;
+            visCnt++;
 
-            for (int[] next : adj[v]) {
-                if (!vis[next[0]]) {
-                    pq.offer(new int[] {next[0], next[1]});
+            for (int[] e : adj[u]) {
+                int v = e[0], w1 = e[1];
+
+                if (!vis[v]) {
+                    pq.offer(new int[] {v, w1});
                 }
             }
         }
@@ -42,3 +51,24 @@ class Solution {
     }
 }
 
+
+/**
+
+
+0: [1, 1], [2, 2], [3, 2]
+
+1: [0, 1], [2, 1]
+
+2: [0, 2], [1, 1], [3, 1]
+
+3: [0, 2], [2, 1]
+
+
+pq: [2, 2], [3, 2], [3, 1]
+
+vis: 1, 2, 2
+
+0 -> 1: 1, 2 -> 1: 1, 2 -> 3: 1
+minCost : 0 + 1 + 1 + 1
+
+ */

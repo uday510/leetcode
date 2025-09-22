c2de6b978c420a4cce3bdb6a9dc51f4e293c5807
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-22 15:01:50 +0530

    Time: 37 ms (85.41%) | Memory: 85.1 MB (89.01%) - LeetSync

--- 1308-smallest-string-with-swaps/smallest-string-with-swaps.java
+++ 1308-smallest-string-with-swaps/smallest-string-with-swaps.java
@@ -1,43 +1,46 @@
 class Solution {
     public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
-        int len = s.length();
-        DSU dsu = new DSU(len);
+        int n = s.length();
+        UnionFind uf = new UnionFind(n);
 
-        for (var pair : pairs) {
-            dsu.union(pair.get(0), pair.get(1));
+        for (List<Integer> p : pairs) {
+            uf.union(p.get(0), p.get(1));
         }
 
-        var components = new HashMap<Integer, List<Integer>>();
-        for (int vertex = 0; vertex < len; ++vertex) {
-            int root = dsu.find(vertex);
-            components.computeIfAbsent(root, k -> new ArrayList<>()).add(vertex);
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int root = uf.find(i);
+            map.computeIfAbsent(root, k -> new ArrayList<>()).add(i);
         }
 
-        char[] result = new char[len];
-        for (var component : components.values()) {
-            var chars = new ArrayList<Character>();
-            for (int idx : component) {
-                chars.add(s.charAt(idx));
+        char[] res = new char[n];
+
+        for (List<Integer> v : map.values()) {
+            List<Character> chars = new ArrayList<>();
+            for (int i : v) {
+                chars.add(s.charAt(i));
             }
+
             Collections.sort(chars);
-            for (int idx = 0; idx < component.size(); ++idx) {
-                result[component.get(idx)] = chars.get(idx);
+            for (int i = 0; i < v.size(); i++) {
+                res[v.get(i)] = chars.get(i);
             }
         }
 
-        return new String(result);
+        return new String(res);
     }
-    class DSU {
-        int[] rank;
-        int[] root;
 
-        DSU(int n) {
-            rank = new int[n];
+    class UnionFind {
+        int[] root, rank;
+
+        UnionFind(int n) {
             root = new int[n];
+            rank = new int[n];
 
-            for (int idx = 0; idx < n; ++idx) {
-                rank[idx] = 1;
-                root[idx] = idx;
+            for (int i = 0; i < n; i++) {
+                root[i] = i;
+                rank[i] = 1;
             }
         }
 
@@ -45,26 +48,26 @@ class Solution {
             int rootX = find(x);
             int rootY = find(y);
 
-            if (rootX == rootY) {
-                return;
-            }
+            if (rootX == rootY) return;
 
             if (rank[rootX] > rank[rootY]) {
                 root[rootY] = rootX;
-            } else if (rank[rootY] > rank[rootY]) {
-                rank[rootX] = rootY;
+            } else if (rank[rootY] > rank[rootX]) {
+                root[rootX] = rootY;
             } else {
                 root[rootY] = rootX;
                 rank[rootX]++;
             }
         }
-
+        
         int find(int x) {
-            if (x == root[x]) {
-                return x;
-            }
+            if (root[x] == x) return x;
+
             return root[x] = find(root[x]);
         }
-    }
 
+        boolean connected(int x, int y) {
+            return find(x) == find(y);
+        }
+    }
 }

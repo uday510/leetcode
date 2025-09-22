e1c27c0771dfd7a9dc5cea1afb41acefd6e36bba
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-22 23:01:02 +0530

    Time: 31 ms (45.00%) | Memory: 47.3 MB (94.44%) - LeetSync

--- 1144-optimize-water-distribution-in-a-village/optimize-water-distribution-in-a-village.java
+++ 1144-optimize-water-distribution-in-a-village/optimize-water-distribution-in-a-village.java
@@ -1,7 +1,34 @@
 class Solution {
     public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
-        int minCost = prims(n, wells, pipes);
+        int minCost = kruskal(n, wells, pipes);
         
+        return minCost;
+    }
+
+    private int kruskal(int n, int[] wells, int[][] pipes) {
+        List<int[]> edges = new ArrayList<>();
+
+        for (int[] e : pipes) {
+            edges.add(new int[]{e[0], e[1], e[2]});
+        }
+
+        for (int v = 1; v <= n; v++) {
+            edges.add(new int[] {0, v, wells[v - 1]});
+        }
+
+        edges.sort(Comparator.comparingInt(a -> a[2]));
+
+        int minCost = 0;
+        UnionFind uf = new UnionFind(n + 1);
+
+        for (int[] e : edges) {
+            int u = e[0], v = e[1], w = e[2];
+            if (!uf.connected(u, v)) {
+                uf.union(u, v);
+                minCost += w;
+            }
+        }
+
         return minCost;
     }
 
@@ -48,6 +75,46 @@ class Solution {
         }
 
         return minCost;
+    }
+
+    static class UnionFind {
+        int[] root, rank;
+
+        UnionFind(int n) {
+            root = new int[n];
+            rank = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                root[i] = i;
+                rank[i] = 1;
+            }
+        }
+        
+        void union(int x, int y) {
+            int rootX = root[x];
+            int rootY = root[y];
+
+            if (rootX == rootY) return;
+
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if (rank[rootY] > rank[rootX]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
+
+        int find(int x) {
+            if (x == root[x]) return x;
+
+            return root[x] = find(root[x]);
+        }
+
+        boolean connected(int x, int y) {
+            return find(x) == find(y);
+        }
     }
 }
 

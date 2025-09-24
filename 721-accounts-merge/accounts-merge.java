b74e9fe176e4c1358606fa84f3561e5c7374f422
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-24 14:10:45 +0530

    Time: 31 ms (53.48%) | Memory: 49.8 MB (20.28%) - LeetSync

--- 721-accounts-merge/accounts-merge.java
+++ 721-accounts-merge/accounts-merge.java
@@ -1,22 +1,27 @@
 class Solution {
+
     Map<String, List<String>> adj;
     Set<String> vis;
+
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
         adj = new HashMap<>();
         vis = new HashSet<>();
 
         for (var act : accounts) {
+            
             String u = act.get(1);
             for (int i = 2; i < act.size(); i++) {
                 String v = act.get(i);
+                
                 adj.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
                 adj.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
+
             }
         }
 
         List<List<String>> res = new ArrayList<>();
-
         for (var act : accounts) {
+
             String name = act.get(0);
             String u = act.get(1);
 
@@ -31,17 +36,28 @@ class Solution {
             res.add(cur);
         }
 
-        return res;    
+        return res;
     }
 
-    private void dfs(String email, List<String> cur) {
-        vis.add(email);
-        cur.add(email);
+    private void dfs(String u, List<String> cur) {
+        cur.add(u);
+        vis.add(u);
 
-        for (String v : adj.getOrDefault(email, new ArrayList<>())) {
+        for (String v : adj.getOrDefault(u, new ArrayList<>())) {
             if (vis.contains(v)) continue;
             dfs(v, cur);
         }
     }
-
 }
+
+/**
+
+"johnsmith@mail.com" -> [ "john_newyork@gmail.com" ]
+
+"john_newyork@gmail.com" -> [ "johnsmith@mail.com" ]
+
+"john00@mail.com" -> [ "john_newyork@gmail.com" ]
+
+
+
+ */  

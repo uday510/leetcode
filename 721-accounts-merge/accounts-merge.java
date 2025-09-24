bee6ce5d3bbc013fbd479aaa287723ae9c2c0e3e
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-24 22:48:53 +0530

    Time: 34 ms (41.88%) | Memory: 49.7 MB (21.33%) - LeetSync

--- 721-accounts-merge/accounts-merge.java
+++ 721-accounts-merge/accounts-merge.java
@@ -4,51 +4,53 @@ class Solution {
     Set<String> vis;
 
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        adj = new HashMap<>();
-        vis = new HashSet<>();
+            adj = new HashMap<>();
+            vis = new HashSet<>();
 
-        for (var act : accounts) {
-            
+        for (List<String> act : accounts) {
             String u = act.get(1);
-            for (int i = 2; i < act.size(); i++) {
-                String v = act.get(i);
-                
-                adj.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
-                adj.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
 
+            for (int j = 2; j < act.size(); ++j) {
+                String v = act.get(j);
+
+                adj.computeIfAbsent(u, _ -> new ArrayList<>()).add(v);
+                adj.computeIfAbsent(v, _ -> new ArrayList<>()).add(u);
             }
         }
 
         List<List<String>> res = new ArrayList<>();
-        for (var act : accounts) {
-
-            String name = act.get(0);
-            String u = act.get(1);
-
+        
+        for (List<String> act : accounts) {
+            String name = act.get(0), u = act.get(1);
+            
             if (vis.contains(u)) continue;
-
+            
             List<String> cur = new ArrayList<>();
             cur.add(name);
-
+            
             dfs(u, cur);
-
+            
             Collections.sort(cur.subList(1, cur.size()));
+            
             res.add(cur);
         }
+        
 
         return res;
     }
 
     private void dfs(String u, List<String> cur) {
-        cur.add(u);
         vis.add(u);
+        cur.add(u);
 
         for (String v : adj.getOrDefault(u, new ArrayList<>())) {
             if (vis.contains(v)) continue;
+
             dfs(v, cur);
         }
     }
 }
+
 
 /**
 

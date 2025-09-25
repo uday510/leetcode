95dc2eba841ac59e93af94b1cb66b17a51afb157
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-25 19:14:32 +0530

    Time: 2 ms (48.76%) | Memory: 48.8 MB (5.36%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -3,59 +3,70 @@ class Solution {
         int n = edges.length;
         
         UnionFind uf = new UnionFind(n);
-        int components = n;
 
         for (int u = 0; u < n; u++) {
            for (int v = 0; v < edges[u].length; v++) {
                 if (edges[u][v] == 1 && !uf.connected(u, v)) {
                     uf.union(u, v);
-                    components--;
                 }
            }
         }
         
-        return components;
+        return uf.getComponents();
     }
-    
-    class UnionFind {
-        int[] root, rank;
-        
-        UnionFind(int n) {
-            root = new int[n];
-            rank = new int[n];
-            
-            for (int i = 0; i < n; i++) {
-                root[i] = i;
-                rank[i] = 1;
-            }
+}
+
+class UnionFind {
+    private final int[] root;
+    // Use a rank array to record the height of each vertex, i.e., the "rank" of each vertex.
+    private final int[] rank;
+
+    private int components;
+
+    public UnionFind(int size) {
+        root = new int[size];
+        rank = new int[size];
+        components = size;
+
+        for (int i = 0; i < size; i++) {
+            root[i] = i;
+            rank[i] = 1;  // The initial "rank" of each vertex is 1, because each of them is
+            // a standalone vertex with no connection to other vertices.
         }
-        
-        int find(int x) {
-            if (x == root[x]) {
-                return x;
-            }
-            
-            return root[x] = find(root[x]);
+    }
+
+    // The find function here is the same as that in the disjoint set with path compression.
+    public int find(int x) {
+        if (x == root[x]) return x;
+
+        // Some ranks may become obsolete so they are not updated
+        return root[x] = find(root[x]);
+    }
+
+    // The union function with union by rank
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX == rootY) return;
+
+        if (rank[rootX] > rank[rootY]) {
+            root[rootY] = rootX;
+        } else if (rank[rootX] < rank[rootY]) {
+            root[rootX] = rootY;
+        } else {
+            root[rootY] = rootX;
+            rank[rootX] += 1;
         }
-        
-        void union(int x, int y) {
-            int rootX = find(x);
-            int rootY = find(y);
-            
-            if (rootX == rootY) return;
-            
-            if (rank[rootX] > rank[rootY]) {
-                root[rootY] = rootX;
-            } else if (rank[rootY] > rank[rootX]) {
-                root[rootX] = rootY;
-            } else {
-                root[rootY] = rootX;
-                rank[rootX]++;
-            }
-        }
-        
-        boolean connected(int x, int y) {
-            return find(x) == find(y);
-        }
+
+        components--;
+    }
+
+    public boolean connected(int x, int y) {
+        return find(x) == find(y);
+    }
+
+    public int getComponents() {
+        return components;
     }
 }

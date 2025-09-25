31bef0210428d3e5ee9a6b90265cb79e8d3216eb
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-25 18:58:51 +0530

    Time: 13 ms (65.30%) | Memory: 43.3 MB (47.47%) - LeetSync

--- 869-similar-string-groups/similar-string-groups.java
+++ 869-similar-string-groups/similar-string-groups.java
@@ -25,7 +25,6 @@ class Solution {
 
 class UnionFind {
     private final int[] root;
-    // Use a rank array to record the height of each vertex, i.e., the "rank" of each vertex.
     private final int[] rank;
 
     private int components;
@@ -37,20 +36,16 @@ class UnionFind {
 
         for (int i = 0; i < size; i++) {
             root[i] = i;
-            rank[i] = 1;  // The initial "rank" of each vertex is 1, because each of them is
-            // a standalone vertex with no connection to other vertices.
+            rank[i] = 1;
         }
     }
 
-    // The find function here is the same as that in the disjoint set with path compression.
     public int find(int x) {
         if (x == root[x]) return x;
 
-        // Some ranks may become obsolete so they are not updated
         return root[x] = find(root[x]);
     }
 
-    // The union function with union by rank
     public void union(int x, int y) {
         int rootX = find(x);
         int rootY = find(y);
@@ -77,11 +72,3 @@ class UnionFind {
         return components;
     }
 }
-
-/**
-
-
-t -> t, r, a, s
-
-
- */

42f0eaba8f5c5dc330733cc99424a2ee6a0e8956
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-30 03:32:25 +0530

    Time: 11 ms (91.77%) | Memory: 45.5 MB (91.77%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,26 +1,34 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> freqMap = new HashMap<>();
 
         for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(a -> map.get(a)));
+        int n = nums.length;
+        List<Integer>[] buckets = new ArrayList[n + 1];
 
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            int key = entry.getKey(), freq = entry.getValue();
 
-        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
-            int key= e.getKey();
-            pq.offer(key);
+            if (buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
 
-            if (pq.size() > k) pq.poll();
+            buckets[freq].add(key);
         }
 
         int[] res = new int[k];
         int idx = 0;
 
-        while (!pq.isEmpty()) {
-            res[idx++] = pq.poll();
+        for (int i = n; i > -1 && idx < k; --i) {
+            if (buckets[i] == null) continue;
+
+            for (int key : buckets[i]) {
+                res[idx++] = key;
+                if (idx == k) break;
+            }
         }
 
         return res;

b8820eae0188cc3070c3ca1d730017aefc8a6dad
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-30 02:09:59 +0530

    Time: 1 ms (100.00%) | Memory: 42.3 MB (81.10%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,5 +1,5 @@
 class Solution {
-
+    
     private final int MAX = Integer.MAX_VALUE;
     private final int MIN = Integer.MIN_VALUE;
 
@@ -7,26 +7,23 @@ class Solution {
         int i = 0, sign = 1, n = s.length();
         long num = 0;
 
-        while (i < n && s.charAt(i) == ' ') i++;
-        
+        while (i < n && s.charAt(i) == ' ') { i++; };
+
         if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
             if (s.charAt(i++) == '-') sign = -1;
         }
 
-        while (i < n && s.charAt(i) == '0') i++;
-
-
-        while (i < n && Character.isDigit(s.charAt(i))) {
-            int digit = s.charAt(i) - '0';
-
-            num = num * 10 + digit;
+        while (i < n && s.charAt(i) == '0') { i++; };
+        
+        while (i < n && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+            num = num * 10 + s.charAt(i) - '0';
 
             if (sign == 1 && num > MAX) return MAX;
             if (sign == -1 && -num < MIN) return MIN;
-
             i++;
         }
 
         return sign * (int) num;
     }
+
 }

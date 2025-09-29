fe4bae48e32eb1432dc4c26723c21c1c22ee5cd9
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-29 15:26:42 +0530

    Time: 4 ms (99.40%) | Memory: 44.6 MB (97.90%) - LeetSync

--- 1074-high-five/high-five.java
+++ 1074-high-five/high-five.java
@@ -1,39 +1,35 @@
 class Solution {
     public int[][] highFive(int[][] items) {
-       Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
-       int k = 5;
+        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+        int k = 5;
 
-       for (int[] i : items) {
-            int id = i[0], src = i[1];
+        for (int[] i : items) {
+            int id = i[0], sc = i[1];
 
-            if (!map.containsKey(id)) {
-                map.put(id, new PriorityQueue<>((a, b) -> a - b));
-            }
+            PriorityQueue<Integer> scores = map.computeIfAbsent(id,
+             _ -> new PriorityQueue<Integer>((a, b) -> Integer.compare(a, b)));
 
-            var pq = map.get(id);
-            pq.offer(src);
-            if (pq.size() > k) pq.poll();
-       }
 
-       int[][] res = new int[map.size()][2];
-       int idx = 0;
+            scores.offer(sc);
+           if (scores.size() > k) scores.poll();
+        }
 
-       for (var es : map.entrySet()) {
-            int key = es.getKey();
+        int[][] res = new int[map.size()][2];
+        int i = 0;
 
-            int cur = 0;
-            var pq = map.get(key);
-            int size = pq.size();
+        for (var es : map.entrySet()) {
+            int id = es.getKey();
+            int sum = 0;
+            PriorityQueue<Integer> scores = es.getValue();
+            int size = scores.size();
 
-            while (!pq.isEmpty()) {
-                cur += pq.poll();
-            }
-            
-            res[idx][0] = key;
-            res[idx][1] = cur / size;
-            idx++;
-       }
+            // while (!scores.isEmpty()) sum += scores.poll();
+            for (int scr : scores) sum += scr;
 
-       return res;
+            res[i][0] = id;
+            res[i++][1] = sum / size;
+        }
+
+        return res;
     }
 }

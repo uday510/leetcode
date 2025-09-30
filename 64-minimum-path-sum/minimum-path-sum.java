0ed1f560ae8295c86b669570b89769f8d3f04e69
uday <BUDAYTEJA009@GMAIL.COM>
2025-09-30 06:37:47 +0530

    Time: 4 ms (59.71%) | Memory: 48.2 MB (8.28%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,21 +1,33 @@
 class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length, n = grid[0].length;
-        int[][] dp = new int[m][n];
+        int[] dp = new int[n];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                dp[i][j] = grid[i][j];
-
-                if (i > 0 && j > 0) 
-                    dp[i][j] += Math.min(dp[i - 1][j], dp[i][j - 1]);
-                else if (i > 0) 
-                    dp[i][j] += dp[i - 1][j];
-                else if (j > 0) 
-                    dp[i][j] += dp[i][j - 1];
+                if (i == 0 && j == 0) {
+                    dp[j] = grid[i][j];
+                } else if (i == 0) {
+                    dp[j] = dp[j - 1] + grid[i][j];
+                } else if (j == 0) {
+                    dp[j] = dp[j] + grid[i][j];
+                } else {
+                    dp[j] = Math.min(dp[j], dp[j - 1]) + grid[i][j];
+                }
             }
         }
 
-        return dp[m - 1][n - 1];
+        return dp[n - 1];
     }
 }
+
+/**
+
+
+[1, 2, 3]
+[1, 1, 1]
+
+[1, 3]
+
+
+ */

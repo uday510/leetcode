10ef440ac797ce6411b26f434047da6d9520a928
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-01 15:35:48 +0530

    Time: 16 ms (61.73%) | Memory: 45.7 MB (26.52%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -1,17 +1,24 @@
 class Solution {
     public String removeDuplicates(String s) {
-        ArrayDeque<Character> st = new ArrayDeque<>();
+        Deque<Character> st = new ArrayDeque<>();
 
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if (!st.isEmpty() && st.peekLast() == ch) {
-                st.pollLast();
-            } else st.offerLast(ch);
+
+            if (st.isEmpty()) {
+                st.offerLast(ch);
+            } else {
+                if (st.peekLast() == ch) {
+                    st.pollLast();
+                } else {
+                    st.offerLast(ch);
+                }
+            }
         }
 
-       StringBuilder sb = new StringBuilder();
-       for (char c : st) sb.append(c);
-       
-       return sb.toString();
+        StringBuilder sb = new StringBuilder();
+        for (char ch : st) sb.append(ch);
+
+        return sb.toString();
     }
 }

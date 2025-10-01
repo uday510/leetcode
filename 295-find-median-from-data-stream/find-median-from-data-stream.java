bfcb91d4b6be9243c73efdcbaaba437f54ee09ce
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-01 15:30:23 +0530

    Time: 154 ms (32.31%) | Memory: 64.1 MB (21.39%) - LeetSync

---
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -0,0 +1,34 @@
+class MedianFinder {
+
+    // [1, 2, 3, 4, 5]
+    PriorityQueue<Integer> maxHeap; // first half [1, 2, 3]
+    PriorityQueue<Integer> minHeap; // second half = [4, 5]
+
+    public MedianFinder() {
+        maxHeap = new PriorityQueue<>();
+        minHeap = new PriorityQueue<>(Comparator.reverseOrder());    
+    }
+    
+    public void addNum(int num) {
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+        if (maxHeap.size() < minHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+    
+    public double findMedian() {
+        if (maxHeap.size() > minHeap.size()) {
+            return maxHeap.peek();
+        }
+
+        return (minHeap.peek() + maxHeap.peek()) / 2.0;
+    }
+}
+
+/**
+
+maxHeap - stores first half 
+minHeap - stores second half
+
+ */

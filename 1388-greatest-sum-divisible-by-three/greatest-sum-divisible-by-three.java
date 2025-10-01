87956a89e9e5ad6f118ee5214e2622cc9cf9bceb
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-01 14:36:00 +0530

    Time: 15 ms (55.37%) | Memory: 50.9 MB (17.91%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
@@ -1,32 +1,30 @@
 class Solution {
-    int[][] dp;
+
+    int n, k;
     int[] nums;
-    int n, k, MIN;
+    int[][] dp;
 
     public int maxSumDivThree(int[] nums) {
+        n = nums.length;
+        k = 3;
         this.nums = nums;
-        this.n = nums.length;
-        this.k = 3;
         this.dp = new int[n][k];
-        this.MIN = Integer.MIN_VALUE;
-
-        for (int[] row : dp) Arrays.fill(row, MIN);
+        for (int[] row : dp) Arrays.fill(row, -1);
 
         return dfs(0, 0);
     }
-
+    
     private int dfs(int i, int mod) {
-        if (i == n) {
-            return mod == 0 ? 0 : MIN;
-        }
-
-        if (dp[i][mod] != MIN) return dp[i][mod];
+        if (i >= n) return mod == 0 ? 0 : Integer.MIN_VALUE;
+        
+        if (dp[i][mod] != -1) return dp[i][mod];
 
         int skip = dfs(i + 1, mod);
-
-        int newMod = (mod + nums[i]) % 3;
+        
+        int newMod = (mod + nums[i]) % k;
         int take = nums[i] + dfs(i + 1, newMod);
-
+     
         return dp[i][mod] = Math.max(skip, take);
     }
+
 }

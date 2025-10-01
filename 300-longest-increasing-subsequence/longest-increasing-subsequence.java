0afd0ed06e20a77b4cedc413204c8209a14b6fc7
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-01 18:40:09 +0530

    Time: 4 ms (93.41%) | Memory: 44.2 MB (78.96%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,29 +1,32 @@
 class Solution {
-    int[][] dp;
-    int[] arr;
-    int n;
+    
+    List<Integer> lis;
+    int[] nums;
+
     public int lengthOfLIS(int[] nums) {
-        arr = nums;
-        n = arr.length;
-        dp = new int[n + 1][n];
-        for (int[] row : dp) Arrays.fill(row, -1);
+        this.nums = nums;
+        lis = new ArrayList<>();
 
-        return dfs(-1, 0);
-    }
-
-    private int dfs(int i, int j) {
-        if (j >= n) return 0;
-
-        if (dp[i + 1][j] != -1) return dp[i + 1][j];
-
-        int skip = dfs(i, j + 1);
-
-        int take = 0;
-
-        if (i == -1 || arr[i] < arr[j]) {
-            take = 1 + dfs(j, j + 1);
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int index = bs(num);
+            if (index == lis.size()) lis.add(num);
+            lis.set(index, num);
         }
 
-        return dp[i + 1][j] = Math.max(skip, take);
+        return lis.size();
+    }
+
+    private int bs(int target) {
+        int l = 0, r = lis.size();
+
+        while (l < r) {
+            int m = (l + r) >> 1;
+
+            if (lis.get(m) < target) l = m + 1;
+            else r = m;
+        }
+
+        return l;
     }
 }

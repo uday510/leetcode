d58008fdfa676b068d272813a1bdf30b98ae4187
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-01 15:44:36 +0530

    Time: 16 ms (86.11%) | Memory: 45.2 MB (92.33%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,36 +1,38 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
-        ArrayDeque<Pair> st = new ArrayDeque<>();
+        Deque<Pair> st = new ArrayDeque<>();
 
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
 
-            if (!st.isEmpty()) {
-                if (ch == st.peekLast().ch){
-                    if (st.peekLast().cnt + 1 >= k) st.pollLast();
-                    else st.peekLast().cnt++;
-                } else st.offerLast(new Pair(ch));
-            } else {
+            if (st.isEmpty()) {
                 st.offerLast(new Pair(ch));
+            } else {
+                if (st.peekLast().ch == ch) {
+                    st.peekLast().cnt += 1; 
+                    if (st.peekLast().cnt == k) st.pollLast();
+                } else {
+                    st.offerLast(new Pair(ch));
+                }
             } 
         }
 
         StringBuilder sb = new StringBuilder();
 
         for (Pair p : st) {
-            for (int i = 0; i < p.cnt; i++) sb.append(p.ch);
+            for (int i = 0; i < p.cnt; i++) 
+                sb.append(p.ch);
         }
 
         return sb.toString();
     }
+}
 
-    private class Pair {
-        char ch;
-        int cnt;
-        Pair(char ch) {
-            this.ch = ch;
-            cnt = 1;
-        }
+class Pair {
+    char ch;
+    int cnt;
+    Pair(char ch) {
+        this.ch = ch;
+        cnt = 1;
     }
-
 }

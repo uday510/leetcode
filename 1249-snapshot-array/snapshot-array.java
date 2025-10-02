a829d902a0f6c262cad87a179d1dc898db07f9c7
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-02 12:36:10 +0530

    Time: 53 ms (87.09%) | Memory: 79.6 MB (49.93%) - LeetSync

--- 1249-snapshot-array/snapshot-array.java
+++ 1249-snapshot-array/snapshot-array.java
@@ -1,21 +1,25 @@
-class SnapshotArray {
-    List<int[]>[] records;
+public class SnapshotArray {
+
     int snapId;
+    List<Pair>[] records;
 
     public SnapshotArray(int length) {
-        records = new ArrayList[length];
         snapId = 0;
+        records = new ArrayList[length];
+
         for (int i = 0; i < length; i++) {
             records[i] = new ArrayList<>();
-            records[i].add(new int[]{0, 0});
+            records[i].add(new Pair(0, 0));
         }
     }
 
     public void set(int index, int val) {
-        if (records[index].get(records[index].size() - 1)[0] == snapId) {
-            records[index].get(records[index].size() - 1)[1] = val;
+        List<Pair> record = records[index];
+
+        if (record.getLast().snapId == snapId) {
+            record.getLast().val = val;
         } else {
-            records[index].add(new int[]{snapId, val});
+            record.add(new Pair(snapId, val));
         }
     }
 
@@ -23,20 +27,41 @@ class SnapshotArray {
         return snapId++;
     }
 
-    public int get(int index, int targetSnap) {
-        List<int[]> list = records[index];
-        int l = 0, r = list.size() - 1, ans = 0;
-
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (list.get(mid)[0] <= targetSnap) {
-                ans = list.get(mid)[1];
-                l = mid + 1;
+    public int get(int index, int snap_id) {
+        List<Pair> record = records[index];
+        int l = 0, r = record.size();
+        int res = - 1;
+        
+        while (l < r) {
+            int m = (l + r) >> 1;
+                
+            Pair p = record.get(m);
+            if (p.snapId <= snap_id) {
+                res = p.val;
+                l = m + 1;
             } else {
-                r = mid - 1;
+                r = m;
             }
         }
+        
+        return res;
+    }
 
-        return ans;
+    class Pair {
+        int snapId;
+        int val;
+
+        public Pair(int snapId, int val) {
+            this.snapId = snapId;
+            this.val = val;
+        }
     }
 }
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray obj = new SnapshotArray(length);
+ * obj.set(index,val);
+ * int param_2 = obj.snap();
+ * int param_3 = obj.get(index,snap_id);
+ */

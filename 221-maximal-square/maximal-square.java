ebaee9221037e15b4e22c75ab1e4a6c509d8cc8a
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-02 18:14:36 +0530

    Time: 10 ms (21.73%) | Memory: 60.2 MB (75.07%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -9,16 +9,14 @@ class Solution {
                 char ch = matrix[i][j];
                 if (ch == '0') continue;
 
-                int dia = 0, left = 0, up = 0;
-                if (i > 0 && j > 0) dia = dp[i - 1][j - 1];
-                if (i > 0) up = dp[i - 1][j];
-                if (j > 0) left = dp[i][j - 1];
+                int dia = (i > 0 && j > 0) ? dp[i-1][j-1] : 0;
+                int up = (i > 0) ? dp[i-1][j] : 0;
+                int left = (j > 0) ? dp[i][j-1] : 0;
 
                 dp[i][j] = 1 + Math.min(dia, Math.min(left, up));
                 maxLen = Math.max(maxLen, dp[i][j]);
             }
         }
-
 
         return maxLen * maxLen;
     }

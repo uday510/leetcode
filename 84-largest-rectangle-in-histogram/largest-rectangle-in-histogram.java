b14b781d5f5e13e5d1271a0c9251719ce108370a
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-02 17:44:24 +0530

    Time: 39 ms (88.63%) | Memory: 57.3 MB (72.93%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,13 +1,12 @@
 class Solution {
-    public int largestRectangleArea(int[] H) {
-        int n = H.length;
-        int[] NSL = getNSL(H, n);
-        int[] NSR = getNSR(H, n);
+    public int largestRectangleArea(int[] heights) {
+        int[] NSL = getNSL(heights);
+        int[] NSR = getNSR(heights);
         int maxArea = 0;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < heights.length; i++) {
             int w = NSR[i] - NSL[i] - 1;
-            int a = w * H[i];
+            int a = w * heights[i];
 
             maxArea = Math.max(maxArea, a);
         }
@@ -15,36 +14,41 @@ class Solution {
         return maxArea;
     }
 
-    private int[] getNSL(int[] H, int n) {
+    private int[] getNSL(int[] H) {
         Deque<Integer> st = new ArrayDeque<>();
-        int[] NSL = new int[n];
-        NSL[0] = -1;
+        int[] NSL = new int[H.length];
 
-        for (int i = 0; i < n; i++) {
-            
-            while (!st.isEmpty() && H[st.peek()] >= H[i]) st.pop();
+        for (int i = 0; i < H.length; i++) {
+            int h = H[i];
+
+            while (!st.isEmpty() && H[st.peekLast()] >= h) {
+                st.pollLast();
+            }
 
             if (st.isEmpty()) NSL[i] = -1;
-            else NSL[i] = st.peek();
+            else NSL[i] = st.peekLast();
 
-            st.push(i);
+            st.offerLast(i);
         }
-
+        
         return NSL;
     }
 
-    private int[] getNSR(int[] H, int n) {
+    private int[] getNSR(int[] H) {
         Deque<Integer> st = new ArrayDeque<>();
-        int[] NSR = new int[n];
-        NSR[n - 1] = n;
+        int[] NSR = new int[H.length];
 
-        for (int i = n - 1; i > -1; i--) {
-            while (!st.isEmpty() && H[st.peek()] >= H[i]) st.pop();
+        for (int i = H.length - 1; i > -1; i--) {
+            int h = H[i];
+            
+            while (!st.isEmpty() && H[st.peekLast()] >= h) {
+                st.pollLast();
+            }
 
-            if (st.isEmpty()) NSR[i] = n;
-            else NSR[i] = st.peek();
+            if (st.isEmpty()) NSR[i] = H.length;
+            else NSR[i] = st.peekLast();
 
-            st.push(i);
+            st.offerLast(i);
         }
 
         return NSR;

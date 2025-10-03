d6b1f895e7627a5045d70517ba4dea0cfe0b72d3
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-03 18:08:14 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (44.28%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,26 +1,34 @@
 class Solution {
-    public List<Integer> spiralOrder(int[][] mat) {
-        int n = mat.length, m = mat[0].length, r1 = 0, c1 = 0, r2 = n - 1, c2 = m - 1;
+    public List<Integer> spiralOrder(int[][] A) {
+        int N = A.length, M = A[0].length;
+        int r1 = 0, r2 = N - 1;
+        int c1 = 0, c2 = M - 1;
 
         List<Integer> res = new ArrayList<>();
 
         while (r1 <= r2 && c1 <= c2) {
+            
+            for (int j = c1; j <= c2; j++) {
+                res.add(A[r1][j]);
+            }
 
-            for (int c = c1; c <= c2; c++) res.add(mat[r1][c]);
-
-            for (int r = r1 + 1; r <= r2; r++) res.add(mat[r][c2]);
+            for (int r = r1 + 1; r <= r2; r++) {
+                res.add(A[r][c2]);
+            }
 
             if (r1 == r2) break;
-
-            for (int c = c2 - 1; c >= c1; c--) res.add(mat[r2][c]);
+            for (int c = c2 - 1; c >= c1; c--) {
+                res.add(A[r2][c]);
+            }
 
             if (c1 == c2) break;
-            for (int r = r2 - 1; r > r1; r--) res.add(mat[r][c1]);
+            
+            for (int r = r2 - 1; r > r1; r--) {
+                res.add(A[r][c1]);
+            }
 
-            r1++;
-            c1++;
-            r2--;
-            c2--;
+            r1++; r2--;
+            c1++; c2--;
         }
 
         return res;

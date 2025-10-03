919fe4dc1d8f1fbf0d6a34ce299d432138d6a194
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-03 10:51:51 +0530

    Time: 4 ms (89.14%) | Memory: 55.5 MB (80.29%) - LeetSync

--- 1165-meeting-scheduler/meeting-scheduler.java
+++ 1165-meeting-scheduler/meeting-scheduler.java
@@ -1,31 +1,25 @@
 class Solution {
     public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
-        Arrays.sort(slots1, Comparator.comparingInt(a -> a[0]));
-        Arrays.sort(slots2, Comparator.comparingInt(a -> a[0]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
 
-        int p1 = 0, p2 = 0, n = slots1.length, m = slots2.length;
+        for (int[] s : slots1) {
+            if (s[1] - s[0] >= duration) pq.offer(s);
+        }
 
-        while (p1 < n && p2 < m) {
-            int st = Math.max(slots1[p1][0], slots2[p2][0]);
-            int en = Math.min(slots1[p1][1], slots2[p2][1]);
+        for (int[] s : slots2) {
+            if (s[1] - s[0] >= duration) pq.offer(s);
+        }
+
+        while(pq.size() > 1) {
+            int[] s1 = pq.poll();
+            int[] s2 = pq.peek();
+
+            int st = Math.max(s1[0], s2[0]);
+            int en = Math.min(s1[1], s2[1]);
 
             if (en - st >= duration) return new ArrayList<>(Arrays.asList(st, st + duration));
-
-            if (slots1[p1][1] < slots2[p2][1]) p1++;
-            else p2++;
         }
 
         return new ArrayList<Integer>();
     }
 }
-
-/**
-
-
-[60, 200]
-
-[140, 210]
-
-140, 200
-
- */

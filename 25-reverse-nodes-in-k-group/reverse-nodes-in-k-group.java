6db0084f7d442fb9b39f3dd79b38bb1ce17c6b2a
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-04 12:33:29 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (95.37%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -10,26 +10,46 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        return dfs(head, k);
-    }
-    private ListNode dfs(ListNode node, int k) {
-        ListNode cur = node;
-        for (int i = 0; i < k; i++) {
-            if (cur == null) return node;
-            cur = cur.next;
+        ListNode newHead = null, tail = null;
+        ListNode cur = head;
+
+        while (cur != null) {
+            int cnt = 0;
+            ListNode ptr = cur;
+            while (cnt < k && ptr != null) {
+                ptr = ptr.next;
+                cnt++;
+            }
+
+            if (cnt == k) {
+                ListNode revHead = reverse(cur, k);
+                if (newHead == null) newHead = revHead;
+
+                if (tail != null) tail.next = revHead;
+
+                tail = cur;
+                cur = ptr;
+            } else {
+                if (tail != null) tail.next = cur;
+                break;
+            }
         }
 
-        ListNode prev = null;
-        cur = node;
-        for (int i = 0; i < k; i++) {
+        return newHead == null ? head : newHead;
+    }
+
+    private ListNode reverse(ListNode node, int k) {
+        ListNode prev = null, cur = node;
+
+        while (k > 0) {
             ListNode next = cur.next;
             cur.next = prev;
             prev = cur;
             cur = next;
+            
+            k--;
         }
 
-        node.next = dfs(cur, k);
-        
         return prev;
     }
 }

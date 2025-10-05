fa3dd16444cb7b006a95c89eaaf3593b9b2d8bc6
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-05 12:49:50 +0530

    Time: 1 ms (100.00%) | Memory: 45.7 MB (97.56%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,45 +1,38 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums2.length < nums1.length)
+        if (nums2.length < nums1.length) {
             return findMedianSortedArrays(nums2, nums1);
+        }
 
         int n = nums1.length, m = nums2.length;
-        int total = m + n;
-        int target = (total + 1) >> 1;
+        int target = (n + m + 1) >> 1;
 
         int l = 0, r = n;
 
         while (l <= r) {
-            int m1 = (l + r) >> 1, m2 = target - m1;
-            
-            int l1 = safeGet(m1 - 1, nums1), l2 = safeGet(m2 - 1, nums2);
-            int r1 = safeGet(m1, nums1), r2 = safeGet(m2, nums2);
+            int p1 = (l + r) >> 1, p2 = target - p1;
 
-            if (l1 > r2) r = m1 - 1;
-            else if (l2 > r1) l = m1 + 1;
+            int l1 = safeGet(p1 - 1, nums1), r1 = safeGet(p1, nums1); // nums1
+            int l2 = safeGet(p2 - 1, nums2), r2 = safeGet(p2, nums2); // nums2
 
+            if (l1 > r2) r = p1 - 1;
+            if (l2 > r1) l = p1 + 1;
             else if (l1 <= r2 && l2 <= r1) {
                 double median = -1;
-
-                if ((total & 1) == 1) {
+                if (((m + n) & 1) == 1) {
                     median = Math.max(l1, l2);
                 } else {
-                    median = (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0; 
+                    median = (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
                 }
-
                 return median;
             }
         }
-
         return -1;
     }
 
     private int safeGet(int i, int[] nums) {
-        int val = -1;
-        if (i < 0) val = Integer.MIN_VALUE;
-        else if (i >= nums.length) val = Integer.MAX_VALUE;
-        else val = nums[i];
-
-        return val;  
+        if (i < 0) return Integer.MIN_VALUE;
+        if (i >= nums.length) return Integer.MAX_VALUE;
+        return nums[i];
     }
 }

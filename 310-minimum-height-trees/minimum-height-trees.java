977f37dd04d480edfcf90e5c63c9ae4d915afdf2
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-08 20:40:50 +0530

    Time: 14 ms (96.85%) | Memory: 54.2 MB (99.95%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.java
+++ 310-minimum-height-trees/minimum-height-trees.java
@@ -1,37 +1,36 @@
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
         if (n == 1) return Collections.singletonList(0);
-
+        
         List<Integer>[] adjList = new ArrayList[n];
-
+        int[] d = new int[n];
         for (int i = 0; i < n; i++) adjList[i] = new ArrayList<>();
-
+        
         for (int[] e : edges) {
             int u = e[0], v = e[1];
             adjList[u].add(v);
             adjList[v].add(u);
+            d[u]++; d[v]++;
         }
 
         Deque<Integer> queue = new ArrayDeque<>();
-
-        for (int i = 0; i < n; i++) {
-            if (adjList[i].size() == 1)
-                queue.offerLast(i);
-        }
-
+        for (int i = 0; i < n; i++) if (d[i] == 1) queue.offer(i);
         int rem = n;
+        
         while (rem > 2) {
-            rem -= queue.size();
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
+            int sz = queue.size();
+            rem -= sz;
+            for (int i = 0; i < sz && !queue.isEmpty(); i++) {
                 int u = queue.pollFirst();
-                int v = adjList[u].get(0);
-                adjList[v].remove(Integer.valueOf(u));
-                if (adjList[v].size() == 1) 
-                    queue.offerLast(v);
+                d[u] = 0;
+                for (int v : adjList[u]) {
+                    if (d[v] == 0) continue;
+                    d[v]--;
+                    if (d[v] == 1) queue.offerLast(v);
+                }
             }
         }
-
-        return new ArrayList<Integer>(queue);
+        
+        return new ArrayList<>(queue);
     }
 }

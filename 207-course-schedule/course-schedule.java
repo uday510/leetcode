0eb25a785b8ffd17848793a6089b6219203bb1b1
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-08 15:18:44 +0530

    Time: 6 ms (72.39%) | Memory: 45 MB (83.50%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,41 +1,35 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        
         List<Integer>[] adjList = new ArrayList[numCourses];
+        int[] indegree = new int[numCourses];
 
         for (int i = 0; i < numCourses; i++) adjList[i] = new ArrayList<>();
 
-        int[] indegree = new int[numCourses];
-
-        for(int[] pre : prerequisites) {
-            int u = pre[1], v = pre[0];
-
-            adjList[u].add(v);
-            indegree[v]++;
+        for (int[] p : prerequisites) {
+            int u = p[0], v = p[1];
+            
+            adjList[v].add(u);
+            indegree[u]++;
         }
-
+        
         Queue<Integer> queue = new ArrayDeque<>();
-
+        int finished = 0;
+        
         for (int i = 0; i < numCourses; i++) {
             if (indegree[i] == 0) queue.offer(i);
         }
 
-        int completedCourses = 0;
-
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()) {
             int u = queue.poll();
-
-            completedCourses++;
-
+            finished++;
+            
             for (int v : adjList[u]) {
-                
                 if (--indegree[v] == 0) {
                     queue.offer(v);
                 }
             }
         }
 
-
-        return completedCourses == numCourses;
+        return finished == numCourses;
     }
 }

e509892b9da80dafffbe23772869ea4f80312694
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-16 11:50:23 +0530

    Time: 34 ms (84.14%) | Memory: 69.8 MB (69.70%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,32 +1,34 @@
 class Solution {
+
     List<int[]>[] adjList;
     boolean[] vis;
     int cnt;
-    public int minReorder(int n, int[][] connections) {
+
+    public int minReorder(int n, int[][] edges) {
         adjList = new ArrayList[n];
         vis = new boolean[n];
         cnt = 0;
 
-        for (int i = 0; i < n; ++i) {
-            adjList[i] = new ArrayList<>();
+        for (int i = 0; i < n; i++) adjList[i] = new ArrayList<>();
+
+        for (int[] e : edges) {
+            int u = e[0], v = e[1];
+            adjList[u].add(new int[]{v, 1});
+            adjList[v].add(new int[]{u, 0});
         }
 
-        for (int[] connection : connections) {
-            adjList[connection[0]].add(new int[]{connection[1], 1});
-            adjList[connection[1]].add(new int[]{connection[0], 0});
-        }
-        
         dfs(0);
         return cnt;
     }
-    private void dfs(int node) {
-        if (vis[node]) return;
-        
-        vis[node] = true;
-        for (int[] curr : adjList[node]) {
-            if (vis[curr[0]]) continue;
-            cnt += curr[1];
-            dfs(curr[0]);
+
+    private void dfs(int u) {
+        vis[u] = true;
+        for (int[] e : adjList[u]) {
+            int v = e[0], w = e[1];
+            if (vis[v]) continue;
+            dfs(v);
+            cnt += w;
         }
     }
 }
+

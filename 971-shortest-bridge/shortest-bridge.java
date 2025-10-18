041f103fd4e4367e3fec9d356f6ef9526f3011a4
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-18 18:00:13 +0530

    Time: 117 ms (5.05%) | Memory: 45.6 MB (37.12%) - LeetSync

--- 971-shortest-bridge/shortest-bridge.java
+++ 971-shortest-bridge/shortest-bridge.java
@@ -1,19 +1,20 @@
 class Solution {
-
-    int[][] dirs = {{0,1},{1,0},{-1,0},{0,-1}};
-    Queue<int[]> queue;
-    int n;
-    boolean[][] visited;
+    
     int[][] grid;
+    boolean[][] vis;
+    int n;
+    Queue<int[]> queue;
+    int[][] dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+
     public int shortestBridge(int[][] grid) {
         n = grid.length;
-        visited = new boolean[n][n];
+        vis = new boolean[n][n];
         queue = new ArrayDeque<>();
         this.grid = grid;
 
         boolean found = false;
-        for (int i = 0; i < n && !found; ++i) {
-            for (int j = 0; j < n && !found; ++j) {
+        for (int i = 0; i < n && !found; i++) {
+            for (int j = 0; j < n && !found; j++) {
                 if (grid[i][j] == 1) {
                     dfs(i, j);
                     found = true;
@@ -22,43 +23,55 @@ class Solution {
         }
 
         while (!queue.isEmpty()) {
-            int[] curr = queue.poll();
+            int[] cur = queue.poll();
+            System.out.println(Arrays.toString(cur));
+            int dx = cur[0], dy = cur[1], w = cur[2];
 
             for (int[] dir : dirs) {
-                int i = curr[0] + dir[0];
-                int j = curr[1] + dir[1];
-                int d = curr[2];
+                int nx = dir[0] + dx, ny = dir[1] + dy;
+                if (!isValid(nx, ny)) continue;
 
-                if (isOutOfBounds(i, j) || isVisited(i, j)) continue;
-
-                if (grid[i][j] == 1) return d;
-
-                visited[i][j] = true;
-                queue.offer(new int[]{i, j, d + 1});
+                if (grid[nx][ny] == 1) return w;
+                queue.offer(new int[]{nx, ny, w + 1});
+                vis[nx][ny] = true;
             }
         }
 
         return -1;
     }
-    
+
     private void dfs(int i, int j) {
-        if (isOutOfBounds(i, j) || grid[i][j] == 0 || isVisited(i, j)) return;
+        if (!isValid(i, j) || grid[i][j] == 0) return;
 
-        queue.offer(new int[]{i, j, 0});
-        visited[i][j] = true;
+        queue.offer(new int[] {i, j, 0});
+        vis[i][j] = true;
+
         for (int[] dir : dirs) {
-            int nextI = dir[0] + i;
-            int nextJ = dir[1] + j;
-
-            dfs(nextI, nextJ);
+            int nx = dir[0] + i, ny = dir[1] + j;
+            dfs(nx, ny);
         }
     }
-    
-    private boolean isOutOfBounds(int i, int j) {
-        return (i < 0 || i >= n || j < 0 || j >= n);
-    }
 
-    private boolean isVisited(int i, int j) {
-        return visited[i][j];
+    private boolean isValid(int i, int j) {
+        return i >= 0 && i < n && j >= 0 && j < n && !vis[i][j];
     }
 }
+
+/**
+
+0 1
+1 0 
+
+0 1 0
+0 0 0
+0 0 1
+
+------------
+1,1,1,1,1
+1,0,0,0,1
+1,0,1,0,1
+1,0,0,0,1
+1,1,1,1,1
+-----------
+
+*/

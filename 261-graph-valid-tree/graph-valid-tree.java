ee68a21be47f44c38fa9c43c577be6edfb852402
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-21 23:07:06 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (90.36%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.java
+++ 261-graph-valid-tree/graph-valid-tree.java
@@ -7,50 +7,50 @@ class Solution {
             int u = e[0], v = e[1];
 
             if (uf.connected(u, v)) return false;
-               
+
             uf.union(u, v);
         }
 
         return true;
     }
+}
 
-    class UnionFind {
-        int[] root, rank;
+class UnionFind {
+    private int[] root;
+    private int[] rank;
 
-        UnionFind(int n) {
-            root = new int[n];
-            rank = new int[n];
+    UnionFind (int n) {
+        root = new int[n];
+        rank = new int[n];
 
-            for (int i = 0; i < n; i++) {
-                root[i] = i;
-                rank[i] = 1;
-            }
+        for (int i = 0; i < n; i++) {
+            root[i] = i;
+            rank[i] = 1;
         }
+    }
+        
+    int find(int x) {
+        if (x == root[x]) return x;
+        return root[x] = find(root[x]);
+    }
 
-        int find(int x) {
-            if (root[x] == x) return x;
+    void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
 
-            return root[x] = find(root[x]);
+        if (rootX == rootY) return;
+
+        if (rank[rootX] > rank[rootY]) {
+            root[rootY] = rootX;
+        } else if (rank[rootY] > rank[rootX]) {
+            root[rootX] = rootY;
+        } else {
+            root[rootY] = rootX;
+            rank[rootX]++;
         }
+    }
 
-        void union(int x, int y) {
-            int rootX = find(x);
-            int rootY = find(y);
-
-            if (rootX == rootY) return;
-
-            if (rank[rootX] > rank[rootY]) {
-                root[rootY] = rootX;
-            } else if (rank[rootY] > rank[rootX]) {
-                root[rootX] = rootY;
-            } else {
-                root[rootY] = rootX;
-                rank[rootX]++;
-            }
-        }
-
-        boolean connected(int x, int y) {
-            return find(x) == find(y);
-        }
+    boolean connected(int x, int y) {
+        return find(x) == find(y);
     }
 }

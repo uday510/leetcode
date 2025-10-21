abe29544ba03fa5c864bddaa6cb9a53a9fdaf4d7
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-21 14:09:30 +0530

    Time: 9 ms (35.81%) | Memory: 45.6 MB (79.31%) - LeetSync

---
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -0,0 +1,65 @@
+class Solution {
+    
+    private static final int[][] DIRs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+    int m, n;
+    int[][] grid;
+
+    public List<List<Integer>> pacificAtlantic(int[][] grid) {
+        this.grid = grid;
+        m = grid.length;
+        n = grid[0].length;
+        List<List<Integer>> list = new ArrayList<>();
+
+        Queue<int[]> pacificQueue = new ArrayDeque<>();
+        Queue<int[]> atlanticQueue = new ArrayDeque<>();
+
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            pacificQueue.offer(new int[] {i, 0});
+            atlanticQueue.offer(new int[] {i, n - 1});
+
+            pacific[i][0] = atlantic[i][n - 1] = true;
+        }
+
+        for (int j = 0; j < n; j++) {
+            pacificQueue.offer(new int[] {0, j});
+            atlanticQueue.offer(new int[] {m - 1, j});
+
+            pacific[0][j] = atlantic[m - 1][j] = true;
+        }
+
+        bfs(pacificQueue, pacific);
+        bfs(atlanticQueue, atlantic);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    list.add(List.of(i, j));
+                }
+            }
+        }
+
+        return list;
+
+    }
+
+    private void bfs(Queue<int[]> queue, boolean[][] vis) {
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int dx = cur[0], dy = cur[1];
+
+            for (int[] dir : DIRs) {
+                int nx = dx + dir[0], ny = dy + dir[1];
+
+                if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+
+                if (vis[nx][ny] || grid[nx][ny] < grid[dx][dy]) continue;
+
+                vis[nx][ny] = true;
+                queue.offer(new int[] {nx, ny});
+            }
+        }
+    }
+}

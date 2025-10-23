0b19b917ca88324d2fa6a03ea1f6da8b09a738d8
uday <BUDAYTEJA009@GMAIL.COM>
2025-10-23 10:32:30 +0530

    Time: 14 ms (58.08%) | Memory: 45.5 MB (82.56%) - LeetSync

---
+++ 1738-maximal-network-rank/maximal-network-rank.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maximalNetworkRank(int n, int[][] edges) {
+        Set<Integer>[] adjList = new HashSet[n];
+
+        for (int i = 0; i < n; i++) adjList[i] = new HashSet<Integer>();
+
+        for (int[] e : edges) {
+            int u = e[0], v = e[1];
+            adjList[u].add(v);
+            adjList[v].add(u);
+        }
+
+        int maxRank = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int curRank = adjList[i].size() + adjList[j].size();
+                if (adjList[i].contains(j)) {
+                    curRank--;
+                }
+
+                maxRank = Math.max(curRank, maxRank);
+            }
+        }
+
+        return maxRank;
+    }
+}

8ddcc14724311959a7edbf0662887f6f9f34c1cc
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-01 09:20:42 +0530

    Time: 1 ms (83.21%) | Memory: 43.1 MB (5.07%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,30 +1,19 @@
 class Solution {
-    int[] dp;
-    String s;
-    int n;
-
     public int numDecodings(String s) {
-        this.s = s;
-        this.n = s.length();
-        dp = new int[n];
-        Arrays.fill(dp, -1);
+        int n = s.length();
+        if (n == 0 || s.charAt(0) == '0') return 0;
 
-        return dfs(0);
-    }
-    private int dfs(int i) {
-      if (i >= n) return 1;
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
 
-      if (s.charAt(i) == '0') return 0;
-      
-      if (dp[i] != -1) return dp[i];
+        for (int i = 2; i <= n; i++) {
+            int one = s.charAt(i - 1) - '0';
+            int two = (s.charAt(i - 2) - '0') * 10 + one;
 
-       int t1 = dfs(i + 1);
-       int t2 = i + 1 < n && num(i) * 10 + num(i + 1) <= 26 ? dfs(i + 2) : 0;
+            if (one >= 1) dp[i] = dp[i - 1];
+            if (two >= 10 && two <= 26) dp[i] += dp[i - 2];
+        }
 
-       return dp[i] = t1 + t2;
-    }
-
-    private int num(int i) {
-        return s.charAt(i) - '0';
+        return dp[n];
     }
 }

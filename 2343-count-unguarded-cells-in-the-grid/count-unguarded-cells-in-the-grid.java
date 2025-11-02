952f0d548c02d47d46a49fed70f68da78be1f39d
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-03 00:40:47 +0530

    Time: 22 ms (71.53%) | Memory: 147 MB (5.11%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
@@ -1,27 +1,20 @@
 class Solution {
     
-    Set<String> wall;
-    Set<String> guard;
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        
         int visited = 0;
-        boolean[][] vis = new boolean[m][n];
+        int[][] vis = new int[m][n];
         int[][] dirs = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
-        wall = new HashSet<>();
-        guard = new HashSet<>();
 
         for (int[] g : guards) {
             int x = g[0], y = g[1];
-            vis[x][y] = true;
+            vis[x][y] = 2;
             visited++;
-            guard.add(x + ":" + y);
         }
 
         for (int[] w : walls) {
             int x = w[0], y = w[1];
-            vis[x][y] = true;
+            vis[x][y] = 2;
             visited++;
-            wall.add(x + ":" + y);
         }
 
         for (int[] g : guards) {
@@ -30,12 +23,12 @@ class Solution {
             for (int[] dir : dirs) {
                 int nx = dir[0] + x, ny = dir[1] + y;
 
-                while (nx >= 0 && nx < m && ny >= 0 && ny < n && !isGuardOrWall(nx, ny)) {
-                    if (!vis[nx][ny]) {
-                        vis[nx][ny] = true;
+                while (nx >= 0 && nx < m && ny >= 0 && ny < n && vis[nx][ny] < 2) {
+                    if (vis[nx][ny] == 0) {
+                        vis[nx][ny] = 1;
                         visited++;
                     }
-                    
+
                     nx += dir[0];
                     ny += dir[1];
                 }
@@ -45,9 +38,4 @@ class Solution {
         return (m * n) - visited;
     }
 
-    private boolean isGuardOrWall(int x, int y) {
-        String k = x + ":" + y;
-        
-        return guard.contains(k) || wall.contains(k);
-    }
 }

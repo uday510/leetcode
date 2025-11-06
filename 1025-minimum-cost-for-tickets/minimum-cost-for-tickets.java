583e680fe034911a27fd16613a64024ee30b3163
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-06 14:25:52 +0530

    Time: 3 ms (21.08%) | Memory: 43.7 MB (7.44%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -1,32 +1,22 @@
 class Solution {
+    public int mincostTickets(int[] days, int[] costs) {
+        int n = days[days.length - 1];
+        int[] dp = new int[n + 1];
+        Set<Integer> travel = new HashSet<>();
+        for (int d : days) travel.add(d);
 
-    int n;
-    Set<Integer> days;
-    int[] dp, c;
+        for (int i = 1; i <= n; i++) {
+            if (!travel.contains(i)) {
+                dp[i] = dp[i - 1];
+            } else {
+                int p1 = dp[Math.max(0, i - 1)] + costs[0];
+                int p2 = dp[Math.max(0, i - 7)] + costs[1];
+                int p3 = dp[Math.max(0, i - 30)] + costs[2];
 
-    public int mincostTickets(int[] _days, int[] costs) {
-        n = _days[_days.length - 1];
-        c = costs;
-        days = new HashSet<>();
-        dp = new int[n + 1];
-        
-        for (int d : _days) days.add(d);
+                dp[i] = Math.min(p1, Math.min(p2, p3));
+            }
+        }
 
-        Arrays.fill(dp, -1);
-
-        return dfs(_days[0]);
-    }
-
-    private int dfs(int i) {
-        if (i > n) return 0;
-
-        if (dp[i] != -1) return dp[i];
-        if (!days.contains(i)) return dp[i] = dfs(i + 1);
-
-        int p1 = c[0] + dfs(i + 1);
-        int p2 = c[1] + dfs(i + 7);
-        int p3 = c[2] + dfs(i + 30);
-
-        return dp[i] = Math.min(p1, Math.min(p2, p3));
+        return dp[n];
     }
 }

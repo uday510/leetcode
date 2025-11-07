2334058590bd66bb8986dab491d44f8a22d8daf9
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-07 19:13:37 +0530

    Time: 1 ms (36.21%) | Memory: 42.4 MB (5.98%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.java
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.java
@@ -1,20 +1,20 @@
 class Solution {
+
+    static final int MOD = (int) 1e9 + 7;
+
     public int numTilings(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-       int MOD = (int) 1e9 + 7;
+        if (n <= 2) return n;
 
-       int dp1 = 1;
-       int dp2 = 2;
-       int dp3 = 5;
+        int[] dp = new int[n + 1];
+        dp[0] = 1; 
+        dp[1] = 1;
+        dp[2] = 2;
 
-       for (int i = 4; i <= n; ++i) {
-            int dp4 = ((2 * dp3) % MOD + dp1) % MOD;
-            dp1 = dp2;
-            dp2 = dp3;
-            dp3 = dp4;
-       }
+        for (int i = 3; i <= n; i++) {
+            dp[i] = ((2 * dp[i - 1]) % MOD + dp[i - 3]) % MOD;
+        }
+        
+        return dp[n];
 
-       return dp3;
-    }
+    }   
 }

3ec36b92ede5cb468e80915217839c32e1682003
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-09 21:17:48 +0530

    Time: 33 ms (93.35%) | Memory: 46.9 MB (18.38%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.java
+++ 1129-longest-string-chain/longest-string-chain.java
@@ -3,36 +3,37 @@ class Solution {
     Map<String, Integer> dp;
     Set<String> ws;
     int n;
+
     public int longestStrChain(String[] words) {
         n = words.length;
         ws = new HashSet<>();
         dp = new HashMap<>();
-        
-        for (String w : words) ws.add(w);
+
+        Collections.addAll(ws, words);
 
         int res = 1;
         for (String w : words) {
             res = Math.max(res, dfs(w));
         }
-        
+
         return res;
     }
 
     private int dfs(String w) {
-        Integer data = dp.get(w);
-        if (data != null) return data;
-        
+        Integer cached = dp.get(w);
+        if (cached != null) return cached;
+
         int cur = 1;
         for (int i = 0; i < w.length(); i++) {
             String s = w.substring(0, i) + w.substring(i + 1);
-            
+
             if (ws.contains(s)) {
                 cur = Math.max(cur, 1 + dfs(s));
             }
         }
-        
+
         dp.put(w, cur);
         return cur;
     }
-
+    
 }

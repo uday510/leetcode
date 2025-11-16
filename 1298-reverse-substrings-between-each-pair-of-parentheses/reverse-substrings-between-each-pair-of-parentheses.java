cbf15e59859c6ec46b52b27471e9a7db3ef4e0fe
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-16 14:36:22 +0530

    Time: 3 ms (58.27%) | Memory: 42.7 MB (69.93%) - LeetSync

---
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String reverseParentheses(String s) {
+        Deque<Integer> st = new ArrayDeque<>();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') st.push(sb.length());
+            else if (ch == ')') reverse(st.pop(), sb.length() - 1, sb);
+            else sb.append(ch);
+        }
+
+        return sb.toString();
+    }
+
+    private void reverse(int i, int j, StringBuilder sb) {
+
+        while (i < j) {
+            char tmp = sb.charAt(i);
+            sb.setCharAt(i++, sb.charAt(j));
+            sb.setCharAt(j--, tmp);
+        }
+
+    }
+}

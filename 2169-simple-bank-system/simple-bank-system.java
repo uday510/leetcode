c6a1f66fd3afc1e6dfce664f43d96620297038d3
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-16 12:36:51 +0530

    Time: 106 ms (10.42%) | Memory: 124.1 MB (5.15%) - LeetSync

--- 2169-simple-bank-system/simple-bank-system.java
+++ 2169-simple-bank-system/simple-bank-system.java
@@ -7,46 +7,37 @@ class Bank {
         this.balance = balance;
         this.n = balance.length;
     }
-    
+
     public boolean transfer(int acc1, int acc2, long money) {
         if (!validAcc(acc1) || !validAcc(acc2)) return false;
-
-        if (!validAmtForTransfer(acc1, money)) return false;
+        if (!validMoney(money)) return false;
+        if (balance[acc1 - 1] < money) return false;
 
         balance[acc1 - 1] -= money;
         balance[acc2 - 1] += money;
-
         return true;
     }
-    
+
     public boolean deposit(int acc, long money) {
-        if (!validAcc(acc) || !validAmtForDeposit(money)) {
-            return false;
-        }
+        if (!validAcc(acc) || !validMoney(money)) return false;
 
         balance[acc - 1] += money;
-
         return true;
     }
-    
+
     public boolean withdraw(int acc, long money) {
-        if (!validAcc(acc) || !validAmtForTransfer(acc, money)) return false;
+        if (!validAcc(acc) || !validMoney(money)) return false;
+        if (balance[acc - 1] < money) return false;
 
         balance[acc - 1] -= money;
-
         return true;
     }
-    
+
     private boolean validAcc(int acc) {
-        return acc >= 0 && acc <= n;
+        return acc >= 1 && acc <= n;
     }
 
-    private boolean validAmtForTransfer(int acc, long money) {
-        return balance[acc - 1] >= money;
-    }
-
-    private boolean validAmtForDeposit(long money) {
+    private boolean validMoney(long money) {
         return money >= 0;
     }
-    
 }

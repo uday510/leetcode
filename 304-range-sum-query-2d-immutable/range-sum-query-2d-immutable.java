845d10a8213d6edeb38a6597523dda436f677b71
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-16 14:28:50 +0530

    Time: 100 ms (70.57%) | Memory: 141.8 MB (11.75%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -4,23 +4,21 @@ class NumMatrix {
 
     public NumMatrix(int[][] matrix) {
         int n = matrix.length, m = matrix[0].length;
-        dp = new int[n][m];
+        dp = new int[n + 1][m + 1];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                dp[i][j] = matrix[i][j] 
-                         + (i > 0 ? dp[i-1][j] : 0)
-                         + (j > 0 ? dp[i][j-1] : 0)
-                         - (i > 0 && j > 0 ? dp[i-1][j-1]: 0);
+                dp[i + 1][j + 1] = matrix[i][j] + dp[i + 1][j] + dp[i][j + 1] - dp[i][j];
             }
         }
     }
     
     public int sumRegion(int r1, int c1, int r2, int c2) {
-        int base = dp[r2][c2];
-        int top = r1 > 0 ? dp[r1 - 1][c2] : 0;
-        int left = c1 > 0 ? dp[r2][c1 - 1] : 0;
-        int overlap = (r1 > 0 && c1 > 0 ? dp[r1 - 1][c1 - 1] : 0);
+        int base = dp[r2 + 1][c2 + 1];
+        int top = dp[r1][c2 + 1];
+        int left = dp[r2 + 1][c1];
+
+        int overlap = dp[r1][c1];
 
         return base - top - left + overlap;
     }

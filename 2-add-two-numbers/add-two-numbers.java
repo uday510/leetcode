b81d10deb8635300d7a645bc84f19c30ccdf8659
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-17 22:47:49 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (6.32%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,22 +10,36 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = new ListNode(-1);
-        ListNode cur = head, p1 = l1, p2 = l2;
-        int rem = 0;
+        ListNode h = new ListNode(-1);
+        ListNode cur = h, p1 = l1, p2 = l2;
+        int c = 0;
 
-        while (p1 != null || p2 != null || rem != 0) {
-            int sum = (p1 != null ? p1.val : 0) + (p2 != null ? p2.val : 0) + rem;
+        while (p1 != null || p2 != null || c != 0) {
+            int sum = getSum(p1, p2, c);
 
-            ListNode newNode = new ListNode(sum % 10);
-            cur.next = newNode;
-            cur = cur.next;
+            ListNode n = new ListNode(sum % 10);
+            cur.next = n;
+            cur = n;
 
-            rem = sum / 10;
-            p1 = (p1 != null ? p1.next : null);
-            p2 = (p2 != null ? p2.next : null);
+            c = sum / 10;
+
+            p1 = getNextNode(p1);
+            p2 = getNextNode(p2);
         }
 
-        return head.next;
+        return h.next;
+    }
+
+    private int getSum(ListNode p1, ListNode p2, int c) {
+        int sum = c;
+        
+        sum += p1 != null ? p1.val : 0;
+        sum += p2 != null ? p2.val : 0;
+
+        return sum;
+    }
+
+    private ListNode getNextNode(ListNode p) {
+        return p == null ? null : p.next;
     }
 }

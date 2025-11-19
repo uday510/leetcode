fcf5a0cf0a3b31943db64db436b0ac8dc70c0dbf
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-19 11:06:27 +0530

    Time: 2 ms (14.61%) | Memory: 46.8 MB (9.08%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,41 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        if (root == null) {
-            return new ArrayList<>();
-        }
-        var levels = new ArrayList<List<Integer>>();
-        var queue = new LinkedList<TreeNode>();
+        List<List<Integer>> levels = new ArrayList<>();
+        if (root == null) return levels;
 
-        queue.offer(root);
-        while(!queue.isEmpty()) {
-            var level = getLevel(queue);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
 
-            levels.add(level);
+        while (!queue.isEmpty()) {
+            levels.add(new ArrayList<Integer>());
+
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.remove();
+
+                levels.getLast().add(node.val);
+
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
         }
         return levels;
-    }
-    private List<Integer> getLevel(LinkedList<TreeNode> queue) {
-        int size = queue.size();
-        var level = new ArrayList<Integer>();
-
-        for (int i = 0; i < size; ++i) {
-            TreeNode node = queue.poll();
-
-            if (node == null) {
-                continue;
-            }
-
-            level.add(node.val);
-
-            if(node.left != null) {
-                queue.offer(node.left);
-            }
-
-            if (node.right != null) {
-                queue.offer(node.right);
-            }
-        }
-
-        return level;
+        
     }
 }

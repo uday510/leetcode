32a8c715a854658099cdf407a1a5663445f71688
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-19 20:29:32 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (15.40%) - LeetSync

--- 86-partition-list/partition-list.java
+++ 86-partition-list/partition-list.java
@@ -1,20 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode partition(ListNode head, int x) {
         ListNode beforeHead = new ListNode(-1);
         ListNode afterHead = new ListNode(-1);
-        ListNode before = beforeHead;
-        ListNode after = afterHead;
-        ListNode curr = head;
+        ListNode before = beforeHead, after = afterHead, cur = head;
 
-        while (curr != null) {
-            if (curr.val < x) {
-                before.next = curr;
+        while (cur != null) {
+            if (cur.val < x) {
+                before.next = cur;
                 before = before.next;
             } else {
-                after.next = curr;
+                after.next = cur;
                 after = after.next;
             }
-            curr = curr.next;
+            cur = cur.next;
         }
 
         before.next = afterHead.next;

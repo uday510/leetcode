3389e6211f429536eb6d01c7a34352b717b79da6
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-19 17:37:26 +0530

    Time: 1 ms (99.85%) | Memory: 75.5 MB (44.87%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,25 +1,19 @@
 class Solution {
+
+    private final static int inf = Integer.MAX_VALUE;
+
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int[] l = new int[n];
-        int[] r = new int[n + 1];
+        int t1Cost = inf, t1Profit = 0;
+        int t2Cost = inf, t2Profit = 0;
 
-        int lMin = prices[0], rMax = prices[n - 1];
+        for (int p : prices) {
+            t1Cost = Math.min(t1Cost, p);
+            t1Profit = Math.max(t1Profit, p - t1Cost);
 
-        for (int i = 1; i < n; i++) {
-            lMin = Math.min(prices[i], lMin);
-            l[i] = Math.max(l[i - 1], prices[i] - lMin);
-
-            int rIdx = n - i - 1;
-            rMax = Math.max(rMax, prices[rIdx]);
-            r[rIdx] = Math.max(r[rIdx + 1], rMax - prices[rIdx]);
-        }
-        
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max, l[i] + r[i + 1]);
+            t2Cost = Math.min(t2Cost, p - t1Profit);
+            t2Profit = Math.max(t2Profit, p - t2Cost);
         }
 
-        return max;
+        return t2Profit;
     }
 }

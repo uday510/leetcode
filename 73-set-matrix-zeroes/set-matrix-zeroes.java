ea4e93dcdaf1584a527d1295a95699606dcde319
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-19 10:55:46 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (10.80%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,33 +1,36 @@
 class Solution {
-    public void setZeroes(int[][] mat) {
+    public void setZeroes(int[][] A) {
         boolean col0 = false;
-        int n = mat.length, m = mat[0].length;
+        int n = A.length, m = A[0].length;
 
         for (int i = 0; i < n; i++) {
-            if (mat[i][0] == 0) col0 = true;
+            if (A[i][0] == 0) col0 = true;
             for (int j = 1; j < m; j++) {
-                if (mat[i][j] == 0) 
-                    mat[i][0] = mat[0][j] = 0; 
+                if (A[i][j] == 0) {
+                    A[i][0] = A[0][j] = 0;
+                }
             }
         }
 
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
-                if (mat[i][0] == 0 || mat[0][j] == 0) 
-                    mat[i][j] = 0;
+                if (A[i][0] == 0 || A[0][j] == 0) {
+                    A[i][j] = 0;
+                }
             }
         }
 
-        if (mat[0][0] == 0) {
+        if (A[0][0] == 0) {
             for (int j = 0; j < m; j++) {
-                mat[0][j] = 0;
+                A[0][j] = 0;
             }
         }
 
         if (col0) {
             for (int i = 0; i < n; i++) {
-                mat[i][0] = 0;
+                A[i][0] = 0;
             }
         }
+
     }
 }

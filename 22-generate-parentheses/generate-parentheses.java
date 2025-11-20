055cec686b0b127e56978acf0cbc4133af1b3fb2
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-20 13:00:09 +0530

    Time: 1 ms (81.07%) | Memory: 44.5 MB (25.20%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,30 +1,35 @@
 class Solution {
+
+    List<String> res;
     int n;
-    List<String> list;
+    static final String OPEN = "(";
+    static final String CLOSE = ")";
+
     public List<String> generateParenthesis(int n) {
+        res = new ArrayList<>();
         this.n = n;
-        this.list = new ArrayList<>();
 
         dfs(0, 0, new StringBuilder());
-        return list;
+        return res;    
     }
 
-    private void dfs(int left, int right, StringBuilder sb) {
-        if (sb.length() >= 2 * n) {
-            list.add(sb.toString());
+    private void dfs(int l, int r, StringBuilder sb) {
+        if (sb.length() == 2 * n) {
+            res.add(sb.toString());
             return;
         }
 
-        if (left < n) {
-            sb.append("(");
-            dfs(left + 1, right, sb);
+        if (l < n) {
+            sb.append(OPEN);
+            dfs(l + 1, r, sb);
             sb.deleteCharAt(sb.length() - 1);
         }
 
-        if (right < left) {
-            sb.append(")");
-            dfs(left, right + 1, sb);
+        if (r < l) {
+            sb.append(CLOSE);
+            dfs(l, r + 1, sb);
             sb.deleteCharAt(sb.length() - 1);
         }
+
     }
 }

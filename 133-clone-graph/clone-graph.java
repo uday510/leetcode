7656b1eedc2bef34128edd50b5362b55a0213a63
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-20 15:02:31 +0530

    Time: 27 ms (26.35%) | Memory: 44 MB (30.17%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -1,41 +1,21 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-*/
-
 class Solution {
     public Node cloneGraph(Node node) {
-        if (node == null) return node;
+        if (node == null) return null;
+
         Queue<Node> queue = new ArrayDeque<>();
         Map<Node, Node> map = new HashMap<>();
+
         queue.offer(node);
         map.put(node, new Node(node.val));
 
         while (!queue.isEmpty()) {
-            Node curr = queue.poll();
-            for (Node nei : curr.neighbors) {
-                if (map.containsKey(nei)) {
-                    map.get(curr).neighbors.add(map.get(nei));
-                    continue;
+            Node cur = queue.poll();
+            for (Node nei : cur.neighbors) {
+                if (!map.containsKey(nei)) {
+                    map.put(nei, new Node(nei.val));
+                    queue.offer(nei);
                 }
-                map.put(nei, new Node(nei.val));
-                map.get(curr).neighbors.add(map.get(nei));
-                queue.offer(nei);
+                map.get(cur).neighbors.add(map.get(nei));
             }
         }
 

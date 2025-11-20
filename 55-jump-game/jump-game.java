901121d55254ef00529b5dbd8e037719390eba86
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-20 13:54:02 +0530

    Time: 1 ms (99.91%) | Memory: 47.9 MB (9.96%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        int lastPos = n - 1;
+        int n = nums.length, lastPos = n - 1;
 
         for (int i = n - 1; i > -1; i--) {
-            if (nums[i] + i >= lastPos) lastPos = i;
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
         }
 
         return lastPos == 0;

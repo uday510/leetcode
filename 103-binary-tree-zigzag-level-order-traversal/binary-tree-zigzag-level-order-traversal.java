f4c9b6109d86b7133a51966705cc9886d61f94ca
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-20 09:26:19 +0530

    Time: 1 ms (68.67%) | Memory: 43 MB (26.60%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -1,60 +1,31 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         if (root == null) return new ArrayList<>();
 
-        List<List<Integer>> levels = new ArrayList<>();
-        Queue<TreeNode> queue = new ArrayDeque<>();
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<TreeNode> queue = new ArrayDeque<>();
         boolean leftToRight = true;
 
         queue.offer(root);
-
         while (!queue.isEmpty()) {
-            List<Integer> curLevel = getCurLevel(queue, leftToRight);
 
-            levels.add(curLevel);
+            List<Integer> curLevel = new LinkedList<>();
+            int sz = queue.size();
 
+            for (int i = 0; i < sz; i++) {
+                TreeNode cur = queue.poll();
+
+                if (leftToRight) curLevel.addLast(cur.val);
+                else curLevel.addFirst(cur.val);
+
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
+            }
+
+            res.add(curLevel);
             leftToRight = !leftToRight;
         }
 
-        return levels;
-    }
-
-    private List<Integer> getCurLevel(Queue<TreeNode> queue, boolean leftToRight) {
-        List<Integer> level = new LinkedList<>();
-        int size = queue.size();
-
-        for (int i = 0; i < size; i++) {
-            TreeNode cur = queue.poll();
-
-            if (leftToRight) level.addLast(cur.val);
-            else level.addFirst(cur.val);
-
-            if (cur.left != null) queue.offer(cur.left);
-            if (cur.right != null) queue.offer(cur.right);
-        }
-
-        return level;
+        return res;
     }
 }
-
-/**
-
-[3]
-[9,20]
-[15,7]
- */

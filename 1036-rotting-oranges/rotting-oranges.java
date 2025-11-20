4451802e02818a077721ee9b14e721115a682054
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-20 19:50:54 +0530

    Time: 2 ms (72.71%) | Memory: 43.9 MB (28.39%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,38 +1,48 @@
 class Solution {
+
+    private final static int[][] dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+    private final static int ROTTEN = 2;
+    private final static int FRESH = 1;
+    
     public int orangesRotting(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+
+        boolean[][] vis = new boolean[n][m];
         Queue<int[]> queue = new ArrayDeque<>();
-        int fresh = 0, n = grid.length, m = grid[0].length;
+        int fresh = 0, time = -1;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) fresh++;
-                else if (grid[i][j] == 2) queue.offer(new int[] {i, j});
+                if (grid[i][j] == FRESH) {
+                    fresh++;
+                } else if (grid[i][j] == ROTTEN) {
+                    vis[i][j] = true;
+                    queue.offer(new int[] {i, j});
+                } 
             }
         }
 
-        if (fresh == 0) return 0;
-        int t = -1;
+        if (fresh == 0) return 0;  
 
         while (!queue.isEmpty()) {
-            t++;
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
+            int sz = queue.size();
+            time++;
+            for (int i = 0; i < sz; i++) {
                 int[] cur = queue.poll();
-                for (int dx = -1; dx <= 1; dx++) {
-                    for (int dy = -1; dy <= 1; dy++) {
-                        if (Math.abs(dx) + Math.abs(dy) != 1) continue;
-                        int nx = cur[0] + dx, ny = cur[1] + dy;
 
-                        if (nx < 0 || nx >= n || ny < 0 || ny >= m || grid[nx][ny] != 1) continue;
+                for (int[] dir : dirs) {
+                    int nx = dir[0] + cur[0], ny = dir[1] + cur[1];
 
-                        queue.offer(new int[]{nx, ny});
-                        grid[nx][ny] = 2;
-                        fresh--;
-                    }
+                    if (nx < 0 || nx >= n || ny < 0 || ny >= m ||
+                     vis[nx][ny] || grid[nx][ny] != FRESH) continue;
+
+                     vis[nx][ny] = true;
+                     --fresh;
+                    queue.offer(new int[] {nx, ny});
                 }
             }
         }
 
-        return fresh == 0 ? t : -1;
+        return fresh == 0 ? time : -1;
     }
 }

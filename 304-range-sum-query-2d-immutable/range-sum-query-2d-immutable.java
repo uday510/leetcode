400012760e7b81abec8f550bb3daf203441deeda
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-20 20:14:56 +0530

    Time: 101 ms (66.30%) | Memory: 142.2 MB (5.21%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -2,24 +2,24 @@ class NumMatrix {
 
     private int[][] dp;
 
-    public NumMatrix(int[][] matrix) {
-        int n = matrix.length, m = matrix[0].length;
+    public NumMatrix(int[][] A) {
+        int n = A.length, m = A[0].length;
         dp = new int[n + 1][m + 1];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                dp[i + 1][j + 1] = matrix[i][j] + dp[i + 1][j] + dp[i][j + 1] - dp[i][j];
+                dp[i + 1][j + 1] = A[i][j] + dp[i + 1][j] + dp[i][j + 1] - dp[i][j];
             }
         }
     }
     
     public int sumRegion(int r1, int c1, int r2, int c2) {
-        int base = dp[r2 + 1][c2 + 1];
-        int top = dp[r1][c2 + 1];
-        int left = dp[r2 + 1][c1];
-
-        int overlap = dp[r1][c1];
-
-        return base - top - left + overlap;
+        return dp[r2 + 1][c2 + 1] - dp[r1][c2 + 1] - dp[r2 + 1][c1] + dp[r1][c1];
     }
 }
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */

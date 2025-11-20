48e4ba3fa6326b97e309d6c2c005186e0b1cdb01
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-20 19:24:20 +0530

    Time: 6 ms (23.43%) | Memory: 51.6 MB (43.50%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,57 +1,49 @@
 class Solution {
 
-    private char[][] grid;
-    private int n, m;
-    private final int[][] dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-    private boolean[][] vis;
-    
+    private static final int[][] dirs = { {0, 1}, {1, 0}, {-1, 0}, {0, -1} };
+    private final static char WATER = '0';
+    private final static char LAND = '1';
+
+    boolean[][] vis;
+    int n, m;
+    char[][] grid;
+
     public int numIslands(char[][] grid) {
-        initialize(grid);
-        
-        int islands = 0;
-        
+        n = grid.length;
+        m = grid[0].length;
+        vis = new boolean[n][m];
+        int numIslands = 0;
+        this.grid = grid;
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (vis[i][j] || grid[i][j] == '0') continue;
-                
-                islands++;
+                if (vis[i][j] || grid[i][j] == WATER) continue;
+
+                numIslands++;
                 bfs(i, j);
             }
         }
-        
-        
-        return islands;
+
+        return numIslands;
     }
-    
+
     private void bfs(int i, int j) {
         Queue<int[]> queue = new ArrayDeque<>();
-        
+
         vis[i][j] = true;
         queue.offer(new int[] {i, j});
-        
+
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
-            int dx = cur[0], dy = cur[1];
-            
+
             for (int[] dir : dirs) {
-                int nx = dx + dir[0], ny = dy + dir[1];
-                
-                if (!valid(nx, ny)) continue;
-                
+                int nx = dir[0] + cur[0], ny = dir[1] + cur[1];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || vis[nx][ny] || grid[nx][ny] == WATER) continue;
+
                 vis[nx][ny] = true;
                 queue.offer(new int[] {nx, ny});
             }
         }
     }
-    
-    private boolean valid(int nx, int ny) {
-        return (nx >= 0 && nx < n && ny >= 0 && ny < m && !vis[nx][ny] && grid[nx][ny] == '1');
-    }
-    
-    private void initialize(char[][] grid) {
-        this.grid = grid;
-        n = grid.length;
-        m = grid[0].length;
-        vis = new boolean[n][m];
-    } 
 }

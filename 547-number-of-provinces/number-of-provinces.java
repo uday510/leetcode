1877ecdf89f0889173c9f1017401e8288315d55d
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-23 10:29:09 +0530

    Time: 2 ms (48.74%) | Memory: 47.4 MB (62.81%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,40 +1,64 @@
 class Solution {
-
-    int[][] edges;
-    int n;
-    boolean[] vis;
-
-    public int findCircleNum(int[][] isConnected) {
-        edges = isConnected;
-        n = edges.length;
-        vis = new boolean[n];
-
-        int provinces = 0;
+    public int findCircleNum(int[][] edges) {
+        int n = edges.length;
+        UnionFind uf = new UnionFind(n);
 
         for (int i = 0; i < n; i++) {
-            if (vis[i]) continue;
-
-            provinces++;
-            bfs(i);
-        }
-
-        return provinces;
-    }
-
-    private void bfs(int node) {
-        vis[node] = true;
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.offer(node);
-
-        while (!queue.isEmpty()) {
-            int u = queue.poll();
-
-            for (int v = 0; v < n; v++) {
-                if (edges[u][v] == 0 || vis[v]) continue;
-
-                vis[v] = true;
-                queue.offer(v);
+            for (int j = 0; j < n; j++) {
+                int w = edges[i][j];
+                if (w == 1) {
+                    uf.union(i, j);
+                }
             }
         }
+
+        return uf.numComponents;
     }
+}
+
+class UnionFind {
+    int[] root, rank;
+    int numComponents;
+
+    UnionFind (int n) {
+        this.root = new int[n];
+        this.rank = new int[n];
+        numComponents = n;
+
+        for (int i = 0; i < n; i++) {
+            root[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX == rootY) return;
+
+        numComponents--;
+
+        if (rank[rootX] > rank[rootY]) {
+            rank[rootX]++;
+            root[rootY] = rootX; 
+        } else if (rank[rootY] > rank[rootX]) {
+            rank[rootY]++;
+            root[rootX] = rootY;
+        } else {
+            rank[rootX]++;
+            root[rootY] = rootX; 
+        }
+    }
+
+    int find(int x) {
+        if (root[x] == x) return x;
+
+        return x = find(root[x]);
+    }
+
+    boolean isConnected(int x, int y) {
+        return find(x) == find(y);
+    }
+
 }

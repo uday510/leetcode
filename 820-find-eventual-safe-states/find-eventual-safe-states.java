4e628818dd962a1c9d75eb6d6aae52c6c3e06677
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-23 12:51:55 +0530

    Time: 16 ms (54.83%) | Memory: 63.8 MB (9.41%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,75 +1,58 @@
 class Solution {
-    int[][] edges;
-    int[] dp;
-    int n;
+    private final boolean SAFE = true;
 
-    public List<Integer> eventualSafeNodes(int[][] graph) {
-        n = graph.length;
-        edges = graph;
-        dp = new int[n];
+    public List<Integer> eventualSafeNodes(int[][] edges) {
+        int n = edges.length;
+        List<Integer>[] adjList = new ArrayList[n];
+        int[] out = new int[n];
 
-        return reversedKahns();
-
-        // List<Integer> list = new ArrayList<>();
-        // for (int i = 0; i < n; i++) {
-        //     if (dfs(i)) {
-        //         list.add(i);
-        //     }
-        // }
-        // return list;
-    }
-
-    private boolean dfs(int u) {
-        if (dp[u] == 1) return false; // cycle
-        if (dp[u] == 2) return true; // safe
-
-        dp[u] = 1; // visited
-        for (int v : edges[u]) {
-            if (!dfs(v)) {
-                return false;
-            }
-        }
-
-        dp[u] = 2; // safe
-        return true;
-    }
-
-    private List<Integer> reversedKahns() {
-        int[] outdegree = new int[n];
-        List<Integer>[] reversedAdjList = new ArrayList[n];
-
-        for (int i = 0; i < n; i++) reversedAdjList[i] = new ArrayList<>();
+        for (int i = 0; i < n; i++) adjList[i] = new ArrayList<>();
 
         for (int u = 0; u < n; u++) {
             for (int v : edges[u]) {
-                reversedAdjList[v].add(u);
+                adjList[v].add(u);
             }
-            outdegree[u] = edges[u].length;
+            out[u] = edges[u].length;
         }
 
         Queue<Integer> queue = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
-            if (outdegree[i] == 0) queue.offer(i);
+            if (out[i] == 0) queue.offer(i);
         }
 
-        boolean[] safe = new boolean[n];
+        boolean[] nodeStates = new boolean[n];
         while (!queue.isEmpty()) {
             int u = queue.poll();
-            safe[u] = true;
+            nodeStates[u] = SAFE;
 
-            for (int v : reversedAdjList[u]) {
-                if (--outdegree[v] == 0) {
+            for (int v : adjList[u]) {
+                if (--out[v] == 0) {
                     queue.offer(v);
                 }
             }
         }
 
         List<Integer> list = new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
-            if (safe[i]) list.add(i);
+            if (nodeStates[i] == SAFE) {
+                list.add(i);
+            }
         }
 
         return list;
     }
-
 }
+
+// 0: 3, 4
+// 1: 0
+// 2: 0, 1
+// 3: 1
+// 4:
+// 5: 2, 3, 4
+// 6:
+
+// out:
+
+// safe: 4, 5, 
+

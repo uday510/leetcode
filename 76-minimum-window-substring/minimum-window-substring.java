d135c91e5f188ef7c310a23257af406c66c68179
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-23 16:06:56 +0530

    Time: 16 ms (66.77%) | Memory: 47.2 MB (19.97%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,37 +1,34 @@
 class Solution {
-
     public String minWindow(String s, String t) {
         int n = s.length(), m = t.length();
-        var map = new HashMap<Character, Integer>();
-        int st = 0, min = (int) 1e9;
-        
+        int st = -1, min = n + 1, cnt = 0;
+
+        Map<Character, Integer> map = new HashMap<>();
+
         for (int i = 0; i < m; i++) map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
-        
-        int cnt = 0;
+
         for (int l = 0, r = 0; r < n; r++) {
-            char rightChar = s.charAt(r);
-            if (map.containsKey(rightChar)) {
-                map.put(rightChar, map.get(rightChar) - 1);
-                if (map.get(rightChar) >= 0) cnt++;
+            char c2 = s.charAt(r); 
+            if (map.containsKey(c2)) {
+                map.put(c2, map.get(c2) - 1);
+                if (map.get(c2) >= 0) cnt++;
             }
-            
-            while (cnt == m) {     
-                if (min > r - l + 1) {
-                    min = r - l + 1;
+
+            while (cnt == m) {
+                if (r - l + 1 < min) {
                     st = l;
+                    min = r - l + 1;
                 }
-                
-                char leftChar = s.charAt(l);
-                if (map.containsKey(leftChar)) {
-                    map.put(leftChar, map.get(leftChar) + 1);
-                    if (map.get(leftChar) > 0) cnt--;
+
+                char c1 = s.charAt(l);
+                if (map.containsKey(c1)) {
+                    map.put(c1, map.get(c1) + 1);
+                    if (map.get(c1) > 0) cnt--;
                 }
-                
                 l++;
             }
         }
-        
-        
-        return min == (int) 1e9 ? "" : s.substring(st, st + min);
+
+        return st == -1 ? "" : s.substring(st, st + min);
     }
 }

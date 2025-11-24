e8a772adfe73dcf3002e12b13efdefc192b794b1
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-24 18:07:38 +0530

    Time: 5 ms (77.87%) | Memory: 77.6 MB (8.69%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,9 +1,11 @@
 class Solution {
+
     public int maxArea(int[] H) {
+
         int maxArea = 0;
         int l = 0, r = H.length - 1;
 
-        while (l <= r) {
+        while ( l <= r ) {
             int curArea = Math.min(H[l], H[r]) * (r - l);
 
             maxArea = Math.max(maxArea, curArea);
@@ -14,4 +16,5 @@ class Solution {
 
         return maxArea;
     }
+
 }

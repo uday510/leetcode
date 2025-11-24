ed09f98f39fc46e26d9058d9ee665909eafad938
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-24 14:56:00 +0530

    Time: 2 ms (63.99%) | Memory: 93.9 MB (32.74%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,23 +1,22 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        return maxProfitWithKTransactions(1, prices);
-    }
-    
-    private int maxProfitWithKTransactions(int k, int[] prices) {
-        int n = prices.length;
 
-        int INF = Integer.MAX_VALUE;
+    public int maxProfit(int[] prices) {
+        return maxProfitWithKTransactions(prices, 1);    
+    }
+    private int maxProfitWithKTransactions(int[] prices, int k) {
+        
         int[] buy = new int[k + 1];
         int[] profit = new int[k + 1];
-
-        Arrays.fill(buy, INF);
+        Arrays.fill(buy, Integer.MAX_VALUE);
 
         for (int price : prices) {
+
             for (int i = 1; i <= k; i++) {
                 // profit[i - 1] ==> profit from previous stock to reinvest
                 buy[i] = Math.min(buy[i], price - profit[i - 1]);
                 profit[i] = Math.max(profit[i], price - buy[i]);
             }
+
         }
 
         return profit[k];

1256478175eabde36478b0e61fb9d25b0b96df0b
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-24 16:29:37 +0530

    Time: 3 ms (84.31%) | Memory: 47.6 MB (22.24%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,22 +1,22 @@
 class Solution {
-    public int candy(int[] r) {
-        int n = r.length;
-        int[] arr = new int[n];
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] cnts = new int[n];
 
-        arr[0] = 1;
+        cnts[0] = 1;
         for (int i = 1; i < n; i++) {
-            if (r[i] > r[i - 1]) arr[i] = arr[i - 1] + 1;
-            else arr[i] = 1;
+            if (ratings[i] > ratings[i - 1]) cnts[i] = cnts[i - 1] + 1;
+            else cnts[i] = 1;
         }
 
-        int sum = arr[n - 1];
+        int total = cnts[n - 1];
         for (int i = n - 2; i >= 0; i--) {
-            if (r[i] > r[i + 1]) {
-                arr[i] = Math.max(arr[i], arr[i + 1] + 1);
+            if (ratings[i] > ratings[i + 1]) {
+                cnts[i] = Math.max(cnts[i], cnts[i + 1] + 1);
             }
-            sum += arr[i];
+            total += cnts[i];
         }
-
-        return sum;
+        
+        return total;
     }
 }

e6425e21e8840fa532588e3a90a87c25fccbb25f
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-24 17:11:28 +0530

    Time: 3 ms (99.02%) | Memory: 46.1 MB (29.55%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,5 +1,5 @@
 class Solution {
-    
+
     private static final String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
     private static final int[] ints = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};

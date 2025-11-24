78fb6b666e43a8cb732bc35aadf7896d3881a8a0
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-24 17:47:04 +0530

    Time: 2 ms (98.61%) | Memory: 44.2 MB (53.85%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -2,18 +2,17 @@ class Solution {
     public boolean isPalindrome(String s) {
         int i = 0, j = s.length() - 1;
 
-
         while (i < j) {
 
             if (!Character.isLetterOrDigit(s.charAt(i))) i++;
             else if (i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
 
-            else if (lower(s.charAt(i)) != lower(s.charAt(j))) {
-                return false;
-            } else {
+            else if (lower(s.charAt(i)) != lower(s.charAt(j))) return false;
+            else {
                 i++;
                 j--;
             }
+
         }
 
         return true;

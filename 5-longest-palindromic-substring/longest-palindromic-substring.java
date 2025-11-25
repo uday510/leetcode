91813e5255c2024916e169b4bf07a4b82d83cc74
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-25 18:32:30 +0530

    Time: 17 ms (53.42%) | Memory: 46.1 MB (23.86%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -10,7 +10,7 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             int[] odd = getLen(i - 1, i + 1);
-            int[] even = getLen(i, i + 1);
+            int[] even = getLen(i - 1, i);
 
             int[] cur = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
             res = cur[1] - cur[0] > res[1] - res[0] ? cur : res;
@@ -20,10 +20,8 @@ class Solution {
     }
 
     private int[] getLen(int i, int j) {
-
         while (i > -1 && j < n && s.charAt(i) == s.charAt(j)) {
-            i--;
-            j++;
+            i--; j++;
         }
 
         return new int[] {i + 1, j};

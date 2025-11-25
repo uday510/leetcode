d5abb78517e3ea872709cad45ff78cde5d210873
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-25 18:28:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (14.49%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,13 +1,15 @@
 class Solution {
-
+    
     int n, m;
-    int[][] grid, dp;
+    int[][] G, dp;
 
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        n = obstacleGrid.length;
-        m = obstacleGrid[0].length;
-        grid = obstacleGrid;
+    public int uniquePathsWithObstacles(int[][] O) {
+        n = O.length;
+        m = O[0].length;
+        G = O;
         dp = new int[n][m];
+
+        if (O[n - 1][m - 1] == 1) return 0;
 
         for (int[] row : dp) Arrays.fill(row, -1);
 
@@ -15,11 +17,11 @@ class Solution {
     }
 
     private int dfs(int i, int j) {
-        if (i >= n || j >= m || grid[i][j] == 1) return 0;
+        if (i >= n || j >= m || G[i][j] == 1) return 0;
 
         if (dp[i][j] != -1) return dp[i][j];
 
-        if (i == n - 1 && j == m - 1) return grid[i][j] == 1 ? 0 : 1;
+        if (i == n - 1 && j == m - 1) return 1;
 
         return dp[i][j] = dfs(i + 1, j) + dfs(i, j + 1);
     }

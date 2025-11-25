97f813ec00abafd62e66aa337c2300ba632ada0c
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-25 16:26:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (32.56%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -8,10 +8,10 @@ class Solution {
             if (nums[m] == target) return m;
 
             if (nums[l] <= nums[m]) {
-                if (nums[l] <= target && target < nums[m]) r = m - 1;
+                if (nums[l] <= target && target <= nums[m]) r = m - 1;
                 else l = m + 1;
             }  else {
-                if (nums[m] < target && target <= nums[r]) l = m + 1;
+                if (nums[m] <= target && target <= nums[r]) l = m + 1;
                 else r = m - 1;
             }
         }

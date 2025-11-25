f17a76c6a243da2004cfca783b4ca334b94c7da3
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-25 16:03:52 +0530

    Time: 4 ms (94.66%) | Memory: 46.7 MB (26.79%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,38 +1,37 @@
 class Solution {
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<Integer>[] adjList = new ArrayList[numCourses];
-        int[] indegree = new int[numCourses];
+    public int[] findOrder(int n, int[][] pre) {
+        List<Integer>[] adjList = new ArrayList[n];
+        int[] indegree = new int[n];
 
-        for (int i = 0; i < numCourses; i++) adjList[i] = new ArrayList<>();
+        for (int i = 0; i < n; i++) adjList[i] = new ArrayList<>();
 
-        for (int[] p : prerequisites) {
-            int u = p[1], v = p[0];
-
-            adjList[u].add(v);
-            indegree[v]++;
+        for (int[] p : pre) {
+            int u = p[0], v = p[1];
+            adjList[v].add(u);
+            indegree[u]++;
         }
 
         Queue<Integer> queue = new ArrayDeque<>();
 
-        for (int i = 0; i < numCourses; i++) {
+        for (int i = 0; i < n; i++) {
             if (indegree[i] == 0) queue.offer(i);
         }
 
-        System.out.println(queue);
-
-        int[] res = new int[numCourses];
-        int index = 0;
+        int[] res = new int[n];
+        int numFinished = 0, idx = 0;
 
         while (!queue.isEmpty()) {
             int u = queue.poll();
-            res[index++] = u;
+            numFinished++;
+            res[idx++] = u;
 
             for (int v : adjList[u]) {
-                if (--indegree[v] == 0) queue.offer(v);
+                if (--indegree[v] == 0) {
+                    queue.offer(v);
+                }
             }
         }
 
-        return index < res.length ? new int[] {} : res;
+        return numFinished == n ? res : new int[] {};
     }
 }
-

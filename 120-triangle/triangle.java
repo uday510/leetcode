7b32b736faad8b4f32afd4e2f65e4f23fa05268d
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-25 18:21:49 +0530

    Time: 4 ms (59.70%) | Memory: 45.4 MB (68.71%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,27 +1,31 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
+    public int minimumTotal(List<List<Integer>> T) {
+        int n = T.size();
 
-        if (n == 1) return triangle.get(0).get(0);
+        if (n == 1) return T.get(0).get(0);
 
         int[][] dp = new int[n][n];
-        dp[0][0] = triangle.get(0).get(0);
+        dp[0][0] = T.get(0).get(0);
         int min = (int) 1e9;
 
-        for (int i = 1; i < n; ++i) {
-            int m = triangle.get(i).size();
-            int curr = min;
-            for (int j = 0; j < m; ++j) {
-                dp[i][j] = triangle.get(i).get(j);
-                if(j > 0) {
+        for (int i = 1; i < n; i++) {
+            int m = T.get(i).size();
+            int cur = min;
+            
+            for (int j = 0; j < m; j++) {
+                dp[i][j] = T.get(i).get(j);
+
+                if (j > 0) {
                     if (j != m - 1) dp[i][j] += Math.min(dp[i - 1][j], dp[i - 1][j - 1]);
                     else dp[i][j] += dp[i - 1][j - 1];
-                } 
-                else dp[i][j] += dp[i - 1][j];
-
-                curr = Math.min(curr, dp[i][j]);
+                } else {
+                    dp[i][j] += dp[i - 1][j];
+                }
+                
+                cur = Math.min(cur, dp[i][j]);
             }
-            if (i == n - 1) min = Math.min(curr, min);
+
+            if (i == n - 1) min = Math.min(min, cur);
         }
 
         return min;

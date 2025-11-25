ab36e131dcadc32255c2c6b36e6172acf5e1dc39
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-25 18:43:45 +0530

    Time: 1 ms (90.38%) | Memory: 43.2 MB (25.50%) - LeetSync

---
+++ 97-interleaving-string/interleaving-string.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    String s1, s2, s3;
+    Boolean[][] dp;
+    int n, m, t;
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+        this.s1 = s1;
+        this.s2 = s2;
+        this.s3 = s3;
+        n = s1.length();
+        m = s2.length();
+        t = s3.length();
+        dp = new Boolean[n + 1][m + 1];
+
+        if (n + m != t) return false;
+
+        return dfs(0, 0);
+    }
+
+    private boolean dfs(int i, int j) {
+        if (i == n && j == m) return true;
+
+        if (dp[i][j] != null) return dp[i][j];
+
+        int k = i + j;
+        boolean ok = false;
+
+        if (i < n && s1.charAt(i) == s3.charAt(k))
+            ok |= dfs(i + 1, j);
+        
+        if (j < m && s2.charAt(j) == s3.charAt(k)) 
+            ok |= dfs(i, j + 1);
+
+        return dp[i][j] = ok;
+    }
+}

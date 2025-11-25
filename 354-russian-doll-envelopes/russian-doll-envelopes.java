05356e42aa90d1a69a335e61314fbf2e7ef7743c
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-25 18:13:42 +0530

    Time: 54 ms (29.37%) | Memory: 104.9 MB (16.35%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -1,48 +1,35 @@
 class Solution {
+
     List<Integer> lis;
-    public int maxEnvelopes(int[][] arr) {
-        int n = arr.length;
 
-        Arrays.sort(arr, (a, b) -> {
-            if (a[0] == b[0]) return b[1] - a[1];
-            return a[0] - b[0];
-        });
-
+    public int maxEnvelopes(int[][] E) {
         lis = new ArrayList<>();
 
-        for (int i = 0; i < n; ++i) {
-            int idx = bs(arr[i][1]);
-            if (idx == lis.size()) lis.add(arr[i][1]);
-            lis.set(idx, arr[i][1]);
+        Arrays.sort(E, (o1, o2) -> {
+            if (o1[0] == o2[0]) return o2[1] - o1[1];
+            return o1[0] - o2[0];
+        });
+
+        for (int i = 0; i < E.length; i++) {
+            int num = E[i][1];
+            int nxtIdx = bs(num);
+            if (nxtIdx == lis.size()) lis.add(num);
+            lis.set(nxtIdx, num);
         }
 
         return lis.size();
     }
+
     private int bs(int target) {
         int l = 0, r = lis.size();
 
         while (l < r) {
             int m = (l + r) >> 1;
+
             if (lis.get(m) < target) l = m + 1;
             else r = m;
         }
-        
+
         return l;
     }
 }
-
-
-/**
-
-// eg: envelopes = [[4,5],[4,6],[6,7],[2,3],[1,1]]
-
-
-Consider an input [[1, 3], [1, 4], [1, 5], [2, 3]].
-If we simply sort and extract the second dimension we get [3, 4, 5, 3], which implies that we can fit three envelopes (3, 4, 5). The problem is that we can only fit one envelope, since envelopes that are equal in the first dimension can't be put into each other.
-
-In order fix this, we don't just sort increasing in the first dimension - we also sort decreasing on the second dimension, so two envelopes that are equal in the first dimension can never be in the same increasing subsequence.
-
-Now when we sort and extract the second element from the input we get [5, 4, 3, 3], which correctly reflects an LIS of one.
-
-
- */

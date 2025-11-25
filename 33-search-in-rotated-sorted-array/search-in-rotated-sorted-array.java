4892278e5bc4ca396d1cd28bb0e889949447c6c9
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-25 16:29:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (20.12%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -8,13 +8,14 @@ class Solution {
             if (nums[m] == target) return m;
 
             if (nums[l] <= nums[m]) {
-                if (nums[l] <= target && target <= nums[m]) r = m - 1;
+                if (nums[l] <= target && target < nums[m]) r = m - 1;
                 else l = m + 1;
-            }  else {
-                if (nums[m] <= target && target <= nums[r]) l = m + 1;
+            } else {
+                if (nums[m] < target && target <= nums[r]) l = m + 1;
                 else r = m - 1;
             }
         }
+
         return -1;
     }
 }

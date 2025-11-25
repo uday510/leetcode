c535db2b5c00580cea1566e9a5ebc993c029ae70
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-25 17:55:52 +0530

    Time: 14 ms (86.05%) | Memory: 45.9 MB (43.08%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,29 +1,18 @@
 class Solution {
-    int[] coins;
-    int inf = (int) 1e9;
-    int[] dp;
+    public int coinChange(int[] C, int A) {
+        int n = C.length;
+        int[] dp = new int[A + 1];
 
-    public int coinChange(int[] coins, int amount) {
-        this.coins = coins;
-        dp = new int[amount + 1];
-        Arrays.fill(dp, -1);
-        dp[0] = 0;
+        for (int a = 1; a <= A; a++) {
+            dp[a] = A + 1;
 
-        dfs(amount);
-        
-        return dp[amount] == inf ? -1 : dp[amount];
-    }
-    private int dfs(int req) {
-        if (req < 0) return (int) 1e9;
-
-        if (dp[req] != -1) return dp[req];
-
-        int cur = inf;
-        for (int c : coins) {
-
-            cur = Math.min(cur, 1 + dfs(req - c));
+            for (int c : C) {
+                if (a - c >= 0) {
+                    dp[a] = Math.min(dp[a], 1 + dp[a - c]);
+                }
+            }
         }
 
-        return dp[req] = cur;
+        return dp[A] > A ? -1 : dp[A];
     }
 }

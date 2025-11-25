bc415cfd784f424bfb9efc03d41b37480f858015
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-25 18:53:24 +0530

    Time: 3 ms (95.67%) | Memory: 47.1 MB (17.35%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,13 +1,16 @@
 class Solution {
+
     int m, n;
     String s1, s2;
     int[][] dp;
-    public int minDistance(String word1, String word2) {
-        m = word1.length();
-        n = word2.length();
-        s1 = word1;
-        s2 = word2;
+
+    public int minDistance(String w1, String w2) {
+        m = w1.length();
+        n = w2.length();
+        s1 = w1;
+        s2 = w2;
         dp = new int[m][n];
+
         for (int[] row : dp) Arrays.fill(row, -1);
 
         return dfs(0, 0);
@@ -20,12 +23,14 @@ class Solution {
 
         if (dp[i][j] != -1) return dp[i][j];
 
-        if (s1.charAt(i) == s2.charAt(j))
-             return dp[i][j] = dfs(i + 1, j + 1);
+        if (s1.charAt(i) == s2.charAt(j)) {
+            return dp[i][j] = dfs(i + 1, j + 1);
+        } 
 
         int ins = dfs(i, j + 1);
         int del = dfs(i + 1, j);
         int repl = dfs(i + 1, j + 1);
+
 
         return dp[i][j] = 1 + Math.min(ins, Math.min(del, repl));
     }

832c16cc780e0225c525f3e679af50a7807365c3
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-25 20:01:19 +0530

    Time: 869 ms (11.11%) | Memory: 231.9 MB (20.37%) - LeetSync

--- 1697-strings-differ-by-one-character/strings-differ-by-one-character.java
+++ 1697-strings-differ-by-one-character/strings-differ-by-one-character.java
@@ -1,61 +1,65 @@
 class Solution {
 
-    TrieNode root;
+    private Trie root;
 
     public boolean differByOne(String[] dict) {
-        root = new TrieNode();
+        
+        root = new Trie();
 
         for (String s : dict) {
-            if (dfs(0, 0, s, root)) 
-                return true;
+            if (dfs(s, root, 0, 0)) return true;
             insert(s);
         }
 
         return false;
     }
 
-    private boolean dfs(int i, int mismatches, String s, TrieNode node) {
-        if (node == null) return false;
+    private boolean dfs(String s, Trie cur, int i, int mismatches) {
+        if (cur == null) return false;
 
-        if (i == s.length()) {
-            return node.eow && mismatches == 1;
-        }
+        if (i == s.length()) return cur.eow && mismatches == 1;
 
         int idx = s.charAt(i) - 'a';
 
         for (int c = 0; c < 26; c++) {
-            if (node.children[c] == null) continue;
+            Trie nxt = cur.children[c];
+            if (nxt == null) continue;
 
-            int newMismatch = mismatches + (c == idx ? 0 : 1);
-            if (newMismatch <= 1) {
-                if (dfs(i + 1, newMismatch, s, node.children[c])) {
+            int newMismatches = mismatches + (c == idx ? 0 : 1);
+            if (newMismatches <= 1) {
+                if (dfs(s, nxt, i + 1, newMismatches)) {
                     return true;
                 }
             }
-        }        
+        }
+
         return false;
     }
 
     private void insert(String s) {
-        TrieNode cur = root;
+        Trie cur = root;
+
         for (int i = 0; i < s.length(); i++) {
-             int idx = s.charAt(i) - 'a';
-             if (cur.children[idx] == null) {
-                cur.children[idx] = new TrieNode();
-             }
-             cur = cur.children[idx];
+            int idx = s.charAt(i) - 'a';
+
+            if (cur.children[idx] == null) {
+                cur.children[idx] = new Trie();
+            }
+            
+            cur = cur.children[idx];
         }
+
         cur.eow = true;
     }
 }
 
-class TrieNode {
-
-    TrieNode[] children;
+class Trie {
+    Trie[] children;
     boolean eow;
-    
-    TrieNode () {
-        children = new TrieNode[26];
+
+    Trie () {
+        children = new Trie[26];
         eow = false;
     }
+
 }

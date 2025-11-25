cb76dd7b16531a1b2c91dd0deba1d98cfded897c
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-25 15:56:44 +0530

    Time: 2 ms (30.76%) | Memory: 47 MB (12.38%) - LeetSync

--- 399-evaluate-division/evaluate-division.java
+++ 399-evaluate-division/evaluate-division.java
@@ -3,42 +3,42 @@ class Solution {
     Map<String, List<Pair>> adjList;
     int n;
 
-    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        n = equations.size();
+    public double[] calcEquation(List<List<String>> E, double[] V, List<List<String>> Q) {
+        n = E.size();
         adjList = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
-            var eq = equations.get(i);
+            var e = E.get(i);
 
-            String u = eq.get(0), v = eq.get(1);
-            double d = values[i];
+            String u = e.get(0), v = e.get(1);
+            double d = V[i];
 
             adjList.computeIfAbsent(u, k -> new ArrayList<>()).add(new Pair(v, d));
-            adjList.computeIfAbsent(v, k -> new ArrayList<>()).add(new Pair(u, 1.0/d));
+            adjList.computeIfAbsent(v, k -> new ArrayList<>()).add(new Pair(u, 1.0 / d));
         }
 
-        double[] dist = new double[queries.size()];
+        double[] D = new double[Q.size()];
 
-        for (int i = 0; i < dist.length; i++) {
-            String u = queries.get(i).get(0), v = queries.get(i).get(1);
+        for (int i = 0; i < Q.size(); i++) {
+            String u = Q.get(i).get(0), v = Q.get(i).get(1);
 
-            dist[i] = dfs(u, v, new HashSet<>());
+            D[i] = dfs(u, v, new HashSet<>());
         }
 
-        return dist;
+        return D;
     }
 
     private double dfs(String u, String v, Set<String> vis) {
         if (!adjList.containsKey(u) || !adjList.containsKey(v) || !vis.add(u)) return -1.0;
-        
+
         if (u.equals(v)) return 1.0;
 
         for (Pair nei : adjList.get(u)) {
             double d = dfs(nei.v, v, vis);
-            
+
             if (d != -1.0) return nei.d * d;
         }
-        
+
         return -1.0;
     }
 }
@@ -47,7 +47,7 @@ class Pair {
     String v;
     Double d;
 
-    public Pair(String v, Double d) {
+    Pair(String v, Double d) {
         this.v = v;
         this.d = d;
     }

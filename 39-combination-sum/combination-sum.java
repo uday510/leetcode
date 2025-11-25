c8076fbf1531ccb8863271045527f34598da7869
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-25 14:19:31 +0530

    Time: 2 ms (89.48%) | Memory: 45.3 MB (36.86%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,8 +1,9 @@
 class Solution {
+
     List<List<Integer>> res;
     int[] c;
-    int t;
-    int n;
+    int t, n;
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         res = new ArrayList<>();
         c = candidates;
@@ -12,19 +13,21 @@ class Solution {
         dfs(0, new ArrayList<>(), 0);
         return res;
     }
+
     private void dfs(int i, List<Integer> list, int cur) {
         if (cur == t) {
             res.add(new ArrayList<>(list));
             return;
         }
+
         if (cur > t || i >= n) {
             return;
-        } 
+        }
+
+        dfs(i + 1, list, cur);
 
         list.add(c[i]);
         dfs(i, list, cur + c[i]);
-
         list.removeLast();
-        dfs(i + 1, list, cur);
     }
 }

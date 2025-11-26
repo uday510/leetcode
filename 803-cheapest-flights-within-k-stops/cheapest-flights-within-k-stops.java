d661f48a5e54c560017a0678d2b60d86bfde2db8
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-26 12:00:42 +0530

    Time: 9 ms (27.93%) | Memory: 46.5 MB (26.37%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,10 +1,12 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] edges, int src, int dst, int k) {
-        List<int[]>[] adjList  = new ArrayList[n];
+        List<int[]>[] adjList = new ArrayList[n];
+
         for (int i = 0; i < n; i++) adjList[i] = new ArrayList<>();
 
         for (int[] e : edges) {
             int u = e[0], v = e[1], w = e[2];
+
             adjList[u].add(new int[] {v, w});
         }
 
@@ -12,22 +14,22 @@ class Solution {
         int inf = Integer.MAX_VALUE;
         Arrays.fill(dist, inf);
 
-        PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(x -> x[2]));
-        queue.offer(new int[] {src, 0, 0});
+        Queue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(x -> x[2]));
         dist[src] = 0;
+        pq.offer(new int[] {src, 0, 0});
 
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
             int u = cur[0], w = cur[1], stops = cur[2];
 
             if (stops > k) continue;
-            
+
             for (int[] nxt : adjList[u]) {
                 int v = nxt[0], w1 = nxt[1];
 
                 if (w + w1 < dist[v]) {
                     dist[v] = w + w1;
-                    queue.offer(new int[] {v, w + w1, stops + 1});
+                    pq.offer(new int[] {v, w + w1, stops + 1});
                 }
             }
         }

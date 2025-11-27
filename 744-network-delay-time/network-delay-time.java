c73fe0cbaef356071dad37358731ceea8df405b1
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-27 08:09:02 +0530

    Time: 11 ms (69.96%) | Memory: 49.3 MB (31.25%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,24 +1,22 @@
 class Solution {
-
-    final static int INF = Integer.MAX_VALUE;
-
     public int networkDelayTime(int[][] edges, int n, int src) {
-        List<int[]>[] adjList = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) adjList[i] = new ArrayList<>();
+        List<int[]>[] adj = new ArrayList[n + 1];
+
+        for (int i = 1; i <= n; i++) adj[i] = new ArrayList<>();
 
         for (int[] e : edges) {
             int u = e[0], v = e[1], w = e[2];
-            adjList[u].add(new int[] {v, w});
+            adj[u].add(new int[] {v, w});
         }
 
-        // PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-        Queue<int[]> pq = new ArrayDeque<>();
+        Queue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(x -> x[1]));
 
+        int unknown = Integer.MAX_VALUE;
         int[] dists = new int[n + 1];
-        Arrays.fill(dists, INF);
-
-        pq.offer(new int[] {src, 0});
+        Arrays.fill(dists, unknown);
+        
         dists[src] = 0;
+        pq.offer(new int[] {src, 0});
 
         while (!pq.isEmpty()) {
             int[] cur = pq.poll();
@@ -26,7 +24,7 @@ class Solution {
 
             if (dists[u] < w) continue;
 
-            for (int[] nxt : adjList[u]) {
+            for (int[] nxt : adj[u]) {
                 int v = nxt[0], w1 = nxt[1];
 
                 if (w + w1 < dists[v]) {
@@ -36,10 +34,12 @@ class Solution {
             }
         }
 
-        int max = Integer.MIN_VALUE;
-
+        int max = Integer.MIN_VALUE;;
         for (int i = 1; i <= n; i++) {
-            if (dists[i] == INF) return -1;
+            if (dists[i] == unknown) {
+                max = -1;
+                break;
+            }
             max = Math.max(max, dists[i]);
         }
 

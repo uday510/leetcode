ec0eab5031c57ee40d9832844eb7cbcdd8bd3715
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-27 16:51:59 +0530

    Time: 4 ms (37.39%) | Memory: 48.9 MB (6.07%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -1,39 +1,41 @@
 class Solution {
 
-    Map<Integer, List<String>> memo;
+    Map<Integer, List<String>> dp;
     Set<String> dict;
     String s;
     int n;
 
     public List<String> wordBreak(String s, List<String> wordDict) {
+        this.dp = new HashMap<>();
+        this.dict = new HashSet<>(wordDict);
         this.s = s;
         this.n = s.length();
-        this.dict = new HashSet<>(wordDict);
-        this.memo = new HashMap<>();
+
         return dfs(0);
     }
 
     private List<String> dfs(int i) {
-        if (memo.containsKey(i)) {
-            System.out.println("CACHED");
-            return memo.get(i);
-        }
-        
-        if (i == n) return List.of("");
+        if (i >= n) return List.of("");
 
-        List<String> result = new ArrayList<>();
+        if (dp.containsKey(i)) {
+            System.out.println("CACHED");
+            return dp.get(i);
+        }
+
+        List<String> res = new ArrayList<>();
 
         for (int j = i; j < n; j++) {
             String cur = s.substring(i, j + 1);
             if (!dict.contains(cur)) continue;
 
             for (String suffix : dfs(j + 1)) {
-                if (suffix.isEmpty()) result.add(cur);
-                else result.add(cur + " " + suffix);
+                if (suffix.isEmpty()) res.add(cur);
+                else res.add(cur + " " + suffix);
             }
         }
 
-        memo.put(i, result);
-        return result;
+        dp.put(i, res);
+
+        return res;
     }
 }

4533527c82d788fc5e4a3a944a815b06c993903c
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-27 13:28:28 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (74.49%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -2,13 +2,20 @@ class Solution {
     public void reverseString(char[] s) {
         int l = 0, r = s.length - 1;
 
-        while (l < r) {
-            char tmp = s[l];
-            s[l] = s[r];
-            s[r] = tmp;
+        while (l < r) swap(l++, r--, s);
+    }
 
-            l++; r--;
-        }
-        
+    private void swap(int i, int j, char[] s) {
+        char tmp = s[i];
+        s[i] = s[j];
+        s[j] = tmp;
     }
 }
+
+/**
+
+     l        r 
+["o","e","l","l","h"]
+
+
+ */

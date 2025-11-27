c94f2873b482a0643615aa470261e657de3159ee
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-27 10:42:23 +0530

    Time: 63 ms (31.51%) | Memory: 44.4 MB (37.86%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,24 +1,26 @@
 class Solution {
 
     int[] dp;
-    int INF = 0x7ffffff;
 
     public int numSquares(int n) {
         dp = new int[n + 1];
         Arrays.fill(dp, -1);
+        dp[0] = 0;
+        dp[1] = 1;
 
-        return dfs(n);
+        return dfs(n);  
     }
+
     private int dfs(int n) {
-        if (n < 0) return INF;
+        if (n < 0) return -1;
         if (n == 0) return 0;
 
         if (dp[n] != -1) return dp[n];
 
-        int min = n;
+        int min = (int) 1e9;
         for (int i = 1; i * i <= n; i++) {
             int cur = dfs(n - i * i);
-            if (cur != INF) min = Math.min(min, 1 + cur);
+            if (cur != -1) min = Math.min(min, cur + 1);
         }
 
         return dp[n] = min;

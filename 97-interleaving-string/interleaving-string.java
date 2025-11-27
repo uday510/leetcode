63611e25dd3f5dfe8100c6df97bb1db78b90cc10
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-27 12:32:04 +0530

    Time: 1 ms (90.47%) | Memory: 43.3 MB (26.66%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -15,23 +15,24 @@ class Solution {
 
         if (n + m != t) return false;
 
-        return dfs(0, 0);
+        return dfs(0, 0);    
     }
 
     private boolean dfs(int i, int j) {
         if (i == n && j == m) return true;
 
         if (dp[i][j] != null) return dp[i][j];
-
+        
         int k = i + j;
         boolean ok = false;
 
-        if (i < n && s1.charAt(i) == s3.charAt(k))
+        if (i < n && s1.charAt(i) == s3.charAt(k)) 
             ok |= dfs(i + 1, j);
         
         if (j < m && s2.charAt(j) == s3.charAt(k)) 
             ok |= dfs(i, j + 1);
-
+        
         return dp[i][j] = ok;
     }
+    
 }

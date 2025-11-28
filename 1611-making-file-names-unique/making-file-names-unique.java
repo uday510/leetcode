0aa93f0ef9beaa1e17fdc9c337bc16b9b50bd122
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-28 10:30:42 +0530

    Time: 60 ms (61.19%) | Memory: 83.5 MB (50.00%) - LeetSync

--- 1611-making-file-names-unique/making-file-names-unique.java
+++ 1611-making-file-names-unique/making-file-names-unique.java
@@ -1,10 +1,12 @@
 class Solution {
     public String[] getFolderNames(String[] names) {
+        
         int n = names.length;
         String[] res = new String[n];
         Map<String, Integer> map = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
+
             String name = names[i];
 
             if (!map.containsKey(name)) {
@@ -21,11 +23,11 @@ class Solution {
             }
 
             res[i] = newName;
-            map.put(name, k);
+            map.put(name, k + 1);
 
             map.put(newName, 1);
         }
-        
+
         return res;
     }
 }

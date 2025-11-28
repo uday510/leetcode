7013432f81fc0acedf0dbd6e5c41e445616a2fca
uday <BUDAYTEJA009@GMAIL.COM>
2025-11-28 08:59:03 +0530

    Time: 49 ms (38.94%) | Memory: 133.6 MB (8.10%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,36 +1,57 @@
 class LRUCache {
-    
-    private final Map<Integer, Node> lru;
-    private final int capacity;
-    private Node head, tail;
+
+    private Map<Integer, Node> lru;
+    private int capacity;
+    private Node head;
+    private Node tail;
 
     public LRUCache(int capacity) {
         lru = new HashMap<>();
         this.capacity = capacity;
         head = new Node(-1, -1);
         tail = new Node(-1, -1);
+
         head.next = tail;
         tail.prev = head;
     }
     
     public int get(int key) {
-        Node cur = lru.get(key);
-        if (cur == null) return -1;
+        Node node = lru.get(key);
 
-        remove(cur);
-        add(cur);
+        if (node == null) return -1;
 
-        return cur.v;
+        remove(node);
+        add(node);
+
+        return node.v;
+    }
+    
+    public void put(int key, int value) {
+        Node node = lru.get(key);
+
+        if (node != null) {
+            remove(node);
+            node.v = value;
+        } else {
+            if (lru.size() == capacity) {
+                remove(head.next);
+            }
+
+            node = new Node(key, value);
+        }
+
+        add(node);
     }
 
     private void add(Node node) {
         lru.put(node.k, node);
 
         Node tailPrev = tail.prev;
-        node.prev = tailPrev;
+
         tailPrev.next = node;
-        node.next = tail;
+        node.prev = tailPrev;
         tail.prev = node;
+        node.next = tail;
     }
 
     private void remove(Node node) {
@@ -39,30 +60,16 @@ class LRUCache {
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
-    
-    public void put(int key, int value) {
-        Node cur = lru.get(key);
-        
-        if (cur != null) {
-            remove(cur);
-            cur.v = value;
-        } else {
-            if (lru.size() == capacity) {
-                remove(head.next);
-            }
-            cur = new Node(key, value);
-        }
-
-        add(cur);
-    }
 }
 
 class Node {
     Node prev, next;
+
     int k, v;
 
-    Node (int k, int v) { 
+    Node (int k, int v) {
         this.k = k;
         this.v = v;
     }
+
 }

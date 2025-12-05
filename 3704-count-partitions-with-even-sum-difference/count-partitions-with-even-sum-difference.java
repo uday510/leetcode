7022058f9feba2c614421c80a1a78f7b59351569
uday <BUDAYTEJA009@GMAIL.COM>
2025-12-05 09:10:49 +0530

    Time: 4 ms (6.35%) | Memory: 44 MB (6.04%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
@@ -1,15 +1,5 @@
 class Solution {
     public int countPartitions(int[] nums) {
-        int rSum = Arrays.stream(nums).sum();
-        int lSum = 0, res = 0;
-
-        for (int i = 0; i < nums.length - 1; i++) {
-            lSum += nums[i];
-            rSum -= nums[i];
-
-            res += (Math.abs(lSum - rSum) & 1) == 0 ? 1 : 0;
-        }
-
-        return res;
+        return (Arrays.stream(nums).sum() & 1) == 0 ? nums.length - 1 : 0;
     }
 }

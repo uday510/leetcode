4c34c31ec890a805b591b889cd7ae146add6ff8c
uday <BUDAYTEJA009@GMAIL.COM>
2025-12-06 15:30:21 +0530

    Time: 5 ms (15.44%) | Memory: 42.9 MB (40.56%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.java
+++ 376-wiggle-subsequence/wiggle-subsequence.java
@@ -1,17 +1,27 @@
 class Solution {
     public int wiggleMaxLength(int[] nums) {
-        int up = 1, down = 1;
+        
+        int n = nums.length;
 
-        for (int i = 1; i < nums.length; i++) {
+        int[] up = new int[n];
+        int[] down = new int[n];
+        up[0] = down[0] = 1;
 
-            if (nums[i] - nums[i - 1] > 0) {
-                up = down + 1;
-            } else if (nums[i] - nums[i - 1] < 0) {
-                down = up + 1;
+        for (int i = 1; i < n; i++) {
+            up[i] = down[i] = 1;
+
+            for (int j = 0; j < i; j++) {
+
+                if (nums[i] > nums[j]) {
+                    up[i] = Math.max(up[i], down[j] + 1);
+                } else if (nums[i] < nums[j]) {
+                    down[i] = Math.max(down[i], up[j] + 1);
+                }
+
             }
-            
+
         }
 
-        return Math.max(up, down);
+        return Math.max(up[n - 1], down[n - 1]);
     }
 }

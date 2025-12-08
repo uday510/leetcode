3c3d629aff8e03a1e44852647f73d3b6e14e8b47
uday <BUDAYTEJA009@GMAIL.COM>
2025-12-08 17:34:00 +0530

    Time: 6 ms (7.25%) | Memory: 48.5 MB (12.15%) - LeetSync

---
+++ 930-all-possible-full-binary-trees/all-possible-full-binary-trees.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    Map<Integer, List<TreeNode>> dp;
+
+    public List<TreeNode> allPossibleFBT(int n) {
+        dp = new HashMap<>();
+
+        return dfs(n);
+    }
+
+    private List<TreeNode> dfs(int n) {
+        if (n < 1) return new ArrayList<TreeNode>();
+
+        if (n == 1) return Arrays.asList(new TreeNode());
+
+        List<TreeNode> list = new ArrayList<>();
+
+        for (int i = 1; i < n; i += 2) {
+            List<TreeNode> left = dfs(i);
+            List<TreeNode> right = dfs(n - i - 1);
+
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode root = new TreeNode(0, l, r);
+                    list.add(root);
+                }
+            }
+        }
+
+        dp.put(n, list);
+
+        return list;
+    }
+}

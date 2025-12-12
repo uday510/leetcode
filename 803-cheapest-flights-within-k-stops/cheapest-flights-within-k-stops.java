6a97cab6a16f78c556fce12a1dd48d96fcdb6064
uday <BUDAYTEJA009@GMAIL.COM>
2025-12-12 14:33:03 +0530

    Time: 13 ms (15.12%) | Memory: 46.5 MB (32.13%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,38 +1,45 @@
 class Solution {
-    public int findCheapestPrice(int n, int[][] edges, int src, int dst, int k) {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        
         List<int[]>[] adjList = new ArrayList[n];
+
         for (int i = 0; i < n; i++) adjList[i] = new ArrayList<>();
 
-        for (int[] e : edges) {
-            int u = e[0], v = e[1], w = e[2];
+        for (int[] flight : flights) {
+            int u = flight[0], v = flight[1], w = flight[2];
+
             adjList[u].add(new int[] {v, w});
         }
 
-        int inf = Integer.MAX_VALUE;
-        int[] dist = new int[n];
-        Arrays.fill(dist, inf);
+        int[][] dists = new int[k + 2][n];
+        int unknown = (int) 1e9;
 
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[] {src, 0, 0});
-        dist[src] = 0;
+        for (int[] row : dists) Arrays.fill(row, unknown);
 
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int u = cur[0], curK = cur[1], w = cur[2];
+        Queue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt( x -> x[1]));
 
-            if (curK > k) continue;
+        dists[0][src] = 0;
+        pq.offer(new int[] {src, 0, 0});
+
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int u = cur[0], cost = cur[1], stops = cur[2];
+
+            if (u == dst) return cost;
+
+            if (stops == k + 1) continue;
 
             for (int[] nxt : adjList[u]) {
-                int v = nxt[0], w1 = nxt[1];
+                int v = nxt[0], w = nxt[1];
 
-                if (w + w1 < dist[v]) {
-                    dist[v] = w + w1;
-                    System.out.println(u + " -> " + v + " " + dist[v]);
-                    queue.offer(new int[]{v, curK + 1, w + w1});
+                if (cost + w < dists[stops + 1][v]) {
+                    dists[stops + 1][v] = cost + w;
+                    pq.offer(new int[] {v, cost + w, stops + 1});
                 }
             }
         }
-    return dist[dst] == inf ? -1 : dist[dst];
 
+
+        return -1;
     }
 }

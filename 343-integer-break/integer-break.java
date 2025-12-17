b88fd84323b0854f36ee1b06be070aa1804a8758
uday <BUDAYTEJA009@GMAIL.COM>
2025-12-17 12:47:42 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (38.59%) - LeetSync

--- 343-integer-break/integer-break.java
+++ 343-integer-break/integer-break.java
@@ -1,60 +1,20 @@
 class Solution {
-
     int[] dp;
 
     public int integerBreak(int n) {
-        if (n <= 3) return n - 1;
-        
         dp = new int[n + 1];
         Arrays.fill(dp, -1);
-        
         return dfs(n);
     }
 
     private int dfs(int n) {
+        if (n == 1) return 1;
         if (dp[n] != -1) return dp[n];
 
-        int cur = 1;
-        for (int i = 1; i <= n; i++) {
-            cur = Math.max(cur, i * dfs(n - i));
+        int best = 0;
+        for (int i = 1; i < n; i++) {
+            best = Math.max(best, i * Math.max(n - i, dfs(n - i)));
         }
-
-        return dp[n] = cur;
+        return dp[n] = best;
     }
 }
-
-/**
-
-n = 5
-
-dfs(5)
-
-
-max = max, 1 * dfs(4) (4) = 4
-    = max, 2 * dfs(3) (3) = 6
-    = max, 3 * dfs(2) (2) = 6
-    = max, 4 * dfs(1) (1) = 4
-    = max, 5 * dfs(0) (1) = 5
-
-dfs(4) = 4
-    max = max, 1 * dfs(3) (2)
-        = max, 2 * dfs(2) (2)
-        = max, 3 * dfs(1) (1)
-        = max, 4 * dfs(0) (1)
-
-
-dfs(3) = 3
-    max = max, 1 * dfs(2) (1)
-        = max, 2 * dfs(1) (1)
-        = max, 3 * dfs(0) (1)
-
-
-
-dfs(2) = 2
-    max = max, 1 * dfs(1) , 1
-        = max, 2 * dfs(0) (1)
-
-dfs(1) = 1
-dfs(0) = 1
-
- */

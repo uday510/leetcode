488b541aef8e0966ef839bc9cd04a898e7aa6469
uday <BUDAYTEJA009@GMAIL.COM>
2025-12-20 12:11:21 +0530

    Time: 12 ms (7.36%) | Memory: 47.1 MB (39.84%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
@@ -2,13 +2,14 @@ class Solution {
     public int minDeletionSize(String[] strs) {
         int rows = strs.length, cols = strs[0].length();
         int cnt = 0;
+        boolean valid;
 
         for (int c = 0; c < cols; c++) {
-            for (int r = 1; r < rows; r++) {
+            valid = true;
+            for (int r = 1; r < rows && valid; r++) {
 
-                boolean valid = getAscii(strs[r].charAt(c)) - getAscii(strs[r - 1].charAt(c)) >= 0;
+                valid = getAscii(strs[r].charAt(c)) - getAscii(strs[r - 1].charAt(c)) >= 0;
                 cnt += !valid ? 1 : 0;
-                if (!valid) break;
             }
         }
 

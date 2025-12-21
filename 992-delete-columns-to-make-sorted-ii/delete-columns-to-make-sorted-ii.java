90e2d037018ee6c617ead76c15e5687f0cbfe5a5
uday <BUDAYTEJA009@GMAIL.COM>
2025-12-21 18:16:26 +0530

    Time: 1 ms (78.08%) | Memory: 43.6 MB (50.68%) - LeetSync

--- 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.java
+++ 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.java
@@ -30,7 +30,7 @@ class Solution {
 
     private void updateRows(int col) {
         for (int row = 0; row < rows - 1; row++) {
-            if (strs[row].charAt(col) < strs[row + 1].charAt(col)) {
+            if (!sorted[row] && strs[row].charAt(col) < strs[row + 1].charAt(col)) {
                 sorted[row] = true;
             }
         }

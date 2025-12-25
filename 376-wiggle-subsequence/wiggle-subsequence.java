22f44e01fff14f85e9c7ec87b6858d3504c4df47
uday <BUDAYTEJA009@GMAIL.COM>
2025-12-25 22:58:26 +0530

    Time: 4 ms (15.51%) | Memory: 43 MB (45.04%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.java
+++ 376-wiggle-subsequence/wiggle-subsequence.java
@@ -7,7 +7,7 @@ class Solution {
         int mx = 1;
         for (int i = 0; i < n; i++) {
             dw[i] = up[i] = 1;
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < i; j++) {
                 if (nums[i] < nums[j]) {
                     dw[i] = Math.max(dw[i], up[j] + 1);
                 } else if (nums[i] > nums[j]) {

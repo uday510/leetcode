cb3a7037d6e850bb43ab56eb7fe99264e2c4f118
uday <BUDAYTEJA009@GMAIL.COM>
2025-12-31 10:28:18 +0530

    Time: 70 ms (58.38%) | Memory: 76.6 MB (78.68%) - LeetSync

--- 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.java
+++ 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.java
@@ -1,28 +1,28 @@
 class Solution {
 
     private final int[][] DIRs = { {0, 1}, {1, 0}, {-1, 0}, {0, -1} };
-    private int r, c;
+    private int n, m;
     private int[][] cells;
 
-    public int latestDayToCross(int r, int c, int[][] cells) {
-        this.r = r;
-        this.c = c;
+    public int latestDayToCross(int rows, int cols, int[][] cells) {
+        n = rows;
+        m = cols;
         this.cells = cells;
 
-        int left = 1, right = r * c + 1;
+        int l = 1, r = n * m + 1;
 
-        while (left < right) {
-            int m = (left + right) >> 1;
+        while (l < r) {
+            int m = (l + r) >> 1;
 
-            if (bfs(m)) left = m + 1;
-            else right = m;
+            if (bfs(m)) l = m + 1;
+            else r = m;
         }
 
-        return left - 1;
+        return l - 1;
     }
 
     public boolean bfs(int d) {
-        int[][] mat = new int[r][c];
+        int[][] mat = new int[n][m];
         int cnt = 0;
 
         for (int i = 0; i < d; i++) {
@@ -32,7 +32,7 @@ class Solution {
 
         Queue<int[]> queue = new ArrayDeque<>();
 
-        for (int i = 0; i < c; i++) {
+        for (int i = 0; i < m; i++) {
             if (mat[0][i] == 0) {
                 queue.offer(new int[]{0, i});
                 mat[0][i] = -1;
@@ -43,12 +43,12 @@ class Solution {
             int[] cur = queue.poll();
             int dx = cur[0], dy = cur[1];
 
-            if (dx == r - 1) return true;
+            if (dx == n - 1) return true;
 
             for (int[] dir : DIRs) {
                 int nx = dx + dir[0], ny = dy + dir[1];
 
-                if (nx < 0 || nx >= r || ny < 0 || ny >= c || mat[nx][ny] != 0) continue;
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || mat[nx][ny] != 0) continue;
 
                 mat[nx][ny] = -1;
                 queue.offer(new int[]{nx, ny});

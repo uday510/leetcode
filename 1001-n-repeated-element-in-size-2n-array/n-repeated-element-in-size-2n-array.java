bc38c523c87a16ae8faf5dea9618debbc23fc9f2
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-02 10:22:13 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (12.74%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -1,14 +1,17 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
         
-        for (int k = 1; k < 4; k++) {
-            for (int i = 0; i < nums.length - k; i++) {
-                if (nums[i] == nums[i + k]) {
-                    return nums[i];
-                }
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (foundDuplicateNum(nums, i)) {
+                return nums[i];
             }
         }
 
-        return -1;
+        return nums[nums.length - 1];
+    }
+
+    private boolean foundDuplicateNum(int[] nums, int idx) {
+        return  nums[idx] == nums[idx + 1] || 
+                nums[idx] == nums[idx + 2];
     }
 }

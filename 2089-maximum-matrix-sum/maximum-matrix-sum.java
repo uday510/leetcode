42803878a776325cfd0cd31f24274266172f5a80
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-05 16:27:06 +0530

    Time: 7 ms (47.88%) | Memory: 68 MB (52.73%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.java
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.java
@@ -1,25 +1,27 @@
 class Solution {
-
     public long maxMatrixSum(int[][] matrix) {
-        long totalSum = 0;
-        int minAbsVal = Integer.MAX_VALUE;
-        int negativeCount = 0;
+        long sum = 0;
+        int negative = 0;
+        int minVal = 100000;
 
         for (int[] row : matrix) {
             for (int val : row) {
-                totalSum += Math.abs(val);
+                sum += Math.abs(val);
+
                 if (val < 0) {
-                    negativeCount++;
+                    ++negative;
                 }
-                minAbsVal = Math.min(minAbsVal, Math.abs(val));
+
+                negative %= 2;
+
+                minVal = Math.min(minVal, Math.abs(val));
             }
         }
 
-        // Adjust if the count of negative numbers is odd
-        if (negativeCount % 2 != 0) {
-            totalSum -= 2 * minAbsVal;
+        if (negative == 1) {
+            sum -= 2 * minVal;
         }
 
-        return totalSum;
+        return sum;
     }
 }

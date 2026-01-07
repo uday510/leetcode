47a62eb998a8859aa75fc9aa5fbc4f877c4836da
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-07 18:35:04 +0530

    Time: 5 ms (34.86%) | Memory: 46.5 MB (65.35%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -24,14 +24,13 @@ class Solution {
         while (idx < n) {
 
             if (idx < n - 1) {
-                String doubleStr = s.substring(idx, idx + 2);
+                String dStr = s.substring(idx, idx + 2);
 
-                if (vals.containsKey(doubleStr)) {
-                    res += vals.get(doubleStr);
+                if (vals.containsKey(dStr)) {
+                    res += vals.get(dStr);
                     idx += 2;
                     continue;
                 }
-
             }
 
             String single = s.substring(idx, idx + 1);
@@ -41,5 +40,4 @@ class Solution {
 
         return res;
     }
-
 }

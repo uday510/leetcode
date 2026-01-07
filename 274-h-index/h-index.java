6697dbd0858b1cfbbbed93ca99134698f15207fc
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-07 17:18:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (57.29%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,19 +1,23 @@
 class Solution {
-    public int hIndex(int[] citations) {
-        int n = citations.length;
+    public int hIndex(int[] C) {
+        
+        int n = C.length;
         int[] cnt = new int[n + 1];
 
-        for (int cit : citations) {
-            int cur = Math.min(cit, n);
+        for (int c : C) {
+            int cur = Math.min(c, n);
             cnt[cur]++;
         }
 
         int papers = 0;
-        for (int h = n; h >= 0; h--) {
-            papers += cnt[h];
-            if (papers >= h) return h;
+        for (int i = n; i >= 0; i--) {
+            papers += cnt[i];
+
+            if (papers >= i) {
+                return i;
+            }
         }
 
-        return 0;
+        return -1;
     }
 }

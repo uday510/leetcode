bbad35b59b27cef12da93213727e67f944cbd38d
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-07 15:34:10 +0530

    Time: 1 ms (44.97%) | Memory: 61.3 MB (56.17%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -10,6 +10,7 @@ class Solution {
         rotate(0, n - 1);
         rotate(0, k - 1);
         rotate(k, n - 1);
+        
     }
 
     private void rotate(int i, int j) {
@@ -21,5 +22,5 @@ class Solution {
         nums[i] = nums[j];
         nums[j] = tmp;
     }
-    
+
 }

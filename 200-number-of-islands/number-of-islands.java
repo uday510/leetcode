e87a0691d9bf09de7719e4943d02be3d040de246
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-08 22:08:06 +0530

    Time: 5 ms (33.12%) | Memory: 51.7 MB (77.80%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,33 +1,35 @@
 class Solution {
 
-    private final static int[][] dirs =  { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
-    private final static char WATER = '0';
-    private final static char LAND = '1';
+    private final static int[][] dirs = { {0, 1}, {1, 0}, {-1, 0}, {0, -1} };
+    private final static char W = '0';
+    private final static char L = '1';
 
-    boolean[][] vis;
-    int n, m;
+    private boolean[][] vis;
+    private int n, m;
     char[][] grid;
 
     public int numIslands(char[][] grid) {
         n = grid.length;
         m = grid[0].length;
         vis = new boolean[n][m];
-        int numIslands = 0;
+        int islands = 0;
         this.grid = grid;
+
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (vis[i][j] || grid[i][j] == WATER) continue;
+                if (vis[i][j] || grid[i][j] == W) continue;
 
-                numIslands++;
+                islands++;
                 bfs(i, j);
             }
         }
 
-        return numIslands;
+        return islands;
     }
 
     private void bfs(int i, int j) {
+
         Queue<int[]> queue = new ArrayDeque<>();
 
         vis[i][j] = true;
@@ -39,11 +41,12 @@ class Solution {
             for (int[] dir : dirs) {
                 int nx = dir[0] + cur[0], ny = dir[1] + cur[1];
 
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m || vis[nx][ny] || grid[nx][ny] == WATER) continue;
+                if (nx < 0 || nx >= n || ny < 0
+                 || ny >= m || vis[nx][ny] || grid[nx][ny] == W) continue;
 
                 vis[nx][ny] = true;
                 queue.offer(new int[] {nx, ny});
             }
         }
-    }
+    }   
 }

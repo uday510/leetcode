18143b8ec170cd0ddd4b35285089b0c27cdd1688
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-08 23:55:00 +0530

    Time: 29 ms (8.11%) | Memory: 47.8 MB (73.41%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -4,24 +4,25 @@ class Solution {
 
     public int minEatingSpeed(int[] piles, int h) {
         this.piles = piles;
+
         int l = 1, r = Arrays.stream(piles).max().getAsInt();
 
         while (l < r) {
-            int m = (l + r) >> 1;
 
+            int m = (l + r) >> 1;
             if (canEat(m, h)) r = m;
             else l = m + 1;
-        }
+        }    
 
         return l;
     }
 
-    private boolean canEat(int perHr, int reqHrs) {
-        int curHrs = 0;
+    private boolean canEat(int pHr, int rHrs) {
+        int cHrs = 0;
 
         for (int pile : piles) {
-            curHrs += Math.ceil((double) pile / perHr);
-            if (curHrs > reqHrs) return false;
+            cHrs += Math.ceil((double) pile / pHr);
+            if (cHrs > rHrs) return false;
         }
 
         return true;

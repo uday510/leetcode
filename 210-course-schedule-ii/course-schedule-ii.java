c071f91299f297a377b3bce4e935bb702b353c7b
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-08 22:37:02 +0530

    Time: 4 ms (93.89%) | Memory: 46.9 MB (59.90%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,37 +1,40 @@
 class Solution {
-    public int[] findOrder(int n, int[][] pre) {
-        List<Integer>[] adjList = new ArrayList[n];
-        int[] indegree = new int[n];
+    public int[] findOrder(int n, int[][] preq) {
 
-        for (int i = 0; i < n; i++) adjList[i] = new ArrayList<>();
+        List<Integer>[] adj = new ArrayList[n];
 
-        for (int[] p : pre) {
+        for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
+
+        int[] in = new int[n];
+
+        for (int[] p : preq) {
             int u = p[0], v = p[1];
-            adjList[v].add(u);
-            indegree[u]++;
+            adj[v].add(u);
+            in[u]++;
         }
 
         Queue<Integer> queue = new ArrayDeque<>();
 
         for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) queue.offer(i);
+            if (in[i] == 0) queue.offer(i);
         }
 
         int[] res = new int[n];
-        int numFinished = 0, idx = 0;
+        int fi = 0, idx = 0;
 
         while (!queue.isEmpty()) {
             int u = queue.poll();
-            numFinished++;
+            fi++;
             res[idx++] = u;
 
-            for (int v : adjList[u]) {
-                if (--indegree[v] == 0) {
+            for (int v : adj[u]) {
+                if (--in[v] == 0) {
                     queue.offer(v);
                 }
             }
         }
 
-        return numFinished == n ? res : new int[] {};
+        return fi == n ? res : new int[] {};
+        
     }
 }

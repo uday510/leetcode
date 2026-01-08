aacd9540eb704d5b3dd68f925b5b6d48408623b5
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-08 22:55:38 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (49.49%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -3,14 +3,16 @@ class Solution {
         
         return bs(nums, target);
     }
+
     private int bs(int[] nums, int target) {
         int l = 0, r = nums.length;
 
         while (l < r) {
-            int mid = (l + r) >> 1;
 
-            if (nums[mid] < target) l = mid + 1;
-            else r = mid;
+            int m = (l + r) >> 1;
+
+            if (nums[m] < target) l = m + 1;
+            else r = m;
         }
 
         return l;

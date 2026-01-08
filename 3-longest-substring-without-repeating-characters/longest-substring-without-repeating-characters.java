83341aac2225cb344523ca9bbc13ee60e3cfb175
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-08 10:33:35 +0530

    Time: 2 ms (97.29%) | Memory: 44.6 MB (94.30%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,30 +1,18 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        int longest = 0;
+        int[] freq = new int[128];
+        int longest = 0, n = s.length();
 
-        Set<Character> seen;
+        for (int i = 0, j = 0; j < n; j++) {
+            freq[s.charAt(j)]++;
 
-        for (int i = 0; i < n; i++) {
-            seen = new HashSet<>();
-
-            for (int j = i; j < n; j++) {
-                char ch = s.charAt(j);
-                if (!seen.add(ch)) break;
-
-                longest = Math.max(longest, j - i + 1);
-
+            while (i <= j && freq[s.charAt(j)] > 1) {
+                freq[s.charAt(i++)]--;
             }
+
+            longest = Math.max(longest, j - i + 1);
         }
 
         return longest;
     }
 }
-
-
-/**
-
-abcabcbb
-
-
- */

99e9a439d7336e8a9f4ca3a0b9cf69da7e502f23
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-08 23:23:17 +0530

    Time: 1 ms (99.62%) | Memory: 48.9 MB (56.31%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,11 +1,12 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums2.length < nums1.length) 
+        if (nums2.length < nums1.length)
             return findMedianSortedArrays(nums2, nums1);
 
         int n = nums1.length, m = nums2.length;
-        int total = m + n, target = (total + 1) >> 1;
+        int total = n + m, target = (total + 1) >> 1;
         int l = 0, r = n;
+
         double median = -1;
 
         while (l <= r) {
@@ -17,11 +18,12 @@ class Solution {
             if (l1 > r2) r = p1 - 1;
             else if (l2 > r1) l = p1 + 1;
             else if (l1 <= r2 && l2 <= r1) {
-                
-                if ((total & 1) == 1) 
+
+                if ( (total & 1) == 1) {
                     median = Math.max(l1, l2);
-                else 
+                } else {
                     median = (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                }
 
                 break;
             }
@@ -30,9 +32,20 @@ class Solution {
         return median;
     }
 
-    private int safeGet(int idx, int[] nums) {
-        if (idx < 0) return Integer.MIN_VALUE;
-        if (idx >= nums.length) return Integer.MAX_VALUE;
-        return nums[idx];
+    private int safeGet(int i, int[] nums) {
+        if (i < 0) return Integer.MIN_VALUE;
+        else if (i >= nums.length) return Integer.MAX_VALUE;
+        else return nums[i];
     }
 }
+
+/**
+
+
+[1, 2, 3, 4]
+
+
+[5, 6, 7]
+
+
+ */

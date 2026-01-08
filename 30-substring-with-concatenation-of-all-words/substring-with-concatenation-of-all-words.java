744746a5fd1e05ab099c5169558452e12c3ebfa0
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-08 10:46:16 +0530

    Time: 11 ms (97.20%) | Memory: 47 MB (93.79%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -1,14 +1,15 @@
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
+        
         int n = s.length();
         int window = words[0].length();
         List<Integer> res = new ArrayList<>();
         int m = words.length;
 
-        Map<String, Integer> wordsCntMap = new HashMap<String, Integer>();
-        for (String w : words) wordsCntMap.put(w, wordsCntMap.getOrDefault(w, 0) + 1);
+        Map<String, Integer> wCntMap = new HashMap<>();
+        for (String w : words) wCntMap.put(w, wCntMap.getOrDefault(w, 0) + 1);
 
-        Map<String, Integer> curCntMap = new HashMap<String, Integer>();
+        Map<String, Integer> curCntMap = new HashMap<>();
         for (int i = 0; i < window; i++) {
             curCntMap.clear();
             int l = i, r = i;
@@ -18,7 +19,7 @@ class Solution {
                 String curStr = s.substring(r, r + window);
                 r += window;
 
-                if (!wordsCntMap.containsKey(curStr)) {
+                if (!wCntMap.containsKey(curStr)) {
                     curCnt = 0;
                     curCntMap.clear();
                     l = r;
@@ -26,10 +27,11 @@ class Solution {
                     curCntMap.put(curStr, curCntMap.getOrDefault(curStr, 0) + 1);
                     ++curCnt;
 
-                    while (curCntMap.get(curStr) > wordsCntMap.get(curStr)) {
+                    while (curCntMap.get(curStr) > wCntMap.get(curStr)) {
                         String lStr = s.substring(l, l + window);
 
-                        curCntMap.put(lStr, curCntMap.get(lStr) - 1);
+                        curCntMap.put(lStr, curCntMap.getOrDefault(lStr, 0) - 1);
+
                         l += window;
                         --curCnt;
                     }
@@ -37,8 +39,8 @@ class Solution {
                     if (curCnt == m) res.add(l);
                 }
             }
-        }
 
+        }
 
         return res;
     }

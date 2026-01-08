567a73a45c763c86b9c106bac649bf97e1b84fb2
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-08 12:34:29 +0530

    Time: 3 ms (90.02%) | Memory: 44.7 MB (56.68%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,12 +1,15 @@
 class Solution {
+
     int[] cnt;
+
     public boolean isAnagram(String s, String t) {
+
         cnt = new int[128];
 
         updateCnt(s, 1);
         updateCnt(t, -1);
 
-        for (int i = 97; i < 128; ++i) {
+        for (int i = 97; i < 128; i++) {
             if (cnt[i] != 0) return false;
         }
 
@@ -14,8 +17,11 @@ class Solution {
     }
 
     private void updateCnt(String s, int inc) {
+
         for (int i = 0; i < s.length(); i++) {
             cnt[s.charAt(i)] += inc;
         }
+
     }
+    
 }

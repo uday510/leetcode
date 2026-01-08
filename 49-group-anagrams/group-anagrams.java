6cd91c7db01f2718f4dc0d3a7336da042ad56997
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-08 12:37:17 +0530

    Time: 18 ms (26.99%) | Memory: 50.6 MB (15.02%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,17 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         
-        Map<String, List<String>> groups = new HashMap<>();
+
+        Map<String, List<String>> grps = new HashMap<>();
 
         int[] cnts = new int[26];
 
-        for (String s : strs) {
 
+        for (String s : strs) {
 
             for (int i = 0; i < s.length(); i++) {
                 cnts[s.charAt(i) - 'a']++;
             }
-            
+
             StringBuilder sb = new StringBuilder();
 
             for (int i = 0; i < 26; i++) {
@@ -20,11 +21,11 @@ class Solution {
             }
 
             String key = new String(sb);
-            
-            groups.computeIfAbsent(key, _ -> new ArrayList<>()).add(s);
+
+            grps.computeIfAbsent(key, _ -> new ArrayList<>()).add(s);
+
         }
 
-
-        return new ArrayList<>(groups.values());
+        return new ArrayList<>(grps.values());
     }
 }

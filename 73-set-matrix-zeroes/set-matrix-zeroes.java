22edc63306fc7c05479da52650f74e18107a7a72
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-08 12:18:04 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (16.18%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,17 +1,19 @@
 class Solution {
-
     public void setZeroes(int[][] A) {
+        
 
         boolean col0 = false;
         int n = A.length, m = A[0].length;
 
         for (int i = 0; i < n; i++) {
             if (A[i][0] == 0) col0 = true;
+
             for (int j = 1; j < m; j++) {
                 if (A[i][j] == 0) {
                     A[i][0] = A[0][j] = 0;
                 }
             }
+
         }
 
         for (int i = 1; i < n; i++) {
@@ -35,5 +37,4 @@ class Solution {
         }
 
     }
-
 }

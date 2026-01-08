c8eae4aebe95b596a43d83a743c0948f6f6f68b5
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-08 21:29:14 +0530

    Time: 10 ms (22.74%) | Memory: 71.1 MB (40.27%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -2,22 +2,24 @@ class Solution {
     public int maximalSquare(char[][] A) {
         int n = A.length, m = A[0].length;
         int[][] dp = new int[n][m];
-        int max = 0;
+        int mx = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 char ch = A[i][j];
+
                 if (ch == '0') continue;
 
                 int dia = (i > 0 && j > 0 ? dp[i - 1][j - 1] : 0);
                 int up = (i > 0 ? dp[i - 1][j] : 0);
                 int left = (j > 0 ? dp[i][j - 1] : 0);
 
-                dp[i][j] = 1 + Math.min(dia, Math.min(left, up));
-                max = Math.max(max, dp[i][j]);
+                dp[i][j] = 1 + Math.min(dia, Math.min(up, left));
+
+                mx = Math.max(mx, dp[i][j]);
             }
         }
 
-        return max * max;
+        return mx * mx;
     }
 }

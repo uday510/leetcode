ab2bd93a47d36b21fbab91589f7123a6bbdc5327
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-08 23:35:16 +0530

    Time: 24 ms (86.27%) | Memory: 103.6 MB (81.98%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,19 +1,18 @@
 class Solution {
-    public int[] dailyTemperatures(int[] T) {
+    public int[] dailyTemperatures(int[] tmps) {
         Deque<Integer> st = new ArrayDeque<>();
-        int n = T.length;
+        int n = tmps.length;
         int[] res = new int[n];
 
         for (int i = 0; i < n; i++) {
-            int cur = T[i];
+            int cur = tmps[i];
 
-            while (!st.isEmpty() && cur > T[st.peek()]) {
+            while (!st.isEmpty() && cur > tmps[st.peek()]) {
                 res[st.peek()] = i - st.pop();
             }
 
             st.push(i);
         }
-
 
         return res;
     }

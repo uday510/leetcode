20f2845f2cb79696d2c0707e728d8a1fb8165834
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-08 21:09:17 +0530

    Time: 16 ms (55.93%) | Memory: 46.3 MB (44.37%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -4,22 +4,26 @@ class Solution {
     int n;
 
     public String longestPalindrome(String s) {
-        int[] res = {0, 1};
+        int[] r = {0, 1};
         this.n = s.length();
         this.s = s;
 
-        for (int i = 0; i < n; i++) {
-            int[] odd = getLen(i - 1, i + 1);
-            int[] even = getLen(i - 1, i);
+        int[] o, e, c;
 
-            int[] cur = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
-            res = cur[1] - cur[0] > res[1] - res[0] ? cur : res;
+        for (int i = 0; i < n; i++) {
+            o = getLen(i - 1, i + 1);
+            e = getLen(i - 1, i);
+
+            c = o[1] - o[0] > e[1] - e[0] ? o : e;
+            r = c[1] - c[0] > r[1] - r[0] ? c : r;
         }
 
-        return s.substring(res[0], res[1]);
+        return s.substring(r[0], r[1]);
+
     }
 
     private int[] getLen(int i, int j) {
+
         while (i > -1 && j < n && s.charAt(i) == s.charAt(j)) {
             i--; j++;
         }

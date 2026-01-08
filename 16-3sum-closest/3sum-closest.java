77b7000f1f5d768bfadeb81dc513c84ac13cd65c
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-08 10:09:28 +0530

    Time: 15 ms (97.50%) | Memory: 45.6 MB (30.31%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,55 +1,33 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int minDiff = (int) 1e9, N = nums.length;
-        int closest = (int) 1e9;
+        
+        Arrays.sort(nums);
 
-        for (int i = 0; i < N; ++i) {
-            for (int j = i + 1; j < N; ++j) {
-                for (int k = j + 1; k < N; ++k) {
-                    int sum = nums[i] + nums[j] + nums[k];
-                    if (Math.abs(sum - target) < minDiff) {
-                        closest = sum;
-                        minDiff = Math.abs(sum - target);
-                    } 
+        int minDif, curDif, curSum;
+        int n = nums.length;
+
+        minDif = curDif = (int) 1e9;
+        int closest = minDif;
+
+        for (int i = 0; i < n - 2; i++) {
+            int j = i + 1, k = n - 1;
+
+            while (j < k) {
+                curSum = nums[i] + nums[j] + nums[k];
+
+                if (curSum - target == 0) return curSum;
+                else if (curSum - target < 0) j++;
+                else k--;
+
+                curDif = Math.abs(curSum - target);
+                if (curDif < minDif) {
+                    closest = curSum;
+                    minDif = curDif;
                 }
+
             }
         }
-        
+
         return closest;
     }
 }
-
-/**
-
--4 -1 1 2
-
-4 
-
-
-ans 6
-
-1 1 4 5 6 7 
-
-t = 1
-
-
--4 -1 1 2
-t = 1
-closest = -1, minDiff = 2
-
-curr = -1 
-diff = 2
-
--------------------
-1 1 4 5 6 7 
-
-t = 4
-
-minDiff = 2 
-closest = 6
-
-curr = 8
-diff = 4
-
-
-*/

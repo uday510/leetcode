3f740f7c86e76c712303943f65d9a5c376ce9487
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-08 13:59:35 +0530

    Time: 1 ms (97.54%) | Memory: 47.3 MB (19.04%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,21 +1,31 @@
 class Solution {
-    public int[][] insert(int[][] intervals, int[] newInterval) {
-        int N = intervals.length;
+    public int[][] insert(int[][] ints, int[] A) {
+        int N = ints.length;
         int i = 0;
-        var res = new ArrayList<int[]>();
+        List<int[]> res = new ArrayList<>();
 
-        while (i < N && intervals[i][1] < newInterval[0]) 
-            res.add(intervals[i++]);
+        while (i < N && ints[i][1] < A[0]) {
+            res.add(ints[i++]);
+        } 
 
-        while (i < N && newInterval[1] >= intervals[i][0]) {
-            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-            newInterval[1] = Math.max(newInterval[1], intervals[i++][1]);
+        while (i < N && A[1] >= ints[i][0]) {
+            A[0] = Math.min(A[0], ints[i][0]);
+            A[1] = Math.max(A[1], ints[i++][1]);
         }
-        res.add(newInterval);
 
-        while (i < N) 
-            res.add(intervals[i++]);
+        res.add(A);
+
+        while (i < N) res.add(ints[i++]);
 
         return res.toArray(new int[res.size()][]);
     }
 }
+
+/**
+
+
+[1, 2] [3, 4] [4, 6] [ 6, 7]
+
+[4, 5]
+
+ */

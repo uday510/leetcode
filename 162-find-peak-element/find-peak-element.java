000af1bdec799370635a5d1beb800c96a88d663f
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-08 23:01:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (18.22%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,14 +1,15 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int l = 0, r = nums.length - 1;
         
+        int l = 0, r = nums.length - 1;
+
         while (l < r) {
             int m = (l + r) >> 1;
 
             if (nums[m] < nums[m + 1]) l = m + 1;
             else r = m;
         }
-        
+
         return l;
     }
 }

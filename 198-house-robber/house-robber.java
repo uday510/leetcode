36966d25345383368057cd98f9692f2287f7fa7b
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-19 10:31:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (63.23%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,10 +1,9 @@
 class Solution {
     public int rob(int[] nums) {
-        
         int rob1 = 0, rob2 = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            int curRob = Math.max(nums[i] + rob1, rob2);
+        for (int num : nums) {
+            int curRob = Math.max(num + rob1, rob2);
 
             rob1 = rob2;
             rob2 = curRob;

289b60c4b587ee40289bfa198e0b48c86ea2abeb
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-20 08:31:19 +0530

    Time: 13 ms (20.17%) | Memory: 46.6 MB (77.21%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -1,35 +1,34 @@
 class Solution {
-
-    Map<Integer, Integer> map;
-    Map<Integer, Integer> cache;
-
     public int deleteAndEarn(int[] nums) {
-        int n = -1;
-        map = new HashMap<>();
-        cache = new HashMap<>();
+        
+        Map<Integer, Integer> cnts = new HashMap<>();
+        int mx = -1;
 
         for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + num);
-            n = Math.max(n, num);
+            cnts.put(num, cnts.getOrDefault(num, 0) + num);
+            mx = Math.max(mx, num);
         }
 
-        return dfs(n);
-    }
+        int[] dp = new int[mx + 1];
+        dp[1] = cnts.getOrDefault(1, 0);
 
-    private int dfs(int n) {
-        if (n < 0) return 0;
-
-        if (cache.containsKey(n)) {
-            System.out.println("cached");
-            return cache.get(n);
+        for (int i = 2; i <= mx; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + cnts.getOrDefault(i, 0));
         }
 
-        int pick = map.getOrDefault(n, 0) + dfs(n - 2);
-        int dont = dfs(n - 1);
-
-        int cur = Math.max(pick, dont);
-        cache.put(n, cur);
-
-        return cur;
+        return dp[mx];
     }
 }
+
+/**
+
+2 - 4
+3 - 9
+4 - 4
+
+
+p0 = 0, p1 = 0
+
+
+
+ */

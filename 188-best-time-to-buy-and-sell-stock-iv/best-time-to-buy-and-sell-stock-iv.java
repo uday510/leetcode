859447aa0d7e3b33a52d49da70fd802b3cf08308
uday <BUDAYTEJA009@GMAIL.COM>
2026-01-26 00:09:40 +0530

    Time: 1 ms (99.71%) | Memory: 43.2 MB (93.02%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,25 +1,27 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
-        return maxProfitWithKTransactions(k, prices);
+        return maxProfitWithKTransactions(prices, k);
     }
 
-    private int maxProfitWithKTransactions(int k, int[] prices) {
+    private int maxProfitWithKTransactions(int[] prices, int k) {
 
         int[] profit = new int[k + 1];
         int[] buy = new int[k + 1];
-        
-        Arrays.fill(buy, (int) 1e9);
 
-        for (int p : prices) {
+        Arrays.fill(buy, Integer.MAX_VALUE);
+
+        for (int price : prices) {
 
             for (int i = 1; i <= k; i++) {
 
-                buy[i] = Math.min(buy[i], p - profit[i - 1]);
-                profit[i] = Math.max(profit[i], p - buy[i]);
+                buy[i] = Math.min(buy[i], price - profit[i - 1]);
+                profit[i] = Math.max(profit[i], price - buy[i]);
 
             }
+
         }
 
         return profit[k];
+
     }
 }

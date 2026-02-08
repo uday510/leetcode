77d056a3d500e13488c38923814abd7881ebe03a
uday <BUDAYTEJA009@GMAIL.COM>
2026-02-08 19:36:53 +0530

    Time: 5 ms (79.34%) | Memory: 46.8 MB (23.66%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -1,41 +1,38 @@
 class Solution {
 
-    private char OPEN = '(';
-
     public int longestValidParentheses(String s) {
         
-        Deque<Integer> stack = new ArrayDeque<>();
+
+        Deque<Integer> st = new ArrayDeque<>();
+        st.push(-1);
         int longest = 0;
-
-        stack.push(-1);
+        
         for (int i = 0; i < s.length(); i++) {
-            char cur = s.charAt(i);
+            char ch = s.charAt(i);
 
-            if (cur == OPEN) {
-                stack.push(i);
+            if (ch == '(') {
+                st.push(i);
             } else {
-               stack.pop();
-               if (stack.isEmpty()) {
-                stack.push(i);
-               } else {
-                longest = Math.max(longest, i - stack.peek());
-              }
+                st.pop();
+                if (st.isEmpty()) {
+                    st.push(i);
+                } else {
+                    longest = Math.max(longest, i - st.peek());
+                }
             }
-
         }
 
         return longest;
     }
-
 }
+
 
 /**
 
 
-0 1 2 3 4 5
-) ( ) ( ) )
-
--1, 1
 
 
-*/
+
+
+
+ */

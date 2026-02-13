e43d3ea03e79355dd3686342114d3d5ac5ec01d6
uday <BUDAYTEJA009@GMAIL.COM>
2026-02-13 13:53:10 +0530

    Time: 27 ms (28.35%) | Memory: 69.9 MB (25.00%) - LeetSync

--- 639-decode-ways-ii/decode-ways-ii.java
+++ 639-decode-ways-ii/decode-ways-ii.java
@@ -3,44 +3,47 @@ class Solution {
     private String s;
     private int n;
     private long[] dp;
-    private final int MOD = (int) 1e9 + 7;
+    private static final int mod = (int) 1e9 + 7;
 
     public int numDecodings(String s) {
+
         this.s = s;
         n = s.length();
         dp = new long[n];
+
         Arrays.fill(dp, -1);
 
-        return (int) dfs(0);
+        return (int) dfs(0);    
     }
-
+    
     private long dfs(int i) {
         if (i >= n) return 1;
 
         if (dp[i] != -1) return dp[i];
 
+        long cur = 0;
         char c1 = s.charAt(i);
         char c2 = i + 1 < n ? s.charAt(i + 1) : '#';
 
-        long cnt = 0;
+        if (c1 == '*') cur = 9 * dfs(i+ 1) % mod;
+        else if (c1 >= '1') cur = dfs(i + 1) % mod;
 
-        if (c1 == '*') cnt = 9 * dfs(i + 1) % MOD;
-        else if (c1 >= '1') cnt = dfs(i + 1) % MOD;
+        if (c2 == '#') return dp[i] = cur;
 
-        if (c2 == '#') return dp[i] = cnt;
 
         if (c1 == '*') {
-            if (c2 == '*') cnt += 15 * dfs(i + 2) % MOD;
-            else if (c2 >= '0' && c2 <= '6') cnt += 2 * dfs(i + 2) % MOD;
-            else cnt += dfs(i + 2) % MOD;
+            if (c2 == '*') cur += 15 * dfs(i + 2) % mod;
+            else if (c2 >= '0' && c2 <= '6') cur += 2 * dfs(i + 2) % mod;
+            else cur += dfs(i + 2) % mod;
         } else if (c1 == '1') {
-            if (c2 == '*') cnt += 9 * dfs(i + 2) % MOD;
-            else cnt += dfs(i + 2) % MOD;
+            if (c2 == '*') cur += 9 * dfs(i + 2) % mod;
+            else cur += dfs(i + 2) % mod;
         } else if (c1 == '2') {
-            if (c2 == '*') cnt += 6 * dfs(i + 2) % MOD;
-            else if (c2 >= '0' && c2 <= '6') cnt += dfs(i + 2) % MOD;
+            if (c2 == '*') cur += 6 * dfs(i + 2) % mod;
+            else if (c2 >= '0' && c2 <= '6') cur += dfs(i + 2) % mod;
         }
-        
-        return dp[i] = cnt % MOD;
+
+        return dp[i] = cur % mod;
+
     }
 }

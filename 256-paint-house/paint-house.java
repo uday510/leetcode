64f5f5626177086fdaa720f7e55b08df6fc97c1a
uday <BUDAYTEJA009@GMAIL.COM>
2026-02-14 18:55:26 +0530

    Time: 1 ms (86.52%) | Memory: 44.7 MB (58.18%) - LeetSync

--- 256-paint-house/paint-house.java
+++ 256-paint-house/paint-house.java
@@ -1,32 +1,35 @@
 class Solution {
 
-    private int[][] costs;
-    private int[][] dp;
-    private int k, n;
-
+    private int[][] costs, dp;
+    private int n, k;
+    
     public int minCost(int[][] costs) {
+        
         this.costs = costs;
-        k = costs[0].length;
         n = costs.length;
+        k = costs[0].length;
         dp = new int[n][k + 1];
-
+        
         for (int[] row : dp) Arrays.fill(row, -1);
-
-        return dfs(0, -1);    
+        
+        return dfs(0, -1);
     }
-
+    
     private int dfs(int i, int exc) {
+     
         if (i >= n) return 0;
-
+        
         if (dp[i][exc + 1] != -1) return dp[i][exc + 1];
-
-        int cur = (int) 1e9 + 1;
+        
+        int cur = (int) 1e9;
+        
         for (int j = 0; j < k; j++) {
             if (j == exc) continue;
-
+            
             cur = Math.min(cur, costs[i][j] + dfs(i + 1, j));
         }
-
+        
         return dp[i][exc + 1] = cur;
     }
+    
 }

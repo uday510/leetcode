90e7e47948b9bbb6a5a78e05f923dc2cbd2b7d34
uday <BUDAYTEJA009@GMAIL.COM>
2026-02-24 23:45:25 +0530

    Time: 1 ms (99.62%) | Memory: 49.2 MB (14.70%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,13 +1,13 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         
-        if (nums2.length < nums1.length) {
+        if (nums2.length < nums1.length) 
             return findMedianSortedArrays(nums2, nums1);
-        }
 
         int n = nums1.length, m = nums2.length;
-        int total = n + m, target = (total + 1) >> 1;
+        int total = n + m, target = (n + m + 1) >> 1;
         int l = 0, r = n;
+        double mid = -1;
 
         while (l <= r) {
 
@@ -16,29 +16,35 @@ class Solution {
             int l1 = safeGet(p1 - 1, nums1), r1 = safeGet(p1, nums1);
             int l2 = safeGet(p2 - 1, nums2), r2 = safeGet(p2, nums2);
 
-            if (l1 > r2) {
-                r = p1 - 1;
-            } else if (l2 > r1) {
-                l = p1 + 1;
-            } else if (l1 <= r2 & l2 <= r1) {
-                double median = -1;
+            if (l1 > r2) r = p1 - 1;
+            else if (l2 > r1) l = p1 + 1;
+            else if (l1 <= r2 && l2 <= r1) {
 
-                if ( (total & 1) == 1) {
-                    median = Math.max(l1, l2);
-                } else {
-                    median = (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
-                }
+                if ( (total & 1) == 1) mid = Math.max(l1, l2);
+                else mid = (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
 
-                return median;
+                break;
             }
         }
 
-        return -1;
+        return mid;
     }
 
-    private int safeGet(int idx, int[] nums) {
-        if (idx < 0) return Integer.MIN_VALUE;
-        if (idx >= nums.length) return Integer.MAX_VALUE;
-        return nums[idx];
+    private int safeGet(int i, int[] nums) {
+        if (i < 0) return Integer.MIN_VALUE;
+        if (i >= nums.length) return Integer.MAX_VALUE;
+        return nums[i];
     }
 }
+
+/**
+
+[0, 0]  [0, 0]
+
+n = 2, m = 2, target = 2
+
+l = 0, r = 2
+
+p1 = 1, p2 = 1
+
+ */

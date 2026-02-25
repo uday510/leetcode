219a03b83fbdfd960cffe06f70b59160aed59845
uday <BUDAYTEJA009@GMAIL.COM>
2026-02-25 20:13:37 +0530

    Time: 8 ms (61.40%) | Memory: 46.5 MB (92.84%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -5,14 +5,19 @@ class Solution {
         for (int i = 0; i < n; ++i) tmp[i] = arr[i];
 
         Arrays.sort(tmp, (o1, o2) -> {
-            int cnt1 = Integer.bitCount(o1);
-            int cnt2 = Integer.bitCount(o2);
+            int c1 = Integer.bitCount(o1);
+            int c2 = Integer.bitCount(o2);
 
-            if (cnt1 == cnt2) return o1 - o2;
-            return cnt1 - cnt2;
+            if (c1 != c2) {
+                return c1 - c2;
+            }
+
+            return Integer.compare(o1, o2);
+
         });
 
         int[] res = new int[n];
+        
         for (int i = 0; i < n; i++) {
             res[i] = tmp[i];
         }

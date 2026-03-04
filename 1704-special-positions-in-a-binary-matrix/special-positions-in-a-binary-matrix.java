20ff5f9781f0ae32e2f1264bb15f5f727eaac93e
uday <BUDAYTEJA009@GMAIL.COM>
2026-03-04 19:09:31 +0530

    Time: 1 ms (97.69%) | Memory: 47.3 MB (44.84%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
@@ -1,28 +1,36 @@
 class Solution {
 
-    public int numSpecial(int[][] arr) {
-        int n = arr.length, m = arr[0].length;
-        int[] r = new int[n];
-        int[] c = new int[m];
+    private int n, m, cnt;
+    private int[] r, c;
+    private int[][] arr;
 
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
+    public int numSpecial(int[][] arr) {
+        this.arr = arr;
+        n = arr.length;
+        m = arr[0].length;
+        r = new int[n];
+        c = new int[m];
+
+        compute(false);
+        compute(true);
+
+        return cnt;
+    }
+    
+    private void compute(boolean add) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (arr[i][j] == 1) {
-                    r[i]++;
-                    c[j]++;
+                    if (!add) {
+                        r[i]++;
+                        c[j]++;
+                    } else if (r[i] == 1 && c[j] == 1) {
+                        cnt++;
+                    }
                 }
             }
         }
 
-        int cnt = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (arr[i][j] == 1 && r[i] == 1 && c[j] == 1) {
-                    cnt++;
-                }
-             }
-        }
-
-        return cnt;
     }
 }

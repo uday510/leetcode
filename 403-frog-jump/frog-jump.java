62760ebb999e2d9817e42082b873d5efb456a521
uday <BUDAYTEJA009@GMAIL.COM>
2026-03-08 19:23:19 +0530

    Time: 36 ms (71.09%) | Memory: 47.8 MB (97.22%) - LeetSync

--- 403-frog-jump/frog-jump.java
+++ 403-frog-jump/frog-jump.java
@@ -4,36 +4,46 @@ class Solution {
     Set<Integer> stones;
     private int lastStone;
 
-
     public boolean canCross(int[] arr) {
-        lastStone = arr[arr.length - 1];
+
         dp = new HashMap<>();
         stones = new HashSet<>();
 
-        for (int s : arr) stones.add(s);
+        for (int s : arr)
+            stones.add(s);
+
+        lastStone = arr[arr.length - 1];
 
         return dfs(0, 0);
     }
 
-    private boolean dfs(int idx, int k) {
-        if (idx > lastStone) return false;
-        if (idx == lastStone) return true;
-        String key = idx + ":" + k;
+    private boolean dfs(int pos, int k) {
 
-        if (dp.containsKey(key)) return dp.get(key);
+        if (pos == lastStone)
+            return true;
 
-        for (int nxt : new int[] {k - 1, k, k + 1}) {
-            int jmp = idx + nxt;
-            if (nxt <= 0 || !stones.contains(jmp)) continue;
+        String key = pos + ":" + k;
 
-            boolean cur = dfs(jmp,  nxt);
+        if (dp.containsKey(key)) {
+            return dp.get(key);
+        }
 
-            if (cur) {
+        for (int jump = k - 1; jump <= k + 1; jump++) {
+
+            if (jump <= 0)
+                continue;
+
+            int next = pos + jump;
+
+            if (!stones.contains(next))
+                continue;
+
+            if (dfs(next, jump)) {
                 dp.put(key, true);
                 return true;
             }
         }
-        
+
         dp.put(key, false);
         return false;
     }

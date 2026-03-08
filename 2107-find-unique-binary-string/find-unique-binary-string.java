9ac4d21425faac4daf6845c28aecfd7bb2bcedfb
uday <BUDAYTEJA009@GMAIL.COM>
2026-03-08 15:56:49 +0530

    Time: 1 ms (59.94%) | Memory: 43.3 MB (64.70%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,50 +1,14 @@
 class Solution {
-
-    private static final Character ZERO;
-    private static final Character ONE;
-
-    private Set<String> vaidStrs;
-    private String res;
-    private boolean isFound;
-
-    static {
-        ZERO = '0';
-        ONE = '1';
-    }
-
     public String findDifferentBinaryString(String[] nums) {
-        vaidStrs = new HashSet<>();
-        isFound = false;
-
-        for (String str : nums) vaidStrs.add(str);
-
-        int n = nums[0].length();
-
-        dfs(0, n, new StringBuilder());
         
-        return res;
-    }
+        StringBuilder sb = new StringBuilder();
 
-    private void dfs(int idx, int n, StringBuilder sb) {
-        if (isFound) return;
+        for (int idx = 0; idx < nums.length; idx++) {
+            char cur = nums[idx].charAt(idx) == '0' ? '1' : '0';
 
-        if (idx == n) {
-            String cur = sb.toString();
-            if (!vaidStrs.contains(cur)) {
-                isFound = true;
-                res = cur;
-            }
-
-            return;
+            sb.append(cur);
         }
-        
-        sb.append(ZERO);
-        dfs(idx + 1, n, sb);
-        sb.deleteCharAt(sb.length() - 1);
 
-        sb.append(ONE);
-        dfs(idx + 1, n, sb);
-        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
     }
-
 }

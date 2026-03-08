20d92d34f396e1d9c29f3ff5a99ef4e292bed85a
uday <BUDAYTEJA009@GMAIL.COM>
2026-03-08 19:08:26 +0530

    Time: 36 ms (71.09%) | Memory: 50.8 MB (46.33%) - LeetSync

--- 403-frog-jump/frog-jump.java
+++ 403-frog-jump/frog-jump.java
@@ -1,29 +1,39 @@
 class Solution {
-    Map<String, Boolean> dp;
+
+    private Map<String, Boolean> dp;
     Set<Integer> stones;
-    public boolean canCross(int[] stones) {
+    private int lastStone;
+
+
+    public boolean canCross(int[] arr) {
+        lastStone = arr[arr.length - 1];
         dp = new HashMap<>();
-        this.stones = new HashSet<>();
-        for (int stone : stones) this.stones.add(stone);
+        stones = new HashSet<>();
 
-        return dfs(0, 0, stones[stones.length - 1]);
+        for (int s : arr) stones.add(s);
+
+        return dfs(0, 0);
     }
-    private boolean dfs(int curr, int k, int last) {
-        if (curr == last) return true;
 
-        String key = curr + "_" + k;
+    private boolean dfs(int idx, int k) {
+        if (idx > lastStone) return false;
+        if (idx == lastStone) return true;
+        String key = idx + ":" + k;
+
         if (dp.containsKey(key)) return dp.get(key);
 
-        for (int nextK : new int[]{k - 1, k, k + 1}) {
-            int jump = curr + nextK;
-            if (nextK <= 0 || !stones.contains(jump)) continue;
+        for (int nxt : new int[] {k - 1, k, k + 1}) {
+            int jmp = idx + nxt;
+            if (nxt <= 0 || !stones.contains(jmp)) continue;
 
-            if (dfs(jump, nextK, last)) {
+            boolean cur = dfs(jmp,  nxt);
+
+            if (cur) {
                 dp.put(key, true);
                 return true;
             }
         }
-
+        
         dp.put(key, false);
         return false;
     }

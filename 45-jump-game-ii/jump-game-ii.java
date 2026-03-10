e9789073d8895e1858d46006f6bdc67f9b911c16
uday <BUDAYTEJA009@GMAIL.COM>
2026-03-10 23:02:11 +0530

    Time: 89 ms (8.41%) | Memory: 47.2 MB (85.78%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,17 +1,31 @@
 class Solution {
+
+    private int[] nums, dp;
+    private int n;
+
     public int jump(int[] nums) {
-        int n = nums.length, res = 0;
-        int curEnd = 0, curFar = 0;
 
-        for (int i = 0; i < n - 1; i++) {
-            curFar = Math.max(curFar, i + nums[i]);
+        this.nums = nums;
+        this.n = nums.length;
+        this.dp = new int[n];
 
-            if (curEnd == i) {
-                curEnd = curFar;
-                res++;
-            }
+        Arrays.fill(dp, -1);
+
+        return dfs(0);    
+    }
+
+    private int dfs(int i) {
+        if (i >= n - 1) return 0;
+    
+        if (dp[i] != -1) return dp[i];
+
+        int mx = Math.min(i + nums[i], n - 1);
+
+        int min = (int) 1e9;
+        for (int j = i + 1; j <= mx; j++) {
+            min = Math.min(min, 1 + dfs(j));
         }
 
-        return res;
+        return dp[i] = min;
     }
 }

b0f97d900392dee42a7d9000f7d8bd61796481f3
uday <BUDAYTEJA009@GMAIL.COM>
2026-03-12 09:49:35 +0530

    Time: 2 ms (99.17%) | Memory: 119.2 MB (93.72%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -1,18 +1,42 @@
 class Solution {
-    public int canCompleteCircuit(int[] gas, int[] cost) {
-        int total = 0, tank = 0, idx = 0, cur, n = gas.length;
 
+    private int[] gas, cost;
+    private int n;
+
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        this.gas = gas;
+        this.cost = cost;
+        this.n = gas.length;
+
+        int total = 0, tank = 0, st = 0, cur = 0;
+        
         for (int i = 0; i < n; i++) {
+//            if (canComplete(i)) {
+//                return i;
+//            }
             cur = gas[i] - cost[i];
             total += cur;
             tank += cur;
 
             if (tank < 0) {
                 tank = 0;
-                idx = i + 1;
+                st = i + 1;
             }
         }
+        
+        return total < 0 ? -1 : st;
+    }
 
-        return total < 0 ? -1 : idx;
+    private boolean canComplete(int st) {
+
+        int cur = 0;
+        for (int i = 0; i < n; i++) {
+            int idx = (i + st) % n;
+            cur += gas[idx] - cost[idx];
+
+            if (cur < 0) return false;
+        }
+
+        return true;
     }
 }

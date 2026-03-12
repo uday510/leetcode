69c900f24a902ad279e928a15f4870eaa2aa90a3
uday <BUDAYTEJA009@GMAIL.COM>
2026-03-12 22:48:27 +0530

    Time: 143 ms (5.41%) | Memory: 43.1 MB (67.13%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public int strStr(String h, String ne) {
-        
+
         int n = h.length(), m = ne.length();
 
         for (int i = 0; i < n; i++) {
@@ -10,13 +10,13 @@ class Solution {
                 j < n && k < m 
                 && h.charAt(j) == ne.charAt(k)
             ) {
-                j++;
-                k++;
+                j++; k++;
             }
 
             if (k == m) return i;
         }
 
         return -1;
+        
     }
 }

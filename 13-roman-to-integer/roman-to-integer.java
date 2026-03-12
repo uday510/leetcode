9d7f3dbde7d43fbf028acd4b568d2fac33904715
uday <BUDAYTEJA009@GMAIL.COM>
2026-03-12 15:57:29 +0530

    Time: 5 ms (35.75%) | Memory: 46.6 MB (49.01%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,40 +1,41 @@
 class Solution {
 
-    private static Map<String, Integer> vals = new HashMap<>();
-
+    private static Map<String, Integer> map = new HashMap<>();
     static {
-        vals.put("I", 1);
-        vals.put("V", 5);
-        vals.put("X", 10);
-        vals.put("L", 50);
-        vals.put("C", 100);
-        vals.put("D", 500);
-        vals.put("M", 1000);
-        vals.put("IV", 4);
-        vals.put("IX", 9);
-        vals.put("XL", 40);
-        vals.put("XC", 90);
-        vals.put("CD", 400);
-        vals.put("CM", 900);
+        map.put("I", 1);
+        map.put("V", 5);
+        map.put("X", 10);
+        map.put("L", 50);
+        map.put("C", 100);
+        map.put("D", 500);
+        map.put( "M", 1000);
+
+        map.put("IV", 4);
+        map.put("IX", 9);
+        map.put("XL", 40);
+        map.put("XC", 90);
+        map.put("CD", 400);
+        map.put("CM", 900);
     }
 
     public int romanToInt(String s) {
+        
         int res = 0, idx = 0, n = s.length();
 
         while (idx < n) {
 
             if (idx < n - 1) {
-                String dStr = s.substring(idx, idx + 2);
+                String d = s.substring(idx, idx + 2);
 
-                if (vals.containsKey(dStr)) {
-                    res += vals.get(dStr);
+                if (map.containsKey(d)) {
+                    res += map.get(d);
                     idx += 2;
                     continue;
                 }
             }
 
-            String single = s.substring(idx, idx + 1);
-            res += vals.get(single);
+            res += map.get(s.substring(idx, idx + 1));
+
             idx += 1;
         }
 

987e026b8ee41276547a7925d107080c5b3300fe
uday <62304758+uday510@users.noreply.github.com>
2026-03-13 12:40:47 +0530

    Time: 35 ms (43.97%) | Memory: 60 MB (17.52%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,36 +1,40 @@
 class Solution {
 
-    private int[] nums;
+    private int[] arr;
     private int n;
 
     public List<List<Integer>> threeSum(int[] nums) {
-        this.nums = nums;
-        this.n = nums.length;
+        arr = nums;
+        n = nums.length;
 
         Arrays.sort(nums);
 
-        return dfs(0, 3, 0);   
+        return dfs(0, 3, 0);
     }
 
     private List<List<Integer>> dfs(int i, int k, int target) {
-        if (k == 2) return twoSum(i, target);
+
+        if (k == 2) {
+            return twoSum(i, target);
+        }
 
         List<List<Integer>> res = new ArrayList<>();
 
         for (int j = i; j < n; j++) {
-            if (j > i && nums[j] == nums[j - 1]) continue;
+            if (j > i && arr[j] == arr[j - 1]) continue;
 
-            for (List<Integer> sub : dfs(j + 1, k - 1, target - nums[j])) {
-                List<Integer> cur = new ArrayList<>();
-                cur.add(nums[j]);
-                cur.addAll(sub);
+            for (List<Integer> subList : dfs(j + 1, k - 1, target - arr[j])) {
+                List<Integer> curList = new ArrayList<>();
+                curList.add(arr[j]);
+                curList.addAll(subList);
 
-                res.add(cur);
+                res.add(curList);
             }
 
         }
 
         return res;
+ 
     }
 
     private List<List<Integer>> twoSum(int idx, int target) {
@@ -39,18 +43,20 @@ class Solution {
         int i = idx, j = n - 1;
 
         while (i < j) {
-            int cur = nums[i] + nums[j];
+            int cur = arr[i] + arr[j];
 
             if (cur < target) i++;
             else if (cur > target) j--;
             else {
-                list.add(Arrays.asList(nums[i++], nums[j--]));
-
-                while (i < n && nums[i - 1] == nums[i]) i++;
-                while (j > -1 && nums[j] == nums[j + 1]) j--;
-            }
+                list.add(new ArrayList<>(List.of(arr[i], arr[j])));
+                
+                i++; j--;
+                while (i < n && arr[i] == arr[i - 1]) i++;
+                while (j > -1 && arr[j] == arr[j + 1]) j--;
+            } 
         }
-
+        
         return list;
     }
+
 }

e781cf27f80a03fef2fc06dd4131dee5942e0498
uday <62304758+uday510@users.noreply.github.com>
2026-03-13 13:24:40 +0530

    Time: 34 ms (60.14%) | Memory: 60.2 MB (14.08%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,62 +1,55 @@
 class Solution {
 
-    private int[] arr;
-    private int n;
-
     public List<List<Integer>> threeSum(int[] nums) {
-        arr = nums;
-        n = nums.length;
-
         Arrays.sort(nums);
-
-        return dfs(0, 3, 0);
+        return dfs(0, 0, 3, nums);
     }
 
-    private List<List<Integer>> dfs(int i, int k, int target) {
-
-        if (k == 2) {
-            return twoSum(i, target);
-        }
-
+    public List<List<Integer>> dfs(int idx, int target, int k, int[] arr) {
+        if (k == 2) return twoSum(idx, target, arr);
+        
+        int n = arr.length;
         List<List<Integer>> res = new ArrayList<>();
-
-        for (int j = i; j < n; j++) {
-            if (j > i && arr[j] == arr[j - 1]) continue;
-
-            for (List<Integer> subList : dfs(j + 1, k - 1, target - arr[j])) {
-                List<Integer> curList = new ArrayList<>();
-                curList.add(arr[j]);
-                curList.addAll(subList);
-
-                res.add(curList);
+        
+        for (int i = idx; i < n; i++) {
+            if (i > idx && arr[i] == arr[i - 1]) continue;
+            
+            for (List<Integer> sub : dfs(i + 1, target - arr[i], k - 1, arr)) {
+                List<Integer> cur = new ArrayList<>();
+                cur.add(arr[i]);
+                cur.addAll(sub);
+                
+                res.add(cur);
             }
 
         }
-
-        return res;
- 
-    }
-
-    private List<List<Integer>> twoSum(int idx, int target) {
-        List<List<Integer>> list = new ArrayList<>();
-
-        int i = idx, j = n - 1;
-
-        while (i < j) {
-            int cur = arr[i] + arr[j];
-
-            if (cur < target) i++;
-            else if (cur > target) j--;
-            else {
-                list.add(new ArrayList<>(List.of(arr[i], arr[j])));
-                
-                i++; j--;
-                while (i < n && arr[i] == arr[i - 1]) i++;
-                while (j > -1 && arr[j] == arr[j + 1]) j--;
-            } 
-        }
         
-        return list;
+        return res;
+    }
+    
+    private List<List<Integer>> twoSum(int st, int target, int[] arr) {
+       int i = st, n = arr.length, j = n - 1;
+        
+       List<List<Integer>> res = new ArrayList<>();
+        
+       while (i < j) {
+           int cur = arr[i] + arr[j];
+           
+           if (cur < target) {
+               i++;
+           } else if (cur > target) {
+               j--;
+           } else {
+               res.add(Arrays.asList(arr[i], arr[j]));
+               
+               i++; j--;
+               while (i < n && arr[i] == arr[i - 1]) i++;
+               while (j > -1 && arr[j] == arr[j + 1]) j--;
+            }
+        }
+       
+       return res;
+
     }
 
 }

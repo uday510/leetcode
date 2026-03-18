e29b9f525848af2a3e95a9d6159ef4f3eaef838f
uday <62304758+uday510@users.noreply.github.com>
2026-03-18 21:22:50 +0530

    Time: 51 ms (27.48%) | Memory: 105.8 MB (5.15%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -1,32 +1,32 @@
 class Solution {
 
-    List<Integer> lis;
+    private List<Integer> lis;
 
     public int maxEnvelopes(int[][] E) {
 
         lis = new ArrayList<>();
 
         Arrays.sort(E, (o1, o2) -> {
-            if (o1[0] == o2[0]) return o2[1] - o1[1];
-            return o1[0] - o2[0];
-        });
+            return o1[0] == o2[0] ? o2[1] - o1[1] : o1[0] - o2[0];
+        });  
 
         for (int i = 0; i < E.length; i++) {
             int num = E[i][1];
-            int nxtIdx = bs(num);
-            if (nxtIdx == lis.size()) lis.add(num);
-            lis.set(nxtIdx, num);
+            int index = bs(num);
+            if (index == lis.size()) lis.add(num);
+            lis.set(index, num);
         }
 
         return lis.size();
-    }
+    }   
 
     private int bs(int target) {
         int l = 0, r = lis.size();
 
         while (l < r) {
-            int m = (l + r) >> 1;
 
+            int m = (l + r) >> 1;
+            
             if (lis.get(m) < target) l = m + 1;
             else r = m;
         }
@@ -34,3 +34,11 @@ class Solution {
         return l;
     }
 }
+
+/**
+
+
+[2, 3] [5, 4] [6, 7] [6, 4]
+
+[3, 4, 7]
+ */

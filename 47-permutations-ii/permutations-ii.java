97d700f1096c60e83b431adea76f5eac710405a6
uday <62304758+uday510@users.noreply.github.com>
2026-03-19 22:06:16 +0530

    Time: 4 ms (30.46%) | Memory: 47.2 MB (15.96%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    private List<List<Integer>> res;
+    private List<Integer> arr;
+    private int n;
+    
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+
+        res = new ArrayList<>();
+        arr = new ArrayList<>();
+        for (int num : nums) arr.add(num);
+        n = arr.size();
+
+        dfs(0);
+        return res;    
+    }
+
+    private void dfs(int idx) {
+        if (idx == n) {
+            res.add(new ArrayList<>(arr));
+            return;
+        }
+
+        Set<Integer> seen = new HashSet<>();
+        for (int curIdx = idx; curIdx < n; curIdx++) {
+            if (!seen.add(arr.get(curIdx))) continue;
+            swap(curIdx, idx);
+            dfs(idx + 1);
+            swap(curIdx, idx);
+        }
+        
+    }
+
+    private void swap(int i, int j) {
+        int tmp = arr.get(i);
+        arr.set(i, arr.get(j));
+        arr.set(j, tmp);
+    }
+
+}

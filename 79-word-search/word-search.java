5a0b77dbf8bb55de70dc81e9d3608b5cd1186a19
uday <62304758+uday510@users.noreply.github.com>
2026-03-22 15:23:34 +0530

    Time: 165 ms (26.83%) | Memory: 43 MB (71.34%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,56 +1,48 @@
 class Solution {
 
-    char[][] board;
-    String w;
-    int n, m, wLen;
-    boolean[][] vis;
-    int[][] dirs = { {0, 1}, {1, 0}, {-1, 0}, {0, -1} };
+    private char[][] B;
+    private String w;
+    private int n, m, wLen;
+    private boolean[][] vis;
+    private static final int[][] dirs = {
+        {0, 1}, {1, 0}, {-1, 0}, {0, -1}
+    };
 
     public boolean exist(char[][] board, String word) {
-        this.board = board;
+        B = board;
         w = word;
-        n = board.length;
-        m = board[0].length;
+        n = B.length;
+        m = B[0].length;
         wLen = w.length();
         vis = new boolean[n][m];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (w.charAt(0) == board[i][j]) {
-                    if (dfs(i, j, 0)) {
-                        return true;
-                    }
+                if (dfs(i, j, 0)) {
+                    return true;
                 }
             }
         }
 
         return false;
-    }
+    }   
 
-    private boolean dfs(int x, int y, int idx) {
+    private boolean dfs(int i, int j, int idx) {
         if (idx >= wLen) return true;
+        if (i >= n || i < 0 ||
+             j < 0 || j >= m || 
+            vis[i][j] || B[i][j] != w.charAt(idx)) return false;
 
-        if (!isValid(x, y, idx)) return false;
-
-        vis[x][y] = true;
+        vis[i][j] = true;
         for (int[] dir : dirs) {
-            int R = dir[0] + x, C = dir[1] + y;
+            int R = dir[0] + i, C = dir[1] + j;
 
             if (dfs(R, C, idx + 1)) {
                 return true;
             }
-
         }
 
-        vis[x][y] = false;
+        vis[i][j] = false;
         return false;
-    }
-
-    private boolean isValid(int x, int y, int idx) {
-        return (
-            x >= 0 && x < n &&
-            y >= 0 && y < m &&
-            !vis[x][y] && board[x][y] == w.charAt(idx)
-        );
     }
 }

d52288b49d40dd732f4860eecd5e6b2e4e5dede1
uday <62304758+uday510@users.noreply.github.com>
2026-03-23 01:13:52 +0530

    Time: 131 ms (85.60%) | Memory: 109.3 MB (20.75%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.java
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -1,43 +1,43 @@
 class TimeMap {
 
     Map<String, List<Pair<String, Integer>>> hm;
+
     public TimeMap() {
-        hm = new HashMap<>();
+        hm = new HashMap<>();    
     }
     
     public void set(String key, String value, int timestamp) {
-        hm.computeIfAbsent(key, k -> new ArrayList<>()).add(new Pair<>(value, timestamp));
+        hm.computeIfAbsent(key, _ -> new ArrayList<>()).add(new Pair<>(value, timestamp));
     }
     
     public String get(String key, int timestamp) {
-        List<Pair<String, Integer>> list = hm.get(key);
+        var cur = hm.get(key);
 
-        timestamp++;
+        if (cur == null) return "";
 
-        if (list == null) return "";
-
-        int l = 0;
-        int r = list.size();
+        int l = 0, r = cur.size();
 
         while (l < r) {
             int m = (l + r) >> 1;
-            if (list.get(m).v < timestamp) l = m + 1;
+            if (cur.get(m).v <= timestamp) l = m + 1;
             else r = m;
         }
+        
+        timestamp++;
 
-        if (l - 1 < 0 || l - 1 >= list.size()) return "";
+        if (l - 1 < 0 || l - 1 >= cur.size()) return "";
 
-        return list.get(l - 1).k;
+        return cur.get(l - 1).k;
     }
+}
 
-    class Pair<K, V> {
-        K k;
-        V v;
 
-        Pair(K k, V v) {
-            this.k = k;
-            this.v = v;
-        }
+class Pair<K, V> {
+    K k;
+    V v;
+
+    Pair (K k, V v) {
+        this.k = k;
+        this.v = v;
     }
-
 }

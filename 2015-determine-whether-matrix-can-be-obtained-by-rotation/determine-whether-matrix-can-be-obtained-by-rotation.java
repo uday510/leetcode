6bff9d6649695f0220af36237d4e9d9cb88b851d
uday <62304758+uday510@users.noreply.github.com>
2026-03-22 12:23:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (54.23%) - LeetSync

---
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -0,0 +1,62 @@
+class Solution {
+    public boolean findRotation(int[][] A, int[][] T) {
+        for (int k = 0; k < 4; k++) {
+            if (isEqual(A, T)) return true;
+            rotate(A);
+        }
+        return false;
+    }
+
+    public boolean isEqual(int[][] A, int[][] T) {
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[0].length; j++) {
+                if (A[i][j] != T[i][j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public void rotate(int[][] A) {
+        transpose(A);
+        reverse(A);
+    }
+
+    private void transpose(int[][] A) {
+        int n = A.length;
+        int m = A[0].length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < m; j++) {
+
+                int tmp = A[i][j];
+                A[i][j] = A[j][i];
+                A[j][i] = tmp;
+
+            }
+        }
+    }
+
+    private void reverse(int[][] A) {
+        for (int[] row : A) {
+            reverse(row);
+        }
+    }
+
+    private void reverse(int[] A) {
+        int l = 0, r = A.length - 1;
+
+        while (l < r) {
+            swap(l++, r--, A);
+        }
+    }
+
+    private void swap(int i, int j, int[] arr) {
+        int tmp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = tmp;
+    }
+
+}

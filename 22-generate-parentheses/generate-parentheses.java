dd323c7a55f5f80bf420c42bacbde05d164224e9
uday <62304758+uday510@users.noreply.github.com>
2026-03-22 14:38:54 +0530

    Time: 1 ms (85.58%) | Memory: 44.2 MB (95.39%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,37 +1,37 @@
 class Solution {
 
-    private final static String O = "(";
-    private final static String C = ")";
-    List<String> res;
-    int n;
+    private static final char OPEN_BRACKET = '(';
+    private static final char CLOSE_BRACKET = ')';
+    private List<String> res;
+    private StringBuilder sb;
+    private int n;
 
     public List<String> generateParenthesis(int n) {
-
         res = new ArrayList<>();
+        sb = new StringBuilder();
         this.n = n;
 
-        dfs(0, 0, new StringBuilder());
+        dfs(0, 0);
 
-        return res;    
+        return res;        
     }
 
-    private void dfs(int l, int r, StringBuilder sb) {
+    private void dfs(int l, int r) {
         if (sb.length() == 2 * n) {
             res.add(sb.toString());
             return;
         }
 
         if (l < n) {
-            sb.append(O);
-            dfs(l + 1, r, sb);
+            sb.append(OPEN_BRACKET);
+            dfs(l + 1, r);
             sb.deleteCharAt(sb.length() - 1);
         }
 
         if (r < l) {
-            sb.append(C);
-            dfs(l, r + 1, sb);
+            sb.append(CLOSE_BRACKET);
+            dfs(l, r + 1);
             sb.deleteCharAt(sb.length() - 1);
         }
-        
     }
 }

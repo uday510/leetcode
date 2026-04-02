986b9966efd6bd6bef560346ba1a3eb27b3c8821
uday <62304758+uday510@users.noreply.github.com>
2026-04-02 19:33:49 +0530

    Time: 182 ms (25.84%) | Memory: 278.5 MB (6.74%) - LeetSync

---
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
@@ -0,0 +1,69 @@
+class Solution {
+
+    private Integer[][][] dp;
+    private int[][] coins;
+    private int n, m;
+
+    public int maximumAmount(int[][] coins) {
+        this.coins = coins;
+        this.n = coins.length;
+        this.m = coins[0].length;
+        dp = new Integer[n][m][3];
+
+        return dfs(0, 0, 2);
+    }
+
+    private int dfs(int i, int j, int k) {
+        if (i == n - 1 && j == m - 1) {
+            if (coins[i][j] >= 0) return coins[i][j];
+            return k > 0 ? 0 : coins[i][j];
+        }
+
+        if (i >= n || j >= m) return Integer.MIN_VALUE;
+
+        if (dp[i][j][k] != null) {
+            return dp[i][j][k];
+        }
+
+        int cur = Integer.MIN_VALUE;
+
+        if (coins[i][j] >= 0) {
+            cur = Math.max(
+                dfs(i + 1, j, k),
+                dfs(i, j + 1, k)
+            ) + coins[i][j];
+        } else {
+            
+            int t1 = Math.max(dfs(i + 1, j, k), dfs(i, j + 1, k)) + coins[i][j];
+            cur = Math.max(cur, t1);
+
+            if (k > 0) {
+                cur = Math.max(
+                        cur,
+                        Math.max(
+                            dfs(i + 1, j, k - 1),
+                            dfs(i, j + 1, k - 1)
+                        )
+                    );
+            }
+        }
+
+
+        return dp[i][j][k] = cur;
+    }
+
+}
+
+/**
+
+
+[ 
+
+    [0, 1, -1]
+    [1, -2, 3]
+    [2, -3, 4]
+
+]
+
+
+ */

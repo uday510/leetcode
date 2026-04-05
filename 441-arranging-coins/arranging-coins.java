bbff08fbe452c6e8c71341f9318deb1fdc90d07b
uday <62304758+uday510@users.noreply.github.com>
2026-04-05 17:57:39 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (52.64%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -2,22 +2,16 @@ class Solution {
     public int arrangeCoins(int n) {
         if (n <= 1) return n;
 
-        long l = 1, r = n;
+        long l = 1, r = (long) n + 1;
 
         while (l < r) {
             long m = l + ( (r - l) >> 1);
 
             long coins = (long) ( m * (m + 1) / 2);
 
-            if (coins == n) return (int) m;
-
-            if (coins < n) l = m + 1;
+            if (coins <= n) l = m + 1;
             else r = m;
         }
-
-        long cur = (long) (l * (l + 1) / 2);
-
-        if (cur == n) return (int) l;
 
         return (int) l - 1;
     }

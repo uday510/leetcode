6f59f0d8495283f11ff65be3f5e628e7e7c6735a
uday <62304758+uday510@users.noreply.github.com>
2026-04-05 11:32:31 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (60.32%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,17 +1,22 @@
 class Solution {
-    public int[] searchRange(int[] arr, int target) {
-        int l = bs(arr, target);
-        if (l >= arr.length || arr[l] != target) return new int[] {-1, -1};
+    public int[] searchRange(int[] arr, int t) {
+        int p1 = bs(arr, t);
 
-        return new int[] {l, bs(arr, target + 1) - 1};
+        if (p1 == arr.length || arr[p1] != t) return new int[] {-1, -1};
+
+        int p2 = bs(arr, t + 1) - 1;
+
+        return new int[] {p1, p2};
     }
-    private int bs(int[] arr, int target) {
+
+    private int bs(int[] arr, int t) {
+
         int l = 0, r = arr.length;
 
         while (l < r) {
-            int m = (l + r) >> 1;
+            int m = l + ((r - l) >> 1);
 
-            if (arr[m] < target) l = m + 1;
+            if (arr[m] < t) l = m + 1;
             else r = m;
         }
 

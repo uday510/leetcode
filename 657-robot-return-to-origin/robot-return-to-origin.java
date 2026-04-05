11129018fc81bb18e889e61b52f2cd91943bff0f
uday <62304758+uday510@users.noreply.github.com>
2026-04-05 11:05:01 +0530

    Time: 13 ms (6.44%) | Memory: 45.7 MB (65.59%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -1,17 +1,24 @@
 class Solution {
+
+    private static Set<Character> pos = new HashSet<>();
+    static { pos.add('U'); pos.add('D'); }
+
     public boolean judgeCircle(String moves) {
         int x = 0, y = 0;
 
-        for (char move : moves.toCharArray()) {
+        for (int i = 0; i < moves.length(); i++) {
+            char move = moves.charAt(i);
 
-            if (move == 'U') x++;
-            else if (move == 'D') x--;
-
-            if (move == 'R') y++;
-            else if (move == 'L') y--;
+            if (pos.contains(move)) 
+                x += getCnt(move);
+            else y += getCnt(move);
 
         }
 
         return x == 0 && y == 0;
+    }
+
+    private int getCnt(char dir) {
+        return dir == 'U' || dir == 'R' ? 1 : -1;
     }
 }

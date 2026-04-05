516d0dcfa884f694469c9edcf478213f2af96964
uday <62304758+uday510@users.noreply.github.com>
2026-04-05 18:31:08 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (65.43%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,23 +1,28 @@
 class Solution {
-    public int hIndex(int[] C) {
-        
-        int n = C.length;
+    public int hIndex(int[] arr) {
+        int n = arr.length;
         int[] cnt = new int[n + 1];
 
-        for (int c : C) {
-            int cur = Math.min(c, n);
-            cnt[cur]++;
+        for (int a : arr) {
+            cnt[Math.min(a, n)]++;
         }
 
-        int papers = 0;
-        for (int i = n; i > -1; i--) {
-            papers += cnt[i];
+        int cur = 0;
 
-            if (papers >= i) {
+        for (int i = n; i > -1; i--) {
+            cur += cnt[i];
+
+            if (cur >= i) {
                 return i;
             }
-        } 
 
+        }
+        
         return -1;
     }
 }
+
+//   0  1  2  3  5
+//  [1  1  0  1  2]
+
+// 3

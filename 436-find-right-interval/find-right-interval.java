fd32fcfef2b5b7213bc49c5b2ea176f41278d571
uday <62304758+uday510@users.noreply.github.com>
2026-04-05 11:48:19 +0530

    Time: 14 ms (57.23%) | Memory: 53.9 MB (38.00%) - LeetSync

--- 436-find-right-interval/find-right-interval.java
+++ 436-find-right-interval/find-right-interval.java
@@ -1,37 +1,49 @@
 class Solution {
     public int[] findRightInterval(int[][] intervals) {
         int n = intervals.length;
-        int[] arr = new int[n];
-        Map<Integer, Integer> map = new HashMap<>();
+        int[][] arr = new int[n][2];
 
-        for (int i = 0; i < n; ++i) {
-            arr[i] = intervals[i][0];
-            map.put(intervals[i][0], i);
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = intervals[i][0];
+            arr[i][1] = i;
         }
 
-        Arrays.sort(arr);
+        Arrays.sort(arr, Comparator.comparingInt(k -> k[0]));
+
         int[] res = new int[n];
 
-        for (int i = 0; i < n; ++i) {
-            int idx = bs(intervals[i][1], arr);
-            if (idx == n) res[i] = -1;
-            else res[i] = map.get(arr[idx]);
+        for (int i = 0; i < n; i++) {
+            int[] in = intervals[i];
+
+            int index = bs(arr, in[1]);
+            res[i] = index == n ? -1 : arr[index][1];
         }
 
         return res;
     }
 
-    private int bs(int t, int[] arr) {
-        int l = 0;
-        int r = arr.length;
+    private int bs(int[][] arr, int t) {
+        int l = 0, r = arr.length;
 
         while (l < r) {
-            int m = (l + r) >> 1;
+            int m = l + (r - l) / 2;
 
-            if (arr[m] < t) l = m + 1;
+            if (arr[m][0] < t) l = m + 1;
             else r = m;
         }
 
         return l;
     }
 }
+
+/**
+
+[1, 2, 2] [2, 3, 1] [3, 4, 0]
+
+[3, 4] [2, 3] [1, 2]
+
+
+-1 0 1
+
+
+ */

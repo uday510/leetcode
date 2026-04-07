00ed11ee7986af38aa967a6ce6f0c359fbf0afc0
uday <62304758+uday510@users.noreply.github.com>
2026-04-07 16:05:20 +0530

    Time: 1 ms (100.00%) | Memory: 48.8 MB (78.23%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,50 +1,47 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        
-        if (nums2.length < nums1.length) 
-            return findMedianSortedArrays(nums2, nums1);
 
-        int n = nums1.length, m = nums2.length;
+
+    public double findMedianSortedArrays(int[] arr1, int[] arr2) {
+
+        if (arr2.length < arr1.length) {
+            return findMedianSortedArrays(arr2, arr1);
+        }
+
+        int n = arr1.length, m = arr2.length;
         int total = n + m, target = (n + m + 1) >> 1;
         int l = 0, r = n;
-        double mid = -1;
 
         while (l <= r) {
 
             int p1 = (l + r) >> 1, p2 = target - p1;
+            int l1 = safeGet(p1 - 1, arr1), r1 = safeGet(p1, arr1);
+            int l2 = safeGet(p2 - 1, arr2), r2 = safeGet(p2, arr2);
 
-            int l1 = safeGet(p1 - 1, nums1), r1 = safeGet(p1, nums1);
-            int l2 = safeGet(p2 - 1, nums2), r2 = safeGet(p2, nums2);
-
-            if (l1 > r2) r = p1 - 1;
-            else if (l2 > r1) l = p1 + 1;
-            else if (l1 <= r2 && l2 <= r1) {
-
-                if ( (total & 1) == 1) mid = Math.max(l1, l2);
-                else mid = (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
-
-                break;
+            if (l2 > r1) {
+                l = p1 + 1;
+            } else if (l1 > r2) {
+                r = p1 - 1;
+            } else if (l1 <= r2 && l2 <= r1) {
+                
+                double median = -1;
+                
+                if ( (total & 1) == 1) {
+                    median = Math.max(l1, l2);
+                } else {
+                    median = (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                }
+                
+                return median;
             }
         }
+        
+        return -1;
 
-        return mid;
     }
 
-    private int safeGet(int i, int[] nums) {
-        if (i < 0) return Integer.MIN_VALUE;
-        if (i >= nums.length) return Integer.MAX_VALUE;
-        return nums[i];
+    private int safeGet(int idx, int[] arr) {
+        if (idx < 0) return Integer.MIN_VALUE;
+        if (idx >= arr.length) return Integer.MAX_VALUE;
+        return arr[idx];
     }
 }
-
-/**
-
-[0, 0]  [0, 0]
-
-n = 2, m = 2, target = 2
-
-l = 0, r = 2
-
-p1 = 1, p2 = 1
-
- */

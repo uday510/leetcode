52fdd9697834dc416ecbbf3cdc1c14d61fc88b99
uday <62304758+uday510@users.noreply.github.com>
2026-04-07 15:12:50 +0530

    Time: 5 ms (54.43%) | Memory: 48 MB (39.72%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,59 +1,39 @@
 class Solution {
 
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-
+        
         int n = arr.length;
         int l = 0, r = n;
         
         while (l < r) {
             
-            int m = l + ( (r - l) >> 1);
+            int m = l + ((r - l) >> 1);
             
             if (arr[m] < x) l = m + 1;
             else r = m;
         }
         
-        int left = l - 1;
-        int right = l;
         
-        while (right - left - 1 < k) {
-            
-            if (left == -1) {
-                right++;
-            } else if (right == n) {
-                left--;
-            } else if (Math.abs(arr[left] - x) <= Math.abs(arr[right] - x)) {
-                left--;
+        r = l;
+        
+        while (r - l - 1 < k) {
+            if (l == -1) {
+                r++;
+            } else if (r == n || Math.abs(arr[l] - x) <= Math.abs(arr[r] - x)) {
+                l--;
             } else {
-                right++;
+                r++;
             }
         }
         
+        
         List<Integer> res = new ArrayList<>();
         
-        for (int idx = left + 1; idx < right; idx++) {
+        for (int idx = l + 1; idx < r; idx++) {
             res.add(arr[idx]);
         }
         
         return res;
-        
+
     }
-
 }
-
-/**
-
-
-
-
-l = 2 => 3
-
- 0  1  2  3  4     
-[1, 2, 3, 4, 5]
-
-left = 0 => 
-right = 3 =>
-
-
-
- */

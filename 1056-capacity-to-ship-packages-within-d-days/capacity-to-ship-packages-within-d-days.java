0424cc357787316e5e952162f879e96064a37470
uday <62304758+uday510@users.noreply.github.com>
2026-04-10 14:57:47 +0530

    Time: 11 ms (70.63%) | Memory: 50.2 MB (64.03%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,37 +1,35 @@
 class Solution {
-    
     public int shipWithinDays(int[] weights, int days) {
-        int l = 0, r = 0;
-
+        int l = 1, r = 1;
         for (int w : weights) {
-            l = Math.max(l, w); // need max, if min choose cannot fit max
+            l = Math.max(l, w);
             r += w;
         }
 
         while (l < r) {
+
             int m = l + ((r - l) >> 1);
-            if (canShip(weights, m, days)) r = m;
-            else l = m + 1;
+
+            if (!canShip(m, weights, days)) l = m + 1; // increase load
+            else r = m;
         }
 
         return l;
     }
 
-    private boolean canShip(int[] w, int m, int d) {
-        int curr = 0;
-        int used = 1;
+    private boolean canShip(int loadPerDay, int[] weights, int days) {
+        int cur = 0, d = 1;
 
-        for (int x : w) {
-            if (x > m || used > d) return false;
-            if (curr + x > m) {
-                used++;
-                curr = x;
-            } else {
-                curr += x;
-            }
+        for (int w : weights) {
+            if (cur + w > loadPerDay) {
+                d++;
+                cur = 0;
+            } 
+
+            cur += w;
+            if (d > days) return false;
         }
 
-        return used <= d;
+        return true;
     }
-
 }

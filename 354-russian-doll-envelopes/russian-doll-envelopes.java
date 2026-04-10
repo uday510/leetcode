50d3d3944d7ebc726fafb2bbeebb21a46e91d228
uday <62304758+uday510@users.noreply.github.com>
2026-04-10 11:54:04 +0530

    Time: 53 ms (20.92%) | Memory: 105.7 MB (5.27%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -1,44 +1,52 @@
 class Solution {
-
     private List<Integer> lis;
-
-    public int maxEnvelopes(int[][] E) {
-
+    public int maxEnvelopes(int[][] envs) {
         lis = new ArrayList<>();
 
-        Arrays.sort(E, (o1, o2) -> {
-            return o1[0] == o2[0] ? o2[1] - o1[1] : o1[0] - o2[0];
-        });  
+        Arrays.sort(envs, (o1, o2) -> {
+            if (o1[0] == o2[0]) return o2[1] - o1[1];
+            return o1[0] - o2[0];
+        });
 
-        for (int i = 0; i < E.length; i++) {
-            int num = E[i][1];
-            int index = bs(num);
-            if (index == lis.size()) lis.add(num);
-            lis.set(index, num);
+        int n = envs.length;
+
+        for (int i = 0; i < n; i++) {
+
+            int cur = envs[i][1];
+            int index = bs(cur);
+
+            if (index == lis.size()) lis.add(cur);
+            lis.set(index, cur);
         }
 
         return lis.size();
-    }   
+    }
 
-    private int bs(int target) {
+    private int bs(int t) {
         int l = 0, r = lis.size();
 
         while (l < r) {
 
-            int m = (l + r) >> 1;
-            
-            if (lis.get(m) < target) l = m + 1;
+            int m = l + ( (r - l) >> 1);
+
+            if (lis.get(m) < t) l = m + 1;
             else r = m;
         }
+
 
         return l;
     }
 }
 
-/**
+
+/***
 
 
-[2, 3] [5, 4] [6, 7] [6, 4]
+[1, 1] [2, 3] [4, 6], [4, 5] [6, 7]
 
-[3, 4, 7]
+[1, 3, 5, 7]
+
+
+
+
  */

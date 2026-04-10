66f37ea128939d57b87fba77ab551cdf13adfa75
uday <62304758+uday510@users.noreply.github.com>
2026-04-10 11:45:43 +0530

    Time: 5 ms (88.73%) | Memory: 46.4 MB (26.04%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,27 +1,32 @@
 class Solution {
 
-    List<Integer> lis;
+    private int n;
+    private int[] arr;
+    private List<Integer> lis;
 
-    public int lengthOfLIS(int[] nums) {
+    public int lengthOfLIS(int[] arr) {
+        n = arr.length;
+        this.arr = arr;
         lis = new ArrayList<>();
-        
-        for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            int index = bs(num);
-            if (index == lis.size()) lis.add(num);
-            lis.set(index, num);
+
+
+        for (int i = 0; i < n; i++) {
+            int index = bs(arr[i]);
+            if (index == lis.size()) lis.add(arr[i]);
+            lis.set(index, arr[i]);
         }
 
         return lis.size();
     }
 
-    private int bs(int target) {
+    private int bs(int t) {
         int l = 0, r = lis.size();
 
         while (l < r) {
-            int m = (l + r) >> 1;
 
-            if (lis.get(m) < target) l = m + 1;
+            int m = l + ((r - l) >> 1);
+
+            if (lis.get(m) < t) l = m + 1;
             else r = m;
         }
 

9658d37cfd7052187e3b5ff3f77c9452e55d4d89
uday <62304758+uday510@users.noreply.github.com>
2026-04-12 14:54:49 +0530

    Time: 5 ms (76.89%) | Memory: 47.1 MB (68.08%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean canFinish(int n, int[][] preq) {
+        
 
-        List<Integer>[] adj = new ArrayList[n];   
+        List<Integer>[] adj = new ArrayList[n];
         int[] in = new int[n];
 
         for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
 
         for (int[] e : preq) {
-
+            
             int u = e[0], v = e[1];
 
             adj[v].add(u);
@@ -24,8 +25,9 @@ class Solution {
         while (!queue.isEmpty()) {
             int u = queue.poll();
             fi++;
-            
+
             for (int v : adj[u]) {
+
                 if (--in[v] == 0) {
                     queue.offer(v);
                 }

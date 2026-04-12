59514eb63525ecea7d643d10fa19ab8a755eb7cb
uday <62304758+uday510@users.noreply.github.com>
2026-04-12 10:57:28 +0530

    Time: 14 ms (60.00%) | Memory: 47.9 MB (36.15%) - LeetSync

---
+++ 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.java
@@ -0,0 +1,39 @@
+class Solution {
+   
+    private Integer[][][] dp;
+
+    public int minimumDistance(String word) {
+        dp = new Integer[word.length()][27][27];
+        
+        return dfs(0, -1, -1, word);
+    }
+
+    private int dfs(int idx, int f1, int f2, String w) {
+        if (idx >= w.length()) return 0;
+
+        if (dp[idx][f1 + 1][f2 + 1] != null) return dp[idx][f1 + 1][f2 + 1];
+        
+        int cur = w.charAt(idx) - 'A';
+
+        int c1 = getDist(cur, f1);
+        int c2 = getDist(cur, f2);
+
+        int t1 = c1 + dfs(idx + 1, cur, f2, w);
+        int t2 = c2 + dfs(idx + 1, f1, cur, w);
+
+        return dp[idx][f1 + 1][f2 + 1] = Math.min(t1, t2);
+
+    }
+
+    private int getDist(int a, int b) {
+        if (a < 0 || b < 0) return 0;
+        
+        int x1 = a / 6, y1 = a % 6;
+        int x2 = b / 6, y2 = b % 6;
+
+        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+
+    }
+
+
+}

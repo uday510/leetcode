cc44aeabe23263f1271284608cac4b2e95279577
uday <62304758+uday510@users.noreply.github.com>
2026-04-12 11:00:43 +0530

    Time: 17 ms (45.38%) | Memory: 47.5 MB (50.00%) - LeetSync

--- 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.java
+++ 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.java
@@ -1,17 +1,23 @@
 class Solution {
    
-    private Integer[][][] dp;
+    private int[][][] dp;
+    private int n;
 
     public int minimumDistance(String word) {
-        dp = new Integer[word.length()][27][27];
+        n = word.length();
+        dp = new int[n][27][27];
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < 27; j++)
+                Arrays.fill(dp[i][j], -1);
         
         return dfs(0, -1, -1, word);
     }
 
     private int dfs(int idx, int f1, int f2, String w) {
-        if (idx >= w.length()) return 0;
+        if (idx >= n) return 0;
 
-        if (dp[idx][f1 + 1][f2 + 1] != null) return dp[idx][f1 + 1][f2 + 1];
+        if (dp[idx][f1 + 1][f2 + 1] != -1) return dp[idx][f1 + 1][f2 + 1];
         
         int cur = w.charAt(idx) - 'A';
 
@@ -27,7 +33,7 @@ class Solution {
 
     private int getDist(int a, int b) {
         if (a < 0 || b < 0) return 0;
-        
+
         int x1 = a / 6, y1 = a % 6;
         int x2 = b / 6, y2 = b % 6;
 

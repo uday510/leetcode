c812df207431a40888223ee53a060e7df0619515
uday <62304758+uday510@users.noreply.github.com>
2026-04-14 15:44:32 +0530

    Time: 53 ms (43.55%) | Memory: 44.5 MB (79.03%) - LeetSync

--- 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.java
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.java
@@ -1,41 +1,48 @@
 class Solution {
 
-    private List<Integer> r;
-    private List<Integer> p;
+    private List<Integer> robot;
+    private int[][] factory;
     private long[][] dp;
 
-    public long minimumTotalDistance(List<Integer> r, int[][] f) {
-        this.r = r;
-        p = new ArrayList<>();
+    public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
+        Collections.sort(robot);
+        Arrays.sort(factory, Comparator.comparingInt(a -> a[0]));
 
-        Collections.sort(r);
-        Arrays.sort(f, Comparator.comparingInt(k -> k[0]));
-        
-        for (int[] a : f) {
-            for (int i = 0; i < a[1]; i++) {
-                p.add(a[0]);
-            }
-        }
+        this.robot = robot;
+        this.factory = factory;
 
+        int n = robot.size();
+        int m = factory.length;
 
-        dp = new long[r.size()][p.size()];
+        dp = new long[n][m];
         for (long[] row : dp) Arrays.fill(row, -1);
-        
+
         return dfs(0, 0);
     }
 
     private long dfs(int i, int j) {
-        if (i >= r.size()) return 0;
-        
-        if (j >= p.size()) return Long.MAX_VALUE/2;
-        
+        // all robots assigned
+        if (i == robot.size()) return 0;
+
+        // no factories left
+        if (j == factory.length) return (long) 1e15;
+
         if (dp[i][j] != -1) return dp[i][j];
-        
-        long pick = Math.abs(r.get(i) - p.get(j)) +
-                    dfs(i + 1, j + 1);
-        
-        long dont = dfs(i, j + 1);
-        
-        return dp[i][j] = Math.min(pick, dont);
+
+        long res = dfs(i, j + 1); // skip this factory
+
+        long cost = 0;
+
+        // try assigning k robots to factory j
+        for (int k = 1; k <= factory[j][1] && i + k - 1 < robot.size(); k++) {
+            cost += Math.abs(robot.get(i + k - 1) - factory[j][0]);
+
+            long next = dfs(i + k, j + 1);
+            if (next != (long) 1e15) {
+                res = Math.min(res, cost + next);
+            }
+        }
+
+        return dp[i][j] = res;
     }
 }

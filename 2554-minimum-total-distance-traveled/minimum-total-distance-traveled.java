833a414a8f83fab1f3a62cc674ca881602110ae9
uday <62304758+uday510@users.noreply.github.com>
2026-04-14 16:21:51 +0530

    Time: 36 ms (70.97%) | Memory: 44.3 MB (93.55%) - LeetSync

--- 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.java
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.java
@@ -4,8 +4,8 @@ class Solution {
 
     private List<Integer> robots;
     private int[][] factory;
-    private long[][] dp;
     private int n, m;
+    private long[][] dp;
 
     public long minimumTotalDistance(List<Integer> robots, int[][] factory) {
         this.robots = robots;
@@ -17,6 +17,7 @@ class Solution {
         Arrays.sort(factory, Comparator.comparingInt(k -> k[0]));
 
         dp = new long[n][m];
+
         for (long[] row : dp) Arrays.fill(row, -1);
 
         return dfs(0, 0);
@@ -24,28 +25,26 @@ class Solution {
 
     private long dfs(int i, int j) {
         if (i >= n) return 0;
-
         if (j >= m) return Long.MAX_VALUE;
 
         if (dp[i][j] != -1) return dp[i][j];
 
-        long res = dfs(i, j + 1);
+        long t1 = dfs(i, j + 1);
 
-        long cur = 0;
-        int curFactoryLimit = factory[j][1];
-        for (int idx = 1; idx <= curFactoryLimit && i + idx - 1 < n; idx++) {
-            
-            cur += Math.abs(robots.get(i + idx - 1) - factory[j][0]);
-            
-            long t2 = dfs(i + idx, j + 1);
-            if (t2 != Long.MAX_VALUE) {
-                res = Math.min(res, cur + t2);
+        long cost = 0;
+        long t2 = Long.MAX_VALUE;
+
+        for (int idx = 1; idx <= factory[j][1] && i + idx - 1 < n; idx++) {
+
+            cost += Math.abs(factory[j][0] - robots.get((i + idx - 1)));
+
+            long nxt = dfs(i + idx, j + 1);
+            if (nxt != Long.MAX_VALUE) {
+                t2 = Math.min(t2, cost + nxt);
             }
-            
+
         }
 
-
-        return dp[i][j] = res;
-
+        return dp[i][j] = Math.min(t1, t2);
     }
 }

99c20e5d2985c370d0ed5a47de02c53a26fbe438
uday <62304758+uday510@users.noreply.github.com>
2026-04-15 18:26:21 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (28.57%) - LeetSync

--- 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.java
+++ 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.java
@@ -3,20 +3,19 @@ class Solution {
     public int closestTarget(String[] arr, String t, int st) {
 
         int n = arr.length;
-        int mn = n;
 
        for (int idx = 0; idx < n; idx++) {
 
-           int idx1 = (st + idx) % n;
-           int idx2 = (st - idx + n) % n;
+           int l = (st + idx) % n;
+           int r = (st - idx + n) % n;
 
-            if (arr[idx1].equals(t) || arr[idx2].equals(t)) {
-                mn = Math.min(mn, idx);
+            if (arr[l].equals(t) || arr[r].equals(t)) {
+                return idx;
             }
            
        }
 
-        return mn == n ? -1 : mn;
+        return -1;
     }
 
 }

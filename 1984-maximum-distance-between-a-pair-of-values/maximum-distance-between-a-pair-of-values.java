20de0f00738e255d2548220157fca34ad616e973
uday <62304758+uday510@users.noreply.github.com>
2026-04-19 18:40:43 +0530

    Time: 12 ms (21.36%) | Memory: 89.8 MB (90.51%) - LeetSync

---
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maxDistance(int[] arr1, int[] arr2) {
+        
+        int mx = 0;
+        int n = arr1.length;
+
+        for (int i = 0; i < n; i++) {
+
+            int idx = bs(i, arr1, arr2);
+            mx = Math.max(mx, Math.abs(i -  idx));
+        }
+
+        return Math.max(0, mx - 1);
+    }
+
+    private int bs(int i, int[] arr1, int[] arr2) {
+
+        int l = i, r = arr2.length;
+
+
+        while (l < r) {
+
+            int m = l + ((r - l) >> 1);
+
+            if (i <= m && arr1[i] <= arr2[m]) l = m + 1;
+            else r = m;
+        }
+
+        return l;
+    }
+}
+
+/**
+
+
+ */

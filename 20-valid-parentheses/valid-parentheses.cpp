3de5167fe1380e6d46afba3d8f4b890ba530d6c9
uday <62304758+uday510@users.noreply.github.com>
2026-04-19 19:24:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (20.42%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,30 @@
+#include <unordered_map>
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        unordered_map<char, char> map = {
+            {')', '('},
+            {'}', '{'},
+            {']', '['}
+        };
+
+        stack<char> st;
+
+        for (char ch : s) {
+            if (map.find(ch) == map.end()) {
+                st.push(ch);
+            } else {
+                if (st.empty() || map[ch] != st.top()) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};

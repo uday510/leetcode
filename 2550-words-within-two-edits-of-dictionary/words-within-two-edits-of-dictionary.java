a1c4520e375beb0f93d69a5ac4e195fc390bd12a
uday <62304758+uday510@users.noreply.github.com>
2026-04-22 20:49:54 +0530

    Time: 5 ms (78.68%) | Memory: 47 MB (25.00%) - LeetSync

---
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+    public List<String> twoEditWords(String[] q, String[] d) {
+        Trie root = new Trie();
+
+        for (String s : d) insert(root, s);
+
+        List<String> list = new ArrayList<>();
+
+        for (String s : q) {
+
+            if (dfs(root, 0, s, 0)) {
+                list.add(s);
+            }
+        }
+
+        return list;
+
+    }
+
+    private void insert(Trie root, String w) {
+
+        Trie cur = root;
+
+        for (int idx = 0; idx < w.length(); idx++) {
+
+            int index = w.charAt(idx) - 'a';
+
+            if (cur.child[index] == null) {
+                cur.child[index] = new Trie();
+            }
+
+            cur = cur.child[index];
+        }
+
+        cur.isEnd = true;
+    }
+
+    private boolean dfs(Trie node, int idx, String w, int cur) {
+        if (cur > 2 || node == null) return false;
+
+        if (idx >= w.length()) return node.isEnd;
+
+        int index = w.charAt(idx) - 'a';
+
+        if (node.child[index] != null) {
+            if (dfs(node.child[index], idx + 1, w, cur)) return true;
+        }
+                
+        for (int i = 0; i < 26; i++) {
+            if (i == index || node.child[i] == null) continue;
+
+            if (dfs(node.child[i], idx + 1, w, cur + 1)) {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+}
+
+class Trie {
+
+    Trie[] child;
+    boolean isEnd;
+
+    Trie() {
+        child = new Trie[26];
+        isEnd = false;
+    }
+
+}

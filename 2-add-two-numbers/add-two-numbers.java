920059ab8e47c7b0ba7300a04d88b0cfaefc8cde
uday <62304758+uday510@users.noreply.github.com>
2026-04-28 21:44:45 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (38.83%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -1,47 +1,73 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
-        ListNode h = new ListNode(-1);
-        ListNode cur = h, p1 = l1, p2 = l2;
-        int cry = 0;
+        ListNode output = new ListNode();
+        ListNode head = output;
 
-        while (p1 != null || p2 != null || cry != 0) {
-            int sm = getSm(new ListNode[]{p1, p2}, cry);
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        int carry = 0;
 
-            ListNode node = new ListNode(sm % 10);
-            cur.next = node;
-            cur = node;
+        while (p1 != null || p2 != null || carry != 0) {
+            int curSum = (p1 != null ? p1.val : 0) + (p2 != null ? p2.val : 0) + carry;
 
-            cry = sm / 10;
+            int newNodeVal = curSum % 10;
 
-            p1 = getNextNode(p1);
-            p2 = getNextNode(p2);
+            ListNode newNode = new ListNode(newNodeVal);
+            output.next = newNode;
+
+            output = output.next;
+
+            int curCarry = curSum / 10;
+            carry = curCarry;
+
+            p1 = (p1 != null ? p1.next : null);
+            p2 = (p2 != null ? p2.next : null);
         }
 
-        return h.next;
-    }
 
-    private int getSm(ListNode[] nodes, int c) {
-        int sum = c;
-
-        for (ListNode l : nodes) {
-            sum += l != null ? l.val : 0;
-        }
-
-        return sum;
-    }
-
-    private ListNode getNextNode(ListNode p) {
-        return p == null ? null : p.next;
+        return head.next;
     }
 }
+
+/**
+
+input: 
+l1 = 2 -> 4 -> 3 -> NULL
+l2 = 4 -> 6 -> 4 -> NULL
+
+output: 
+6 -> 0 -> 8 -> NULL
+
+p1 = NULL
+p2 = NULL
+
+         head                 |  
+output = [] -> [6] ->[0] -> [8]
+carry = 0
+
+curSum = (p1 != null ? p1.val : 0) + (p2 != null ? p2.val : 0) + carry 
+        = 2 + 4 + 0 => 6
+        = 4 + 6 + 0 => 10
+        = 3 + 4 + 1 => 8
+
+newNodeVal = curSum % 10
+           = 6 % 10 = 6
+           = 10 % 10 = 0
+           = 8 % 10 = 8
+
+newNode = new Node(newNodeVal)
+
+output.next = newNode
+output = output.next
+
+p1 = (p1 != null ? p1.next : null)
+p2 = (p2 != null ? p2.next : null)
+
+curCarry = curSum / 10
+      = 6 / 10 => 0
+      = 10 / 10 => 1
+      = 8 / 10 => 0
+
+carry = curCarry
+
+*/

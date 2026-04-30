e96631cfdc83ce12311e20c30abdc8a6d951c838
uday <62304758+uday510@users.noreply.github.com>
2026-04-30 20:17:42 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (16.32%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,43 +1,36 @@
 class Solution {
-    public void rotate(int[][] A) {
+    public void rotate(int[][] arr) {
         
-        transpose(A);
-        reverse(A);
-    }
-
-    private void transpose(int[][] A) {
-        int n = A.length;
-        int m = A[0].length;
+        int n = arr.length, m = arr[0].length;
 
         for (int i = 0; i < n; i++) {
             for (int j = i; j < m; j++) {
-
-                int tmp = A[i][j];
-                A[i][j] = A[j][i];
-                A[j][i] = tmp;
-
+                
+                int tmp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = tmp;
             }
         }
-    }
 
-    private void reverse(int[][] A) {
-        for (int[] row : A) {
+        for (int[] row : arr) {
             reverse(row);
         }
     }
 
-    private void reverse(int[] A) {
-        int l = 0, r = A.length - 1;
+    private void reverse(int[] arr) {
 
-        while (l < r) {
-            swap(l++, r--, A);
+        int i = 0, j = arr.length - 1;
+
+        while (i < j) {
+            swap(i, j, arr);
+            i++; j--;
         }
     }
 
     private void swap(int i, int j, int[] arr) {
-        int tmp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = tmp;
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 
 }

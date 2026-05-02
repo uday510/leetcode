5eb352764f8b7ac8ff8400fee565b26c4a144249
uday <62304758+uday510@users.noreply.github.com>
2026-05-02 20:10:59 +0530

    Time: 3 ms (54.99%) | Memory: 46.6 MB (45.53%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -2,29 +2,43 @@ class Solution {
     public boolean isValidSudoku(char[][] board) {
         
         int n = 9;
-        Set<Character>[] rows = new HashSet[n];
-        Set<Character>[] cols = new HashSet[n];
-        Set<Character>[] boxes = new HashSet[n];
+        Set<Character>[] rows = new HashSet[9];
+        Set<Character>[] cols = new HashSet[9];
+        Set<Character>[] boxes = new HashSet[9];
 
         for (int i = 0; i < 9; i++) {
             rows[i] = new HashSet<>();
             cols[i] = new HashSet<>();
             boxes[i] = new HashSet<>();
-        }
+        }   
 
         for (int r = 0; r < n; r++) {
+
             for (int c = 0; c < n; c++) {
+
                 char ch = board[r][c];
 
-                if (ch == '.') continue;
+                if (ch == '.') continue; // empty
 
-                int boxId = ((r / 3) * 3) + c / 3;
 
-                if (!rows[r].add(ch) || !cols[c].add(ch) || !boxes[boxId].add(ch)) {
+                int boxId = ((r / 3) * 3) + (c / 3);
+
+                if (
+                    rows[r].contains(ch) || 
+                    cols[c].contains(ch) || 
+                    boxes[boxId].contains(ch)
+                ) {
                     return false;
                 }
+
+                rows[r].add(ch);
+                cols[c].add(ch);
+                boxes[boxId].add(ch);
             }
         }
+
+
+
 
         return true;
     }

2da33a04ccaea908411413431ff3ff321639d4ed
uday <62304758+uday510@users.noreply.github.com>
2026-05-02 22:31:47 +0530

    Time: 3 ms (91.63%) | Memory: 42 MB (70.36%) - LeetSync

--- 804-rotated-digits/rotated-digits.java
+++ 804-rotated-digits/rotated-digits.java
@@ -1,12 +1,13 @@
 class Solution {
 
-    Set<Integer> invalid = Set.of(
-        3, 4, 7
-    );
+    static boolean[] invalid = new boolean[10];
+    static boolean[] valid = new boolean[10];
 
-    Set<Integer> valid = Set.of(
-        2, 5, 6, 9
-    );
+    static {
+        invalid[3] = invalid[4] = invalid[7] = true;
+        valid[2] = valid[5] = valid[6] = valid[9] = true;
+    }
+
     public int rotatedDigits(int n) {
         
         int cnt = 0;
@@ -30,9 +31,9 @@ class Solution {
         while (num > 0) {
             int cur = num % 10;
 
-            if (invalid.contains(cur)) return false;
+            if (invalid[cur]) return false;
 
-            if (valid.contains(cur)) {
+            if (valid[cur]) {
                 safe = true;
             }
 

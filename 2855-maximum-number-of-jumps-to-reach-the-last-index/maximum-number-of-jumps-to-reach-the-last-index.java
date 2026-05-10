83e90490a3a040320061c078cc6558f0895c4bab
uday <62304758+uday510@users.noreply.github.com>
2026-05-10 19:41:57 +0530

    Time: 61 ms (5.36%) | Memory: 57.5 MB (8.33%) - LeetSync

---
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.java
@@ -0,0 +1,51 @@
+class Solution {
+
+    private int[] arr;
+    int[][] dp;
+    int t, n;
+
+    public int maximumJumps(int[] arr, int t) {
+
+        this.arr = arr;
+        this.t = t;
+        this.n = arr.length;
+
+        dp = new int[n + 1][n];
+
+        for (int[] r : dp) {
+            Arrays.fill(r, Integer.MIN_VALUE);
+        }
+
+        int res = dfs(-1, 0);
+
+        return res < 0 ? -1 : res;
+    }
+
+    private int dfs(int i, int j) {
+        if (j >= n) {
+            return i == n - 1 ? 0 : -(int)1e9;
+        }
+
+        if (dp[i + 1][j] != Integer.MIN_VALUE) {
+            return dp[i + 1][j];
+        }
+
+        int t1 = dfs(i, j + 1);
+
+        int t2 = -(int)1e9;
+
+        if (
+            (i == -1 && j == 0) ||
+            (i != -1 && Math.abs(arr[j] - arr[i]) <= t)
+        ) {
+
+            int nxt = dfs(j, j + 1);
+
+            if (nxt >= 0) {
+                t2 = (i == -1 ? 0 : 1) + nxt;
+            }
+        }
+
+        return dp[i + 1][j] = Math.max(t1, t2);
+    }
+}

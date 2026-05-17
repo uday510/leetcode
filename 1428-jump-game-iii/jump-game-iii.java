1f748786679b79b8b14ce99aef0dac06f9ad3296
uday <62304758+uday510@users.noreply.github.com>
2026-05-17 12:05:37 +0530

    Time: 6 ms (49.70%) | Memory: 48.4 MB (92.50%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.java
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -1,28 +1,33 @@
 class Solution {
     public boolean canReach(int[] arr, int st) {
-        
         int n = arr.length;
         Queue<Integer> queue = new ArrayDeque<>();
         boolean[] vis = new boolean[n];
+        int dst = 0;
 
-        int dest = 0;
         vis[st] = true;
         queue.offer(st);
 
         while (!queue.isEmpty()) {
-            int cur = queue.poll();
-            if (arr[cur] == dest) return true;
+            int u = queue.poll();
+            if (arr[u] == dst) return true;
 
+            for (int v : getPaths(u, arr[u])) {
+                if (!isValid(v, vis)) continue;
 
-            for (int nxt : new int[] {cur + arr[cur], cur - arr[cur]}) {
-
-                if (nxt < 0 || nxt >= n || vis[nxt]) continue;
-
-                vis[nxt] = true;
-                queue.offer(nxt);
+                vis[v] = true;
+                queue.offer(v);
             }
         }
 
         return false;
+    }
+
+    private int[] getPaths(int u, int val) {
+        return new int[] {u + val, u - val};
+    }
+
+    private boolean isValid(int v, boolean[] vis) {
+        return v >= 0 && v < vis.length && !vis[v];
     }
 }

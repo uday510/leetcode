61b334b430dd3178d01befe31dd0909bd6d441f4
uday <62304758+uday510@users.noreply.github.com>
2026-05-21 13:43:50 +0530

    Time: 638 ms (5.36%) | Memory: 79.7 MB (83.08%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
@@ -1,51 +1,59 @@
 class Solution {
+
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
         Trie root = new Trie();
-        for (int num : arr1)
+        
+        for (int num : arr1) {
             insert(root, String.valueOf(num));
+        }
 
         int longest = 0;
-        for (int num : arr2) 
-            longest = Math.max(longest, search(root, String.valueOf(num)));
+        for (int num : arr2) {
+            longest = Math.max(longest, getCommonPrefix(root, String.valueOf(num)));
+        }
 
         return longest;
     }
 
-    private void insert(Trie trie, String str) {
-        for (int i = 0; i < str.length(); ++i) {
-            int idx = getIdx(str, i);
-            if (child(trie, idx) == null)   
-                trie.children[idx] = new Trie();
-            
-            trie = child(trie, idx);
+    private void insert(Trie root, String s) {
+        Trie cur = root;
+        System.out.println(cur);
+
+
+        for (int i = 0; i < s.length(); i++) {
+            int idx = s.charAt(i) - '0';
+
+            if (cur.children[idx] == null) {
+                cur.children[idx] = new Trie();
+            }
+
+            cur = cur.children[idx];
+        }   
+    }
+
+    private int getCommonPrefix(Trie root, String s) {
+        int cnt = 0;
+        Trie cur = root;
+
+        for (int i = 0; i < s.length(); i++) {
+            int idx = s.charAt(i) - '0';
+            Trie child = cur.children[idx];
+            if (child == null) return cnt;
+            cnt++;
+            cur = child;
         }
-    }
 
-    private int search(Trie trie, String str) {
-        int count = 0;
-        for (int i = 0; i < str.length(); ++i) {
-            int idx = getIdx(str, i);
-            if (child(trie, idx) == null)
-                return count;
-            ++count;
-            trie = child(trie, idx);
-        }
-        return count;
-    }
-
-    private int getIdx(String str, int i) {
-        return str.charAt(i) - '0';
-    }
-
-    private Trie child(Trie trie, int i) {
-        return trie.children[i];
+        return cnt;
     }
 
 }
 
+
 class Trie {
     Trie[] children;
+
     public Trie() {
         children = new Trie[10];
     }
+
 }

fe8b52abc8493b9b6f2e7edd7253af74356c49df
uday <62304758+uday510@users.noreply.github.com>
2026-05-22 11:29:13 +0530

    Time: 11 ms (1.41%) | Memory: 44.4 MB (10.66%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,24 +1,25 @@
 class Solution {
     public int search(int[] arr, int t) {
+        
         int l = 0, r = arr.length - 1;
-
         while (l <= r) {
-            int m = l + ((r - l) >> 1);
 
-            int cur = arr[m];
+            int m = (l + r) >> 1;
+ 
+            if (arr[m] == t) return m;
 
-            if (cur == t) return m;
+            System.out.println(arr[l] + " " + arr[m] + " " + arr[r]);
 
             if (arr[l] <= arr[m]) {
-                if (t >= arr[l] && t < arr[m]) r = m - 1;
+                if (arr[l] <= t && t < arr[m]) r = m - 1;
                 else l = m + 1;
             } else {
-                if (t > arr[m] && t <= arr[r]) l = m + 1;
+                if (arr[m] < t && arr[r] >= t) l = m + 1;
                 else r = m - 1;
-            }       
+            }
+
         }
 
         return -1;
-
     }
 }

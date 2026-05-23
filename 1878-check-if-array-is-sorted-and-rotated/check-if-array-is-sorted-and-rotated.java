1c1356bd50960d8bae40ddbcc5fb5e887ffb8e60
uday <62304758+uday510@users.noreply.github.com>
2026-05-23 12:16:28 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (62.78%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,14 +1,11 @@
 class Solution {
     public boolean check(int[] nums) {
-        
         int idx = checkValid(0, nums);
         if (idx == nums.length) {
             return true;
         } 
 
-        idx = checkValid(idx, nums);
-
-        return idx == nums.length && nums[0] >= nums[nums.length - 1];
+        return checkValid(idx, nums) == nums.length && nums[0] >= nums[nums.length - 1];
     }
 
     private int checkValid(int idx, int[] nums) {

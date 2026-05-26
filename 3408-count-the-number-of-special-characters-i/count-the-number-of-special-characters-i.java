bb7556218a6bb9e235e953c66f22ee42cc8b4e48
uday <62304758+uday510@users.noreply.github.com>
2026-05-26 18:29:49 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (46.29%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
@@ -9,15 +9,13 @@ class Solution {
             if (c - 'a' >= 0) {
                 cnt[c - 'a']++;
             } else {
-                cnt[(c - 'A') + 26]++;
+                cnt[c - 'A' + 26]++;
             }
         }
-        
+
         int res = 0;
         for (int i = 0; i < 26; i++) {
-            if (cnt[i] > 0 &&  cnt[i + 26] > 0) {
-                res++;
-            }
+            res += cnt[i] > 0 &&  cnt[i + 26] > 0 ? 1 : 0;
         }
 
         return res;

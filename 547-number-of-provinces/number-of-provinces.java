3f96923bec8637965fad44cfb606a7898610ab28
uday <62304758+uday510@users.noreply.github.com>
2026-05-27 16:00:46 +0530

    Time: 1 ms (86.91%) | Memory: 47 MB (85.55%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,41 +1,39 @@
 class Solution {
-
     public int findCircleNum(int[][] edges) {
-
+        
         int n = edges.length;
         boolean[] vis = new boolean[n];
         int total = 0;
-        
+
         for (int i = 0; i < n; i++) {
-            
             if (!vis[i]) {
                 total++;
                 bfs(i, edges, vis);
             }
         }
-        
+
         return total;
+
     }
-    
+
     private void bfs(int st, int[][] edges, boolean[] vis) {
-        
+
         Queue<Integer> queue = new ArrayDeque<>();
-        
+
         vis[st] = true;
         queue.offer(st);
-        
+
         while (!queue.isEmpty()) {
+            
             int u = queue.poll();
-                        
+            
             for (int v = 0; v < edges[u].length; v++) {
-                
+
                 if (u == v || vis[v] || edges[u][v] == 0) continue;
-                
+
                 vis[v] = true;
                 queue.offer(v);
             }
         }
     }
-
-
 }

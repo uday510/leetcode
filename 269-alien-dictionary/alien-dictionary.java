768aea206bfcf1e7d40e9b5110a7ce244015d46a
uday <62304758+uday510@users.noreply.github.com>
2026-06-03 22:03:02 +0530

    Time: 5 ms (69.94%) | Memory: 45 MB (17.42%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.java
+++ 269-alien-dictionary/alien-dictionary.java
@@ -1,33 +1,35 @@
 class Solution {
+
     public String alienOrder(String[] words) {
-        Map<Character, List<Character>> adj = new HashMap<>();
-        Map<Character, Integer> inorder = new HashMap<>();
-        int n = words.length;
+        
+        Map<Character, Set<Character>> adj = new HashMap<>();
+        Map<Character, Integer> in = new HashMap<>();
 
         for (String w : words) {
-            for (char c : w.toCharArray()) {
-                adj.putIfAbsent(c, new ArrayList<>());
-                inorder.putIfAbsent(c, 0);
+            for (int i = 0; i < w.length(); i++) {
+                char c = w.charAt(i);
+                adj.putIfAbsent(c, new HashSet<>());
+                in.putIfAbsent(c, 0);
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                
-                int m = Math.min(words[i].length(), words[j].length());
+        for (int i = 0; i < words.length - 1; i++) {
+            String w1 = words[i], w2 = words[i + 1];
 
-                for (int k = 0; k < m; k++) {
-                    String w1 = words[i], w2 = words[j];
+            if (w1.length() > w2.length() && w1.startsWith(w2)) {
+                return "";
+            }
 
-                    // "abcd", "ab" invalid 
-                    if (w1.length() > w2.length() && w1.startsWith(w2)) return "";
+            int m = Math.min(w1.length(), w2.length());
 
-                    char c1 = w1.charAt(k), c2 = w2.charAt(k);
+            for (int j = 0; j < m; j++) {
+                char c1 = w1.charAt(j), c2 = w2.charAt(j);
 
-                    if (c1 == c2) continue;
-
-                    adj.get(c1).add(c2);
-                    inorder.put(c2, inorder.getOrDefault(c2, 0) + 1);
+                if (c1 != c2) {
+                    if (!adj.get(c1).contains(c2)) {
+                        adj.get(c1).add(c2);
+                        in.put(c2, in.get(c2) + 1);
+                    }
                     break;
                 }
             }
@@ -35,35 +37,28 @@ class Solution {
 
         Queue<Character> queue = new ArrayDeque<>();
 
-        for (var e : inorder.entrySet()) {
-            char k = e.getKey();
-            int v = e.getValue();
-
-            if (v == 0) {
-                queue.offer(k);
-            }
+        for (Map.Entry<Character, Integer> e : in.entrySet()) {
+            if (e.getValue() == 0) queue.offer(e.getKey());
         }
 
         StringBuilder sb = new StringBuilder();
 
         while (!queue.isEmpty()) {
-            char cur = queue.poll();
+            char u = queue.poll();
+            sb.append(u);
 
-            sb.append(cur);
+            for (char v : adj.get(u)) {
+                in.put(v, in.get(v) - 1);
 
-            for (char v : adj.get(cur)) {
-                inorder.put(v, inorder.getOrDefault(v, 0) - 1);
-
-                if (inorder.get(v) == 0) {
+                if (in.get(v) == 0) {
                     queue.offer(v);
                 }
             }
         }
 
-        if (sb.length() != inorder.size()) {
-            return "";
-        }
+        if (sb.length() != in.size()) return "";
 
         return sb.toString();
+
     }
 }

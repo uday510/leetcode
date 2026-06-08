5834fa1108d71f0265a3cb716b20c224c1b05c5a
uday <62304758+uday510@users.noreply.github.com>
2026-06-08 19:22:16 +0530

    Time: 4 ms (99.77%) | Memory: 63.6 MB (23.11%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,12 +1,12 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] adj) {
         int n = adj.length;
-        int[] vis = new int[n];
+        int[] vis = new int[n + 1];
+        Arrays.fill(vis, -1);
 
         List<Integer> res = new ArrayList<>();
-
         for (int i = 0; i < n; i++) {
-            if (dfs(i, adj, vis)) {
+            if (dfs(i, vis, adj)) {
                 res.add(i);
             }
         }
@@ -14,12 +14,12 @@ class Solution {
         return res;
     }
 
-    private boolean dfs(int u, int[][] adj, int[] vis) {
-        if (vis[u] != 0) return vis[u] == 2;
+    private boolean dfs(int u, int[] vis, int[][] adj) {
+        if (vis[u] != -1) return vis[u] == 2;
 
         vis[u] = 1;
         for (int v : adj[u]) {
-            if (!dfs(v, adj, vis)) {
+            if (!dfs(v, vis, adj)) {
                 return false;
             }
         }

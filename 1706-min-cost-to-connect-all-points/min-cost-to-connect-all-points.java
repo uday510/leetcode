35f6d59c51872866f0aae8fdd2a987d2dc1f2542
uday <62304758+uday510@users.noreply.github.com>
2026-06-09 19:16:08 +0530

    Time: 687 ms (10.06%) | Memory: 171.9 MB (10.40%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,10 +1,12 @@
+import java.util.*;
+
 class Solution {
+
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
+        List<int[]>[] adj = new ArrayList[n];
 
-        List<int[]>[] adjList = new ArrayList[n];
-
-        for (int i = 0; i < n; i++) adjList[i] = new ArrayList<>();
+        for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
             int[] p1 = points[i];
@@ -12,43 +14,42 @@ class Solution {
                 int[] p2 = points[j];
                 int x1 = p1[0], y1 = p1[1], x2 = p2[0], y2 = p2[1];
 
-                int manhattanDistance = getManhattanDistance(x1, y1, x2, y2);
+                int d = dist(x1, y1, x2, y2);
 
-                adjList[i].add(new int[] {j, manhattanDistance});
-                adjList[j].add(new int[] {i, manhattanDistance});
+                adj[i].add(new int[]{j, d});
+                adj[j].add(new int[]{i, d});
             }
         }
 
         boolean[] inMST = new boolean[n];
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(edge -> edge[1]));
-        pq.offer(new int[] {0, 0});
-        int totalWeight = 0, vis = 0;
-
-
+        Queue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(k -> k[1]));
+        pq.offer(new int[]{0, 0});
+        int total = 0, vis = 0;
+        
         while (!pq.isEmpty()) {
             int[] cur = pq.poll();
             int u = cur[0], w = cur[1];
-
+            
             if (inMST[u]) continue;
-
+            
             inMST[u] = true;
             vis++;
-            totalWeight += w;
-
-            for (int[] nxt : adjList[u]) {
+            total += w;
+            
+            for (int[] nxt : adj[u]) {
                 int v = nxt[0], w1 = nxt[1];
-
+                
                 if (!inMST[v]) {
-                    pq.offer(new int[] {v, w1});
+                    pq.offer(new int[]{v, w1});
                 }
             }
         }
-
-        return vis == n ? totalWeight : -1;
-
+        
+        return vis == n ? total : -1;
     }
 
-    private int getManhattanDistance(int x1, int y1, int x2, int y2) {
+    private int dist(int x1, int y1, int x2, int y2) {
         return Math.abs(x1 - x2) + Math.abs(y1 - y2);
     }
+
 }

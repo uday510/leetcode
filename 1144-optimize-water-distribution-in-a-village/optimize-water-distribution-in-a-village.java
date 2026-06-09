c6417c3b471395972a4520ecf972fd444bf04c7e
uday <62304758+uday510@users.noreply.github.com>
2026-06-09 19:37:15 +0530

    Time: 38 ms (31.40%) | Memory: 53.5 MB (22.09%) - LeetSync

--- 1144-optimize-water-distribution-in-a-village/optimize-water-distribution-in-a-village.java
+++ 1144-optimize-water-distribution-in-a-village/optimize-water-distribution-in-a-village.java
@@ -1,47 +1,49 @@
-class Solution {
-    public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
-        List<int[]>[] adjList = new ArrayList[n + 1];
+import java.util.*;
 
-        for (int i = 0; i <= n; i++) adjList[i] = new ArrayList<>();
+class Solution {
+
+    public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
+        List<int[]>[] adj = new ArrayList[n + 1];
+
+        for (int i = 0; i <= n; i++) adj[i] = new ArrayList<>();
 
         for (int[] p : pipes) {
             int u = p[0], v = p[1], w = p[2];
 
-            adjList[u].add(new int[] {v, w});
-            adjList[v].add(new int[] {u, w});
+            adj[u].add(new int[]{v, w});
+            adj[v].add(new int[]{u, w});
         }
 
         for (int i = 0; i < n; i++) {
             int u = 0, v = i + 1, w = wells[i];
 
-            adjList[u].add(new int[] {v, w});
-            adjList[v].add(new int[] {u, w});
+            adj[u].add(new int[] {v, w});
         }
 
         boolean[] inMST = new boolean[n + 1];
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(edge -> edge[1]));
-        pq.offer(new int[] {0, 0});
-        int totalWeight = 0;
-
+        Queue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(k -> k[1]));
+        pq.offer(new int[]{0, 0});
+        int total = 0;
+        
         while (!pq.isEmpty()) {
             int[] cur = pq.poll();
             int u = cur[0], w = cur[1];
-
+            
             if (inMST[u]) continue;
-
+            
             inMST[u] = true;
-            totalWeight += w;
-
-            for (int[] nxt : adjList[u]) {
+            total += w;
+            
+            for (int[] nxt : adj[u]) {
                 int v = nxt[0], w1 = nxt[1];
-
+                
                 if (!inMST[v]) {
                     pq.offer(new int[] {v, w1});
                 }
             }
         }
-
-        return totalWeight;
+        
+        return total;
     }
 }

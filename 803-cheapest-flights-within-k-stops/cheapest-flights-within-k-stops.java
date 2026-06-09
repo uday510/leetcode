14cabeff1fdec96eb8079c82f464eb3ea24f804a
uday <62304758+uday510@users.noreply.github.com>
2026-06-09 18:43:41 +0530

    Time: 16 ms (8.33%) | Memory: 47.4 MB (5.43%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,45 +1,50 @@
+import java.util.*;
+
 class Solution {
+
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        
-        List<int[]>[] adjList = new ArrayList[n];
 
-        for (int i = 0; i < n; i++) adjList[i] = new ArrayList<>();
+        List<int[]>[] adj = new ArrayList[n];
 
-        for (int[] flight : flights) {
-            int u = flight[0], v = flight[1], w = flight[2];
+        for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
 
-            adjList[u].add(new int[] {v, w});
+        for (int[] fli : flights) {
+            int u = fli[0], v = fli[1], w = fli[2];
+
+            adj[u].add(new int[]{v, w});
         }
 
         int[][] dists = new int[k + 2][n];
-        int unknown = (int) 1e9;
+        int un = Integer.MAX_VALUE;
 
-        for (int[] row : dists) Arrays.fill(row, unknown);
+        for (int[] r : dists) Arrays.fill(r, un);
 
-        Queue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt( x -> x[1]));
-
-        dists[0][src] = 0;
-        pq.offer(new int[] {src, 0, 0});
-
+        Queue<int[]> pq = new PriorityQueue<>(Comparator.comparing(x -> x[1]));
+        pq.offer(new int[]{src, 0, 0});
+        
+        pq.offer(new int[]{src, 0, 0});
+        
         while (!pq.isEmpty()) {
             int[] cur = pq.poll();
-            int u = cur[0], cost = cur[1], stops = cur[2];
-
-            if (u == dst) return cost;
-
-            if (stops == k + 1) continue;
-
-            for (int[] nxt : adjList[u]) {
+            int u = cur[0], c = cur[1], s = cur[2];
+            
+            if (u == dst) return c;
+            
+            if (s > k || dists[s][u] < c) continue;
+            
+            for (int[] nxt : adj[u]) {
                 int v = nxt[0], w = nxt[1];
-
-                if (cost + w < dists[stops + 1][v]) {
-                    dists[stops + 1][v] = cost + w;
-                    pq.offer(new int[] {v, cost + w, stops + 1});
+                
+                if (c + w < dists[s + 1][v]) {
+                    
+                    dists[s + 1][v] = c + w;
+                    pq.offer(new int[]{v, c + w, s + 1});
                 }
             }
         }
-
-
+        
         return -1;
+        
     }
+
 }

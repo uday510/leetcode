c00fc5fa2097ab162c79d9576a9fbceb9ff7bfaa
uday <62304758+uday510@users.noreply.github.com>
2026-06-14 17:43:27 +0530

    Time: 45 ms (91.26%) | Memory: 128.9 MB (45.98%) - LeetSync

--- 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
@@ -1,59 +1,31 @@
 class Solution {
-    
-    List<Integer>[] adjList;
-    int[] time;
+    public int numOfMinutes(int n, int h, int[] m, int[] in) {
+        List<Integer>[] adj = new ArrayList[n];
 
-    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        adjList = new ArrayList[n];
-        this.time = informTime;
-        
-        for (int i = 0; i < n; i++) adjList[i] = new ArrayList<>();
+        for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            if (manager[i] != -1) {
-                adjList[manager[i]].add(i);
+            if (m[i] != -1) {
+                adj[m[i]].add(i);
             }
         }
 
-        // return dfs(headID);
-
         Queue<int[]> queue = new ArrayDeque<>();
-        int dist = 0;
-
-        queue.offer(new int[] {headID, 0});
+        int mx = 0;
+        
+        queue.offer(new int[]{h, 0});
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
             int u = cur[0], w = cur[1];
-            dist = Math.max(dist, w);
 
-            for (int v : adjList[u]) {
-                queue.offer(new int[] {v, w + time[u]});
+            mx = Math.max(mx, w);
+            
+            for (int v : adj[u]) {
+                queue.offer(new int[] {v, w + in[u]});
             }
         }
 
-        return dist;
-    }
+        return mx;
 
-    private int dfs(int u) {
-        int dist = 0;
-
-        for (int v : adjList[u]) {
-            dist = Math.max(dist, dfs(v));
-        }
-
-        return dist + time[u];
     }
 }
-
-/**
-
-
-Input: n = 6, headID = 2, manager = [2,2,-1,2,2,2], informTime = [0,0,1,0,0,0]
-
-
-2 -> 0, 1, 3, 4, 5
-
-
-
-
-*/

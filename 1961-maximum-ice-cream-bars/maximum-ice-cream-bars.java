0cd4996abd51b1c2bb34fae37add21aaac3fd545
uday <62304758+uday510@users.noreply.github.com>
2026-06-21 11:52:37 +0530

    Time: 6 ms (89.05%) | Memory: 81.9 MB (97.92%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
@@ -2,19 +2,19 @@ class Solution {
     public int maxIceCream(int[] costs, int coins) {
         int[] cnt = new int[100001];
         long total = 0;
-        int n = costs.length, max = 0;
+        int n = costs.length, mx = 0;
 
         for (int c : costs) {
             cnt[c]++;
             total += c;
-            if (c > max) max = c;
+            mx = Math.max(mx, c);
         }
 
         if (total < coins) return n;
 
         long cur = coins;
         int i = 0, res = 0;
-        while (i <= max && cur > 0) {
+        while (i <= mx && cur > 0) {
             long req = i * cnt[i];
 
             if (cur - req < 0) {

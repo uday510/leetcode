6973cd8dcf2c92ab0d9aaff54f7bd834491e523c
uday <62304758+uday510@users.noreply.github.com>
2026-06-22 08:40:21 +0530

    Time: 3 ms (70.35%) | Memory: 43.3 MB (79.23%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -0,0 +1,34 @@
+class Solution {
+    private static final String s = "balloon";
+    private static final Set<Character> d = new HashSet<>();
+    static {
+        d.add('l');
+        d.add('o');
+    }
+
+    public int maxNumberOfBalloons(String t) {
+        
+        int[] cnt = new int[26];
+
+        for (int i = 0; i < t.length(); i++) {
+            cnt[t.charAt(i) - 'a']++;
+        }
+    
+        int mn = t.length();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int idx = c - 'a';
+
+            if (cnt[idx] == 0) return 0;
+
+
+            if(d.contains(c)) {
+                mn = Math.min(mn, cnt[idx] / 2);
+            } else {
+                mn = Math.min(mn, cnt[idx]);
+            }
+        }
+
+        return mn;
+    }
+}

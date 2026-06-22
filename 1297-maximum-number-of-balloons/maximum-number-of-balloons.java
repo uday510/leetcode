a29acff0ca4a491635328f9965d6da16d9bd9511
uday <62304758+uday510@users.noreply.github.com>
2026-06-22 08:49:58 +0530

    Time: 3 ms (70.35%) | Memory: 43.4 MB (62.01%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,35 +1,34 @@
 class Solution {
-    private static final String s = "balloon";
-    private static final Set<Character> d = new HashSet<>();
-    static {
-        d.add('l');
-        d.add('o');
+
+    public int maxNumberOfBalloons(String s) {
+        return maxNumberOfBalloons(s, "balloon");
     }
 
-    public int maxNumberOfBalloons(String t) {
-        
-        int[] cnt = new int[26];
+    private int maxNumberOfBalloons(String s, String p) {
+        int[] sCnt = new int[26];
+        int[] pCnt = new int[26];
 
-        for (int i = 0; i < t.length(); i++) {
-            cnt[t.charAt(i) - 'a']++;
+        for (int i = 0; i < p.length(); i++) {
+            pCnt[p.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            sCnt[s.charAt(i) - 'a']++;
         }
     
-        int mn = t.length();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            int idx = c - 'a';
-            int cur = cnt[idx];
+        int mn = s.length();
+        for (int i = 0; i < p.length(); i++) {
+            int idx = p.charAt(i) - 'a';
 
-            if (cnt[idx] == 0) return 0;
+            if (sCnt[idx] == 0) return 0;
 
-            
-            if(d.contains(c)) {
-                cur /= 2;
+            if (pCnt[idx] > 0) {
+                mn = Math.min(mn, sCnt[idx] / pCnt[idx]);
             }
-
-            mn = Math.min(mn, cur);
         }
 
         return mn;
     }
+
+
 }

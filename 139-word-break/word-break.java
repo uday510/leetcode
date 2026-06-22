a491b78fccde6306701dc184f15606bc67b6aafb
uday <62304758+uday510@users.noreply.github.com>
2026-06-22 22:20:23 +0530

    Time: 6 ms (68.76%) | Memory: 46.2 MB (35.54%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,28 +1,27 @@
 class Solution {
-
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> words = new HashSet<>(wordDict);
+        
         int[] dp = new int[s.length()];
         Arrays.fill(dp, -1);
 
-        return dfs(0, s, words, dp, s.length());    
+        return dfs(0, s.length(), s, new HashSet<>(wordDict), dp);
     }
 
-    private boolean dfs(int i, String s, Set<String> words, int[] dp, int n) {
+    private boolean dfs(int i, int n, String s, Set<String> words, int[] dp){
         if (i >= n) return true;
 
         if (dp[i] != -1) return dp[i] == 1;
 
         for (int j = i; j < n; j++) {
             String sub = s.substring(i, j + 1);
-            if (words.contains(sub) && dfs(j + 1, s, words, dp, n)) {
+
+            if (words.contains(sub) && dfs(j + 1, n, s, words, dp)) {
                 dp[i] = 1;
                 return true;
             }
         }
 
         dp[i] = 0;
-
         return false;
     }
 }

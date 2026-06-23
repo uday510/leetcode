641250ee8a38c553eade09d0e9cedae3782161a6
uday <62304758+uday510@users.noreply.github.com>
2026-06-23 14:57:06 +0530

    Time: 3 ms (98.37%) | Memory: 47.2 MB (33.68%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,5 +1,6 @@
 class Solution {
     public int minDistance(String w1, String w2) {
+        
         int n = w1.length();
         int m = w2.length();
         int[][] dp = new int[n][m];
@@ -9,36 +10,37 @@ class Solution {
         return dfs(0, 0, n, m, w1, w2, dp);
     }
 
-    private int dfs(int i, int j, int n, int m, String w1, String w2, int[][] dp) {
-        if (i >= n && j >= m) {
-            return 0;
-        }
+    private int dfs(
+        int i,
+        int j,
+        int n,
+        int m,
+        String w1,
+        String w2,
+        int[][] dp
+    ) {
+        
+        if (i >= n && j >= m) return 0;
+        if (i >= n) return m - j;
+        if (j >= m) return n - i;
 
-        if (i >= n) {
-            return m - j;
-        }
-
-        if (j >= m) {
-            return n - i;
-        }
-
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
+        if (dp[i][j] != -1) return dp[i][j];
 
         int cur = n + m;
+
         if (w1.charAt(i) == w2.charAt(j)) {
             cur = dfs(i + 1, j + 1, n, m, w1, w2, dp);
         } else {
-
             int ins = dfs(i + 1, j, n, m, w1, w2, dp);
             int del = dfs(i, j + 1, n, m, w1, w2, dp);
             int repl = dfs(i + 1, j + 1, n, m, w1, w2, dp);
-            
+
             int t = Math.min(ins, Math.min(del, repl)) + 1;
-            cur = Math.min(cur, t);
+
+            cur = Math.min(t, cur);
         }
 
         return dp[i][j] = cur;
     }
+
 }

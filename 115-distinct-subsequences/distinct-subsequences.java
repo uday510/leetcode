af12f76a4e26d83c6699e0436a1dbdc0bc2e41c3
uday <62304758+uday510@users.noreply.github.com>
2026-06-24 00:15:28 +0530

    Time: 24 ms (15.11%) | Memory: 54.4 MB (40.34%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,34 +1,38 @@
 class Solution {
-
-    int m, n;
-    String s, t;
-    int[][] dp;
-
     public int numDistinct(String s, String t) {
-        m = s.length(); n = t.length();
-        this.s = s; this.t = t;
-        dp = new int[m][n];
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n][m];
 
-        for (int[] row : dp) Arrays.fill(row, -1);
+        for (int[] r : dp) Arrays.fill(r, -1);
 
-        return dfs(0, 0);
+        return dfs(0, 0, n, m, s, t, dp);
     }
 
-    private int dfs(int i, int j) {
-        if (j >= n) return 1;
-        if (i >= m) return 0;
+    private int dfs(int i, int j, int n, int m, String s, String t, int[][] dp) {
+        if (j >= m) return 1;
+        if (i >= n) return 0;
 
         if (dp[i][j] != -1) return dp[i][j];
 
-        int cur = -1;
-
+        int cur = 0;
         if (s.charAt(i) == t.charAt(j)) {
-            cur = dfs(i + 1, j + 1) + dfs(i + 1, j);
+            cur = dfs(i + 1, j + 1, n, m, s, t, dp) + dfs(i + 1, j, n, m, s, t, dp);
         } else {
-            cur = dfs(i + 1, j);
+            cur = dfs(i + 1, j, n, m, s, t, dp);
         }
-
 
         return dp[i][j] = cur;
     }
 }
+
+/**
+
+
+
+aabc
+
+abc
+
+
+ */

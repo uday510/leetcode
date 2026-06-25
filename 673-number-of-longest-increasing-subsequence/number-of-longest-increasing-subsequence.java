a495792c94d8bbfa65cd35ec9d041787248b5923
uday <62304758+uday510@users.noreply.github.com>
2026-06-25 13:04:13 +0530

    Time: 180 ms (5.17%) | Memory: 286 MB (5.02%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
@@ -1,46 +1,44 @@
-class Pair {
-    int len;
-    int cnt;
+class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        
+        int n = nums.length;
+        Pair[][] dp = new Pair[n + 1][n];
 
-    Pair(int len, int cnt) { this.len = len; this.cnt = cnt; }
+        return dfs(-1, 0, nums.length, nums, dp).cnt;
+    }
+
+    private Pair dfs(int p, int c, int n, int[] arr, Pair[][] dp) {
+        if (p >= n || c >= n) return new Pair(0, 1);
+
+        if (dp[p + 1][c] != null) return dp[p + 1][c];
+
+        Pair s = dfs(p, c + 1, n, arr, dp);
+        int len1 = s.len, cnt1 = s.cnt;
+        int len = len1, cnt = cnt1;
+
+        if (p == -1 || arr[p] < arr[c]) {
+
+            Pair nxt = dfs(c, c + 1, n, arr, dp);
+            int len2 = 1 + nxt.len;
+            int cnt2 = nxt.cnt;
+
+            if (len2 > len1) {
+                len = len2;
+                cnt = cnt2;
+            } else if (len2 == len1) {
+                cnt += cnt2;
+            }
+        }
+
+        return dp[p + 1][c] = new Pair(len, cnt);
+    }
 }
 
-class Solution {
+class Pair {
+    int len, cnt;
 
-    Pair[][] dp;
-    int n;
-    int[] nums;
-
-    public int findNumberOfLIS(int[] nums) {
-        n = nums.length;
-        this.nums = nums;
-        dp = new Pair[n + 1][n];
-
-        return dfs(-1, 0).cnt;
+    Pair (int len, int cnt) {
+        this.len = len;
+        this.cnt = cnt;
     }
-
-    private Pair dfs(int i, int j) {
-        if (j >= n) return new Pair(0, 1);
-
-        if (dp[i + 1][j] != null) return dp[i + 1][j];
-
-        Pair skip = dfs(i, j + 1);
-        int len = skip.len;
-        int cnt = skip.cnt;
-
-        if (i == -1 || nums[i] < nums[j]) {
-
-            Pair take = dfs(j, j + 1);
-            int tLen = 1 + take.len;
-            int tCnt = take.cnt;
-            
-           if (tLen > len) {
-                len = tLen;
-                cnt = tCnt;
-           } else if (tLen == len) cnt += tCnt;
-        }
-        
-        return dp[i + 1][j] = new Pair(len, cnt);
-    }
-
 }

65a8c6c3693d8a859423ceea7689d2b8ba1ab916
uday <62304758+uday510@users.noreply.github.com>
2026-06-25 13:37:22 +0530

    Time: 177 ms (5.17%) | Memory: 286.2 MB (5.02%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
@@ -1,44 +1,47 @@
 class Solution {
-    public int findNumberOfLIS(int[] nums) {
-        
-        int n = nums.length;
+
+    public int findNumberOfLIS(int[] arr) {
+
+        int n = arr.length;
         Pair[][] dp = new Pair[n + 1][n];
 
-        return dfs(-1, 0, nums.length, nums, dp).cnt;
+        return dfs(-1, 0, n, arr, dp).cnt;
     }
 
     private Pair dfs(int p, int c, int n, int[] arr, Pair[][] dp) {
         if (p >= n || c >= n) return new Pair(0, 1);
 
-        if (dp[p + 1][c] != null) return dp[p + 1][c];
+        if (dp[p + 1][c] != null) {
+            return dp[p + 1][c];
+        }
 
-        Pair s = dfs(p, c + 1, n, arr, dp);
-        int len1 = s.len, cnt1 = s.cnt;
-        int len = len1, cnt = cnt1;
-
+        Pair skip = dfs(p, c + 1, n, arr, dp);
+        int len = skip.len, cnt = skip.cnt;
+        
         if (p == -1 || arr[p] < arr[c]) {
-
             Pair nxt = dfs(c, c + 1, n, arr, dp);
-            int len2 = 1 + nxt.len;
+            int len2 = nxt.len + 1;
             int cnt2 = nxt.cnt;
-
-            if (len2 > len1) {
+            
+            if (len2 > len) {
                 len = len2;
                 cnt = cnt2;
-            } else if (len2 == len1) {
+            } else if (len2 == len) {
                 cnt += cnt2;
             }
         }
-
+        
         return dp[p + 1][c] = new Pair(len, cnt);
     }
 }
 
 class Pair {
+
     int len, cnt;
 
     Pair (int len, int cnt) {
         this.len = len;
         this.cnt = cnt;
     }
+
 }

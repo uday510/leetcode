9cbdad55e33b53423b1b4bab8c91a94121a2bfce
uday <62304758+uday510@users.noreply.github.com>
2026-06-30 16:04:22 +0530

    Time: 50 ms (33.59%) | Memory: 105.5 MB (5.49%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -1,52 +1,48 @@
 class Solution {
-    private List<Integer> lis;
+
     public int maxEnvelopes(int[][] envs) {
-        lis = new ArrayList<>();
+
+        List<Integer> lis = new ArrayList<>();
 
         Arrays.sort(envs, (o1, o2) -> {
             if (o1[0] == o2[0]) return o2[1] - o1[1];
             return o1[0] - o2[0];
         });
 
-        int n = envs.length;
 
-        for (int i = 0; i < n; i++) {
+        for (int[] e : envs) {
 
-            int cur = envs[i][1];
-            int index = bs(cur);
+            int val = e[1];
+            int index = bs(val, lis);
 
-            if (index == lis.size()) lis.add(cur);
-            lis.set(index, cur);
+            if (index == lis.size()) lis.add(val);
+            lis.set(index, val);
+
         }
 
         return lis.size();
     }
 
-    private int bs(int t) {
-        int l = 0, r = lis.size();
+    private int bs(int t, List<Integer> list) {
+
+        int l = 0, r = list.size();
 
         while (l < r) {
+            int m = l + ((r - l) >> 1);
 
-            int m = l + ( (r - l) >> 1);
-
-            if (lis.get(m) < t) l = m + 1;
+            if (list.get(m) < t) l = m + 1;
             else r = m;
         }
 
-
         return l;
     }
+
 }
 
+/**
 
-/***
+[1, 1] [2, 3] [4, 6] [4, 5] [5, 7]
 
-
-[1, 1] [2, 3] [4, 6], [4, 5] [6, 7]
-
-[1, 3, 5, 7]
-
-
-
+1, 3, 5, 7
 
  */

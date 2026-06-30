0d4feb4fbfc3889381c8509ba7253a885d10261d
uday <62304758+uday510@users.noreply.github.com>
2026-06-30 20:47:49 +0530

    Time: 26 ms (23.99%) | Memory: 54 MB (50.71%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,29 +1,39 @@
 class Solution {
-    int[][] dp;
-    String s1, s2;
-    int m, n;
-    public int longestCommonSubsequence(String text1, String text2) {
-        s1 = text1;
-        s2 = text2;
-        m = s1.length();
-        n = s2.length();
+    public int longestCommonSubsequence(String t1, String t2) {
+        
+        
+        int[][] dp = new int[t1.length()][t2.length()];
 
-        dp = new int[m][n];
-        for (int[] row : dp) Arrays.fill(row, -1);
+        for (int[] r : dp) Arrays.fill(r, -1);
 
-        return dfs(0, 0);
+        return dfs(0, 0, t1.length(), t2.length(), t1, t2, dp);
     }
 
-    private int dfs(int i, int j) {
-        if (i >= m || j >= n) return 0;
+    private int dfs(int i, int j, int n, int m, String t1, String t2, int[][] dp) {
+
+        if (i >= n || j >= m) return 0;
 
         if (dp[i][j] != -1) return dp[i][j];
-        if (s1.charAt(i) == s2.charAt(j)) {
-            dp[i][j] = 1 + dfs(i + 1, j + 1);
+
+        if (t1.charAt(i) == t2.charAt(j)) {
+            dp[i][j] = 1 + dfs(i + 1, j + 1, n, m, t1, t2, dp);
         } else {
-            dp[i][j] = Math.max(dfs(i, j + 1), dfs(i + 1, j));
+            dp[i][j] =  Math.max(
+                dfs(i + 1, j, n, m, t1, t2, dp),
+                dfs(i, j + 1, n, m, t1, t2, dp)
+            );
         }
 
         return dp[i][j];
     }
 }
+
+/**
+
+
+ ab
+ ba             1
+0 ->  max  ( (i + 1, j) , (i, j + 1) )
+
+
+ */

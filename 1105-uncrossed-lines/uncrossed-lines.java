7732d6244eb9e89f7673f6544beb5fddffd35a43
uday <62304758+uday510@users.noreply.github.com>
2026-07-01 16:34:49 +0530

    Time: 7 ms (28.24%) | Memory: 45.4 MB (90.33%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.java
+++ 1105-uncrossed-lines/uncrossed-lines.java
@@ -1,30 +1,29 @@
 class Solution {
-    int[] a1, a2;
-    int[][] dp;
-    int n, m;
-    public int maxUncrossedLines(int[] nums1, int[] nums2) {
-        a1 = nums1;
-        a2 = nums2;
-        n = a1.length;
-        m = a2.length;
-        dp = new int[n][m];
 
-        for (int[] row : dp) Arrays.fill(row, -1);
+    public int maxUncrossedLines(int[] arr1, int[] arr2) {
 
-        return dfs(0, 0);
-    }
-    private int dfs(int i, int j) {
-        if (i >= n || j >= m) return 0;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
+        int n = arr1.length, m = arr2.length;
+        int[][] dp = new int[n][m];
         
-        if (a1[i] == a2[j]) {
-            dp[i][j] = 1 + dfs(i + 1, j + 1);
+        for (int[] r : dp) Arrays.fill(r, -1);
+        
+        return dfs(0, 0, n, m, arr1, arr2, dp);
+    }
+    
+    private int dfs(int i, int j, int n, int m, int[] arr1, int[] arr2, int[][] dp) {
+        if (i >= n || j >= m) return 0;
+        
+        if (dp[i][j] != -1) return dp[i][j];
+        
+        if (arr1[i] == arr2[j]) {
+            dp[i][j] = 1 + dfs(i + 1, j + 1, n, m, arr1, arr2, dp);
         } else {
-            dp[i][j] = Math.max(dfs(i + 1, j), dfs(i, j + 1));
+            dp[i][j] = Math.max(
+              dfs(i, j + 1, n, m, arr1, arr2, dp),
+              dfs(i + 1, j, n, m, arr1, arr2, dp)      
+            );
         }
-
+        
         return dp[i][j];
     }
 }

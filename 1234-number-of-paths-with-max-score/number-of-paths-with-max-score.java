2dac9c373d1dae98a4a5bb94146c082e240a9eb5
uday <62304758+uday510@users.noreply.github.com>
2026-07-06 14:49:35 +0530

    Time: 10 ms (82.76%) | Memory: 46.5 MB (68.97%) - LeetSync

--- 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.java
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 class Solution {
 
-    private static char OBS = 'X';
-    static long MOD = (long) 1e9 + 7;
-    static  int[][] DIRs = { {1, 0}, {0, 1}, {1, 1} };
+    private final static char OBS = 'X';
+    static final long MOD = (long) 1e9 + 7;
+    static final int[][] DIRs = { {1, 0}, {0, 1}, {1, 1} };
 
     public int[] pathsWithMaxScore(List<String> b) {
 
@@ -15,50 +15,57 @@ class Solution {
             for (long[] c : r)
                 c[0] = -1;
 
-        dp[n - 1][n - 1][0] = 0;
-        dp[n - 1][n - 1][1] = 1;
+       dp[n - 1][n - 1][0] = 0;
+       dp[n - 1][n - 1][1] = 1;
 
+       for (int x = n - 1; x > -1; x--) {
+           for (int y = n - 1; y > -1; y--) {
 
-        for (int x = n - 1; x > -1; x--) {
-            for (int y = n - 1; y > -1; y--) {
-                if (x == n - 1 && y == n - 1)
+               char ch = b.get(x).charAt(y);
+               if (ch == OBS || (x == n - 1 && y == n - 1))
+                   continue;
+
+               long s = -1, c = 0;
+
+               for (int[] nxt : DIRs) {
+                   int nx = nxt[0] + x;
+                   int ny = nxt[1] + y;
+
+                   if (nx >= n || ny >= n || dp[nx][ny][0] == -1)
+                       continue;
+
+                   long s1 = dp[nx][ny][0], c1 = dp[nx][ny][1];
+                   
+                   if (s1 > s) {
+                       s = s1;
+                       c = c1;
+                   } else if (s1 == s) {
+                       c = (c + c1) % MOD;
+                   }
+               }
+
+               if (s == -1)
                     continue;
-
-                char ch = b.get(x).charAt(y);
-                if (ch == OBS)
-                        continue;
-                long sum = -1, cnt = 0;
-                for (int[] d : DIRs) {
-                    int nx = d[0] + x;
-                    int ny = d[1] + y;
-
-                    if (nx >= n || ny >= n || dp[nx][ny][0] == -1)
-                        continue;
-
-                    long s = dp[nx][ny][0];
-                    long c = dp[nx][ny][1];
-
-                    if (s > sum) {
-                        sum = s;
-                        cnt = c;
-                    } else if (s == sum) {
-                        cnt = (cnt + c) % MOD;
-                    }
-                }
-
-                if (sum == -1)
-                    continue;
-
-                int val = (ch == 'E' || ch == 'S') ? 0 : ch - '0';
-                dp[x][y][0] = sum + val;
-                dp[x][y][1] = cnt;
-            }
-        }
-
-        if (dp[0][0][0] < 0)
-            return new int[] {0, 0};
-
-        return new int[] {(int) dp[0][0][0], (int) dp[0][0][1] };
-
+               
+               long sum = s + (ch == 'E' ? 0 : (ch - '0'));
+               
+               dp[x][y][0] = sum;
+               dp[x][y][1] = c;
+           }
+       }
+       
+       if (dp[0][0][0] == -1)
+           return new int[] {0, 0};
+       
+       return new int[] {(int) dp[0][0][0], (int) dp[0][0][1] };
     }
 }
+
+/**
+ *
+ *  E   2   3
+ *  2   X   2
+ *  1   2   S
+ *
+ *
+ */

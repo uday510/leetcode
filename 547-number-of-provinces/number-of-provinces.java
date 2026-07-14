e28356aa5e2b1fbe3d308e98c66459f65cbab8e2
uday <62304758+uday510@users.noreply.github.com>
2026-07-14 16:03:23 +0530

    Time: 1 ms (87.68%) | Memory: 47.1 MB (74.20%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,37 +1,35 @@
 class Solution {
     public int findCircleNum(int[][] edges) {
-        
         int n = edges.length;
         boolean[] vis = new boolean[n];
-        int total = 0;
+        int provinces = 0;
 
         for (int i = 0; i < n; i++) {
-            if (vis[i]) continue;
-
-            total++;
-            bfs(i, edges, vis);
+            if (!vis[i]) {
+                provinces++;
+                bfs(i, vis, edges);
+            }
         }
 
-        return total;
+        return provinces;
     }
 
-    private void bfs(int st, int[][] edges, boolean[] vis) {
-
+    private void bfs(int st, boolean[] vis, int[][] edges) {
         vis[st] = true;
+
         Queue<Integer> queue = new ArrayDeque<>();
         queue.offer(st);
 
         while (!queue.isEmpty()) {
-            int u = queue.poll();
+            int cur = queue.poll();
 
-            for (int i = 0; i < edges[u].length; i++) {
-                boolean ok = i != u && edges[u][i] == 1 && !vis[i];
-
-                if (!ok) continue;
-
-                vis[i] = true;
-                queue.offer(i);
-            }
+           for (int i = 0; i < edges[cur].length; i++) {
+                if (edges[cur][i] == 1 && !vis[i]) {
+                    vis[i] = true;
+                    queue.offer(i);
+                }
+           }
+           
         }
     }
 }

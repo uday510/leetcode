6eda0cbef8dadfb1328c297b98deac53468c026c
uday <62304758+uday510@users.noreply.github.com>
2026-07-15 12:51:47 +0530

    Time: 4 ms (99.22%) | Memory: 46.4 MB (94.20%) - LeetSync

--- 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.java
+++ 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.java
@@ -1,30 +1,33 @@
 class Solution {
-    public int[] shortestAlternatingPaths(int n, int[][] r1, int[][] b1) {
-        
+
+    public int[] shortestAlternatingPaths(int n, int[][] redEdges, int[][] blueEdges) {
 
         List<int[]>[] adj = new ArrayList[n];
+        for (int i = 0; i < n; i++)
+            adj[i] = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
+        add(0, redEdges, adj);
+        add(1, blueEdges, adj);
 
-        add(n, 0, r1, adj);
-        add(n, 1, b1, adj);
+        int[][] dist = new int[n][2];
+        int unk = Integer.MAX_VALUE;
+
+        for (int[] r : dist)
+                Arrays.fill(r, unk);
 
         Queue<int[]> queue = new ArrayDeque<>();
-        int[][] dist = new int[n][2];
-        for (int[] r : dist) Arrays.fill(r, (int) 1e9);
-
         dist[0][0] = dist[0][1] = 0;
         queue.offer(new int[] {0, 0, -1});
 
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
-            int u = cur[0], w = cur[1], c1 = cur[2];
+            int u = cur[0], w = cur[1], c = cur[2];
 
             for (int[] nxt : adj[u]) {
-                int v = nxt[0], c2 = nxt[1];
-                if (c1 != c2 && w + 1 < dist[v][c2]) {
-                    dist[v][c2] = w + 1;
-                    queue.offer(new int[] {v, w + 1, c2});
+                int v = nxt[0], c1 = nxt[1];
+                if (c != c1 && w + 1 < dist[v][c1]) {
+                    dist[v][c1] = w + 1;
+                    queue.offer(new int[] {v, w + 1, c1});
                 }
             }
         }
@@ -32,31 +35,19 @@ class Solution {
         int[] res = new int[n];
         for (int i = 0; i < n; i++) {
             res[i] = Math.min(dist[i][0], dist[i][1]);
-            res[i] = res[i] == (int) 1e9 ? -1 : res[i];
+            res[i] = res[i] == unk ? -1 : res[i];
         }
-
+        
         return res;
     }
 
-    private void add(int n, int k, int[][] edges, List<int[]>[] adj) {
+    private void add(int k, int[][] edges, List<int[]>[] adj) {
 
-        for (int[] e : edges) {
-            int u = e[0], v = e[1];
-            adj[u].add(new int[] {v, k});
-        }
+       for (int[] e : edges) {
+           int u = e[0], v = e[1];
+           adj[u].add(new int[] {v, k});
+       }
+
     }
+
 }
-
-
-/**
-
-0 -> {1, 0}
-1 ->
-2 -> {1, 1}
-
-
-
-
-
-
- */

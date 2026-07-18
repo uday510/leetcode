94d4d93037309b971c30434db9346d5d876c9e91
uday <62304758+uday510@users.noreply.github.com>
2026-07-18 10:59:25 +0530

    Time: 11 ms (95.67%) | Memory: 43.9 MB (76.73%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -1,42 +1,45 @@
 class Solution {
-
-    Map<TreeNode, TreeNode> pMap;
-    Set<TreeNode> vis;
-    List<Integer> dK;
-    TreeNode t;
-
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        pMap = new HashMap<>();
-        vis = new HashSet<>();
-        dK = new ArrayList<>();
-        t = target;
+        
+        Map<TreeNode, TreeNode> map = new HashMap<>();
+        Set<TreeNode> vis = new HashSet<>();
+        List<Integer> res = new ArrayList<>();
+        
+        assignNodes(root, map);
+        dfs(target, k, vis, map, res);
 
-        assign(root);
-        dfs(t, k);
-
-        return dK;    
+        return res;
     }
 
-    private void dfs(TreeNode node, int k) {
-        if (node == null || !vis.add(node)) return;
+    private void dfs(TreeNode node, 
+    int k, Set<TreeNode> vis, 
+    Map<TreeNode, TreeNode> map, List<Integer> res) {
 
+        if (node == null || vis.contains(node)) return;
+
+        vis.add(node);
         if (k == 0) {
-            dK.add(node.val);
+            res.add(node.val);
             return;
         }
 
-        dfs(pMap.get(node), k - 1);
-        dfs(node.left, k - 1);
-        dfs(node.right, k - 1);
+        dfs(map.get(node), k - 1, vis, map, res);
+        dfs(node.left, k - 1, vis, map, res);
+        dfs(node.right, k - 1, vis, map, res);
     }
 
-    private void assign(TreeNode node) {
+    private void assignNodes(TreeNode node, Map<TreeNode, TreeNode> map) {
         if (node == null) return;
 
-        pMap.put(node.left, node);
-        pMap.put(node.right, node);
+        addNode(node.left, node, map);
+        addNode(node.right, node, map);
 
-        assign(node.left);
-        assign(node.right);
+        assignNodes(node.left, map);
+        assignNodes(node.right, map);
+    }
+
+    private void addNode(TreeNode child, TreeNode parent, Map<TreeNode, TreeNode> map) { 
+        if (child == null) return;       
+        map.put(child, parent);
     }
 }

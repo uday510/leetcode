5f57b28127347d8c2a1a1c03f3a1d526a67830aa
uday <62304758+uday510@users.noreply.github.com>
2026-07-19 19:39:23 +0530

    Time: 12 ms (10.13%) | Memory: 49.6 MB (100.00%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,55 +1,58 @@
 class Solution {
 
-    private static final int[][] DIRs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-    int m, n;
-    int[][] grid;
-    
+    private static final int[][] DIRs = { {0, 1}, {1, 0}, {-1, 0}, {0, -1} };
+
     public int numEnclaves(int[][] grid) {
-        m = grid.length;
-        n = grid[0].length;
-        this.grid = grid;
-        
+
         int cnt = 0;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        boolean foundBoundary = false;
+        int n = grid.length, m = grid[0].length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1) {
-                    cnt += bfs(i, j);
+                    cnt += bfs(i, j, n, m, grid);
                 }
             }
         }
-        
+
         return cnt;
     }
-    
-    private int bfs(int x, int y) {
-        Queue<int[]> queue = new ArrayDeque<>();
+
+    private int bfs(int i, int j, int n, int m, int[][] grid) {
+
         int cnt = 0;
-        queue.offer(new int[] {x, y});
-        grid[x][y] = 0;
-        boolean boundaryPath = false;
+        boolean foundBoundary = false;
+        grid[i][j] = 0;
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[] {i, j});
 
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
             int dx = cur[0], dy = cur[1];
 
-            if (isBoundary(dx, dy)) boundaryPath = true;
-            else cnt++;
-            
-            for (int[] dir : DIRs) {
-                int nx = dx + dir[0], ny = dy + dir[1];
+            cnt++;
+            if (isBoundary(dx, dy, n, m)) {
+               foundBoundary = true;
+            }
+
+            for (int[] nxt : DIRs) {
+                int nx = dx + nxt[0], ny = dy + nxt[1];
                 
-                if (nx < 0 || nx >= m || ny < 0 || ny >= n || grid[nx][ny] == 0) continue;
-                            
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || grid[nx][ny] != 1)
+                    continue;
+                
                 grid[nx][ny] = 0;
+                
                 queue.offer(new int[] {nx, ny});
             }
         }
         
-        return boundaryPath ? 0 : cnt;
+        return foundBoundary ? 0 : cnt;
     }
 
-    private boolean isBoundary(int x, int y) {
-        return (x == 0 || y == 0 || x == m - 1 || y == n - 1);
+    private boolean isBoundary(int i, int j, int n, int m) {
+        return (i == 0 || i == n - 1) || (j == 0 || j == m - 1);
     }
+    
 }

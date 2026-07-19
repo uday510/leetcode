84156ac45968bef087211dc498acd432400824e6
uday <62304758+uday510@users.noreply.github.com>
2026-07-19 20:55:45 +0530

    Time: 3 ms (25.12%) | Memory: 46.5 MB (68.63%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,47 +1,49 @@
+
 class Solution {
 
     private static final int[][] DIRs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-    int[][] grid;
-    int m, n;
-    
+
     public int maxAreaOfIsland(int[][] grid) {
-        this.grid = grid;
-        this.m = grid.length;
-        this.n = grid[0].length;
-        
-        int area = 0;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    area = Math.max(area, bfs(i, j));
+
+        int n = grid.length, m = grid[0].length;
+        int mx = 0;
+
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < m; y++) {
+
+                if (grid[x][y] == 1) {
+                    mx = Math.max(mx, bfs(x, y, n, m, grid));
                 }
             }
         }
-        
-        return area;
+
+        return mx;
     }
-    
-    private int bfs(int x, int y) {
+
+    private int bfs(int x, int y, int n, int m, int[][] grid) {
+
         int cnt = 1;
         Queue<int[]> queue = new ArrayDeque<>();
+        grid[x][y] = 0;
         queue.offer(new int[] {x, y});
-        grid[x][y] = -1;
-        
+
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
             int dx = cur[0], dy = cur[1];
-            
-            for (int[] dir : DIRs) {
-                int nx = dx + dir[0], ny = dy + dir[1];
-                
-                if (nx < 0 || nx >= m || ny < 0 || ny >= n || grid[nx][ny] == 0 || grid[nx][ny] == -1) continue;
+
+            for (int[] nxt : DIRs) {
+                int nx = dx + nxt[0], ny = dy + nxt[1];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || grid[nx][ny] != 1) {
+                    continue;
+                }
                 
                 grid[nx][ny] = 0;
-                queue.offer(new int[] {nx, ny});
                 cnt++;
+                queue.offer(new int[] {nx, ny});
             }
         }
+
         return cnt;
     }
 

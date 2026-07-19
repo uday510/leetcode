0a3d184e07cdb485bc5a47ffad3f801381775c2c
uday <62304758+uday510@users.noreply.github.com>
2026-07-19 14:16:02 +0530

    Time: 3 ms (71.44%) | Memory: 42.9 MB (47.01%) - LeetSync

--- 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.java
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.java
@@ -1,33 +1,35 @@
 class Solution {
     public String smallestSubsequence(String s) {
         
-        int[] pos = new int[26];
+        int[] lastPos = new int[26];
         int n = s.length();
 
         for (int i = 0; i < n; i++) {
             char c = s.charAt(i);
-            pos[c - 'a'] = i;
+            lastPos[c - 'a'] = i;
         }
 
-        Deque<Character> st = new ArrayDeque<>();
         boolean[] inSt = new boolean[26];
+        Deque<Character> st = new ArrayDeque<>();
 
         for (int i = 0; i < n; i++) {
             char c = s.charAt(i);
+
             if (inSt[c - 'a']) continue;
-
-            while (!st.isEmpty() && st.peek() > c && 
-            pos[st.peek() - 'a'] > i ) {
+            while (!st.isEmpty() &&
+             st.peek() > c &&
+             lastPos[st.peek() - 'a'] > i
+             ) {
                 inSt[st.pop() - 'a'] = false;
-            }
+             }
 
-            inSt[c - 'a'] = true;
-            st.push(c);
-        } 
+             inSt[c - 'a'] = true;
+             st.push(c);
+        }
 
         StringBuilder sb = new StringBuilder();
 
-        for (Iterator<Character> it = st.descendingIterator(); it.hasNext();) {
+        for (var it = st.descendingIterator(); it.hasNext();) {
             sb.append(it.next());
         }
 

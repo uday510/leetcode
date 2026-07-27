4dc5a042642677fa1710cf6d2be528c985fd5730
uday <62304758+uday510@users.noreply.github.com>
2026-07-27 15:28:24 +0530

    Time: 27 ms (94.54%) | Memory: 50 MB (33.95%) - LeetSync

--- 721-accounts-merge/accounts-merge.java
+++ 721-accounts-merge/accounts-merge.java
@@ -1,84 +1,101 @@
 class Solution {
-
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        Map<String, Integer> map = new HashMap<>();
+        
         int n = accounts.size();
-        UnionFind uf = new UnionFind(n);
+        DSU dsu = new DSU(n);
+        Map<String, Integer> owner = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
-            List<String> ac = accounts.get(i);
-
-            for (int j = 1; j < ac.size(); j++) {
-                String a = ac.get(j);
-
-                map.putIfAbsent(a, i);
-                uf.union(map.get(a), i);
+            List<String> acc = accounts.get(i);
+            for (int j = 1; j < acc.size(); j++) {
+                Integer id = owner.putIfAbsent(acc.get(j), i);
+                if (id != null) dsu.union(id, i);
             }
         }
 
-        Map<Integer, List<String>> grps = new HashMap<>();
-
-        for (Map.Entry<String, Integer> es : map.entrySet()) {
+        Map<Integer, List<String>> groups = new HashMap<>();
+        for (Map.Entry<String, Integer> es : owner.entrySet()) {
             String email = es.getKey();
-            int grp = es.getValue();
-            int root = uf.find(grp);
-            
-            grps.computeIfAbsent(root, _ -> new ArrayList<>()).add(email);
+            Integer group = dsu.find(es.getValue());
+
+            groups.computeIfAbsent(group, k -> new ArrayList<>()).add(email);
         }
 
         List<List<String>> res = new ArrayList<>();
-        
-        for (Map.Entry<Integer, List<String>> es : grps.entrySet()) {
-            int root = es.getKey();
-            List<String> emails = es.getValue();
-            
+
+        for (Map.Entry<Integer, List<String>> group : groups.entrySet()) {
+            Integer root = group.getKey();
+            List<String> emails = group.getValue();
+
             Collections.sort(emails);
-            emails.add(0, accounts.get(root).get(0));
-            
+            emails.add(0, accounts.get(root).getFirst());
             res.add(emails);
         }
 
         return res;
     }
-
 }
 
-class UnionFind {
-    int[] root, rank;
 
-    UnionFind(int n) {
-        this.root = new int[n];
-        this.rank = new int[n];
+class DSU {
+    private int[] rank;
+    private int[] root;
 
-        for (int idx = 0; idx < n; idx++) {
-            root[idx] = idx;
-            rank[idx] = 1;
+    public DSU (int n) {
+        rank = new int[n];
+        root = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            rank[i] = 1;
+            root[i] = i;
         }
     }
 
-    int find(int x) {
-        if (x == root[x]) return x;
 
+    void union(int x, int y) {
+        int rx = find(x);
+        int ry = find(y);
+
+        if (rx == ry)   
+            return;
+        
+        if (rank[rx] > rank[ry]) {
+            root[ry] = rx;
+        } else if (rank[ry] > rank[rx]) {
+            root[rx] = ry;
+        } else {
+            rank[rx]++;
+            root[ry] = rx;
+        }
+    }
+
+
+    int find(int x) {
+        if (x == root[x])
+            return x;
         return root[x] = find(root[x]);
     }
 
-    void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-
-        if (rootX == rootY) return;
-
-        if (rank[rootX] > rank[rootY]) {
-            root[rootY] = rootX;
-        } else if (rank[rootY] > root[rootX]) {
-            root[rootX] = rootY;
-        } else {
-            root[rootY] = rootX;
-            rank[rootX] += 1;
-        }
-    }
-
-    boolean connected(int x, int y) {
+    boolean isConnected(int x, int y) {
         return find(x) == find(y);
     }
 }
+
+/**
+
+
+johnsmith@mail.com -> 0
+john_newyork@mail.com -> 0
+john00@mail.com -> 1
+mary@mail.com -> 2
+johnnybravo@mail.com -> 3
+
+0 -> [johnsmith@mail.com, john_newyork@mail.com]
+1 -> [john00@mail.com]
+2 -> [mary@mail.com]
+3 -> [johnnybravo@mail.com]
+
+
+
+
+*/

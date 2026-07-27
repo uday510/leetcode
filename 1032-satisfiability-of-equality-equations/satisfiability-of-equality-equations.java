84423115c973e658b52f6086feca3a145dc75a65
uday <62304758+uday510@users.noreply.github.com>
2026-07-27 15:46:44 +0530

    Time: 1 ms (95.36%) | Memory: 43.9 MB (31.18%) - LeetSync

--- 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
@@ -1,83 +1,67 @@
 class Solution {
     public boolean equationsPossible(String[] eqs) {
-        int idx = 0;
-
-        UnionFind uf = new UnionFind(26);
+        DSU dsu = new DSU(26);
 
         for (String eq : eqs) {
-            int i = eq.charAt(0) - 'a', j = eq.charAt(3) - 'a';
-
-            if (isEqual(eq.charAt(1), eq.charAt(2))) {
-                uf.union(i, j);
+            if (eq.charAt(1) == eq.charAt(2)) {
+                dsu.union(
+                    eq.charAt(0) - 'a', 
+                    eq.charAt(3) - 'a');
             }
         }
 
         for (String eq : eqs) {
-            int i = eq.charAt(0) - 'a', j = eq.charAt(3) - 'a';
+            if (eq.charAt(1) != eq.charAt(2) && 
+                dsu.isConnected(
+                    eq.charAt(0) - 'a', 
+                    eq.charAt(3) - 'a'
+                    )) {
 
-            if (!isEqual(eq.charAt(1), eq.charAt(2))) {
-                if (uf.connected(i, j)) return false;
+                return false;
             }
         }
-        
+
         return true;
-    }
-    private boolean isEqual(char c1, char c2) {
-        return c1 == '=' && c2 == '=';
     }
 }
 
-class UnionFind {
-    private final int[] root;
-    // Use a rank array to record the height of each vertex, i.e., the "rank" of each vertex.
-    private final int[] rank;
+class DSU {
+    int[] rank;
+    int[] root;
 
-    private int components;
+    DSU (int n) {
+        rank = new int[n];
+        root = new int[n];
 
-    public UnionFind(int size) {
-        root = new int[size];
-        rank = new int[size];
-        components = size;
-
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < n; i++) {
+            rank[i] = 1;
             root[i] = i;
-            rank[i] = 1;  // The initial "rank" of each vertex is 1, because each of them is
-            // a standalone vertex with no connection to other vertices.
         }
     }
 
-    // The find function here is the same as that in the disjoint set with path compression.
-    public int find(int x) {
-        if (x == root[x]) return x;
+    void union(int x, int y) {
+        int rx = find(x);
+        int ry = find(y);
 
-        // Some ranks may become obsolete so they are not updated
+        if (rx == ry) return;
+
+        if (rank[rx] > rank[ry]) {
+            root[ry] = rx;
+        } else if (rank[ry] > rank[rx]) {
+            root[rx] = ry;
+        } else {
+            rank[rx]++;
+            root[ry] = rx;
+        }
+    }
+
+    int find(int x) {
+        if (x == root[x])
+            return x;
         return root[x] = find(root[x]);
     }
 
-    // The union function with union by rank
-    public void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-
-        if (rootX == rootY) return;
-
-        if (rank[rootX] > rank[rootY]) {
-            root[rootY] = rootX;
-        } else if (rank[rootX] < rank[rootY]) {
-            root[rootX] = rootY;
-        } else {
-            root[rootY] = rootX;
-            rank[rootX] += 1;
-        }
-
-        components--;
-    }
-
-    public boolean connected(int x, int y) {
-        return find(x) == find(y);
-    }
-
-    public int getComponents() {
-        return components;
+    boolean isConnected(int x, int y) {
+        return find(root[x]) == find(root[y]);
     }
 }

e625838f2aee7dc016be3d62b06379932acf6d92
uday <62304758+uday510@users.noreply.github.com>
2026-08-08 12:25:18 +0530

    Time: 49 ms (81.43%) | Memory: 128.8 MB (40.02%) - LeetSync

--- 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
@@ -1,30 +1,30 @@
 class Solution {
-    public int numOfMinutes(int n, int st, int[] mng, int[] infrm) {
+    public int numOfMinutes(int n, int h, int[] mng, int[] infrm) {
+        
         List<Integer>[] adj = new ArrayList[n];
-
         for (int i = 0; i < n; i++)
             adj[i] = new ArrayList<>();
-
+        
         for (int i = 0; i < n; i++) {
             if (mng[i] != -1) {
                 adj[mng[i]].add(i);
             }
         }
 
+        int mx = -1;
         Queue<int[]> queue = new ArrayDeque<>();
-        int mx = 0;
+        queue.offer(new int[] {h, infrm[h]});
 
-        queue.offer(new int[] {st, infrm[st]});
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
             int u = cur[0], w = cur[1];
 
             mx = Math.max(mx, w);
-            for (int v : adj[u]) {
-                queue.offer(new int[] {v, w + infrm[v]});
+            for (int nxt : adj[u]) {
+                queue.offer(new int[] {nxt, w + infrm[nxt]});
             }
         }
-
+        
         return mx;
     }
 }

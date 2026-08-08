4de3cb108b2e23816b5103cfc4c6f129590c3563
uday <62304758+uday510@users.noreply.github.com>
2026-08-08 13:24:45 +0530

    Time: 31 ms (89.43%) | Memory: 115.4 MB (53.41%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,39 +1,38 @@
 class Solution {
+
     public int minReorder(int n, int[][] conns) {
-        
+
         List<int[]>[] adj = new ArrayList[n];
-        for (int i = 0; i < n; i++)
-            adj[i] = new ArrayList<>();
-        
+        for (int idx = 0; idx < n; idx++)
+            adj[idx] = new ArrayList<>();
+
         for (int[] con : conns) {
             int u = con[0], v = con[1];
             adj[u].add(new int[] {v, 1});
             adj[v].add(new int[] {u, 0});
         }
 
-
-        Queue<Integer> queue = new ArrayDeque<>();
         boolean[] vis = new boolean[n];
-
+        Queue<Integer> queue = new ArrayDeque<>();
+        
         vis[0] = true;
         queue.offer(0);
-
-        int mn = 0;
-
+        
+        int res = 0;
         while (!queue.isEmpty()) {
             int u = queue.poll();
-
+        
             for (int[] nxt : adj[u]) {
-                int v = nxt[0], w = nxt[1];
+                int v = nxt[0], w1 = nxt[1];
+                
                 if (vis[v]) continue;
-
+                
                 vis[v] = true;
-                mn += w;
+                res += w1;
                 queue.offer(v);
             }
         }
-
-
-        return mn;
+        
+        return res;
     }
 }

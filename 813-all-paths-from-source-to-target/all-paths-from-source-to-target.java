6b6065c5010a3364450af669520779179220cd55
uday <62304758+uday510@users.noreply.github.com>
2026-08-08 13:35:10 +0530

    Time: 3 ms (25.98%) | Memory: 48.2 MB (13.67%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -1,34 +1,34 @@
 class Solution {
-
-    private List<List<Integer>> res;
     public List<List<Integer>> allPathsSourceTarget(int[][] edges) {
-        
-        res = new ArrayList<>();
+
         int n = edges.length;
         List<Integer>[] adj = new ArrayList[n];
-
-        for (int u = 0; u < n; u++) {
-            adj[u] = new ArrayList<>();
-            for (int v : edges[u]) {
-                adj[u].add(v);
+        for (int idx = 0; idx < n; idx++) 
+            adj[idx] = new ArrayList<>();
+        
+        for (int idx = 0; idx < n; idx++) {
+            for (int v : edges[idx]) {
+                adj[idx].add(v);
             }
         }
 
-        dfs(0, n, new ArrayList<>(List.of(0)), adj);
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(0, n - 1, adj, new ArrayList<>(List.of(0)), res);
+
         return res;
     }
 
-    private void dfs(int u, int n, List<Integer> cur, List<Integer>[] adj) {
-        if (u == n - 1) {
+    private void dfs(int st, int en, List<Integer>[] adj, List<Integer> cur, List<List<Integer>> res) {
+
+        if (st == en) {
             res.add(new ArrayList<>(cur));
             return;
         }
 
-        for (int v : adj[u]) {
-            cur.add(v);
-            dfs(v, n, cur, adj);
+        for (int nxt : adj[st]) {
+            cur.add(nxt);
+            dfs(nxt, en, adj, cur, res);
             cur.removeLast();
         }
-
     }
 }

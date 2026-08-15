43a584c0eb8eff2f82a656e0868b077e650ae984
uday <62304758+uday510@users.noreply.github.com>
2026-08-15 15:55:05 +0530

    Time: 14 ms (23.89%) | Memory: 43.8 MB (91.78%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -1,45 +1,46 @@
 class Solution {
 
-    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+    public List<Integer> distanceK(TreeNode root, TreeNode t, int k) {
 
         Map<TreeNode, TreeNode> map = new HashMap<>();
         Set<TreeNode> vis = new HashSet<>();
         List<Integer> list = new ArrayList<>();
 
         assign(root, map);
-        
-        dfs(target, k, vis, map, list);
+        dfs(t, k, vis, map, list);
         return list;
     }
-    
-    private void dfs(TreeNode node, int k, Set<TreeNode> vis, Map<TreeNode, TreeNode> map, List<Integer> res) {
+
+    private void dfs(TreeNode node, int k, Set<TreeNode> vis, 
+    Map<TreeNode, TreeNode> map, List<Integer> res) {
+
         if (node == null || vis.contains(node)) 
             return;
         
-        vis.add(node);
         if (k == 0) {
             res.add(node.val);
             return;
         }
-            
+
+        vis.add(node);
+
         dfs(map.get(node), k - 1, vis, map, res);
         dfs(node.left, k - 1, vis, map, res);
         dfs(node.right, k - 1, vis, map, res);
+
     }
-    
+
     private void assign(
-            TreeNode parent,
-            Map<TreeNode, TreeNode> map
-            ) {
-        
-        if (parent == null)
-            return;
-        
+        TreeNode parent,
+        Map<TreeNode, TreeNode> map
+    ) {
+
+        if (parent == null) return;
+
         if (parent.left != null) map.put(parent.left, parent);
         if (parent.right != null) map.put(parent.right, parent);
-        
+
         assign(parent.left, map);
         assign(parent.right, map);
     }
-
 }

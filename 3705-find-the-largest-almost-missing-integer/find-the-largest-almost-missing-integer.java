18afb460781f4a30ad4e94e682a700d18ef183c2
uday <62304758+uday510@users.noreply.github.com>
2026-08-18 09:48:22 +0530

    Time: 11 ms (5.96%) | Memory: 45.4 MB (46.67%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
@@ -1,7 +1,7 @@
 class Solution {
     public int largestInteger(int[] arr, int k) {
 
-        int n = arr.length, mx = -1;
+        int n = arr.length;
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < n - k + 1; i++) {
@@ -11,13 +11,16 @@ class Solution {
         }
 
         if (k == n)
-            return map.keySet().stream().max(Comparator.naturalOrder()).orElse(null);
+            return map
+            .keySet()
+            .stream()
+            .max(Comparator.naturalOrder()).orElse(-1);
 
-        for (Map.Entry<Integer, Integer> ks : map.entrySet()) {
-            if (ks.getValue() == 1) 
-                mx = Math.max(mx, ks.getKey());
-        }
+        return map.entrySet().stream()
+        .filter(e -> e.getValue() == 1)
+        .map(Map.Entry::getKey)
+        .max(Comparator.naturalOrder())
+        .orElse(-1);
 
-        return mx;   
     }
 }

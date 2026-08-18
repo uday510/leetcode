3a99fc275e6c9b8997b44b8c46d9be7edd9dfbe7
uday <62304758+uday510@users.noreply.github.com>
2026-08-18 11:19:02 +0530

    Time: 4 ms (17.51%) | Memory: 47.3 MB (5.82%) - LeetSync

--- 1380-number-of-closed-islands/number-of-closed-islands.java
+++ 1380-number-of-closed-islands/number-of-closed-islands.java
@@ -1,52 +1,48 @@
 class Solution {
-
-    private static final int[][] DIRs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-
     public int closedIsland(int[][] grid) {
-
         int n = grid.length, m = grid[0].length;
-        int closedIslands = 0;
+        int total = 0;
 
-        for (int x = 0; x < n; x++) {
-            for (int y = 0; y < m; y++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
 
-                if (grid[x][y] == 0) {
-                    closedIslands += bfs(x, y, n, m, grid);
+                if (grid[i][j] == 0) {
+                    total += bfs(i, j, n, m, grid);
                 }
             }
         }
 
-        return closedIslands;
+        return total;
     }
 
-    private int bfs(int x, int y, int n, int m, int[][] grid) {
-
-        boolean isClosed = true;
+    private int bfs(int i, int j, int n, int m, int[][] grid) {
         Queue<int[]> queue = new ArrayDeque<>();
-        grid[x][y] = 0;
-        queue.offer(new int[] {x, y});
+
+        grid[i][j] = 1;
+        queue.offer(new int[] {i, j});
+
+        boolean flag = false;
 
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
-            int dx = cur[0], dy = cur[1];
+            int x = cur[0], y = cur[1];
 
-            for (int[] nxt : DIRs) {
-                int nx = dx + nxt[0], ny = dy + nxt[1];
+            for (int[] nxt : new int[][] { {0, 1}, {1, 0}, {-1, 0}, {0, -1}}) {
+                int nx = nxt[0] + x, ny = nxt[1] + y;
 
                 if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-                    isClosed = false;
+                    flag = true;
                     continue;
                 }
-                
-                if (grid[nx][ny] == 0) {
-                    grid[nx][ny] = 1;
-                    queue.offer(new int[] {nx, ny});
-                }
                     
+                if (grid[nx][ny] == 1) 
+                    continue;
+                
+                grid[nx][ny] = 1;
+                queue.offer(new int[] {nx, ny});
             }
         }
-        
-        return isClosed ? 1 : 0;
+
+        return flag ? 0 : 1;
     }
-    
 }

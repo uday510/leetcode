8b713f5709bfa841217cfb9f7987147a2ad73e69
uday <62304758+uday510@users.noreply.github.com>
2026-08-18 10:51:12 +0530

    Time: 15 ms (6.42%) | Memory: 55 MB (78.40%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,58 +1,52 @@
 class Solution {
-
-    private static final int[][] DIRs = { {0, 1}, {1, 0}, {-1, 0}, {0, -1} };
-
     public int numEnclaves(int[][] grid) {
-
-        int cnt = 0;
-        boolean foundBoundary = false;
+        
         int n = grid.length, m = grid[0].length;
+        int total = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1) {
-                    cnt += bfs(i, j, n, m, grid);
+                    total += bfs(i, j, n, m, grid);
                 }
             }
         }
 
-        return cnt;
+        return total;
     }
 
     private int bfs(int i, int j, int n, int m, int[][] grid) {
-
-        int cnt = 0;
-        boolean foundBoundary = false;
-        grid[i][j] = 0;
         Queue<int[]> queue = new ArrayDeque<>();
+
+        grid[i][j] = 0;
         queue.offer(new int[] {i, j});
+
+        boolean flag = false;
+        int cnt = 0;
 
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
-            int dx = cur[0], dy = cur[1];
+            int x = cur[0], y = cur[1];
 
             cnt++;
-            if (isBoundary(dx, dy, n, m)) {
-               foundBoundary = true;
-            }
+            if (!flag && isBoundary(x, y, n, m)) 
+                flag = true;
+            
+            for (int[] nxt : new int[][] {{0, 1}, {1, 0}, {-1, 0}, {0, -1}}) {
+                int nx = nxt[0] + x, ny = nxt[1] + y;
 
-            for (int[] nxt : DIRs) {
-                int nx = dx + nxt[0], ny = dy + nxt[1];
-                
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m || grid[nx][ny] != 1)
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || grid[nx][ny] == 0)
                     continue;
                 
                 grid[nx][ny] = 0;
-                
                 queue.offer(new int[] {nx, ny});
             }
         }
-        
-        return foundBoundary ? 0 : cnt;
+
+        return flag ? 0 : cnt;
     }
 
-    private boolean isBoundary(int i, int j, int n, int m) {
-        return (i == 0 || i == n - 1) || (j == 0 || j == m - 1);
+    private boolean isBoundary(int x, int y, int n, int m) {
+        return (x == 0 || x == n - 1 || y == 0 || y == m - 1);
     }
-    
 }

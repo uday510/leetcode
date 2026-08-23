a7f6166e1ba7b29e8f5529781a3f12452a602731
uday <udayteja010@gmail.com>
2026-08-23 21:30:09 +0530

    Time: 5 ms (70.45%) | Memory: 47.3 MB (91.14%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.java
+++ 1738-maximal-network-rank/maximal-network-rank.java
@@ -1,28 +1,23 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] edges) {
         
-        boolean[][] conns = new boolean[n][n];
+        boolean[][] con = new boolean[n][n];
         int[] deg = new int[n];
 
         for (int[] e : edges) {
             int u = e[0], v = e[1];
-            conns[u][v] = true;
-            conns[v][u] = true;
-            deg[v]++;
-            deg[u]++;
+            con[u][v] = con[v][u] = true;
+            deg[u]++; deg[v]++;
         }
 
         int mx = 0;
         for (int u = 0; u < n; u++) {
-            int du = deg[u];
-
             for (int v = u + 1; v < n; v++) {
-                int cur = du + deg[v] - (conns[u][v] ? 1 : 0);
-
-                mx = Math.max(cur, mx);
+                int cur = deg[u] + deg[v] - (con[u][v] ? 1 : 0);
+                mx = Math.max(mx, cur);
             }
         }
-
+        
         return mx;
     }
 }

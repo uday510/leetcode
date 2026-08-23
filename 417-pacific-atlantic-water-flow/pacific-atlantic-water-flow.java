5e5cc01f799574e8503fc739fbf1f0d8fa1266ee
uday <udayteja010@gmail.com>
2026-08-23 19:17:19 +0530

    Time: 9 ms (30.48%) | Memory: 47.6 MB (42.92%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,60 +1,57 @@
 class Solution {
 
-    private static final int[][] DIRs = { {0, 1}, {1, 0}, {-1, 0}, {0, -1} };
-
-    public List<List<Integer>> pacificAtlantic(int[][] h) {
-        
-        int m = h.length;
-        int n = h[0].length;
-        List<List<Integer>> res = new ArrayList<>();
-
-        Queue<int[]> pq = new ArrayDeque<>();
+    private static final int[][] DIRs = { {0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+    public List<List<Integer>> pacificAtlantic(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
         Queue<int[]> aq = new ArrayDeque<>();
+        Queue<int[]> pq = new ArrayDeque<>();
+        boolean[][] visA = new boolean[n][m];
+        boolean[][] visP = new boolean[n][m];
 
-        boolean[][] pv = new boolean[m][n];
-        boolean[][] av = new boolean[m][n];
-
-        for (int i = 0; i < m; i++) {
+       for (int i = 0; i < n; i++) {
+            visA[i][m - 1] = visP[i][0] = true;
+            aq.offer(new int[] {i, m - 1});
             pq.offer(new int[] {i, 0});
-            aq.offer(new int[] {i, n - 1});
-            pv[i][0] = av[i][n - 1] = true;
-        }
+       }
 
-        for (int j = 0; j < n; j++) {
+       for (int j = 0; j < m; j++) {
+            visA[n - 1][j] = visP[0][j] = true;
+            aq.offer(new int[] {n - 1, j});
             pq.offer(new int[] {0, j});
-            aq.offer(new int[] {m - 1, j});
-            pv[0][j] = av[m - 1][j] = true;
-        }
+       }
 
-        bfs(m, n, h, pq, pv);
-        bfs(m, n, h, aq, av);
+       bfs(aq, grid, visA, n, m);
+       bfs(pq, grid, visP, n, m);
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (pv[i][j] && av[i][j]) {
+       List<List<Integer>> res = new ArrayList<>();
+
+       for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (visP[i][j] && visA[i][j]) {
                     res.add(List.of(i, j));
                 }
             }
-        }
+       }
 
-        return res;
-
+       return res;
     }
 
-    private void bfs(int m, int n, int[][] h, Queue<int[]> q, boolean[][] vis) {
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
+    private void bfs(Queue<int[]> queue, int[][] grid, boolean[][] vis, int n, int m) {
+        
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
             int dx = cur[0], dy = cur[1];
+            for (int[] nxt : DIRs) {
+                int nx = nxt[0] + dx;
+                int ny = nxt[1] + dy;
 
-            for (int[] dir : DIRs) {
-                int nx = dx + dir[0], ny = dy + dir[1];
-                if (nx < 0 || nx >= m || ny < 0 || ny >= n || vis[nx][ny] || h[dx][dy] > h[nx][ny]) 
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || 
+                    vis[nx][ny] || grid[dx][dy] > grid[nx][ny]) {
                     continue;
+                }
 
                 vis[nx][ny] = true;
-                q.offer(new int[] {nx, ny});
-
-
+                queue.offer(new int[] {nx, ny});
             }
         }
     }

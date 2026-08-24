7cefc8d0f27e1b218c3f4bb95a4935a4fd1d13a5
uday <udayteja010@gmail.com>
2026-08-24 13:15:52 +0530

    Time: 2 ms (37.12%) | Memory: 47.1 MB (57.20%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -5,22 +5,24 @@ class Solution {
         int[] color = new int[n];
 
         for (int i = 0; i < n; i++) {
-            if (color[i] != 0) continue;
+            if (color[i] != 0)  
+                continue;
 
-            color[i] = 1;
             Queue<Integer> queue = new ArrayDeque<>();
+            color[i] = 1;
             queue.offer(i);
 
             while (!queue.isEmpty()) {
                 int u = queue.poll();
 
                 for (int v : graph[u]) {
-                    if (color[v] == color[u]) 
+                
+                    if (color[u] == color[v]) 
                         return false;
                     
                     if (color[v] != 0)
                         continue;
-
+                    
                     color[v] = -color[u];
                     queue.offer(v);
                 }

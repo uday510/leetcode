e94c259b970d0e9f4a606e20b0c40be0c70477a9
uday <udayteja010@gmail.com>
2026-08-24 13:38:14 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (23.93%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.java
+++ 261-graph-valid-tree/graph-valid-tree.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean validTree(int n, int[][] edges) {
         if (edges.length != n - 1) return false;
-        
         DSU dsu = new DSU(n);
 
         for (int[] e : edges) {
             int u = e[0], v = e[1];
 
-            if (dsu.isConnected(u, v)) 
+            if (dsu.isConnected(u, v)) {
                 return false;
-            
+            }
+
             dsu.union(u, v);
         }
-
+        
         return true;
     }
 }
@@ -24,33 +24,33 @@ class DSU {
     DSU (int n) {
         root = new int[n];
         rank = new int[n];
-
         for (int i = 0; i < n; i++) {
             root[i] = i;
             rank[i] = 1;
         }
     }
 
+    void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX == rootY)
+            return;
+        
+        if (rank[rootX] > rank[rootY]) {
+            root[rootY] = rootX;
+        } else if (rank[rootY] > rank[rootX]) {
+            root[rootX] = rootY;
+        } else {
+            rank[rootX]++;
+            root[rootY] = rootX;
+        }
+
+    }
     int find(int x) {
         if (x == root[x])
             return x;
         return root[x] = find(root[x]);
-    }
-
-    void union(int x, int y) {
-        int rx = find(x);
-        int ry = find(y);
-
-        if (rx == ry) return;
-
-        if (rank[rx] > rank[ry]) {
-            root[ry] = rx;
-        } else if (rank[ry] > rank[rx]) {
-            root[rx] = ry;
-        } else {
-            rank[rx]++;
-            root[ry] = rx;
-        }
     }
 
     boolean isConnected(int x, int y) {

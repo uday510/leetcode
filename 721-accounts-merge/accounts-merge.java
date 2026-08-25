196c22b75030c17b15c9276bd9e54e2c51266b7b
uday <udayteja010@gmail.com>
2026-08-25 10:57:49 +0530

    Time: 30 ms (63.46%) | Memory: 49.1 MB (95.60%) - LeetSync

--- 721-accounts-merge/accounts-merge.java
+++ 721-accounts-merge/accounts-merge.java
@@ -1,41 +1,44 @@
 class Solution {
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        
-        Map<String, Integer> owner = new HashMap<>();
         int n = accounts.size();
         DSU dsu = new DSU(n);
+        Map<String, Integer> owner = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
-            List<String> ac = accounts.get(i);
+            var ac = accounts.get(i);
             for (int j = 1; j < ac.size(); j++) {
-                Integer id = owner.putIfAbsent(ac.get(j), i);
+                String email = ac.get(j);
+                Integer id = owner.putIfAbsent(email, i);
                 if (id != null) {
                     dsu.union(id, i);
                 }
             }
         }
 
-        Map<Integer, List<String>> grps = new HashMap<>();
+        Map<Integer, List<String>> groups = new HashMap<>();
+
         for (Map.Entry<String, Integer> es : owner.entrySet()) {
             String email = es.getKey();
-            Integer grp = dsu.find(es.getValue());
+            Integer id = es.getValue();
 
-            grps.computeIfAbsent(grp, k -> new ArrayList<>()).add(email);
+            int root = dsu.find(id);
+            groups.computeIfAbsent(root, k -> new ArrayList<>()).add(email);
         }
 
         List<List<String>> res = new ArrayList<>();
-        for (Map.Entry<Integer, List<String>> grp : grps.entrySet()) {
-            Integer r = grp.getKey();
-            List<String> emails = grp.getValue();
+        for (Map.Entry<Integer, List<String>> es : groups.entrySet()) {
+            Integer id = es.getKey();
+            List<String> emails = es.getValue();
 
             Collections.sort(emails);
-            emails.add(0, accounts.get(r).getFirst());
+            emails.add(0, accounts.get(id).getFirst());
             res.add(emails);
         }
 
         return res;
     }
 }
+
 
 class DSU {
     int[] rank;
@@ -76,14 +79,15 @@ class DSU {
 
 }
 
+
 /**
 
+{
+    johnnybravo@mail.com=3, 
+    johnsmith@mail.com=0, 
+    john00@mail.com=1, 
+    john_newyork@mail.com=0, 
+    mary@mail.com=2
+}
 
-["John","johnsmith@mail.com","john_newyork@mail.com"],
-["John","johnsmith@mail.com","john00@mail.com"],
-["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]
-
-"johnsmith@mail.com"        ->      0
-"john_newyork@mail.com"     -> 
-
-*/
+ */

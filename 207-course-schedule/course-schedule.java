2d7ecdc7c5da1ee0c438b560ddd70e0c35244adb
uday <udayteja010@gmail.com>
2026-08-30 14:13:35 +0530

    Time: 6 ms (72.24%) | Memory: 47.3 MB (32.94%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,30 +1,30 @@
 class Solution {
     public boolean canFinish(int n, int[][] pre) {
-        List<Integer>[] adj = new ArrayList[n + 1];
-        int[] in = new int[n + 1];
+        List<Integer>[] adj = new ArrayList[n];
+        int[] in = new int[n];
 
         for (int i = 0; i < n; i++) 
             adj[i] = new ArrayList<>();
         
         for (int[] p : pre) {
-            int u = p[0], v = p[1];
+            int u = p[1], v = p[0];
             adj[u].add(v);
             in[v]++;
         }
 
         Queue<Integer> queue = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
-            if (in[i] == 0)
+            if (in[i] == 0) {
                 queue.offer(i);
+            }
         }
 
         int total = 0;
         while (!queue.isEmpty()) {
-            total++;
-            if (total == n)
+            if(++total == n)
                 return true;
+
             int u = queue.poll();
-            
             for (int v : adj[u]) {
                 if (--in[v] == 0)   
                     queue.offer(v);

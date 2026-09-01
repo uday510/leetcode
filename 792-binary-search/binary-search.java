0d6671086580cb033685c01a2ceabcd6440cc1bd
uday <udayteja010@gmail.com>
2026-09-01 12:19:07 +0530

    Time: 0 ms (100.00%) | Memory: 48.6 MB (14.01%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,13 +1,9 @@
 class Solution {
     public int search(int[] arr, int t) {
-        return bs(arr, t);
-    }
-
-    private int bs(int[] arr, int t) {
         int l = 0, r = arr.length;
 
         while (l < r) {
-            int m = l + (r - l) / 2;
+            int m = l + ((r - l) >> 1);
 
             if (arr[m] < t) l = m + 1;
             else r = m;

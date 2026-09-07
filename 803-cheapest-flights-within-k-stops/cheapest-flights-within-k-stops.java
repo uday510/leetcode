6ed525ed6fc701edb934173de0e2b24abd4d8755
uday <udayteja010@gmail.com>
2026-09-07 19:49:53 +0530

    Time: 16 ms (8.84%) | Memory: 46.5 MB (69.67%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,50 +1,46 @@
-import java.util.*;
-
 class Solution {
-
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-
+        
         List<int[]>[] adj = new ArrayList[n];
 
-        for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
+        }
 
         for (int[] fli : flights) {
             int u = fli[0], v = fli[1], w = fli[2];
-
-            adj[u].add(new int[]{v, w});
+            adj[u].add(new int[] {v, w});
         }
 
         int[][] dists = new int[k + 2][n];
-        int un = Integer.MAX_VALUE;
+        int unk = Integer.MAX_VALUE;
 
-        for (int[] r : dists) Arrays.fill(r, un);
+        for (int[] r : dists) {
+            Arrays.fill(r, unk);
+        }
 
-        Queue<int[]> pq = new PriorityQueue<>(Comparator.comparing(x -> x[1]));
-        pq.offer(new int[]{src, 0, 0});
-        
-        pq.offer(new int[]{src, 0, 0});
-        
+        Queue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(x -> x[1]));
+        pq.offer(new int[] {src, 0, 0});
+
         while (!pq.isEmpty()) {
             int[] cur = pq.poll();
+
             int u = cur[0], c = cur[1], s = cur[2];
-            
-            if (u == dst) return c;
-            
-            if (s > k || dists[s][u] < c) continue;
-            
+            if (u == dst) 
+                return c;
+            if (s > k || dists[s][u] < c) 
+                continue;
+
             for (int[] nxt : adj[u]) {
                 int v = nxt[0], w = nxt[1];
-                
+
                 if (c + w < dists[s + 1][v]) {
-                    
                     dists[s + 1][v] = c + w;
-                    pq.offer(new int[]{v, c + w, s + 1});
+                    pq.offer(new int[] {v, c + w, s + 1});
                 }
             }
         }
-        
-        return -1;
-        
-    }
 
+        return -1;
+    }
 }

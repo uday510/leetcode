60ad68bf1a44d1aed9f0fe7b2f32cc2a3e68e0de
uday <udayteja010@gmail.com>
2026-09-08 10:29:35 +0530

    Time: 12 ms (44.62%) | Memory: 49.4 MB (30.02%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,48 +1,52 @@
 class Solution {
-    public int networkDelayTime(int[][] edges, int n, int src) {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        
         List<int[]>[] adj = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            adj[i] = new ArrayList<>();
+        }
 
-        for (int i = 1; i <= n; i++) adj[i] = new ArrayList<>();
-
-        for (int[] e : edges) {
-            int u = e[0], v = e[1], w = e[2];
+        for (int[] t : times) {
+            int u = t[0], v = t[1], w = t[2];
             adj[u].add(new int[] {v, w});
         }
 
+        int[] dists = new int[n + 1];
+        int unk = Integer.MAX_VALUE;
+        
+        Arrays.fill(dists, unk);
+
         Queue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(x -> x[1]));
 
-        int unknown = Integer.MAX_VALUE;
-        int[] dists = new int[n + 1];
-        Arrays.fill(dists, unknown);
-        
-        dists[src] = 0;
-        pq.offer(new int[] {src, 0});
+        dists[k] = 0;
+        pq.offer(new int[] {k, 0});
 
         while (!pq.isEmpty()) {
             int[] cur = pq.poll();
             int u = cur[0], w = cur[1];
-
-            if (dists[u] < w) continue;
-
+            if (dists[u] < w) 
+                continue;
+            
             for (int[] nxt : adj[u]) {
                 int v = nxt[0], w1 = nxt[1];
 
                 if (w + w1 < dists[v]) {
                     dists[v] = w + w1;
                     pq.offer(new int[] {v, w + w1});
-                }
+                } 
             }
         }
 
-        int max = Integer.MIN_VALUE;;
+        int mx = -1;
         for (int i = 1; i <= n; i++) {
-            if (dists[i] == unknown) {
-                max = -1;
-                break;
+            int d = dists[i];
+            if (d == Integer.MAX_VALUE) {
+                return -1;
             }
-            max = Math.max(max, dists[i]);
+
+            mx = Math.max(mx, d);
         }
 
-        return max;
+        return mx;
     }
 }

61a7478bbe18d2b18c25a9c8b054bc1b0c4f91a5
uday <udayteja010@gmail.com>
2026-09-08 21:05:53 +0530

    Time: 32 ms (61.07%) | Memory: 71.4 MB (34.83%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.java
@@ -1,53 +1,58 @@
 class Solution {
-    public double maxProbability(int n, int[][] E, double[] probs, int st, int en) {
+    public double maxProbability(int n, int[][] edges, double[] prob, int st, int en) {
+        
         List<Edge>[] adj = new ArrayList[n];
-        for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
-
-        for (int i = 0; i < E.length; i++) {
-            int u = E[i][0], v = E[i][1];
-            double w = probs[i];
-            adj[u].add(new Edge(v, w));
-            adj[v].add(new Edge(u, w));
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
         }
 
-        double[] dists = new double[n];
-        Arrays.fill(dists, -1.0);
-        
-        Queue<Edge> pq = new PriorityQueue<>((o1, o2) -> Double.compare(o2.w, o1.w));
+        for (int i = 0; i < edges.length; i++) {
+            int[] e = edges[i];
+            double d = prob[i];
+            int u = e[0], v = e[1];
 
+            adj[u].add(new Edge(v, d));
+            adj[v].add(new Edge(u, d));
+        }
+
+        Queue<Edge> pq = new PriorityQueue<>((o1, o2) -> Double.compare(o2.d, o1.d));
+
+        Double[] dists = new Double[n];
+        Arrays.fill(dists, 0.0);
         dists[st] = 1.0;
         pq.offer(new Edge(st, 1.0));
         
-        Edge cur;
-        int u;
-        double p;
         while (!pq.isEmpty()) {
-            cur = pq.poll();
-            u = cur.v;
-            double w = cur.w;
+            Edge e = pq.poll();
+            Integer u = e.v;
+            Double w = e.d;
 
-            if (u == en) return w;
-            if (w < dists[u]) continue;
+            if (u == en) {
+                return w;
+            }
 
-            for (Edge e : adj[u]) {
-                double newDist = w * e.w;
+            for (Edge nxt : adj[u]) {
+                Integer v = nxt.v;
+                Double w1 = w * nxt.d;
 
-                if (newDist > dists[e.v]) {
-                    dists[e.v] = newDist;
-                    pq.offer(new Edge(e.v, newDist));
+                if (w1 > dists[v]) {
+                    dists[v] = w1;
+                    pq.offer(new Edge(v, w1));
                 }
             }
         }
 
-        return 0.0;
+        return 0;
+
     }
 }
 
 class Edge {
-    int v;
-    double w;
-    Edge (int v, double w) {
+    Integer v;
+    Double d;
+
+    Edge (Integer v, Double d) {
         this.v = v;
-        this.w = w;
+        this.d = d;
     }
 }

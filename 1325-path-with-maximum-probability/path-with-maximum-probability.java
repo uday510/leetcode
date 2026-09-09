e2a16709528ab41adcb5d0b1093e5867507483f8
uday <udayteja010@gmail.com>
2026-09-09 14:30:28 +0530

    Time: 32 ms (60.80%) | Memory: 71.4 MB (34.98%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.java
@@ -1,5 +1,5 @@
 class Solution {
-    public double maxProbability(int n, int[][] edges, double[] prob, int st, int en) {
+    public double maxProbability(int n, int[][] edges, double[] probs, int st, int en) {
         
         List<Edge>[] adj = new ArrayList[n];
         for (int i = 0; i < n; i++) {
@@ -7,52 +7,53 @@ class Solution {
         }
 
         for (int i = 0; i < edges.length; i++) {
-            int[] e = edges[i];
-            double d = prob[i];
-            int u = e[0], v = e[1];
-
-            adj[u].add(new Edge(v, d));
-            adj[v].add(new Edge(u, d));
+            int u = edges[i][0], v = edges[i][1];
+            double w = probs[i];
+            adj[u].add(new Edge(v, w));
+            adj[v].add(new Edge(u, w));
         }
 
-        Queue<Edge> pq = new PriorityQueue<>((o1, o2) -> Double.compare(o2.d, o1.d));
-
+        Queue<Edge> pq = new PriorityQueue<>((o1, o2) -> Double.compare(o2.w, o1.w));
         Double[] dists = new Double[n];
+
         Arrays.fill(dists, 0.0);
         dists[st] = 1.0;
         pq.offer(new Edge(st, 1.0));
-        
+
         while (!pq.isEmpty()) {
             Edge e = pq.poll();
             Integer u = e.v;
-            Double w = e.d;
+            Double w = e.w;
 
             if (u == en) {
                 return w;
             }
 
+            if (dists[u] > w) {
+                continue;
+            }
+
             for (Edge nxt : adj[u]) {
                 Integer v = nxt.v;
-                Double w1 = w * nxt.d;
+                Double w1 = w * nxt.w;
 
                 if (w1 > dists[v]) {
                     dists[v] = w1;
                     pq.offer(new Edge(v, w1));
                 }
+
             }
         }
 
         return 0;
-
     }
 }
-
 class Edge {
     Integer v;
-    Double d;
+    Double w;
 
-    Edge (Integer v, Double d) {
+    Edge (Integer v, Double w) {
         this.v = v;
-        this.d = d;
+        this.w = w;
     }
 }

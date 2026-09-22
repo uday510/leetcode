3cf5cf7f6bcb21381c869fa0d02752a843cd2c46
uday <udayteja010@gmail.com>
2026-09-22 21:21:48 +0530

    Time: 1 ms (100.00%) | Memory: 49.2 MB (15.43%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,12 +1,13 @@
 class Solution {
     public double findMedianSortedArrays(int[] arr1, int[] arr2) {
-        
         if (arr2.length < arr1.length) {
             return findMedianSortedArrays(arr2, arr1);
         }
 
-        int n = arr1.length, m = arr2.length;
-        int total = n + m, target = (n + m + 1) >> 1;
+        int n = arr1.length;
+        int m = arr2.length;
+        int total = n + m;
+        int target = (total + 1) >> 1;
         int l = 0, r = n;
 
         while (l <= r) {
@@ -15,44 +16,22 @@ class Solution {
             int l1 = safeGet(p1 - 1, arr1), r1 = safeGet(p1, arr1);
             int l2 = safeGet(p2 - 1, arr2), r2 = safeGet(p2, arr2);
 
-            if (l2 > r1) {
-                l = p1 + 1;
-            } else if (l1 > r2) {
-                r = p1 - 1;
-            } else if (l1 <= r2 && l2 <= r2) {
-                double median = -1;
-
+            if (l1 > r2) r = p1 - 1;
+            else if (l2 > r1) l = p1 + 1;
+            else if (l1 <= r2 && l2 <= r1) {
                 if ( (total & 1) == 1) {
-                    median = Math.max(l1, l2);
-                } else {
-                    median = (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                    return Math.max(l1, l2);
                 }
-
-                return median;
+                return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
             }
         }
 
         return -1;
     }
 
-    private int safeGet(int idx, int[] arr) {
-        if (idx < 0) return Integer.MIN_VALUE;
-        if (idx >= arr.length) return Integer.MAX_VALUE;
-        return arr[idx];
+    private int safeGet(int i, int[] arr) {
+        if (i < 0) return Integer.MIN_VALUE;
+        if (i >= arr.length) return Integer.MAX_VALUE;
+        return arr[i];
     }
 }
-
-/**
-
-
-
-
-      l1        r1
-    1 2         6 7
-    
-        l2      r2
-    1 4 9       7 8 9
-
-
-
- */

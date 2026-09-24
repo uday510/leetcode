2807fb77a2bf4986b376e47c1932db7bcc794e72
uday <udayteja010@gmail.com>
2026-09-24 20:34:26 +0530

    Time: 30 ms (69.87%) | Memory: 143.6 MB (64.96%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,6 +1,6 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        int n = nums.length;
+    public int[] maxSlidingWindow(int[] arr, int k) {
+        int n = arr.length;
         int[] res = new int[n - k + 1];
         Deque<Integer> queue = new ArrayDeque<>();
 
@@ -9,32 +9,19 @@ class Solution {
                 queue.pollFirst();
             }
 
-            while (!queue.isEmpty() && nums[queue.peekLast()] < nums[i]) {
-                queue.pollLast();
+            while (
+                !queue.isEmpty() && 
+                arr[queue.peekLast()] <= arr[i]) {
+                    queue.pollLast();
             }
 
             queue.offerLast(i);
 
             if (i - k + 1 >= 0) {
-                res[i - k + 1] = nums[queue.peekFirst()];
+                res[i - k + 1] = arr[queue.peekFirst()];
             }
         }
 
         return res;
-        
     }
 }
-
-/**
-
-0  1   2   3  4  5  6  7        
-1, 3, -1, -3, 5, 3, 6, 7
-
-
-queue: 7
-
-res: 3 3 5 5 6 7
-
-
-
- */

e5380a722969b765c6324e5faf37f66168041558
uday <udayteja010@gmail.com>
2026-09-25 14:36:49 +0530

    Time: 4 ms (96.89%) | Memory: 47.9 MB (36.60%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,21 +1,16 @@
 class Solution {
-
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        
         int n = arr.length;
         int l = 0, r = n;
-        
+
         while (l < r) {
-            
-            int m = l + ((r - l) >> 1);
-            
+            int m = l + ( (r - l) >> 1);
+
             if (arr[m] < x) l = m + 1;
             else r = m;
         }
-        
-        
+
         r = l;
-        
         while (r - l - 1 < k) {
             if (l == -1) {
                 r++;
@@ -25,15 +20,12 @@ class Solution {
                 r++;
             }
         }
-        
-        
-        List<Integer> res = new ArrayList<>();
-        
-        for (int idx = l + 1; idx < r; idx++) {
-            res.add(arr[idx]);
-        }
-        
-        return res;
 
+        List<Integer> res = new ArrayList<>();
+        for (int i = l + 1; i < r; i++) {
+            res.add(arr[i]);
+        }
+
+        return res;
     }
 }
